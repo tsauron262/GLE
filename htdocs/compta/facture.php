5956438a1f3cea5cf6ee851b399e736f4703caf9
jean <jean@jean-K72Dr>
2012-12-11 12:02:56 +0100

    bug expedition commande.class

--- htdocs/compta/facture.php
+++ htdocs/compta/facture.php
@@ -28,23 +28,23 @@
  *	\brief      Page to create/see an invoice
  */
 
-require '../main.inc.php';
-require DOL_DOCUMENT_ROOT . '/compta/facture/class/facture.class.php';
-require DOL_DOCUMENT_ROOT . '/compta/paiement/class/paiement.class.php';
-require DOL_DOCUMENT_ROOT . '/core/modules/facture/modules_facture.php';
-require DOL_DOCUMENT_ROOT . '/core/class/discount.class.php';
-require DOL_DOCUMENT_ROOT . '/core/class/html.formfile.class.php';
-require DOL_DOCUMENT_ROOT . '/core/class/html.formother.class.php';
-require DOL_DOCUMENT_ROOT . '/core/lib/invoice.lib.php';
+require_once '../main.inc.php';
+require_once DOL_DOCUMENT_ROOT . '/compta/facture/class/facture.class.php';
+require_once DOL_DOCUMENT_ROOT . '/compta/paiement/class/paiement.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/modules/facture/modules_facture.php';
+require_once DOL_DOCUMENT_ROOT . '/core/class/discount.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/class/html.formfile.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/class/html.formother.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/lib/invoice.lib.php';
 require_once DOL_DOCUMENT_ROOT . '/core/lib/functions2.lib.php';
 require_once DOL_DOCUMENT_ROOT . '/core/lib/date.lib.php';
 
 if (! empty($conf->commande->enabled)) {
-	require DOL_DOCUMENT_ROOT . '/commande/class/commande.class.php';
+	require_once DOL_DOCUMENT_ROOT . '/commande/class/commande.class.php';
 }
 if (! empty($conf->projet->enabled)) {
-	require DOL_DOCUMENT_ROOT . '/projet/class/project.class.php';
-	require DOL_DOCUMENT_ROOT . '/core/lib/project.lib.php';
+	require_once DOL_DOCUMENT_ROOT . '/projet/class/project.class.php';
+	require_once DOL_DOCUMENT_ROOT . '/core/lib/project.lib.php';
 }
 
 $langs->load('bills');

eb26a124409a5428d758ad5455b12bc952bf293e
megajean <megajean@megajean-GA-990XA-UD3>
2012-12-12 13:28:47 +0100

    bug capsim ajout adresse auto contact + trad projet dans fiche client

--- htdocs/contact/fiche.php
+++ htdocs/contact/fiche.php
@@ -385,7 +385,8 @@ else
 							});
 
 							$("#copyaddressfromsoc").click(function() {
-								$(\'textarea[name="address"]\').text("'.addslashes($objsoc->address).'");
+                                                                var adresseTemp = "'.  str_replace(array("/r/n", "\r\n", "/n/r", "\n\r", "/n", "/r", "\n", "\r"), "$7£", addslashes($objsoc->address)).'";
+                                                                adresseTemp =  adresseTemp.split("$7£").join("\n");
 								$(\'input[name="zipcode"]\').val("'.addslashes($objsoc->zip).'");
 								$(\'input[name="town"]\').val("'.addslashes($objsoc->town).'");
 								$(\'select[name="country_id"]\').val("'.addslashes($objsoc->country_id).'");

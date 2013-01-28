fbaa47ad416e2e6e37c63ac55dea4a4097a5327b
megajean <megajean@megajean-GA-990XA-UD3>
2013-01-28 19:30:04 +0100

    bug time projet

--- htdocs/projet/fiche.php
+++ htdocs/projet/fiche.php
@@ -445,8 +445,8 @@ if ($_GET["action"] == 'create' && $user->rights->synopsisprojet->creer) {
                              <td style="border-top:0px;border-right:0px;" class="ui-widget-content"><div style="float: left; background-color: #FFFFFF; margin-left: 50%; margin-top: -1px; padding: 2px; padding-left: 5px; padding-right: 5px; opacity: 0.95; " class="ui-corner-all">' . $projet->statAvgProgByGroup[0] . '%</div>';
         //Avancement Horaire
         print '<div id="progressBarQual"></div>';
-        print '<tr><th style="border-top:0px" class="ui-widget-header ui-state-default" width=25%>' . $langs->trans('TempsPrevu') . '</th><td style="border-top:0px; border-right:0px;" class="ui-widget-content">' . sec2time($projet->totDuration) . '</td></tr>';
-        print '<tr><th style="border-top:0px;border-bottom:0px" class="ui-widget-header ui-state-default" width=25%>' . $langs->trans('TempsEffectue') . '</th><td style="border-right:0px; border-top:0px" class="ui-widget-content">' . sec2time($projet->workedDuration) . '</td></tr>';
+        print '<tr><th style="border-top:0px" class="ui-widget-header ui-state-default" width=25%>' . $langs->trans('TempsPrevu') . '</th><td style="border-top:0px; border-right:0px;" class="ui-widget-content">' . sec2time($projet->totDuration) .($projet->totDuration > (3600*24) ? " (".$projet->totDuration/3600 . "h)" : ''). '</td></tr>';
+        print '<tr><th style="border-top:0px;border-bottom:0px" class="ui-widget-header ui-state-default" width=25%>' . $langs->trans('TempsEffectue') . '</th><td style="border-right:0px; border-top:0px" class="ui-widget-content">' . sec2time($projet->workedDuration) .($projet->workedDuration > (3600*24) ? " (".$projet->workedDuration/3600 . "h)" : ''). '</td></tr>';
 
         print '</td></tr>';
         print "</tbody></table>";

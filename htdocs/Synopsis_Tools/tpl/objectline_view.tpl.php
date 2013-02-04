dc3ac878ca6a1e6e94a7b4933f7e7410c8d40906
megajean <megajean@megajean-GA-990XA-UD3>
2013-02-04 17:51:43 +0100

    bug fi pas di

--- htdocs/Synopsis_Tools/tpl/objectline_view.tpl.php
+++ htdocs/Synopsis_Tools/tpl/objectline_view.tpl.php
@@ -93,7 +93,7 @@
 		}
 		?>
 	</td>
-<?php if($type == 1 || $type == 0 || $type == 2){ ?>
+<?php if($type == 1 || $type == 0 || $type == 2 || $type == 3 || $type == 4){ ?>
 	<td align="right" nowrap="nowrap"><?php echo vatrate($line->tva_tx,'%',$line->info_bits); ?></td>
 
 	<td align="right" nowrap="nowrap"><?php echo price($line->subprice); ?></td>

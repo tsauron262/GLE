0d914fbe9ad7bb87cb5849dc00febe2ad1611198
minijean <minijean@minijean-1015CX>
2013-10-30 03:10:57 +0100

    module affaire

--- htdocs/core/modules/synopsisaffaire/mod_affaire_actinote.php
+++ htdocs/core/modules/synopsisaffaire/mod_affaire_actinote.php
@@ -71,7 +71,7 @@ class mod_affaire_actinote extends ModeleNumRefAffaire
 
         // Parametrage du prefix des factures
         $texte.= '<tr><td>'.$langs->trans("Mask").':</td>';
-        $texte.= '<td align="right">'.$form->textwithhelp('<input type="text" class="flat" size="24" name="maskaffaire" value="'.$conf->global->AFFAIRE_ACTINOTE_MASK.'">',$langs->trans("GenericMaskCodes",$langs->transnoentities("Proposal"),$langs->transnoentities("Proposal"),$langs->transnoentities("Proposal")),1,1).'</td>';
+        $texte.= '<td align="right">'.$form->textwithpicto('<input type="text" class="flat" size="24" name="maskaffaire" value="'.$conf->global->AFFAIRE_ACTINOTE_MASK.'">',$langs->trans("GenericMaskCodes",$langs->transnoentities("Proposal"),$langs->transnoentities("Proposal"),$langs->transnoentities("Proposal")),1,1).'</td>';
 
         $texte.= '<td align="left" rowspan="2">&nbsp; <input type="submit" class="button" value="'.$langs->trans("Modify").'" name="Button"></td>';
 

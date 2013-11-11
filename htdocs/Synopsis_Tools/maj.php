c8b070275629eac95e42c45660ae6cf6f61dc2e5
megajean <megajean@megajean-GA-990XA-UD3>
2013-11-11 03:52:26 +0100

    maj bimp netoye processdet

--- htdocs/Synopsis_Tools/maj.php
+++ htdocs/Synopsis_Tools/maj.php
@@ -458,22 +458,22 @@ function getTab() {
 //            array(),
 //            array()
 //        ),
-//        array("Babel_Processdet", MAIN_DB_PREFIX."Synopsis_Processdet",
-//            array(),
-//            array()
-//        ),
-//        array("Babel_Processdet_active", MAIN_DB_PREFIX."Synopsis_Processdet_active",
-//            array(),
-//            array()
-//        ),
-//        array("Babel_Processdet_validation", MAIN_DB_PREFIX."Synopsis_Processdet_validation",
-//            array(),
-//            array()
-//        ),
-//        array("Babel_Processdet_value", MAIN_DB_PREFIX."Synopsis_Processdet_value",
-//            array(),
-//            array()
-//        ),
+        array("Babel_Processdet", MAIN_DB_PREFIX."Synopsis_Processdet",
+            array(),
+            array()
+        ),
+        array("Babel_Processdet_active", MAIN_DB_PREFIX."Synopsis_Processdet_active",
+            array(),
+            array()
+        ),
+        array("Babel_Processdet_validation", MAIN_DB_PREFIX."Synopsis_Processdet_validation",
+            array(),
+            array()
+        ),
+        array("Babel_Processdet_value", MAIN_DB_PREFIX."Synopsis_Processdet_value",
+            array(),
+            array()
+        ),
         array($oldPref . "facture", MAIN_DB_PREFIX . "facture",
             array('rowid', 'facnumber', 'ref_client', 'type', 'increment', 'fk_soc', 'datec', 'datef', 'date_valid', 'paye', 'amount', 'remise_percent', 'remise_absolue', 'remise', 'close_code', 'close_note', 'tva', 'total', 'total_ttc', 'fk_statut', 'fk_user_author', 'fk_user_valid', 'fk_facture_source', 'fk_projet', 'fk_cond_reglement', 'fk_mode_reglement', 'date_lim_reglement', 'note', 'note_public', 'model_pdf'),
             array('rowid', 'facnumber'/* , 'entity', 'ref_ext', 'ref_int' */, 'ref_client', 'type', 'increment', 'fk_soc', 'datec', 'datef', 'date_valid'/* , 'tms' */, 'paye', 'amount', 'remise_percent', 'remise_absolue', 'remise', 'close_code', 'close_note', 'tva', /* 'localtax1', 'localtax2', */ 'total', 'total_ttc', 'fk_statut', 'fk_user_author', 'fk_user_valid', 'fk_facture_source', 'fk_projet'/* , 'fk_account', 'fk_currency' */, 'fk_cond_reglement', 'fk_mode_reglement', 'date_lim_reglement', 'note_private', 'note_public', 'model_pdf'/* , 'import_key', 'extraparams' */)

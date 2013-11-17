2ff54b7defdb8c1c07bae394f83eb12c1d4923c8
megajean <megajean@megajean-GA-990XA-UD3>
2013-11-18 00:38:39 +0100

    synopsis_tools fichier log

---
+++ htdocs/Synopsis_Tools/fichierLog.php
@@ -0,0 +1,49 @@
+<?php
+
+if(isset($_REQUEST['action']) && $_REQUEST['action'] == "save")
+    $textSave = $_REQUEST['text'];
+$_REQUEST['text'] = "";
+/*
+ * GLE by Synopsis et DRSI
+ *
+ * Author: Tommy SAURON <tommy@drsi.fr>
+ * Licence : Artistic Licence v2.0
+ *
+ * Version 1.0
+ * Create on : 4-1-2009
+ *
+ * Infos on http://www.finapro.fr
+ *
+ */
+require_once('../main.inc.php');
+
+$mainmenu = isset($_GET["mainmenu"]) ? $_GET["mainmenu"] : "";
+llxHeader("", "Fichier de log");
+dol_fiche_head('', 'SynopsisTools', $langs->trans("Voir le fichier de log"));
+
+
+if ($user->rights->SynopsisTools->Global->fileInfo != 1) {
+    print "Ce module ne vous est pas accessible";
+    llxFooter();
+    exit(0);
+}
+
+
+$filename = str_replace("DOL_DATA_ROOT", DOL_DATA_ROOT, SYSLOG_FILE);
+
+if(isset($textSave))
+    file_put_contents ($filename, $textSave);
+
+$text = file_get_contents($filename);
+echo "<form action='./fichierLog.php?action=save' method='post'>";
+echo "<textarea name='text' style='width:100%; height:400px;'>".$text."</textarea>";
+echo "<br/><input type='submit' value='Enregistrer'/></form>";
+
+//include_once(DOL_DOCUMENT_ROOT . "/Synopsis_Tools/class/fileInfo.class.php");
+//$fileInfo = new fileInfo($db);
+//echo $fileInfo->getFiles();
+
+
+
+llxFooter();
+?>
--- htdocs/Synopsis_Tools/index.php
+++ htdocs/Synopsis_Tools/index.php
@@ -26,6 +26,10 @@ if (isset($user->rights->SynopsisTools->Global->phpMyAdmin)) {
     print" <br/><br/><a href='myAdmin.php'><span style='float: left;' class='ui-icon ui-icon-extlink'></span><span>PhpMyAdmin</span></a>";
     print" <br/><br/><a href='./Synopsis_MyAdmin/index.php'  target='_blank'><span style='float: left;' class='ui-icon ui-icon-extlink'></span><span>PhpMyAdmin (Nouvelle onglet)</span></a>";
 }
+if (isset($user->rights->SynopsisTools->Global->fileInfo)) {
+    print" <br/><br/><a href='./fichierLog.php'><span style='float: left;' class='ui-icon ui-icon-extlink'></span><span>Fichier de log</span></a>";
+    print" <br/><br/><a href='./listFileInfo.php'><span style='float: left;' class='ui-icon ui-icon-extlink'></span><span>Fichiers de maj</span></a>";
+}
 
 if (isset($user->rights->SynopsisPrepaCom->import->Admin))
     print" <br/><br/><a href='../Synopsis_PrepaCommande/import/testImport.php'><span style='float: left;' class='ui-icon ui-icon-extlink'></span><span>Import 8sens -> GLE</span></a>";

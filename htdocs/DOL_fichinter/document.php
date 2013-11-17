109afe665adbfcf8035caa7b01b095a146c68784
minijean <minijean@minijean-1015CX>
2013-11-17 17:42:15 +0100

    rename fichinter projet fin maj doli

--- htdocs/DOL_fichinter/document.php
+++ htdocs/DOL_fichinter/document.php
@@ -2,13 +2,13 @@
 /* Copyright (C) 2003-2007 Rodolphe Quiedeville  <rodolphe@quiedeville.org>
  * Copyright (C) 2004-2010 Laurent Destailleur   <eldy@users.sourceforge.net>
  * Copyright (C) 2005      Marc Barilley / Ocebo <marc@ocebo.com>
- * Copyright (C) 2005-2009 Regis Houssin         <regis@dolibarr.fr>
+ * Copyright (C) 2005-2009 Regis Houssin         <regis.houssin@capnetworks.com>
  * Copyright (C) 2005      Simon TOSSER          <simon@kornog-computing.com>
  * Copyright (C) 2011-2012 Juanjo Menent         <jmenent@2byte.es>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -26,11 +26,12 @@
  *       \brief      Page des documents joints sur les contrats
  */
 
-require("./pre.inc.php");
-require_once(DOL_DOCUMENT_ROOT."/fichinter/class/fichinter.class.php");
-require_once(DOL_DOCUMENT_ROOT."/core/lib/files.lib.php");
-require_once(DOL_DOCUMENT_ROOT."/core/lib/images.lib.php");
-require_once(DOL_DOCUMENT_ROOT."/core/class/html.formfile.class.php");
+require '../main.inc.php';
+require_once DOL_DOCUMENT_ROOT.'/fichinter/class/fichinter.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/images.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/fichinter.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
 
 $langs->load("other");
 $langs->load("fichinter");
@@ -40,10 +41,11 @@ $langs->load("interventions");
 $id = GETPOST('id','int');
 $ref = GETPOST('ref', 'alpha');
 $action = GETPOST('action','alpha');
+$confirm = GETPOST('confirm','alpha');
 
 // Security check
 if ($user->societe_id) $socid=$user->societe_id;
-$result = restrictedArea($user, 'ficheinter', $id, 'synopsis_fichinter');
+$result = restrictedArea($user, 'ficheinter', $id, 'fichinter');
 
 
 // Get parameters
@@ -62,7 +64,7 @@ $object = new Fichinter($db);
 $object->fetch($id, $ref);
 
 $upload_dir = $conf->ficheinter->dir_output.'/'.dol_sanitizeFileName($object->ref);
-$modulepart='synopsis_fichinter';
+$modulepart='fichinter';
 
 
 /*
@@ -71,41 +73,41 @@ $modulepart='synopsis_fichinter';
 
 if (GETPOST('sendit','alpha') && ! empty($conf->global->MAIN_UPLOAD_DOC))
 {
-	require_once(DOL_DOCUMENT_ROOT."/core/lib/files.lib.php");
+	require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
 
-	if (dol_mkdir($upload_dir) >= 0)
+	dol_add_file_process($upload_dir,0,1,'userfile');
+}
+
+// Delete
+else if ($action == 'confirm_deletefile' && $confirm == 'yes')
+{
+	if ($object->id > 0)
 	{
-		$resupload=dol_move_uploaded_file($_FILES['userfile']['tmp_name'], $upload_dir . "/" . dol_unescapefile($_FILES['userfile']['name']),0,0,$_FILES['userfile']['error']);
-		if (is_numeric($resupload) && $resupload > 0)
-		{
-		    if (image_format_supported($upload_dir . "/" . $_FILES['userfile']['name']) == 1)
-		    {
-                // Create small thumbs for company (Ratio is near 16/9)
-                // Used on logon for example
-                $imgThumbSmall = vignette($upload_dir . "/" . $_FILES['userfile']['name'], $maxwidthsmall, $maxheightsmall, '_small', $quality, "thumbs");
+		$langs->load("other");
+		$object->fetch_thirdparty();
 
-                // Create mini thumbs for company (Ratio is near 16/9)
-                // Used on menu or for setup page for example
-                $imgThumbMini = vignette($upload_dir . "/" . $_FILES['userfile']['name'], $maxwidthmini, $maxheightmini, '_mini', $quality, "thumbs");
-		    }
-		    $mesg = '<div class="ok">'.$langs->trans("FileTransferComplete").'</div>';
-		}
-		else
-		{
-			$langs->load("errors");
-			if ($resupload < 0)	// Unknown error
-			{
-				$mesg = '<div class="error">'.$langs->trans("ErrorFileNotUploaded").'</div>';
-			}
-			else if (preg_match('/ErrorFileIsInfectedWithAVirus/',$resupload))	// Files infected by a virus
-			{
-				$mesg = '<div class="error">'.$langs->trans("ErrorFileIsInfectedWithAVirus").'</div>';
-			}
-			else	// Known error
-			{
-				$mesg = '<div class="error">'.$langs->trans($resupload).'</div>';
-			}
-		}
+		$file = $upload_dir . '/' . GETPOST('urlfile');	// Do not use urldecode here ($_GET and $_REQUEST are already decoded by PHP).
+		$ret=dol_delete_file($file,0,0,0,$object);
+		if ($ret) setEventMessage($langs->trans("FileWasRemoved", GETPOST('urlfile')));
+		else setEventMessage($langs->trans("ErrorFailToDeleteFile", GETPOST('urlfile')), 'errors');
+		header('Location: '.$_SERVER["PHP_SELF"].'?id='.$id);
+		exit;
+	}
+}
+
+// Delete
+else if ($action == 'confirm_deletefile' && $confirm == 'yes')
+{
+	if ($object->id > 0)
+	{
+		$langs->load("other");
+		$object->fetch_thirdparty();
+
+		$file = $upload_dir . '/' . GETPOST('urlfile');	// Do not use urldecode here ($_GET and $_REQUEST are already decoded by PHP).
+		dol_delete_file($file,0,0,0,$object);
+		$_SESSION['DolMessage'] = '<div class="ok">'.$langs->trans("FileWasRemoved",GETPOST('urlfile')).'</div>';
+		Header('Location: '.$_SERVER["PHP_SELF"].'?id='.$id);
+		exit;
 	}
 }
 
@@ -123,22 +125,7 @@ if ($object->id)
 {
 	$object->fetch_thirdparty();
 
-    $soc = new Societe($db);
-    $soc->fetch($object->societe->id);
-
-	if ( $error_msg )
-	{
-		echo '<div class="error">'.$error_msg.'</div><br>';
-	}
-
-	if ($action == 'delete')
-	{
-		$file = $upload_dir . '/' . GETPOST('urlfile','alpha');	// Do not use urldecode here ($_GET and $_REQUEST are already decoded by PHP).
-		$result=dol_delete_file($file);
-		//if ($result >= 0) $mesg=$langs->trans("FileWasRemoced");
-	}
-
-	$head=Synopsis_fichinter_prepare_head($object, $user);
+	$head=fichinter_prepare_head($object, $user);
 
 	dol_fiche_head($head, 'documents',  $langs->trans("InterventionCard"), 0, 'intervention');
 
@@ -154,9 +141,11 @@ if ($object->id)
 
     print '<table class="border" width="100%">';
 
+    $linkback = '<a href="'.DOL_URL_ROOT.'/fichinter/list.php'.(! empty($socid)?'?socid='.$socid:'').'">'.$langs->trans("BackToList").'</a>';
+
 	// Ref
 	print '<tr><td width="30%">'.$langs->trans("Ref").'</td><td>';
-	print $form->showrefnav($object,'ref','',1,'ref','ref');
+	print $form->showrefnav($object, 'ref', $linkback, 1, 'ref', 'ref');
 	print '</td></tr>';
 
 	// Societe
@@ -168,14 +157,22 @@ if ($object->id)
 
     print '</div>';
 
+    /*
+     * Confirmation suppression fichier
+     */
+    if ($action == 'delete')
+    {
+    	$ret=$form->form_confirm($_SERVER["PHP_SELF"].'?id='.$object->id.'&urlfile='.urlencode($_GET["urlfile"]), $langs->trans('DeleteFile'), $langs->trans('ConfirmDeleteFile'), 'confirm_deletefile', '', 0, 1);
+    	if ($ret == 'html') print '<br>';
+    }
 
     // Affiche formulaire upload
    	$formfile=new FormFile($db);
-	$formfile->form_attach_new_file(DOL_URL_ROOT.'/fichinter/document.php?id='.$object->id,'',0,0,$user->rights->synopsisficheinter->creer,50,$object);
+	$formfile->form_attach_new_file(DOL_URL_ROOT.'/fichinter/document.php?id='.$object->id,'',0,0,$user->rights->ficheinter->creer,50,$object);
 
 
 	// List of document
-	//$param='&id='.$object->id;
+	$param='&id='.$object->id;
 	$formfile->list_of_documents($filearray,$object,'ficheinter',$param);
 
 }

3d8f899f829f03869c236be9bb00b81b7674d71d
megajean <megajean@megajean-GA-990XA-UD3>
2013-12-03 17:21:59 +0100

    droit

--- htdocs/Synopsis_Tools/maj.php
+++ htdocs/Synopsis_Tools/maj.php
@@ -114,10 +114,9 @@ if (isset($_GET['action']) && $_GET['action'] == "import") {
                                                                             342, 343, 230001, 161881, 161882,
                                                                             2411,2412,2413,2414));
     $maj->ajoutDroitGr(array(14), array(358, 80881, 80882, 80883, 80884, 80886,106,
-                            1001, 1002, 1003,1004,1005,230002,
-                            21,22,23,24,25,26,27,28,
-                            229201,229202,229203,229204,229205,229206));
-    $maj->ajoutDroitGr(array(2), array(21,22,23,24,25,26,27,28,230002));
+                            1001, 1002, 1003,1004,1005,
+                            229201,229202,229203,229204,229205,229206));//Admin
+    $maj->ajoutDroitGr(array(2, 7, 14), array(21,22,23,24,25,26,27,28,230002));//Propal
 }elseif (isset($_GET['action']) && $_GET['action'] == "fusionChrono") {
     fusionChrono($_REQUEST['id1'], $_REQUEST['id2']);
     $_REQUEST['action'] = "majChrono";

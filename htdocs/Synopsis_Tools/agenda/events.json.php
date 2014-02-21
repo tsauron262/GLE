d5635c0e4d75b42d756a55b4fafda0db9e1fa9de
megajean <megajean@megajean-GA-990XA-UD3>
2014-02-21 15:15:27 +0100

    test agenda2

--- htdocs/Synopsis_Tools/agenda/events.json.php
+++ htdocs/Synopsis_Tools/agenda/events.json.php
@@ -10,7 +10,7 @@ $result = $db->query($sql);
 //echo $sql;
 while($ligne = $db->fetch_object($result)){
     $userId = $newTabUser[$ligne->fk_user_action];
-    if(!$userId > 0)
+    if(!$userId > 0 || $userId === 0)
         die($sql.$userId."-".$ligne->fk_user_action);
         $text = "<a href='" . DOL_URL_ROOT . "/comm/action/fiche.php?id=" . $ligne->id . "'>" . $ligne->label;
         $eventsStr[] = '{"id":'.$ligne->id.', "start":"'.date('c', $db->jdate($ligne->datep)).'", "end":"'.date('c', $db->jdate($ligne->datep2)).'", "title":"'.$text.'", "userId": '.$userId.'}';

a497c44f0838f4ece7490ed24c3f44bfbb43c814
megajean <megajean@megajean-GA-990XA-UD3>
2014-03-05 21:50:25 +0100

    agenda

--- htdocs/Synopsis_Tools/agenda/events.json.php
+++ htdocs/Synopsis_Tools/agenda/events.json.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once('../../main.inc.php');
 require_once("libAgenda.php");
 
@@ -8,26 +9,27 @@ $eventsStr = array();
 $sql = ("SELECT *, (`datep2` - `datep`) as duree FROM " . MAIN_DB_PREFIX . "actioncomm WHERE ((datep < '" . date('Y-m-d 23:59:00', $_REQUEST['end']) . "' AND datep > '" . date('Y-m-d 00:00:00', $_REQUEST['start']) . "') || (datep2 < '" . date('Y-m-d 23:59:00', $_REQUEST['end']) . "' AND datep2 > '" . date('Y-m-d 00:00:00', $_REQUEST['start']) . "')) AND fk_user_action IN (" . implode(",", $newTabUser2) . ") order by duree DESC ");
 $result = $db->query($sql);
 //echo $sql;
-while($ligne = $db->fetch_object($result)){
+while ($ligne = $db->fetch_object($result)) {
     $userId = $newTabUser[$ligne->fk_user_action];
 //        $text = "<a href='" . DOL_URL_ROOT . "/comm/action/fiche.php?id=" . $ligne->id . "'>" . $ligne->label;
-        $text = "<a href='" . DOL_URL_ROOT . "/comm/action/fiche.php?id=" . $ligne->id . "' onclick=\"dispatchePopIFrame('" . DOL_URL_ROOT . "/comm/action/fiche.php?id=" . $ligne->id . "&action=edit&optioncss=print', function(){ $('#calendar').weekCalendar('refresh');}, '" . $ligne->label."', 1); return false;\">" . $ligne->label;
-        $text = str_replace(array("\r\n", "\r", "\n"), "<br />", $text); 
-        $text = str_replace('"', '\"', $text);
-        if(!isset($ligne->datep2))
-            $ligne->datep2 = $ligne->datep;
-        $tabColor = array(50=> "#BBCCFF", 5=>"purple", 2=>"red");
-        $colorStr = '';
-        if(isset($tabColor[$ligne->fk_action]))
-            $colorStr = ', "color":"'.$tabColor[$ligne->fk_action].'"';
-        $eventsStr[] = '{"id":'.$ligne->id.', "start":"'.date('c', $db->jdate($ligne->datep)).'", "end":"'.date('c', $db->jdate($ligne->datep2)).'", "title":"'.$text.'", "userId": '.$userId.$colorStr.'}';
+    $text = "<a href='" . DOL_URL_ROOT . "/comm/action/fiche.php?id=" . $ligne->id . "' onclick=\"dispatchePopIFrame('" . DOL_URL_ROOT . "/comm/action/fiche.php?id=" . $ligne->id . "&action=edit&optioncss=print', function(){ $('#calendar').weekCalendar('refresh');}, '" . $ligne->label . "', 1); return false;\">" . $ligne->label;
+    $text = str_replace(array("\r\n", "\r", "\n"), "<br />", $text);
+    $text = str_replace('"', '\"', $text);
+    if (!isset($ligne->datep2))
+        $ligne->datep2 = $ligne->datep;
+    if (!isset($ligne->datep))
+        $ligne->datep = $ligne->datep2;
+    $tabColor = array(50 => "#BBCCFF", 5 => "purple", 2 => "red");
+    $colorStr = '';
+    if (isset($tabColor[$ligne->fk_action]))
+        $colorStr = ', "color":"' . $tabColor[$ligne->fk_action] . '"';
+    if (isset($ligne->datep))
+        $eventsStr[] = '{"id":' . $ligne->id . ', "start":"' . date('c', $db->jdate($ligne->datep)) . '", "end":"' . date('c', $db->jdate($ligne->datep2)) . '", "title":"' . $text . '", "userId": ' . $userId . $colorStr . '}';
 }
 echo "[";
-    
+
 echo implode(",", $eventsStr);
 
 
 echo "]";
-
-
 ?>

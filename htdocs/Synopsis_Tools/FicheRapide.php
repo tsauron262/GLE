35bbb9b6d72e5a19ef11eced507ae66c666fce69
megajean <megajean@megajean-GA-990XA-UD3>
2014-07-09 14:44:02 +0200

    fiche rapide chrono sav

--- htdocs/Synopsis_Tools/FicheRapide.php
+++ htdocs/Synopsis_Tools/FicheRapide.php
@@ -104,6 +104,7 @@ if (isset($_POST["Descr"]) && $_POST["Descr"] != "" && isset($_REQUEST['socid'])
                 $lien->fetch(3);
                 $lien->setValue($chrono->id, array($chronoProd->id));
                 $chrono->fetch($chrono->id); 
+                $chronoProd->fetch($chronoProd->id); 
                 echo "Enregistrement effecué avec succés. <br/>SAV : ".$chrono->getNomUrl(1). " <br/>Produit : ".$chronoProd->getNomUrl(1);
             } else {
                 echo "Echec de l'Enregistrement";

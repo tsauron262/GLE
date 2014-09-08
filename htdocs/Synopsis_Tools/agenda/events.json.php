58458ddfeaa0ff604a425a959ecbbc234bfc9e86
megajean <megajean@megajean-GA-990XA-UD3>
2014-09-08 21:20:23 +0200

    modif agenda + commit preceent

--- htdocs/Synopsis_Tools/agenda/events.json.php
+++ htdocs/Synopsis_Tools/agenda/events.json.php
@@ -7,10 +7,10 @@ require_once("libAgenda.php");
 
 $eventsStr = array();
 if ($_REQUEST['end'] != "NaN" && $_REQUEST['start'] != "NaN") {
-    $sql = ("SELECT *, (`datep2` - `datep`) as duree FROM " . MAIN_DB_PREFIX . "actioncomm a LEFT JOIN " . MAIN_DB_PREFIX . "actioncomm_extrafields on fk_object = a.id WHERE ((datep < '" . date('Y-m-d 23:59:00', $_REQUEST['end']) . "' AND datep >= '" . date('Y-m-d 00:00:00', $_REQUEST['start']) . "') "
-            . "|| (datep2 < '" . date('Y-m-d 23:59:00', $_REQUEST['end']) . "' AND datep2 > '" . date('Y-m-d 00:00:00', $_REQUEST['start']) . "')"
+    $sql = ("SELECT *, (`datep2` - `datep`) as duree FROM " . MAIN_DB_PREFIX . "actioncomm a LEFT JOIN " . MAIN_DB_PREFIX . "actioncomm_extrafields on fk_object = a.id WHERE ((datep < '" . date('Y-m-d 23:59:00', $_REQUEST['end']) . "' AND datep >= '" . date('Y-m-d 00:00:00', $_REQUEST['start']) . "') "//Rdv dbut ds periode fin aprés
+            . "|| (datep2 <= '" . date('Y-m-d 23:59:00', $_REQUEST['end']) . "' AND datep2 > '" . date('Y-m-d 00:00:00', $_REQUEST['start']) . "')" //fin ds la periode
             . "|| (datep2 > '" . date('Y-m-d 23:59:00', $_REQUEST['end']) . "' AND datep < '" . date('Y-m-d 00:00:00', $_REQUEST['start']) . "')) AND fk_user_action IN (" . implode(",", $newTabUser2) . ") AND (fk_action > 40 || fk_action < 9) order by datep ASC ");
-    $result = $db->query($sql);echo $sql;
+    $result = $db->query($sql);//avant et aprés periode
 //echo $sql;
     $f = 0;
     while ($ligne = $db->fetch_object($result)) {

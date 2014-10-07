73262049cf3758db3a438fd656cf15793f0465b4
megajean <megajean@megajean-GA-990XA-UD3>
2014-10-07 18:21:03 +0200

    ti bug divers

--- htdocs/Synopsis_Tools/class/divers.class.php
+++ htdocs/Synopsis_Tools/class/divers.class.php
@@ -16,7 +16,7 @@ class synopsisHook {
         global $conf, $db, $tabProductType, $tabTypeLigne, $langs, $user, $tabContactPlus, $tabSelectNatureIntrv, $tabCentre;
 
 
-        $tabCentre = array("V" => array("04 75 81 81 54", "sav07@bimp.fr"), "VB" => array("04 75 81 81 54", "sav07@bimp.fr"), "M" => array("04 50 22 15 55", "sav74@bimp.fr"), 'L' => array("04 78 17 30 28", "sav69@bimp.fr"), "S" => array("04 77 81 58 12", "sav42@bimp.fr"), "GB" => array("04 76 23 05 18", "sav38@bimp.fr"), "GA" => array("04 76 23 05 18", "sav38@bimp.fr"), "B" => array("03 81 47 11 71", "sav250@bimp.fr"), "MO" => array("03 81 95 19 20", "sav252@bimp.fr"));
+        $tabCentre = array("V" => array("04 75 81 81 54", "sav07@bimp.fr"), "VB" => array("04 75 81 81 54", "sav07@bimp.fr"), "M" => array("04 50 22 15 55", "sav74@bimp.fr"), "C" => array("04 50 22 15 55", "sav74@bimp.fr"), "CB" => array("04 50 22 15 55", "sav74@bimp.fr"), 'L' => array("04 78 17 30 28", "sav69@bimp.fr"), "S" => array("04 77 81 58 12", "sav42@bimp.fr"), "GB" => array("04 76 23 05 18", "sav38@bimp.fr"), "GA" => array("04 76 23 05 18", "sav38@bimp.fr"), "B" => array("03 81 47 11 71", "sav250@bimp.fr"), "MO" => array("03 81 95 19 20", "sav252@bimp.fr"));
 
 
         if (is_object($db) && isset($conf->global->MAIN_MODULE_SYNOPSISTOOLS)) {

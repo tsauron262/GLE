6ce636de489532d9fcfc2a041227da8adc1658d3
megajean <megajean@megajean-GA-990XA-UD3>
2014-10-20 19:45:08 +0200

    Merge origin/mod_apple_v5 into gle3.6.0
    Conflicts:
    	htdocs/synopsisapple/GSXRequests.php

--- htdocs/synopsisapple/GSXRequests.php
+++ htdocs/synopsisapple/GSXRequests.php
@@ -634,7 +634,9 @@ class GSX_Request {
                                 if (count($subDatasNode) == 1) {
                                     $subDatasNodes = XMLDoc::findChildElements($subDatasNode[0], 'data', null, null, 1);
                                     if ($multiple) {
-                                        if (isset($_POST[$dataName . '_nextIdx'])) {
+                                        if (isset($_POST[$dataName . '_nextIdx']))
+                                            $_POST[$dataName . '_nextIdx'] = 0;
+                                        if (isset($_POST[$dataName . '_nextIdx'])){
                                             $datas[$dataName] = array();
                                             for ($i = 1; $i < (int) $_POST[$dataName . '_nextIdx']; $i++) {
                                                 $datas[$dataName][] = $this->processRequestDatas($subDatasNodes, $i);

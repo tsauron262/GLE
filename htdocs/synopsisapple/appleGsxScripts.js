fcda6ae6c53bb1e62a5d469bdded460a071719c4
megajean <megajean@megajean-GA-990XA-UD3>
2014-10-20 19:40:48 +0200

    Merge origin/mod_apple_v5 into gle3.6.0
    Conflicts:
    	htdocs/synopsisapple/GSXRequests.php

--- htdocs/synopsisapple/appleGsxScripts.js
+++ htdocs/synopsisapple/appleGsxScripts.js
@@ -1425,7 +1425,6 @@ function submitGsxRequestForm(prodId, request, repairRowId) {
 
 function traiteCommandeRetour(html, $resultContainer) {
     if ($resultContainer.length) {
-        $resultContainer.html(html);
         if ($resultContainer.find("prix").size() > 0) {
             prix = $resultContainer.find("prix").html();
             $resultContainer.find("prix").html("");
@@ -1436,9 +1435,10 @@ function traiteCommandeRetour(html, $resultContainer) {
         }
         if (html.indexOf('<formSus>OK</formSus>') !== -1) {
             htmlFormSus = html.replace("<formSus>OK</formSus>", "");
-            alert(htmlFormSus);
             $("#formSus").append(htmlFormSus);
         }
+        else
+        $resultContainer.html(html);
     }
 
 }

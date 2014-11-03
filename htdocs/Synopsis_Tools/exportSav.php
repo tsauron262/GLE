1b5f7097fcb6542aeffe18063306636e6cd5831a
megajean <megajean@megajean-GA-990XA-UD3>
2014-11-03 23:46:39 +0100

    new dossier export

--- htdocs/Synopsis_Tools/exportSav.php
+++ htdocs/Synopsis_Tools/exportSav.php
@@ -34,7 +34,7 @@ $centre = (isset($_POST['centre']) ? $_POST['centre'] : null);
 $typeAff = (isset($_POST['typeAff']) ? $_POST['typeAff'] : null);
 $typeAff2 = (isset($_POST['typeAff2']) ? $_POST['typeAff2'] : null);
 $sortie = (isset($_REQUEST['sortie']) ? $_REQUEST['sortie'] : 'html');
-$paye = (isset($_REQUEST['paye']) ? 1: 0);
+$paye = (isset($_REQUEST['paye']) ? 1 : 0);
 $dateDeb = (isset($_POST['dateDeb']) ? $_POST['dateDeb'] : null);
 $dateFin = (isset($_POST['dateFin']) ? $_POST['dateFin'] : null);
 
@@ -42,22 +42,22 @@ $dateFin = (isset($_POST['dateFin']) ? $_POST['dateFin'] : null);
 echo "<form method='POST'>";
 
 echo "<select name='sortie'>";
-foreach(array("html" => "Affichage", "file" => "Fichier Exel") as $val=> $label) {
+foreach (array("html" => "Affichage", "file" => "Fichier Exel") as $val => $label) {
     $valSelect = $sortie;
     echo "<option value='" . $val . "' " . ($val == $valSelect ? "selected='selected'" : "") . ">" . $label . "</option>";
 }
 echo "</select>";
 
 echo "<select name='typeAff2'>";
-foreach(array("sav" => "SAV", "ca" => "CA", "nb" => "Nombre PC") as $val=> $label) {
+foreach (array("sav" => "SAV", "ca" => "CA", "nb" => "Nombre PC") as $val => $label) {
     $valSelect = $typeAff2;
     echo "<option value='" . $val . "' " . ($val == $valSelect ? "selected='selected'" : "") . ">" . $label . "</option>";
 }
 echo "</select>";
 
 echo "<select name='typeAff'>";
-    echo "<option value=''></option>";
-foreach(array("parTypeMat" => "Par materiel", "parTypeGar" => "Par type de garantie", "parCentre" => "Par centre") as $val=> $label) {
+echo "<option value=''></option>";
+foreach (array("parTypeMat" => "Par materiel", "parTypeGar" => "Par type de garantie", "parCentre" => "Par centre") as $val => $label) {
     $valSelect = $typeAff;
     echo "<option value='" . $val . "' " . ($val == $valSelect ? "selected='selected'" : "") . ">" . $label . "</option>";
 }
@@ -66,7 +66,7 @@ echo "</select><br/>";
 
 echo "<select name='centre'>";
 $result = $db->query("SELECT * FROM `" . MAIN_DB_PREFIX . "Synopsis_Process_form_list_members` WHERE `list_refid` = 11 ");
-    echo "<option value=''></option>";
+echo "<option value=''></option>";
 while ($ligne = $db->fetch_object($result)) {
     $val = $ligne->valeur;
     $label = $ligne->label;
@@ -75,10 +75,10 @@ while ($ligne = $db->fetch_object($result)) {
 }
 echo "</select>";
 
-echo "<label for='paye'>Paye</label><input type='checkbox' name='paye' id='paye'".($paye? " checked='ckecked'" : "")."/>";
+echo "<label for='paye'>Paye</label><input type='checkbox' name='paye' id='paye'" . ($paye ? " checked='ckecked'" : "") . "/>";
 
-echo "Debut <input name='dateDeb' type='text' class='datePicker' value='".$dateDeb."'/>";
-echo "Fin <input name='dateFin' type='text' class='datePicker' value='".$dateFin."'/>";
+echo "Debut <input name='dateDeb' type='text' class='datePicker' value='" . $dateDeb . "'/>";
+echo "Fin <input name='dateFin' type='text' class='datePicker' value='" . $dateFin . "'/>";
 
 echo "<br/><input class='butAction' type='submit' value='Valider'/></form><br/><br/>";
 
@@ -88,8 +88,20 @@ echo "<br/><input class='butAction' type='submit' value='Valider'/></form><br/><
 
 
 
+if (isset($_request['reinitGarantiePa'])) {
+    $result = $db->query("SELECT fact.rowid, COUNT(factdet.rowid) as nbGar FROM `llx_facture` fact, llx_facturedet factdet WHERE factdet.fk_facture = fact.rowid AND factdet.`description` LIKe 'Garantie' AND fact.total = 0 GROUP BY fact.rowid");
+    while ($ligne = $db->fetch_object($result)) {
+//    if($ligne->nbGar == 1){
+        $result2 = $db->query("SELECt SUM(buy_price_ht) as tot FROM llx_facturedet WHERE description not like 'Garantie' AND fk_facture = " . $ligne->rowid);
+        $ligne2 = $db->fetch_object($result2);
 
-
+        $db->query("UPDATe llx_facturedet SET buy_price_ht = -" . ($ligne2->tot / $ligne->nbGar) . " WHERE fk_facture = " . $ligne->rowid . " AND description LIKE 'Garantie'");
+//    }
+//    else
+//        echo "Plusieurs Garantie (".$ligne->nbGar.") facture ".$ligne->rowid;
+    }
+    die;
+}
 
 
 

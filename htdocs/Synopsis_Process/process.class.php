1a99fe138f9b44d555bab85ff7ae6b4da214f3fd
megajean <megajean@megajean-GA-990XA-UD3>
2015-03-12 16:58:37 +0100

    rembourssement accompte sos garantie

--- htdocs/Synopsis_Process/process.class.php
+++ htdocs/Synopsis_Process/process.class.php
@@ -2741,7 +2741,7 @@ class requete extends formulaireSource {
                     $requete = preg_replace('/\[\[indexField\]\]/', $this->tableName . "." . $this->indexField . "='" . $val . "'", $requete);
                 else
                     $requete = preg_replace('/\[\[indexField\]\]/', $this->indexField . "='" . $val . "'", $requete);
-                dol_syslog(LOG_ERR, $requete."mmmm");
+                dol_syslog($requete."mmmm", LOG_ERR);
                 eval("\$requete = \"$requete\";");
                 $sql = $this->db->query($requete);
                 $arr = array();

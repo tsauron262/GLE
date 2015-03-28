c4a41a6820a0dbc69296ef963d7f397d4fa9fdb6
megajean <megajean@megajean-GA-990XA-UD3>
2015-03-28 16:59:41 +0100

    bug divers

--- htdocs/synopsiscaldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
+++ htdocs/synopsiscaldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
@@ -471,7 +471,8 @@ class PDO extends AbstractBackend {
         $user->fetch($calendarId);
 
         $action->type_id = 1;
-        global $objectUriTemp, $objectEtagTemp;
+        global $objectUriTemp, $objectEtagTemp, $objectDataTemp;
+        $objectDataTemp = $calendarData;
         $objectEtagTemp = $extraData['etag'];
         $objectUriTemp = $objectUri;
         $action->add($user);

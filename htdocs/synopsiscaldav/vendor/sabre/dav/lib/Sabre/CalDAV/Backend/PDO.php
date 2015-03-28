7f8902b634c79a267e7f21938ddab4b972a1d07a
megajean <megajean@megajean-GA-990XA-UD3>
2015-03-29 00:32:02 +0100

    corrigé par théo v3

--- htdocs/synopsiscaldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
+++ htdocs/synopsiscaldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
@@ -556,7 +556,7 @@ class PDO extends AbstractBackend {
         foreach ($tabT as $ligneT) {
             $tabT2 = array();
             if (stripos($ligneT, 'BEGIN:') === false && !(stripos($ligneT, 'END:') === 0))
-                $tabT2 = preg_split("/[:;]/", $ligneT);
+                $tabT2 = preg_split("[/:;/]", $ligneT);
             if (isset($tabT2[1]))
                 $tabResult[$tabT2[0]] = str_replace($tabT2[0] . ":", "", $ligneT);
             else

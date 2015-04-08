fd35bf2eb058f60d1689256108e921860d7f584f
megajean <megajean@megajean-GA-990XA-UD3>
2015-04-08 21:41:50 +0200

    flo holiday modifs

---
+++ htdocs/synopsisholiday/core/modules/modSynopsisHoliday.class.php
@@ -0,0 +1,209 @@
+<?php
+/*
+ * GLE by Synopsis et DRSI
+ *
+ * Author: Tommy SAURON <tommy@drsi.fr>
+ * Licence : Artistic Licence v2.0
+ *
+ * Version 1.1
+ * Create on : 4-1-2009
+ *
+ * Infos on http://www.finapro.fr
+ *
+ */
+
+/**     \defgroup   ProspectBabel     Module ProspectBabel
+        \brief      Module pour inclure ProspectBabel dans Dolibarr
+*/
+
+/**
+        \file       htdocs/core/modules/modProspectBabel.class.php
+        \ingroup    ProspectBabel
+        \brief      Fichier de description et activation du module de Prospection Babel
+*/
+
+include_once(DOL_DOCUMENT_ROOT ."/core/modules/DolibarrModules.class.php");
+
+/**     \class      modProspectBabel
+        \brief      Classe de description et activation du module de Prospection Babel
+*/
+
+class modSynopsisHoliday extends DolibarrModules
+{
+
+   /**
+    *   \brief      Constructeur. Definit les noms, constantes et boites
+    *   \param      DB      handler d'acces base
+    */
+    function modSynopsisHoliday($DB)
+    {
+        $this->db = $DB ;
+        $this->numero = 8000;
+
+        $this->family = "Synopsis";
+        $this->name = "Holiday";
+        $this->description = utf8_decode("Cong&eacute;s et RTT");
+        $this->version = '0.1';    // 'experimental' or 'dolibarr' or version
+        $this->const_name = 'MAIN_MODULE_SYNOPSISHOLIDAY';
+        $this->special = 0;
+        $this->picto='holiday@holiday';
+
+        // Dir
+        $this->dirs = array();
+
+        // Config pages
+        //$this->config_page_url = "";
+        $this->config_page_url = "holiday.php@synopsisholiday";
+
+        // Dependences
+        $this->depends = array("modHoliday");
+
+        // Constantes
+        $this->const = array();
+
+//        // Boites
+//        $this->boxes = array();
+
+    // Boites
+//    $this->boxes = array();
+//    $r=0;
+//    $this->boxes[$r][1] = "box_deplacement.php";
+//    $r++;
+
+
+        // Permissions
+        $this->rights = array();
+        $this->rights_class = 'synopsisholiday';
+
+        $r = 0;
+
+        
+
+
+        $this->menus = array();            // List of menus to add
+        $r=0;
+        $s = $r;
+        $r++;
+        
+        
+        $this->menu[$r]=array('fk_menu'=> 'fk_mainmenu=hrm',
+                            'type'=>'left',
+                            'titre'=>'CPTitreMenu',
+                            'mainmenu'=>'hrm',
+                            'leftmenu'=>'holiday',        // To say if we can overwrite leftmenu
+                            'url'=>'/synopsisholiday/index.php?leftmenu=holiday',
+                            'langs' => 'holiday@synopsisholiday',
+                            'position'=>1,
+                            'perms'=>'$user->rights->holiday->write',
+                            'target'=>'',
+                            'user'=>0);
+        
+        
+        $r++;
+        $this->menu[$r]=array('fk_menu'=> 'fk_mainmenu=hrm,fk_leftmenu=holiday',
+                            'type'=>'left',
+                            'titre'=>'MenuAddCP',
+                            'mainmenu'=>'hrm',
+                            'leftmenu'=>'1',        // To say if we can overwrite leftmenu
+                            'url'=>'/synopsisholiday/fiche.php?&action=request',
+                            'langs' => 'holiday@synopsisholiday',
+                            'position'=>1,
+                            'perms'=>'$user->rights->holiday->write',
+                            'target'=>'',
+                            'user'=>0);
+        
+        
+        $r++;
+        $this->menu[$r]=array('fk_menu'=> 'fk_mainmenu=hrm,fk_leftmenu=holiday',
+                            'type'=>'left',
+                            'titre'=>'MenuConfCP',
+                            'mainmenu'=>'hrm',
+                            'leftmenu'=>'1',        // To say if we can overwrite leftmenu
+                            'url'=>'/synopsisholiday/define_holiday.php?&action=request',
+                            'langs' => 'holiday@synopsisholiday',
+                            'position'=>1,
+                            'perms'=>'$user->rights->holiday->define_holiday',
+                            'target'=>'',
+                            'user'=>0);
+        
+        
+        $r++;
+        $this->menu[$r]=array('fk_menu'=> 'fk_mainmenu=hrm,fk_leftmenu=holiday',
+                            'type'=>'left',
+                            'titre'=>'MenuConfRTT',
+                            'mainmenu'=>'hrm',
+                            'leftmenu'=>'1',        // To say if we can overwrite leftmenu
+                            'url'=>'/synopsisholiday/define_rtt.php?&action=request',
+                            'langs' => 'holiday@synopsisholiday',
+                            'position'=>1,
+                            'perms'=>'$user->rights->holiday->define_holiday',
+                            'target'=>'',
+                            'user'=>0);
+        
+        
+        $r++;
+        $this->menu[$r]=array('fk_menu'=> 'fk_mainmenu=hrm,fk_leftmenu=holiday',
+                            'type'=>'left',
+                            'titre'=>'MenuLogCP',
+                            'mainmenu'=>'hrm',
+                            'leftmenu'=>'1',        // To say if we can overwrite leftmenu
+                            'url'=>'/synopsisholiday/view_log.php?&action=request',
+                            'langs' => 'holiday@synopsisholiday',
+                            'position'=>1,
+                            'perms'=>'$user->rights->holiday->view_log',
+                            'target'=>'',
+                            'user'=>0);
+        
+        
+        $r++;
+        $this->menu[$r]=array('fk_menu'=> 'fk_mainmenu=hrm,fk_leftmenu=holiday',
+                            'type'=>'left',
+                            'titre'=>'MenuReportMonth',
+                            'mainmenu'=>'hrm',
+                            'leftmenu'=>'1',        // To say if we can overwrite leftmenu
+                            'url'=>'/synopsisholiday/month_report.php?&action=request',
+                            'langs' => 'holiday@synopsisholiday',
+                            'position'=>5   ,
+                            'perms'=>'$user->rights->holiday->month_report',
+                            'target'=>'',
+                            'user'=>0);
+        
+        
+        $this->tabs = array('user:-paidholidays',
+            'user:+paidholidaysRtt:Congés RTT:@monmodule:/synopsisholiday/index.php?id=__ID__');
+        
+    }
+   /**
+    *   \brief      Fonction appelee lors de l'activation du module. Insere en base les constantes, boites, permissions du module.
+    *               Definit egalement les repertoires de donnees e creer pour ce module.
+    */
+  function init()
+  {
+        $this->remove();
+    $sql = array("ALTER TABLE `llx_holiday` ADD `type_conges` INT( 1 ) UNSIGNED NOT NULL DEFAULT '0' COMMENT '0: ordinaires. 1 : congés exceptionnels. 2: rtt';",
+"ALTER TABLE `llx_holiday` ADD `date_drh_valid` DATETIME NULL DEFAULT NULL AFTER `date_valid`;",
+"ALTER TABLE `llx_holiday` ADD `fk_user_drh_valid` INT( 11 ) NULL DEFAULT NULL AFTER `fk_user_valid` ;",
+"ALTER TABLE `llx_holiday_users` ADD `nb_rtt` DOUBLE NOT NULL DEFAULT '0';",
+"ALTER TABLE `llx_holiday` ADD `fk_actioncomm` INT( 11 ) NULL DEFAULT NULL;",
+"INSERT INTO `llx_holiday_config` (
+`name` ,
+`value`
+) VALUES (
+'drhUserId', '81'),(
+'nbRTTDeducted', '1'
+);");
+
+    return $this->_init($sql);
+  }
+
+  /**
+   *    \brief      Fonction appelee lors de la desactivation d'un module.
+   *                Supprime de la base les constantes, boites et permissions du module.
+   */
+  function remove()
+  {
+    $sql = array();
+    return $this->_remove($sql);
+  }
+}
+?>
---
+++ htdocs/synopsisholiday/define_rtt.php
@@ -0,0 +1,132 @@
+<?php
+
+/* Copyright (C) 2007-2012 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2011      Dimitri Mouillard <dmouillard@teclib.com>
+ * Copyright (C) 2013      Marcos García <marcosgdf@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * 		File that defines the balance of paid holiday of users.
+ *
+ *   	\file       htdocs/holiday/define_holiday.php
+ * 		\ingroup    holiday
+ * 		\brief      File that defines the balance of paid holiday of users.
+ */
+require('../main.inc.php');
+require_once DOL_DOCUMENT_ROOT . '/user/class/user.class.php';
+require_once DOL_DOCUMENT_ROOT . '/synopsisholiday/common.inc.php';
+
+// Protection if external user
+if ($user->societe_id > 0)
+    accessforbidden();
+
+// If the user does not have perm to read the page
+if (!$user->rights->holiday->define_holiday)
+    accessforbidden();
+
+$action = GETPOST('action');
+
+
+/*
+ * View
+ */
+
+$form = new Form($db);
+$userstatic = new User($db);
+$holiday = new Holiday($db);
+
+
+llxHeader(array(), $langs->trans('CPTitreMenu'));
+
+print_fiche_titre($langs->trans('MenuConfRTT'));
+$listUsers = $holiday->fetchUsers(false, false);
+
+// Si il y a une action de mise à jour
+if ($action == 'update' && isset($_POST['update_rtt'])) {
+    $userID = array_keys($_POST['update_rtt']);
+    $userID = $userID[0];
+
+    $userValue = $_POST['nb_rtt'];
+    $userValue = $userValue[$userID];
+
+    if (!empty($userValue)) {
+        $userValue = price2num($userValue, 2);
+    } else {
+        $userValue = 0;
+    }
+
+    //If the user set a comment, we add it to the log comment
+    $comment = ((isset($_POST['note_rtt'][$userID]) && !empty($_POST['note_rtt'][$userID])) ? ' (' . $_POST['note_rtt'][$userID] . ')' : '');
+
+    // We add the modification to the log
+    $holiday->addLogCP($user->id, $userID, $langs->transnoentitiesnoconv('ManualUpdateRTT') . $comment, $userValue, true);
+
+    // Update of the days of the employee
+    $holiday->updateSoldeRTT($userID, $userValue);
+
+    $mesg = '<div class="ok">' . $langs->trans('UpdateConfCPOK') . '</div>';
+
+    dol_htmloutput_mesg($mesg);
+}
+
+$langs->load('users');
+$var = true;
+$i = 0;
+
+dol_fiche_head();
+
+print '<form method="POST" action="' . $_SERVER["PHP_SELF"] . '">' . "\n";
+print '<input type="hidden" name="action" value="update" />';
+print '<table class="noborder" width="100%;">';
+print "<tr class=\"liste_titre\">";
+print '<td width="5%">' . $langs->trans('ID') . '</td>';
+print '<td width="50%">' . $langs->trans('Employee') . '</td>';
+print '<td width="20%" style="text-align:center">' . $langs->trans('Available') . '</td>';
+print '<td width="20%" style="text-align:center">' . $langs->trans('Note') . '</td>';
+print '<td style="text-align:center">' . $langs->trans('UpdateButtonCP') . '</td>';
+print '</tr>';
+
+foreach ($listUsers as $users) {
+
+    $var = !$var;
+
+    print '<tr ' . $bc[$var] . ' style="height: 20px;">';
+    print '<td>' . $users['rowid'] . '</td>';
+    print '<td>';
+    $userstatic->id = $users['rowid'];
+    $userstatic->lastname = $users['name'];
+    $userstatic->firstname = $users['firstname'];
+    print $userstatic->getNomUrl(1);
+    print '</td>';
+    print '<td style="text-align:center">';
+    print '<input type="text" value="' . $holiday->getRTTforUser($users['rowid']) . '" name="nb_rtt[' . $users['rowid'] . ']" size="5" style="text-align: center;"/>';
+    print ' ' . $langs->trans('days') . '</td>' . "\n";
+    print '<td style="text-align:center"><input type="text" value="" name="note_rtt[' . $users['rowid'] . ']" size="30"/></td>';
+    print '<td><input type="submit" name="update_rtt[' . $users['rowid'] . ']" value="' . dol_escape_htmltag($langs->trans("Update")) . '" class="button"/></td>' . "\n";
+    print '</tr>';
+
+    $i++;
+}
+
+print '</table>';
+print '</form>';
+
+dol_fiche_end();
+
+llxFooter();
+
+$db->close();
+?>

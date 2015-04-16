e7758b766efcb6543c1a1521964631d5e333cb88
megajean <megajean@megajean-GA-990XA-UD3>
2015-04-16 11:06:20 +0200

    new type de contrat

--- htdocs/core/menus/standard/eldy.lib.php
+++ htdocs/core/menus/standard/eldy.lib.php
@@ -1103,7 +1103,7 @@ function print_left_eldy_menu($db,$menu_array_before,$menu_array_after,&$tabMenu
 		if ($mainmenu == 'hrm')
 		{
 			// Holiday module
-			if (! empty($conf->holiday->enabled))
+			if (! empty($conf->holiday->enabled2))
 			{
 				$langs->load("holiday");
 

660ee9ac6babd424e5e5daca8fb303ebac74728e
megajean <megajean@megajean-GA-990XA-UD3>
2015-04-16 11:36:57 +0200

    change include

--- htdocs/synopsisholiday/index.php
+++ htdocs/synopsisholiday/index.php
@@ -377,7 +377,7 @@ if (!empty($holiday->holiday)) {
         print '<td align="center">' . dol_print_date($infos_CP['date_debut'], 'day') . '</td>';
         print '<td align="center">' . dol_print_date($infos_CP['date_fin'], 'day') . '</td>';
         print '<td align="right">';
-        $nbopenedday = num_open_day($infos_CP['date_debut_gmt'], $infos_CP['date_fin_gmt'], 0, 1, $infos_CP['halfday']);
+//        $nbopenedday = num_open_day($infos_CP['date_debut_gmt'], $infos_CP['date_fin_gmt'], 0, 1, $infos_CP['halfday']);
         print $nbopenedday . ' ' . $langs->trans('DurationDays');
         print '<td align="right" colspan="2">' . $holidaystatic->LibStatut($infos_CP['statut'], 5) . '</td>';
         print '</tr>' . "\n";

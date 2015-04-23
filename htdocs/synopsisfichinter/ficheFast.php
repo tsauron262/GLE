17d37e9b7bedfbbf60f9b99dbaee94634311a648
megajean <megajean@megajean-GA-990XA-UD3>
2015-04-23 15:06:09 +0200

    netoyage

--- htdocs/synopsisfichinter/ficheFast.php
+++ htdocs/synopsisfichinter/ficheFast.php
@@ -651,6 +651,7 @@ function saveForm() {
                 $fichinterline->fk_contratdet = (isset($_POST['presta' . $i . "_contradet"]) ? $_POST['presta' . $i . "_contradet"] : 0);
                 $fichinterline->typeIntervProd = $_POST['presta' . $i . "_fk_prod"];
                 $fichinterline->isForfait = ($_POST['presta' . $i . "_forfait"] == "on");
+                if($pu_ht > 0)
                 $fichinterline->pu_ht = $pu_ht;
                 $result = $fichinterline->update($user);
             } else

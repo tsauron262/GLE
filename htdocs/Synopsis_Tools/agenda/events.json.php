3a1ce732e94922a6eb33520221e0863b396affe8
megajean <megajean@megajean-GA-990XA-UD3>
2015-04-28 12:28:47 +0200

    holiday

--- htdocs/Synopsis_Tools/agenda/events.json.php
+++ htdocs/Synopsis_Tools/agenda/events.json.php
@@ -24,6 +24,9 @@ if ($_REQUEST['end'] != "NaN" && $_REQUEST['start'] != "NaN") {
 
         if ($ligne->label == "")
             $ligne->label = "N/C";
+        
+        $ligne->label = str_replace("'", "", $ligne->label);
+        $ligne->note = str_replace("'", "", $ligne->note);
 
 
 //        $text = "<a href='" . DOL_URL_ROOT . "/comm/action/fiche.php?id=" . $ligne->id . "'>" . $ligne->label;

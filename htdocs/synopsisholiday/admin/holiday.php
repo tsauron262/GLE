7fb3de6c6ef68a9b03e7e5af0d92dc808ca19726
megajean <megajean@megajean-GA-990XA-UD3>
2015-05-12 18:15:03 +0200

    holiday bug

--- htdocs/synopsisholiday/admin/holiday.php
+++ htdocs/synopsisholiday/admin/holiday.php
@@ -34,7 +34,7 @@
 
 if (!isset($user))
     require '../../main.inc.php';
-require_once DOL_DOCUMENT_ROOT . '/holiday/class/holiday.class.php';
+require_once DOL_DOCUMENT_ROOT . '/synopsisholiday/class/holiday.class.php';
 require_once DOL_DOCUMENT_ROOT . '/core/class/html.form.class.php';
 require_once DOL_DOCUMENT_ROOT . '/user/class/user.class.php';
 require_once DOL_DOCUMENT_ROOT . '/user/class/usergroup.class.php';

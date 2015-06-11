c7e0e6e389ba7cd910d7c4f2ad87dec5a5c5d5b0
megajean <megajean@megajean-GA-990XA-UD3>
2015-06-11 16:52:36 +0200

    Merge origin/holiday_maj into chronoTable
    Conflicts:
    	htdocs/synopsisholiday/index.php

--- htdocs/synopsisapple/centre.inc.php
+++ htdocs/synopsisapple/centre.inc.php
@@ -1,5 +1,6 @@
 <?php
 
+global $tabCentre;
 $tabCentre = array("V" => array("04 75 81 81 54", "sav07@bimp.fr", "Saint Peray (valence)", 19), "VB" => array("04 75 81 81 54", "sav07@bimp.fr", "Valence Boutique", 19),
     "M" => array("04 50 22 15 55", "sav74@bimp.fr", "Meythet", 58), "AB" => array("04 50 22 15 55", "sav74@bimp.fr", "Annecy Boutique", 58), "C" => array("04 50 22 15 55", "sav74@bimp.fr", "Chambery", 58), "CB" => array("04 50 22 15 55", "sav74@bimp.fr", "Chambery boutique", 58),
     'L' => array("04 78 17 30 28", "sav69@bimp.fr", "Lyon", 21),
@@ -8,4 +9,3 @@ $tabCentre = array("V" => array("04 75 81 81 54", "sav07@bimp.fr", "Saint Peray 
     "B" => array("09 70 72 12 33", "sav250@bimp.fr", "Besançon", 83),
     "MO" => array("03 81 95 19 20", "sav252@bimp.fr", "Montbeliard", 84),
     "CO" => array("03 44 200 200", "sav60@bimp.fr", "Compiegne", 106));
-global $tabCentre;

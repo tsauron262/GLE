45b82728da17f2c951132b0adb97af31f6fb5f8b
tijean <tijean@tijean-ThinkPad-Edge-E130>
2015-08-04 19:26:12 +0200

    new gsx

--- htdocs/synopsisapple/GSX.class.php
+++ htdocs/synopsisapple/GSX.class.php
@@ -485,7 +485,7 @@ class GSX {
         $authentication_array = array(
             'AuthenticateRequest' => array(
                 'userId' => $this->gsxDetails['userId'],
-//                'password' => $this->gsxDetails['password'],
+                'password' => $this->gsxDetails['password'],
                 'serviceAccountNo' => $this->gsxDetails['serviceAccountNo'],
                 'languageCode' => $this->gsxDetails['languageCode'],
                 'userTimeZone' => $this->gsxDetails['userTimeZone']

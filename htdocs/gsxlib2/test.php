e81915190ecb15ccd09a302c3f498a7bb51794fe
tijean <tijean@tijean-ThinkPad-Edge-E130>
2015-08-14 15:43:48 +0200

    cc

--- htdocs/gsxlib2/test.php
+++ htdocs/gsxlib2/test.php
@@ -12,7 +12,7 @@ include 'gsxlib.php';
 
         $_ENV['GSX_CERT'] = '/etc/apache2/ssl/Applecare-APP157-0000897316.Test.apple.com.chain.pem';
         $_ENV['GSX_KEYPASS'] = 'freeparty';
-  $gsx = GsxLib::getInstance('0000897316', 'contact@drsi.fr');
+  $gsx = GsxLib::getInstance('0000897316', 'contact@drsi.fr', 'ut');
   $info = $gsx->warrantyStatus('C2QMDSAVFH00');
   echo $info->productDescription;
 

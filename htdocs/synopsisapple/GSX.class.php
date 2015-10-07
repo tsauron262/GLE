cdcfcf5c0a9baf5e9cdd19c77aad54758abaf03a
tijean <tijean@tijean-ThinkPad-Edge-E130>
2015-10-07 10:13:30 +0200

    bug gsx

--- htdocs/synopsisapple/GSX.class.php
+++ htdocs/synopsisapple/GSX.class.php
@@ -592,7 +592,7 @@ class GSX {
                 $requestName = $clientLookup . 'Request';
                 $requestData = $this->_requestBuilder($requestName, $wrapperName, $details);
                 $warrantyDetails = $this->request($requestData, $clientLookup);
-//                dol_syslog(print_r($requestData,3),3);
+                dol_syslog(print_r($requestData,3),3);
 //                print_r($requestData);
                 $errorMessage = $this->_obtainErrorMessage($warrantyDetails);
                 return $this->outputFormat($warrantyDetails[$clientLookup . 'Response'][$responseName], $errorMessage, $returnFormat);

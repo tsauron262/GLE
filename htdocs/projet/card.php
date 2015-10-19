b9546cefba1148c3c5ee7d41d65f58c08804a3a4
megajean <megajean@megajean-GA-990XA-UD3>
2015-10-19 16:45:57 +0200

    divers bug maj bimp

--- htdocs/projet/card.php
+++ htdocs/projet/card.php
@@ -17,6 +17,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+header("Location:".str_replace("/projet/", "/synopsisprojet/", $_SERVER['REQUEST_URI']));
+
 /**
  *	\file       htdocs/projet/card.php
  *	\ingroup    projet

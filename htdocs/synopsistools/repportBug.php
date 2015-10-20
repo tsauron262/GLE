378dd4960c4e550cfa541dcd7a38cea0f1262219
megajean <megajean@megajean-GA-990XA-UD3>
2015-10-20 14:39:51 +0200

    divers bug maj bimp

--- htdocs/synopsistools/repportBug.php
+++ htdocs/synopsistools/repportBug.php
@@ -22,66 +22,66 @@ if (isset($_POST['action']) && $_POST['action'] == "send") {
     bug($user, $_POST['text'], $_POST['oldUrl']);
 }
 if (isset($_GET['action']) && $_GET['action'] == "setResolu") {
-    
-    $requete = "SELECT * FROM ".MAIN_DB_PREFIX."synopsistools_bug where rowid = " . $_GET['id'];
+
+    $requete = "SELECT * FROM " . MAIN_DB_PREFIX . "synopsistools_bug where rowid = " . $_GET['id'];
     $sql = $db->query($requete);
     $obj = $db->fetch_object($sql);
-    
-    
-    $message = 'Bonjour votre bug signalé sur GLE est passé au statut résolu. '."\n\n"
-            .'Si le bug réapparaît ou est toujours présent, merci de le resignaler : '."\n\n"
-            .'Message : '.$obj->text;
+
+
+    $message = 'Bonjour votre bug signalé sur GLE est passé au statut résolu. ' . "\n\n"
+            . 'Si le bug réapparaît ou est toujours présent, merci de le resignaler : ' . "\n\n"
+            . 'Message : ' . $obj->text;
 
     $userT = new User($db);
     $userT->fetch($obj->fk_user);
     mailSyn($userT->email, "Bug Gle résolu", $message);
-    
-    
-    $requete = "UPDATE ".MAIN_DB_PREFIX."synopsistools_bug set resolu = 1 where rowid = " . $_GET['id'];
+
+
+    $requete = "UPDATE " . MAIN_DB_PREFIX . "synopsistools_bug set resolu = 1 where rowid = " . $_GET['id'];
     $db->query($requete);
 }
 if (isset($_GET['action']) && $_GET['action'] == "setAnnuler") {
-    
-    $requete = "SELECT * FROM ".MAIN_DB_PREFIX."synopsistools_bug where rowid = " . $_GET['id'];
+
+    $requete = "SELECT * FROM " . MAIN_DB_PREFIX . "synopsistools_bug where rowid = " . $_GET['id'];
     $sql = $db->query($requete);
     $obj = $db->fetch_object($sql);
-    
-    
-    $message = 'Bonjour votre bug signalé sur GLE est passé au statut Annulé. '."\n\n"
-            .'Message : '.$obj->text;
+
+
+    $message = 'Bonjour votre bug signalé sur GLE est passé au statut Annulé. ' . "\n\n"
+            . 'Message : ' . $obj->text;
 
     $userT = new User($db);
     $userT->fetch($obj->fk_user);
     mailSyn($userT->email, "Bug Gle Annulé", $message);
-    
-    
-    $requete = "UPDATE ".MAIN_DB_PREFIX."synopsistools_bug set resolu = 2 where rowid = " . $_GET['id'];
+
+
+    $requete = "UPDATE " . MAIN_DB_PREFIX . "synopsistools_bug set resolu = 2 where rowid = " . $_GET['id'];
     $db->query($requete);
 }
 
 
 if (isset($_GET['action']) && $_GET['action'] == "setInfo") {
-    
-    $requete = "SELECT * FROM ".MAIN_DB_PREFIX."synopsistools_bug where rowid = " . $_GET['id'];
+
+    $requete = "SELECT * FROM " . MAIN_DB_PREFIX . "synopsistools_bug where rowid = " . $_GET['id'];
     $sql = $db->query($requete);
     $obj = $db->fetch_object($sql);
-    
-    
-    $message = 'Bonjour je n\'ai pas pu traiter votre bug signalé sur GLE merci de le resignaler avec plus d\'informations ou de contacter votre assistance technique. '."\n\n"
-            .'Message : '.$obj->text;
+
+
+    $message = 'Bonjour je n\'ai pas pu traiter votre bug signalé sur GLE merci de le resignaler avec plus d\'informations ou de contacter votre assistance technique. ' . "\n\n"
+            . 'Message : ' . $obj->text;
 
     $userT = new User($db);
     $userT->fetch($obj->fk_user);
     mailSyn($userT->email, "Bug Gle Précisions", $message);
-    
-    
-    $requete = "UPDATE ".MAIN_DB_PREFIX."synopsistools_bug set resolu = 3 where rowid = " . $_GET['id'];
+
+
+    $requete = "UPDATE " . MAIN_DB_PREFIX . "synopsistools_bug set resolu = 3 where rowid = " . $_GET['id'];
     $db->query($requete);
 }
 
 function getBug($user) {
     global $db, $langs;
-    $requete = "SELECT * FROM ".MAIN_DB_PREFIX."synopsistools_bug";
+    $requete = "SELECT * FROM " . MAIN_DB_PREFIX . "synopsistools_bug";
     if (!$user->rights->SynopsisTools->Global->adminBug)
         $requete .= " WHERE fk_user = " . $user->id;
     if ($user->rights->SynopsisTools->Global->adminBug)
@@ -91,57 +91,57 @@ function getBug($user) {
     $sql = $db->query($requete);
     $html = '';
 
-    if($db->num_rows($sql) > 0){
-    echo "<br/><br/>";
-    load_fiche_titre($langs->trans("Vos ancien ticket de bug"));
-    echo "<br/>";
-    $html .= '<table class="noborder fullTable"><tr class="liste_titre">';
-    if ($user->rights->SynopsisTools->Global->adminBug)
-        $html .= "<th>Utilisateur</th>";
-    $html .= "<th>Résolu</th><th>Message</th></tr>";
-    $pair = false;
-    while ($data = $db->fetch_object($sql)) {
-        if ($pair)
-            $class = "pair";
-        else
-            $class = "impaire";
-        $pair = !$pair;
-        $html .= "<tr class='" . $class . "'>";
+    if ($db->num_rows($sql) > 0) {
+        echo "<br/><br/>";
+        load_fiche_titre($langs->trans("Vos ancien ticket de bug"));
+        echo "<br/>";
+        $html .= '<table class="noborder fullTable"><tr class="liste_titre">';
+        if ($user->rights->SynopsisTools->Global->adminBug)
+            $html .= "<th>Utilisateur</th>";
+        $html .= "<th>Résolu</th><th>Message</th></tr>";
+        $pair = false;
+        while ($data = $db->fetch_object($sql)) {
+            if ($pair)
+                $class = "pair";
+            else
+                $class = "impaire";
+            $pair = !$pair;
+            $html .= "<tr class='" . $class . "'>";
 
-        if ($user->rights->SynopsisTools->Global->adminBug) {
-            $userT = new User($db);
-            $userT->fetch($data->fk_user);
-            $html .= "<td>" . $userT->getNomUrl() . "</td>";
-        }
+            if ($user->rights->SynopsisTools->Global->adminBug) {
+                $userT = new User($db);
+                $userT->fetch($data->fk_user);
+                $html .= "<td>" . $userT->getNomUrl() . "</td>";
+            }
 
 
-        $html .= "<td>";
-        
+            $html .= "<td>";
+
             $pictoS = img_picto($langs->trans("Annulé"), 'delete');
             $pictoI = img_picto($langs->trans("Informations"), 'history');
-        if ($data->resolu == 1)
-            $picto = img_picto($langs->trans("Résolu"), 'on');
-        elseif ($data->resolu == 2)
-            $picto = $pictoS;
-        elseif ($data->resolu == 3)
-            $picto = $pictoI;
-        else
-            $picto = img_picto($langs->trans("Non traité"), 'off');
-        if ($user->rights->SynopsisTools->Global->adminBug && !$data->resolu)
-            $html .= '<a href="?action=setResolu&id=' . $data->rowid . '"> ' . $picto . ' </a>';
-        else
-            $html .= $picto;
-        if ($user->rights->SynopsisTools->Global->adminBug && !$data->resolu == 2)
-            $html .= ' <a href="?action=setAnnuler&id=' . $data->rowid . '"> ' . $pictoS . ' </a>';
-        if ($user->rights->SynopsisTools->Global->adminBug && $data->resolu < 1)
-            $html .= ' <a href="?action=setInfo&id=' . $data->rowid . '"> ' . $pictoI . ' </a>';
-        $html .= "</td>";
-        
-        $html .= "<td class='resize'>" . str_replace("\n", "<br/>", $data->text) . "</td>";
-        
-        $html .= "</tr>";
-    }
-    $html .= "</table>";
+            if ($data->resolu == 1)
+                $picto = img_picto($langs->trans("Résolu"), 'on');
+            elseif ($data->resolu == 2)
+                $picto = $pictoS;
+            elseif ($data->resolu == 3)
+                $picto = $pictoI;
+            else
+                $picto = img_picto($langs->trans("Non traité"), 'off');
+            if ($user->rights->SynopsisTools->Global->adminBug && !$data->resolu)
+                $html .= '<a href="?action=setResolu&id=' . $data->rowid . '"> ' . $picto . ' </a>';
+            else
+                $html .= $picto;
+            if ($user->rights->SynopsisTools->Global->adminBug && !$data->resolu == 2)
+                $html .= ' <a href="?action=setAnnuler&id=' . $data->rowid . '"> ' . $pictoS . ' </a>';
+            if ($user->rights->SynopsisTools->Global->adminBug && $data->resolu < 1)
+                $html .= ' <a href="?action=setInfo&id=' . $data->rowid . '"> ' . $pictoI . ' </a>';
+            $html .= "</td>";
+
+            $html .= "<td class='resize'>" . dol_trunc(str_replace("\n", "<br/>", strip_tags($data->text)),1000) . "</td>";
+
+            $html .= "</tr>";
+        }
+        $html .= "</table>";
     }
     return $html;
 }
@@ -152,7 +152,7 @@ function bug($user, $text, $adresse) {
         \n Utlisateur : " . $user->getNomUrl() . "
         \n Message : " . $text;
 
-    $requete = "INSERT into ".MAIN_DB_PREFIX."synopsistools_bug (fk_user, text) VALUES (" . $user->id . ", '" . addslashes($message) . "');";
+    $requete = "INSERT into " . MAIN_DB_PREFIX . "synopsistools_bug (fk_user, text) VALUES (" . $user->id . ", '" . addslashes($message) . "');";
     $db->query($requete);
 
     mailSyn("tommy@drsi.fr", "Bug Gle", $message);

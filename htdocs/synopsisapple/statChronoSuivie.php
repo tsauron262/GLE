857d7a44ece6216ad1a160960a0ca175c9ffe1be
megajean <megajean@megajean-GA-990XA-UD3>
2015-11-02 21:29:37 +0100

    date fin pour appel auto

--- htdocs/synopsisapple/statChronoSuivie.php
+++ htdocs/synopsisapple/statChronoSuivie.php
@@ -10,7 +10,7 @@ print load_fiche_titre("Suivie des interventions SAV");
 
 $sql = $db->query("SELECT c.id, c.ref FROM `" . MAIN_DB_PREFIX . "synopsischrono` c, " . MAIN_DB_PREFIX . "synopsischrono_chrono_105 c105, " . MAIN_DB_PREFIX . "element_element e1, " . MAIN_DB_PREFIX . "propal p, " . MAIN_DB_PREFIX . "facture f"
         . " WHERE c105.Suivie = '' AND c105.id = c.id AND fk_source = p.rowid AND sourcetype = 'propal' AND targettype = 'facture' AND fk_target = f.rowid AND c.propalid = p.rowid AND `facnumber` LIKE 'FA%' "
-        . "AND DATEDIFF(now(), f.`date_valid`) > 2 AND DATEDIFF(now(), f.`date_valid`) < 7");
+        . "AND DATEDIFF(now(), f.`date_valid`) > 2 AND DATEDIFF(now(), f.`date_valid`) < 7 ORDER BY DATEDIFF(now(), f.`date_valid`) ASC");
 
 require_once (DOL_DOCUMENT_ROOT . "/synopsischrono/class/chrono.class.php");
 $chr = new Chrono($db);

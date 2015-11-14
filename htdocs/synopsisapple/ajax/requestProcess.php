498a311c4594868610a4a46e5fe6944bb95127d9
megajean <megajean@megajean-GA-990XA-UD3>
2015-11-14 16:55:26 +0100

    list chrono datepucker search

--- htdocs/synopsisapple/ajax/requestProcess.php
+++ htdocs/synopsisapple/ajax/requestProcess.php
@@ -223,8 +223,8 @@ if (isset($_GET['action'])) {
             if (isset($_GET['serial'])) {
                 if (isset($_GET['request'])) {
                     if (isset($_GET['prodId'])) {
-                        echo "send requete";
                         $datas = new gsxDatas($_GET['serial'], $userId, $password, $serviceAccountNo);
+                        echo "send requete2";
                         $return = $datas->processRequestForm($_GET['prodId'], $_GET['request']);
 
 

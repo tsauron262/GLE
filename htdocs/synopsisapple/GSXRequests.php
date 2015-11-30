2af022be281260005e9c263942f1aef750cc21b6
megajean <megajean@megajean-GA-990XA-UD3>
2015-11-30 19:01:55 +0100

    ajout mod ups

--- htdocs/synopsisapple/GSXRequests.php
+++ htdocs/synopsisapple/GSXRequests.php
@@ -613,8 +613,17 @@ class GSX_Request {
                 $tab2[$champ[0]][] = $champ;
             }
 
-
-            $tab3 = $tab2['iPhone 5s'];
+            $tab3 = array();
+            $sql = $db->query("SELECT `description` as nom FROM `".MAIN_DB_PREFIX."synopsischrono` c, ".MAIN_DB_PREFIX."synopsischrono_chrono_101 cd WHERE c.id = cd.id AND cd.N__Serie = '".$serial."'");
+            if($db->num_rows($sql) > 0){
+                $result = $db->fetch_object($sql);
+                $tab3 = $tab2[$result->nom];
+            }
+            if (count($tab3 == 0)){
+                foreach ($tab2 as $tabT)
+                    foreach($tabT as $tabT2)
+                        $tab3[] =$tabT2;
+            }
             $i = 2;
 
             $html .= "<div class='partDatasBlock'>";
@@ -738,8 +747,8 @@ class GSX_Request {
                                         $default = $dataNode->getAttribute('default');
                                         if (isset($default) && ($default !== '')) {
                                             $valuesArray[] = $default;
-//                                        } else if ($required) {
-//                                            $this->addError('Information obligatoire non renseignée : "' . $defs['label'] . '"');
+                                        } else if ($required) {
+                                            $this->addError('Information obligatoire non renseignée : "' . $defs['label'] . '"');
                                         }
                                         else
                                             $valuesArray[] = "";

980e2a41e92cfebbda52547005fa0a06510e137d
megajean <megajean@megajean-GA-990XA-UD3>
2017-01-16 16:31:40 +0100

    chamaliere shipto

--- htdocs/synopsisapple/certifs.php
+++ htdocs/synopsisapple/certifs.php
@@ -7,8 +7,8 @@ $tabCert = array(
         0 => array('certUtSi.pem', 'freepartyUt'),
         1 => array('certProdFinal.pem', 'freepartyProd')),
     
-    128630 => array(
-        0 => array('clermont/certifTestProdClermont.pem', 'passPhraseTest'),
-        1 => array('clermont/certifFinalProdClermont.pem', 'passPhraseProd')
-    )
+//    128630 => array(
+//        0 => array('clermont/certifTestProdClermont.pem', 'passPhraseTest'),
+//        1 => array('clermont/certifFinalProdClermont.pem', 'passPhraseProd')
+//    )
 );

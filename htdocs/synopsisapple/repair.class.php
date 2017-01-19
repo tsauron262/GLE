dafa78df3033bbef16a416dca93843dc8d0347ab
megajean <megajean@megajean-GA-990XA-UD3>
2017-01-19 15:14:11 +0100

    chamaliere shipto

--- htdocs/synopsisapple/repair.class.php
+++ htdocs/synopsisapple/repair.class.php
@@ -480,9 +480,6 @@ class Repair
                     $requestName = 'UpdateCarryInRequest';
                 }
                 $data['statusCode'] = $status;
-                $data['notes'] = "";
-                $data['technicianId'] = "";
-                $data['orderLines'] = array();
                 break;
 
             case 'repair_or_replace':

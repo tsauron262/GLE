f4a9cddc0366ba909d8949963ae487ddccfd13d4
megajean <megajean@megajean-GA-990XA-UD3>
2017-01-23 17:46:01 +0100

    chamaliere shipto

--- htdocs/synopsisapple/GSXRequests.php
+++ htdocs/synopsisapple/GSXRequests.php
@@ -627,7 +627,7 @@ class GSX_Request {
             }
             $i = 2;
 
-            $html .= "<div class='partDatasBlockHide hidden'>";
+            $html .= "<div class='partDatasBlockHide'>";
             $html .= "<input type='hidden' value=''  name='comptiaGroup_".$i."'>";
             $html .= "<input type='hidden' value=''  name='comptiaCode_".$i."'>";
             $html .= "<input type='hidden' value=''  name='comptiaModifier_".$i."'>";

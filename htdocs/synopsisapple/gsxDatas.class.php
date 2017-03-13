4e30662abebe333f0ea6abfdad7d08e8db898a15
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-03-13 15:29:36 +0100

    log ldap pas de te.fk_soc pour les groupes

--- htdocs/synopsisapple/gsxDatas.class.php
+++ htdocs/synopsisapple/gsxDatas.class.php
@@ -681,7 +681,7 @@ class gsxDatas
                     $valDef['height'] = "4";
                     $valDef['estimatedTotalWeight'] = "4";
                     
-                    if(count($this->repairs) > 1)
+                    if(count($this->repairs) < 1)
                         $this->loadRepairs ($chronoId);
                     foreach($this->repairs as $repair){
                         $tabT = array();

9904b6262dc0b3c307c00a4ccc92b323bbeb1121
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-03-15 15:33:23 +0100

    acces caldav autre user si le drit

--- htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/AbstractBackend.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/AbstractBackend.php
@@ -113,6 +113,7 @@ abstract class AbstractBackend implements BackendInterface {
         $objects = $this->getCalendarObjects($calendarId);
 
         $validator = new \Sabre\CalDAV\CalendarQueryValidator();
+        if(function_exists("dol_syslog"))
         dol_syslog("Filtre envyée : ".print_r($filters,1),3);
         foreach($objects as $object) {
 
@@ -120,6 +121,7 @@ abstract class AbstractBackend implements BackendInterface {
                 $result[] = $object['uri'];
             }
             else{
+        if(function_exists("dol_syslog"))
         dol_syslog("rejeté par le Filtre : ".print_r($object,1),3);
             }
 

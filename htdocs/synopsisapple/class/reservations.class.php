7abd0cae5b08a8611d4e387af59ba5777c15c3e5
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-05-04 16:54:44 +0200

    gestion filtre caldav

--- htdocs/synopsisapple/class/reservations.class.php
+++ htdocs/synopsisapple/class/reservations.class.php
@@ -527,7 +527,6 @@ L’équipe BIMP";
 
     function fetchReservationSummary($soldTo, $shipTo, $currentReservations)
     {
-        return 0;
         global $tabCert, $dateBegin, $dateEnd, $productCodes;
 
         if ($this->display_debug) {

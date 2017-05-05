e00302def4a02c5eb11d77b7ba3d99f1d119dbf8
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-05-05 15:39:22 +0200

    gestion filtre caldav

--- htdocs/user/class/user.class.php
+++ htdocs/user/class/user.class.php
@@ -2216,7 +2216,7 @@ class User extends CommonObject
                     
                     if(isset($this->array_options['options_displayedinglobaladdressbook']) && $this->array_options['options_displayedinglobaladdressbook']){
                         $info['enabledservice'][] = "displayedInGlobalAddressBook";
-                        $info['pager'] = "mail=BIMP@bimp.fr";
+                        $info['pager'] = "BIMP@bimp.fr";
                     }
                 
                     if(isset($info['uid']) && isset($info["mail"])){

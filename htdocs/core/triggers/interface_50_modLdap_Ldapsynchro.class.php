9465c8e98e80ce3bc3060316396a7711eee6b894
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-06-28 18:04:22 +0200

    compte fermé dans ldap

--- htdocs/core/triggers/interface_50_modLdap_Ldapsynchro.class.php
+++ htdocs/core/triggers/interface_50_modLdap_Ldapsynchro.class.php
@@ -66,8 +66,8 @@ class InterfaceLdapsynchro extends DolibarrTriggers
         	{
         		$ldap=new Ldap();
         		$ldap->connect_bind();
-$creationUser = true;
 global $creationUser;
+$creationUser = true;
 				$info=$object->_load_ldap_info();
 				$dn=$object->_load_ldap_dn($info);
 

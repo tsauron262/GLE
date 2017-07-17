ec1b334ea8c98e89dc1175324271d8c4194ca395
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-07-17 11:51:07 +0200

    log admin

--- htdocs/synopsisapple/GSX.class.php
+++ htdocs/synopsisapple/GSX.class.php
@@ -899,6 +899,10 @@ $errorMessage = $this->_obtainErrorMessage ( $repairLookup );
         $SOAPRequest = array();
         try {
             $SOAPRequest = $this->soapClient->$clientLookup($requestData);
+            
+            global $user;
+            if($user->id == 1)
+                dol_syslog("load part".print_r($response,1),3,0,"_admin");
         } catch (SoapFault $f) {
             if (stripos($f->faultstring, "Veuillez saisir les informations relatives au(x) composant(s) ") !== false) {
                 $temp = str_replace(array("Veuillez saisir les informations relatives au(x) composant(s) ", "."), "", $f->faultstring);

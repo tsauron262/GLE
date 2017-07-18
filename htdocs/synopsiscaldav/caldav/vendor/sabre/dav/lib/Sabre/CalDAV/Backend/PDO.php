8427237fef9e01fceb555f86e0f61db46485b82d
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-07-18 13:38:08 +0200

    change adresse

--- htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
@@ -623,7 +623,7 @@ WHERE  `email` LIKE  '" . $mail . "'");
             $req = "UPDATE " . MAIN_DB_PREFIX . "synopsiscaldav_event SET organisateur = '" . $organisateur . "', participentExt = '" . implode(",", $tabMailInc) . "'  ".($sequence > 0 ?", sequence = '" . $sequence . "'" : "")." WHERE fk_object = '" . $action->id . "'";
             $sql = $db->query($req);
             
-            if(strtotime($calendarData2['LAST-MODIFIED']) < strtotime($calendarData2['DTSTAMP']))
+            if(!isset($calendarData2['LAST-MODIFIED']) || strtotime($calendarData2['LAST-MODIFIED']) < strtotime($calendarData2['DTSTAMP']))
                 $calendarData2['LAST-MODIFIED'] = $calendarData2['DTSTAMP'];
             
             $sql = "UPDATE `".MAIN_DB_PREFIX."actioncomm` SET `datec` = '".$db->idate(strtotime($calendarData2['CREATED']))."', `tms` = '".$db->idate(strtotime($calendarData2['LAST-MODIFIED']))."' WHERE `id` = ".$action->id.";";

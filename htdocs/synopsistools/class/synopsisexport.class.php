3370d84c0ff556484da050e44e9e94abf696ea3a
tijean <tijean@tijean-ThinkPad-Edge-E130>
2017-07-21 13:28:15 +0200

    user sans email

--- htdocs/synopsistools/class/synopsisexport.class.php
+++ htdocs/synopsistools/class/synopsisexport.class.php
@@ -411,7 +411,7 @@ WHERE  `list_refid` =11 AND ct.Centre = ls.valeur AND ct.id = chrono.id";
         else {
             $return .= $prefLigne . $separateur;
             foreach ($ligne as $nom => $valeur) {
-                $valeur = str_replace(array("\n", "\t"), array("",""), $valeur);
+                $valeur = preg_replace("/\r\n|\r|\n/",'', $valeur);
                 if($nom == "Centre")
                     continue;
                 if ($nom == 'product_type') {

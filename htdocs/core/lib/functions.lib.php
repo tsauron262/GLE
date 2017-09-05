57cd336d8a722f6c16a8a28a80ba87436448b857
tommy <tommy@192.168.1.104>
2017-09-05 14:37:56 +0200

    bug realpath quand deja real

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -521,7 +521,9 @@ function dol_include_once($relpath, $classname='')
 {
 	global $conf,$langs,$user,$mysoc;   // Do not remove this. They must be defined for files we include. Other globals var must be retreived with $GLOBALS['var']
 
-	$fullpath = dol_buildpath($relpath);
+	/* mod drsi $fullpath = dol_buildpath($relpath);*/
+	$fullpath = (stripos($relpath, DOL_DOCUMENT_ROOT) === false ? dol_buildpath($relpath) : $relpath);
+        /*fmoddrsi*/
 
 	if (!file_exists($fullpath)) {
 		dol_syslog('functions::dol_include_once Tried to load unexisting file: '.$relpath, LOG_ERR);

6d8c9beb9d23029404eeb0748e16bd6d88b76592
tommy <tommy@192.168.1.104>
2017-09-12 12:44:14 +0200

    certif itribut

--- htdocs/synopsisapple/certifs.php
+++ htdocs/synopsisapple/certifs.php
@@ -6,6 +6,9 @@ $tabCert = array(
     897316 => array(
         0 => array('test.pem', ''),
         1 => array('prod.pem', '')),
+    579256 => array(
+        0 => array('Applecare-APP157-0000579256.Prod.apple.com.chain.pem', ''),
+        1 => array('prod.pem', '')),
     
 //    128630 => array(
 //        0 => array('clermont/certifTestProdClermont.pem', 'passPhraseTest'),

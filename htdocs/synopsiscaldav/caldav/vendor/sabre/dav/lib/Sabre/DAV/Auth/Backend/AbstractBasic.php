14a3097ca904db3e5ee10a933b03a94165c75211
tommy <tommy@192.168.1.104>
2017-09-19 17:08:34 +0200

    erreur recurante caldav No basic authentication headers were found

--- htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/DAV/Auth/Backend/AbstractBasic.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/DAV/Auth/Backend/AbstractBasic.php
@@ -70,7 +70,7 @@ abstract class AbstractBasic implements BackendInterface {
         $userpass = $auth->getUserPass();
         if (!$userpass) {
             $auth->requireLogin();
-            throw new DAV\Exception\NotAuthenticated('No basic authentication headers were found');
+            //throw new DAV\Exception\NotAuthenticated('No basic authentication headers were found');
         }
 
         // Authenticates the user

858305bf215f25e684d049c74a20c63c430f195e
tommy <tommy@192.168.1.197>
2017-10-13 15:59:59 +0200

    modif caldav

--- htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
@@ -533,8 +533,8 @@ dol_syslog("Create : ".$calendarId."    |   ".$objectUri."   |".print_r($calenda
             if (isset($calendarData2) && isset($calendarData2['DTSTART']) && stripos($calendarData2['DTSTART'], "DATE:") !== false) {
                 //date_default_timezone_set("GMT");
                 $action->fulldayevent = true;
-                //$extraData['firstOccurence'] -= 3600;
-                //$extraData['lastOccurence'] -= 3660;
+                $extraData['firstOccurence'] -= 7200;
+                $extraData['lastOccurence'] -= 7260;
             } //else
                 //date_default_timezone_set("Europe/Paris");
             
@@ -745,8 +745,8 @@ dol_syslog("UPDATE OBJECT : ".$calendarId."    |   ".$objectUri."   |".print_r($
             if (isset($calendarData2) && isset($calendarData2['DTSTART']) && stripos($calendarData2['DTSTART'], "DATE:") !== false) {
                 //date_default_timezone_set("GMT");
                 $action->fulldayevent = true;
-                //$extraData['firstOccurence'] -= 3600;
-                //$extraData['lastOccurence'] -= 3660;
+                $extraData['firstOccurence'] -= 7200;
+                $extraData['lastOccurence'] -= 7260;
             } else {
                 //date_default_timezone_set("Europe/Paris");
                 $action->fulldayevent = false;

c3531030079c0869d4afd1e0d38c93d50f8db463
tommy <tommy@192.168.1.104>
2017-10-19 20:47:41 +0200

    gros gros bug caldav

--- htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
@@ -836,23 +836,7 @@ dol_syslog("UPDATE OBJECT ETAG : ".$extraData['etag'],3, 0);
             }
         }
 
-        $tabHead2 = array("BEGIN:VTIMEZONE",
-            "TZID:Europe/Paris",
-            "BEGIN:DAYLIGHT",
-            "TZOFFSETFROM:+0100",
-            "TZOFFSETTO:+0200",
-            "TZNAME:CEST",
-            "DTSTART:19700329T020000",
-            "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3",
-            "END:DAYLIGHT",
-            "BEGIN:STANDARD",
-            "TZOFFSETFROM:+0200",
-            "TZOFFSETTO:+0100",
-            "TZNAME:CET",
-            "DTSTART:19701025T030000",
-            "RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10",
-            "END:STANDARD",
-            "END:VTIMEZONE");
+        $tabHead2 = array();
 //        if (isset($tabCore['DTSTART'])) {
 //            $date = explode(":", $tabCore['DTSTART']);
 //            if (isset($date[1]))

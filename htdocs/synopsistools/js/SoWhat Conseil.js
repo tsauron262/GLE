d2f342ce9978f5b479d8dba5c99d335909a6b28b
tommy <tommy@192.168.1.104>
2017-10-25 12:00:33 +0200

    sowhat

--- htdocs/synopsistools/js/SoWhat Conseil.js
+++ htdocs/synopsistools/js/SoWhat Conseil.js
@@ -4,7 +4,7 @@ $(window).on("load", function () {
     selectTmp += "<option>HONORAIRES DE SUIVI</option>";
     selectTmp += "<option>HONORAIRES DE CREATION</option>";
     selectTmp += "<option>IMPRESSION</option>";
-    selectTmp += "<option>IMPRESSION ET LIVRAISO</option>";
+    selectTmp += "<option>IMPRESSION ET LIVRAISON</option>";
     selectTmp += "<option>PRISES DE VUES</option>";
     selectTmp += "<option>PRESTATION VIDEO</option>";
     $("#select_type").parent().append("<div><select id='titreLigne'>"+selectTmp+"</select></div>");

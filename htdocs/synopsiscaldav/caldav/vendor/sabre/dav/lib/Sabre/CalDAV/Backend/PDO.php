74e43ac7af9962b29c0687cb18026cd8f8663a50
tommy <tommy@192.168.1.104>
2017-11-03 14:08:28 +0100

    discount

--- htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
@@ -635,6 +635,8 @@ dol_syslog("Create : ".$calendarId."    |   ".$objectUri."   |".print_r($calenda
         if (is_object($user))
             $user = $user->id;
         $sequence = 0;
+        
+        $action->array_options['options_conf'] = false;
         foreach ($calendarData2 as $nom => $ligne) {
             
             $tab = array( CHR(13) => "", CHR(10) => "", " " => "" ); 
@@ -645,9 +647,6 @@ dol_syslog("Create : ".$calendarId."    |   ".$objectUri."   |".print_r($calenda
             }
             if (stripos($ligne, "CONFIDENTIAL") !== false) {
                 $action->array_options['options_conf'] = true;
-            }
-            else{
-                $action->array_options['options_conf'] = false;
             }
             if (stripos($nom, "DTSTAMP") !== false) {
                 $DTSTAMP = str_replace("DTSTAMP:","",$ligne);

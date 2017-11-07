0d374280010205801485e6d32e2105cf70c4dc85
RomainPe <pelegrinromain@gmail.com>
2017-11-07 11:44:42 +0100

    deletio of previous row, table llx_bimp_cat_cat filled manually, usage of row id forced while creating the llx_categorie set

--- htdocs/bimpproductbrowser/sql/data.sql
+++ htdocs/bimpproductbrowser/sql/data.sql
@@ -1,5 +1,15 @@
 # If you run that's sript manually, make sure you selected the good database befor sending queries
 
+SET FOREIGN_KEY_CHECKS = 0;
+
+# Suppress al rows
+DELETE FROM llx_product;
+DELETE FROM llx_product_price;
+DELETE FROM llx_product_extrafields;
+DELETE FROM llx_categorie;
+DELETE FROM llx_categorie_product;
+DELETE FROM llx_bimp_cat_cat;
+
 # Adding product row
 INSERT INTO llx_product (datec, entity, ref, ref_ext, price_min, price_min_ttc, label, fk_user_author, fk_product_type, price, price_ttc, price_base_type, tobuy, tosell, accountancy_code_buy, accountancy_code_sell, canvas, finished, tobatch, fk_unit) VALUES ('20171107092224', 1, '2', null, 0, 0, 'Iphone1 écran 7p basique', 1, 0, 0, 0, 'HT', 1, 1, '', '', '', null, 0, NULL);
 INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author, price, price_ttc, price_base_type,tosell, tva_tx, default_vat_code, recuperableonly, localtax1_tx, localtax2_tx, localtax1_type, localtax2_type, price_min,price_min_ttc,price_by_qty,entity,fk_price_expression)  VALUES(1, '20171107092224',2,1,0,0,'HT',1,20, null,0, 0, 0, '0', '0', 0,0,0,1,null);
@@ -21,16 +31,17 @@ INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author
 INSERT INTO llx_product_extrafields (fk_object,longdescript) VALUES (7,null);
 
 # Adding categorie row
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (0,'Iphone','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (15,'1','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (16,'7p','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (17,'basique','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (17,'gamer','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (17,'pro','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (16,'8p','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (15,'2','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (22,'7p','','','0',0,null,1);
-INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (22,'8p','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (111, 0, 'Root','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (1, 0, 'Iphone','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (2, 1, '1','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (3, 2,'7p','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (4, 3, 'basique','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (5, 3, 'gamer','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (6, 3, 'pro','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (7, 2, '8p','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (8, 1, '2','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (9, 8, '7p','','','0',0,null,1);
+INSERT INTO llx_categorie (rowid, fk_parent, label, description, color, visible, type, import_key, entity) VALUES (10, 8, '8p','','','0',0,null,1);
 
 # Editting lnik between categorie and product
 INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (18, 2);
@@ -39,3 +50,17 @@ INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (20, 3);
 INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (21, 4);
 INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (23, 5);
 INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (24, 6);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+# Adding link between categories and caregories
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (1, 'Type', 111, 1);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (2, 'Version', 1, 2);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (3, 'Écran', 2, 3);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (4, 'Utilisation', 3, 4);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (5, 'Utilisation', 3, 5);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (6, 'Utilisation', 3, 6);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (7, 'Écran', 2, 7);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (8, 'Version', 1, 8);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (9, 'Écran', 8, 9);
+INSERT INTO llx_bimp_cat_cat (rowid, label, fk_parent_cat, fk_child_cat) VALUES (10, 'Écran', 8, 10);

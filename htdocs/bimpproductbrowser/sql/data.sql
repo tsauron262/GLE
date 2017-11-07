4aed2a88e9c2011e1c9b2e6fd4e838d4261eb966
RomainPe <pelegrinromain@gmail.com>
2017-11-07 10:12:43 +0100

    insertion of a data set

--- htdocs/bimpproductbrowser/sql/data.sql
+++ htdocs/bimpproductbrowser/sql/data.sql
@@ -1,1 +1,41 @@
-d
+# If you run that's sript manually, make sure you selected the good database befor sending queries
+
+# Adding product row
+INSERT INTO llx_product (datec, entity, ref, ref_ext, price_min, price_min_ttc, label, fk_user_author, fk_product_type, price, price_ttc, price_base_type, tobuy, tosell, accountancy_code_buy, accountancy_code_sell, canvas, finished, tobatch, fk_unit) VALUES ('20171107092224', 1, '2', null, 0, 0, 'Iphone1 écran 7p basique', 1, 0, 0, 0, 'HT', 1, 1, '', '', '', null, 0, NULL);
+INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author, price, price_ttc, price_base_type,tosell, tva_tx, default_vat_code, recuperableonly, localtax1_tx, localtax2_tx, localtax1_type, localtax2_type, price_min,price_min_ttc,price_by_qty,entity,fk_price_expression)  VALUES(1, '20171107092224',2,1,0,0,'HT',1,20, null,0, 0, 0, '0', '0', 0,0,0,1,null);
+INSERT INTO llx_product_extrafields (fk_object,longdescript) VALUES (2,null);
+INSERT INTO llx_product (datec, entity, ref, ref_ext, price_min, price_min_ttc, label, fk_user_author, fk_product_type, price, price_ttc, price_base_type, tobuy, tosell, accountancy_code_buy, accountancy_code_sell, canvas, finished, tobatch, fk_unit) VALUES ('20171107092301', 1, '3', null, 0, 0, 'Iphone1 écran 7p pro', 1, 0, 0, 0, 'HT', 1, 1, '', '', '', null, 0, NULL);
+INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author, price, price_ttc, price_base_type,tosell, tva_tx, default_vat_code, recuperableonly, localtax1_tx, localtax2_tx, localtax1_type, localtax2_type, price_min,price_min_ttc,price_by_qty,entity,fk_price_expression)  VALUES(1, '20171107092301',3,1,0,0,'HT',1,20, null,0, 0, 0, '0', '0', 0,0,0,1,null);
+INSERT INTO llx_product_extrafields (fk_object,longdescript) VALUES (3,null);
+INSERT INTO llx_product (datec, entity, ref, ref_ext, price_min, price_min_ttc, label, fk_user_author, fk_product_type, price, price_ttc, price_base_type, tobuy, tosell, accountancy_code_buy, accountancy_code_sell, canvas, finished, tobatch, fk_unit) VALUES ('20171107092326', 1, '4', null, 0, 0, 'Iphone1 écran 8p', 1, 0, 0, 0, 'HT', 1, 1, '', '', '', null, 0, NULL);
+INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author, price, price_ttc, price_base_type,tosell, tva_tx, default_vat_code, recuperableonly, localtax1_tx, localtax2_tx, localtax1_type, localtax2_type, price_min,price_min_ttc,price_by_qty,entity,fk_price_expression)  VALUES(1, '20171107092326',4,1,0,0,'HT',1,20, null,0, 0, 0, '0', '0', 0,0,0,1,null);
+INSERT INTO llx_product_extrafields (fk_object,longdescript) VALUES (4,null);
+INSERT INTO llx_product (datec, entity, ref, ref_ext, price_min, price_min_ttc, label, fk_user_author, fk_product_type, price, price_ttc, price_base_type, tobuy, tosell, accountancy_code_buy, accountancy_code_sell, canvas, finished, tobatch, fk_unit) VALUES ('20171107092358', 1, '5', null, 0, 0, 'Iphone2 écran 7p', 1, 0, 0, 0, 'HT', 1, 1, '', '', '', null, 0, NULL);
+INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author, price, price_ttc, price_base_type,tosell, tva_tx, default_vat_code, recuperableonly, localtax1_tx, localtax2_tx, localtax1_type, localtax2_type, price_min,price_min_ttc,price_by_qty,entity,fk_price_expression)  VALUES(1, '20171107092358',5,1,0,0,'HT',1,20, null,0, 0, 0, '0', '0', 0,0,0,1,null);
+INSERT INTO llx_product_extrafields (fk_object,longdescript) VALUES (5,null);
+INSERT INTO llx_product (datec, entity, ref, ref_ext, price_min, price_min_ttc, label, fk_user_author, fk_product_type, price, price_ttc, price_base_type, tobuy, tosell, accountancy_code_buy, accountancy_code_sell, canvas, finished, tobatch, fk_unit) VALUES ('20171107092421', 1, '6', null, 0, 0, 'Iphone2 écran 8p', 1, 0, 0, 0, 'HT', 1, 1, '', '', '', null, 0, NULL);
+INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author, price, price_ttc, price_base_type,tosell, tva_tx, default_vat_code, recuperableonly, localtax1_tx, localtax2_tx, localtax1_type, localtax2_type, price_min,price_min_ttc,price_by_qty,entity,fk_price_expression)  VALUES(1, '20171107092421',6,1,0,0,'HT',1,20, null,0, 0, 0, '0', '0', 0,0,0,1,null);
+INSERT INTO llx_product_extrafields (fk_object,longdescript) VALUES (6,null);
+INSERT INTO llx_product (datec, entity, ref, ref_ext, price_min, price_min_ttc, label, fk_user_author, fk_product_type, price, price_ttc, price_base_type, tobuy, tosell, accountancy_code_buy, accountancy_code_sell, canvas, finished, tobatch, fk_unit) VALUES ('20171107092544', 1, '1', null, 0, 0, 'Iphone1 écran 7p gamer', 1, 0, 0, 0, 'HT', 1, 1, '', '', '', null, 0, NULL);
+INSERT INTO llx_product_price(price_level,date_price, fk_product, fk_user_author, price, price_ttc, price_base_type,tosell, tva_tx, default_vat_code, recuperableonly, localtax1_tx, localtax2_tx, localtax1_type, localtax2_type, price_min,price_min_ttc,price_by_qty,entity,fk_price_expression)  VALUES(1, '20171107092544',7,1,0,0,'HT',1,20, null,0, 0, 0, '0', '0', 0,0,0,1,null);
+INSERT INTO llx_product_extrafields (fk_object,longdescript) VALUES (7,null);
+
+# Adding categorie row
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (0,'Iphone','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (15,'1','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (16,'7p','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (17,'basique','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (17,'gamer','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (17,'pro','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (16,'8p','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (15,'2','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (22,'7p','','','0',0,null,1);
+INSERT INTO llx_categorie (fk_parent, label, description, color, visible, type, import_key, entity) VALUES (22,'8p','','','0',0,null,1);
+
+# Editting lnik between categorie and product
+INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (18, 2);
+INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (19, 7);
+INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (20, 3);
+INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (21, 4);
+INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (23, 5);
+INSERT INTO llx_categorie_product (fk_categorie, fk_product) VALUES (24, 6);

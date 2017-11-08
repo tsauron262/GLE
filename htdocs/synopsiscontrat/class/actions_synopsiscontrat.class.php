3e59ced134e01399aaed4484a682477efb78fdce
tommy <tommy@192.168.1.104>
2017-11-08 15:18:39 +0100

    aj activé all ligne

--- htdocs/synopsiscontrat/class/actions_synopsiscontrat.class.php
+++ htdocs/synopsiscontrat/class/actions_synopsiscontrat.class.php
@@ -16,7 +16,7 @@ class Actionssynopsiscontrat {
         if(get_class($object) == "Contrat" && $object->statut == 1){
             $afficher = false;
             foreach($object->lines as $ligne)
-                if($ligne->statut != 1)
+                if($ligne->statut != 4)
                     $afficher = true;
             if($afficher)    
             echo '<div class="inline-block divButAction"><a class="butAction" href="'.DOL_URL_ROOT.'/synopsiscontrat/request.php?id='.$object->id.'&action=activerAll">Activer tous les services</a></div>';  

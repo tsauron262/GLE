ccb0dbc7687adb84fe603a3fba11b34c2f01f175
RomainPe <pelegrinromain@gmail.com>
2017-11-10 16:53:56 +0100

    creation js file, added by the browser

---
+++ htdocs/bimpproductbrowser/js/checkboxManager.js
@@ -0,0 +1,17 @@
+/*
+
+jsquery déja dans les header de browse.php
+var scriptCM = document.createElement('scriptCM');
+scriptCM.src = '/includes/jquery/js/jquery.min.js';
+scriptCM.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(scriptCM);
+
+*/
+
+$('#1').trigger('click');
+
+
+
+//    print '<form id="form1" action="/url_arbre" method="post">';
+
+//    print '<script type="text/javascript" language="javascript">$('#1').trigger('click');</script>';

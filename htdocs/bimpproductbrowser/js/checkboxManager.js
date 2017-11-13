c14d73f5ca59d5fe7422491083d7ae1ae9180f5c
RomainPe <pelegrinromain@gmail.com>
2017-11-13 14:45:55 +0100

    manage ajax and CSS

--- htdocs/bimpproductbrowser/js/checkboxManager.js
+++ htdocs/bimpproductbrowser/js/checkboxManager.js
@@ -1,17 +1,36 @@
-/*
+$(document).ready(function()
+	{
+	$("#submitTree").click(function()
+		{
+		var checkedValues = $('input:checkbox:checked').map(function()
+			{
+				return this.id;
+			}).get();
+		$.ajax(
+		{
+			type: "POST",
+			url: "linkCreated.php",
+			data: checkedValues,
+			cache: false,
+			success: function(result)
+			{
+				showalert("<strong>Succès !</strong> Liens créé", 'alert-success');		
+			},
+			error: function() {
+				showalert("<strong>Erreur !</strong> Une erreur inconnu est survenue, merci de le signaler à l\'adresse suivante : r.PELEGRIN@bimp.fr", 'alert-error');
+			}   
+		})
+	return false;
+	});
+});
 
-jsquery déja dans les header de browse.php
-var scriptCM = document.createElement('scriptCM');
-scriptCM.src = '/includes/jquery/js/jquery.min.js';
-scriptCM.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(scriptCM);
-
-*/
-
-$('#1').trigger('click');
-
-
-
-//    print '<form id="form1" action="/url_arbre" method="post">';
-
-//    print '<script type="text/javascript" language="javascript">$('#1').trigger('click');</script>';
+function showalert(message, alerttype)
+{
+	var time;
+	((alerttype == 'alert-success' )? time = 3000 : time = 100000);
+	$('#placeforalert').hide().fadeIn(500).append('<div id="alertdiv" class="alert ' +  alerttype + '"><a class="close" data-dismiss="alert">×</a><span>' + message + '</span></div>')
+	setTimeout(function()
+	{
+		$("#alertdiv").remove();
+	}, time);
+}

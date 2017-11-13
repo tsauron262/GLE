e8af33e9bc9e98965473849381b6408d430b2b78
RomainPe <pelegrinromain@gmail.com>
2017-11-13 14:45:31 +0100

    file creation

---
+++ htdocs/bimpproductbrowser/linkCreated.php
@@ -0,0 +1,41 @@
+<?php
+/* Copyright (C) 2005       Matthieu Valleton   <mv@seeschloss.org>
+ * Copyright (C) 2005       Eric Seigne         <eric.seigne@ryxeo.com>
+ * Copyright (C) 2006-2016  Laurent Destailleur <eldy@users.sourceforge.net>
+ * Copyright (C) 2007       Patrick Raguin      <patrick.raguin@gmail.com>
+ * Copyright (C) 2005-2012  Regis Houssin       <regis.houssin@capnetworks.com>
+ * Copyright (C) 2015       Raphaël Doursenaud  <rdoursenaud@gpcsolutions.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *      \file       htdocs/categories/index.php
+ *      \ingroup    category
+ *      \brief      Home page of category area
+ */
+
+require '../main.inc.php';
+/*
+$db->begin();
+$sql=''
+try
+{
+	$db->query($sql);
+	$db->commit();
+} catch
+$db->rollback();
+*/
+
+$db->close();

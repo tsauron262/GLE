3596f1c7049c47f3ffdeefd3d1d34a16d8e880f5
RomainPe <pelegrinromain@gmail.com>
2017-11-21 11:27:57 +0100

    change settings of the view

--- htdocs/bimpproductbrowser/categoriser.php
+++ htdocs/bimpproductbrowser/categoriser.php
@@ -1,4 +1,5 @@
 <?php
+
 /* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
  * Copyright (C) 2004-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
  * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@capnetworks.com>
@@ -25,87 +26,92 @@
  *   \brief      Tab for notes on products
  *   \ingroup    societe
  */
-
 require '../main.inc.php';
-require_once DOL_DOCUMENT_ROOT.'/core/lib/product.lib.php';
-require_once DOL_DOCUMENT_ROOT.'/product/class/product.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/lib/product.lib.php';
+require_once DOL_DOCUMENT_ROOT . '/product/class/product.class.php';
 
-$arrayofjs=array('/bimpproductbrowser/js/ajax.js');
+$arrayofjs = array('/bimpproductbrowser/js/ajax.js');
 
 $langs->load("companies");
 
 $id = GETPOST('id', 'int');
 $ref = GETPOST('ref', 'alpha');
-$action = GETPOST('action','aZ09');
+$action = GETPOST('action', 'aZ09');
 
 // Security check
-$fieldvalue = (! empty($id) ? $id : (! empty($ref) ? $ref : ''));
-$fieldtype = (! empty($ref) ? 'ref' : 'rowid');
-if ($user->societe_id) $socid=$user->societe_id;
-$result=restrictedArea($user,'produit|service',$fieldvalue,'product&product','','',$fieldtype);
+$fieldvalue = (!empty($id) ? $id : (!empty($ref) ? $ref : ''));
+$fieldtype = (!empty($ref) ? 'ref' : 'rowid');
+if ($user->societe_id) {
+    $socid = $user->societe_id;
+}
+$result = restrictedArea($user, 'produit|service', $fieldvalue, 'product&product', '', '', $fieldtype);
 
 $object = new Product($db);
-if ($id > 0 || ! empty($ref)) $object->fetch($id, $ref);
+if ($id > 0 || !empty($ref)) {
+    $fetch = $object->fetch($id, $ref);
+}
 
-$permissionnote=$user->rights->produit->creer;	// Used by the include of actions_setnotes.inc.php
+$permissionnote = $user->rights->produit->creer; // Used by the include of actions_setnotes.inc.php
 
 
 /*
  * Actions
  */
 
-
 /*
- *	View
+ * 	View
  */
 
 $form = new Form($db);
 
-$helpurl='';
-if (GETPOST("type") == '0' || ($object->type == Product::TYPE_PRODUCT)) $helpurl='EN:Module_Products|FR:Module_Produits|ES:M&oacute;dulo_Productos';
-if (GETPOST("type") == '1' || ($object->type == Product::TYPE_SERVICE)) $helpurl='EN:Module_Services_En|FR:Module_Services|ES:M&oacute;dulo_Servicios';
-
-$title = $langs->trans('ProductServiceCard');
-$shortlabel = dol_trunc($object->label,16);
-if (GETPOST("type") == '0' || ($object->type == Product::TYPE_PRODUCT))
-{
-	$title = $langs->trans('Product')." ". $shortlabel ." - ".$langs->trans('Notes');
-	$helpurl='EN:Module_Products|FR:Module_Produits|ES:M&oacute;dulo_Productos';
+$helpurl = '';
+if (GETPOST("type") == '0' || ($object->type == Product::TYPE_PRODUCT)) {
+    $helpurl = 'EN:Module_Products|FR:Module_Produits|ES:M&oacute;dulo_Productos';
 }
 if (GETPOST("type") == '1' || ($object->type == Product::TYPE_SERVICE))
-{
-	$title = $langs->trans('Service')." ". $shortlabel ." - ".$langs->trans('Notes');
-	$helpurl='EN:Module_Services_En|FR:Module_Services|ES:M&oacute;dulo_Servicios';
+    $helpurl = 'EN:Module_Services_En|FR:Module_Services|ES:M&oacute;dulo_Servicios';
+
+$title = $langs->trans('ProductServiceCard');
+$shortlabel = dol_trunc($object->label, 16);
+if (GETPOST("type") == '0' || ($object->type == Product::TYPE_PRODUCT)) {
+    $title = $langs->trans('Product') . " " . $shortlabel . " - " . $langs->trans('Notes');
+    $helpurl = 'EN:Module_Products|FR:Module_Produits|ES:M&oacute;dulo_Productos';
+}
+if (GETPOST("type") == '1' || ($object->type == Product::TYPE_SERVICE)) {
+    $title = $langs->trans('Service') . " " . $shortlabel . " - " . $langs->trans('Notes');
+    $helpurl = 'EN:Module_Services_En|FR:Module_Services|ES:M&oacute;dulo_Servicios';
 }
 
-llxHeader('','Catégoriser',$help_url,'',0,0,$arrayofjs, '');
+llxHeader('', 'Catégoriser', $help_url, '', 0, 0, $arrayofjs, '');
 // llxHeader('','Recherche Filtrée','','',0,0,$arrayofjs,$arrayofcss);
 
-if ($id > 0 || ! empty($ref))
-{
+if ($id > 0 || !empty($ref)) {
     /*
      * Affichage onglets
      */
-    if (! empty($conf->notification->enabled)) $langs->load("mails");
+    if (!empty($conf->notification->enabled)) {
+        $langs->load("mails");
+    }
 
     $head = product_prepare_head($object);
-    $titre=$langs->trans("CardProduct".$object->type);
-    $picto=($object->type==Product::TYPE_SERVICE?'service':'product');
+    $titre = $langs->trans("CardProduct" . $object->type);
+    $picto = ($object->type == Product::TYPE_SERVICE ? 'service' : 'product');
 
     dol_fiche_head($head, 'categoriser', $titre, -1, $picto);
 
-	$linkback = '<a href="'.DOL_URL_ROOT.'/product/list.php">'.$langs->trans("BackToList").'</a>';
-    $object->next_prev_filter=" fk_product_type = ".$object->type;
+    $linkback = '<a href="' . DOL_URL_ROOT . '/product/list.php">' . $langs->trans("BackToList") . '</a>';
+    $object->next_prev_filter = " fk_product_type = " . $object->type;
 
     $shownav = 1;
-    if ($user->societe_id && ! in_array('product', explode(',',$conf->global->MAIN_MODULES_FOR_EXTERNAL))) $shownav=0;
+    if ($user->societe_id && !in_array('product', explode(',', $conf->global->MAIN_MODULES_FOR_EXTERNAL))) {
+        $shownav = 0;
+    }
 
-	dol_banner_tab($object, 'ref', $linkback, $shownav, 'ref');
+    dol_banner_tab($object, 'ref', $linkback, $shownav, 'ref');
 
-    $cssclass='titlefield';
+    $cssclass = 'titlefield';
     //if ($action == 'editnote_public') $cssclass='titlefieldcreate';
     //if ($action == 'editnote_private') $cssclass='titlefieldcreate';
-
     //print '<div class="fichecenter">';
 
     print '<div class="underbanner clearboth"></div>';

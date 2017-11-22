7fc8d4e5218fbd852d054df305a891cf97e2516d
JeanBonnard <j.bonnard42@gmail.com>
2017-11-22 14:12:53 +0100

    ajout formulaire, date du jour, CDN

--- htdocs/bimpcesu/tabs/attestation.php
+++ htdocs/bimpcesu/tabs/attestation.php
@@ -262,7 +262,43 @@ print "</td></tr></table>";
 //<--- -------------- TABLEAU END --------------- --->
 
 
-   
+
+
+
+
+
+
+//<-- ------------------- FORMULAIRE PERIOD START------------ -->
+
+
+    $dateDuJour = date("Y-m-d"); // recuperation date du jours
+    
+    $annee = date("Y"); //recuperation de l'annee en cours
+    $anneeInt = intval($annee); //conversion chaine en entier
+    $anneeAnterieur = $annee -1; //soustraction d'un an de l'annee en cours
+    $anneeStr = strval($anneeAnterieur); // conversion entier en chaine
+    
+    $jourMois = date("m-d"); //recuperation jour et mois en cours
+    
+    $dateAnterieur = $anneeStr."-".$jourMois; //concaténation annee anterieur et jour/mois
+     
+    // chargement jquery et bootstrap (CDN)
+    echo '<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>';
+    echo '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">';
+    echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>';
+    
+    // View Formulaire
+    echo '<h3>Sélectionnez une periode de facturation</h3>';
+    
+    echo '<form action="/gle/bimpcesu/core/modules/societe/doc/pdf_bimpcesu.modules.php" method="post"> <b>Du</b> <input type="date" name="dateDebut" value="'.$dateAnterieur.'"/>'; //valeur par defaut = date du jours il y a un an 
+    echo '<b>Au</b> <input type="date" name="dateFin" value="'.$dateDuJour.'"/>'; // valeur par defaut = date du jours
+    
+    echo '<button type="button" class="btn btn-primary">ok</button>';
+    
+    echo '<input type="submit" style="width:5em; height:3em; font-size:1em" value="ok"/></form>';
+    
+    
+//<-- ------------------- FORMULAIRE PERIOD END------------ -->
 
 
 

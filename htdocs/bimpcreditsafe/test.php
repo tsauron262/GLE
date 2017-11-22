56c1687a33307bd5aa6b0b18498a73a94861360b
tommy <tommy@192.168.1.104>
2017-11-22 14:42:34 +0100

    credit safe

--- htdocs/bimpcreditsafe/test.php
+++ htdocs/bimpcreditsafe/test.php
@@ -7,6 +7,7 @@ require_once DOL_DOCUMENT_ROOT . '/includes/nusoap/lib/nusoap.php';
 
 
 $siret = GETPOST("siren");// "403554181";//"320387483";   
+$siren = substr($siret, 0,9);
 
 
 
@@ -16,7 +17,7 @@ $xml_data = file_get_contents('request.xml');
 $link = 'https://www.creditsafe.fr/getdata/service/CSFRServices.asmx/GetData';
 
 
-$xml_data = "requestXmlStr=".str_replace("SIREN", str_replace(" ", "", $siret), $xml_data);
+$xml_data = "requestXmlStr=".str_replace("SIREN", str_replace(" ", "", $siren), $xml_data);
 
 
 $context = stream_context_create(array('http'=>array(
@@ -38,7 +39,15 @@ if(stripos($result->xmlresponse->header->reportinformation->reporttype, "Error")
 else{
 
     $summary = $result->xmlresponse->body->company->summary;
-
+    $base = $result->xmlresponse->body->company->baseinformation;
+    $branches = $base->branches->branch;
+    
+    
+    
+    
+    
+    $adress = "".$summary->postaladdress->address." ".$summary->postaladresse->additiontoaddress;
+    
     foreach(array("", "2013") as $annee){
         if($note != "")
             $note .= "\n";
@@ -55,18 +64,40 @@ else{
     }
     
     $tabCodeP = explode(" ", $summary->postaladdress->distributionline);
-
+    $codeP = $tabCodeP[0];
+    
+    $ville = str_replace($tabCodeP[0]." ","",$summary->postaladdress->distributionline);
+    
+    $tel = $summary->telephone;
+    $nom = $summary->companyname;
+    
+    
+    
+    
+    
+    foreach($branches as $branche){
+        if($branche->companynumber == $siret || ($siret == $siren && stripos($branche->type, "Siège") !== false)){
+            $adress = $branche->full_address->address;
+            $nom = $branche->full_address->name;
+            $codeP = $branche->postcode;
+            $ville = $branche->municipality;
+            $siret = $branche->companynumber;
+        }
+           
+    }
      // echo json_encode($summary);die;
 
 
-    $return = array("Nom" => "".$summary->companyname,
-        "Tva" => "".$summary->safenumber,
-        "Tel" => "".$summary->telephone,
+    $return = array("Nom" => "".$nom,
+        "Tva" => "".$base->vatnumber,
+        "Tel" => "".$tel,
         "Naf" => "".$summary->activitycode,
-        "Note" => $note,
-        "Adresse" => "".$summary->postaladdress->address." ".$summary->postaladresse->additiontoaddress,
-        "CodeP" => $tabCodeP[0],
-        "Ville" => str_replace($tabCodeP[0]." ","",$summary->postaladdress->distributionline),
+        "Note" => "".$note,
+        "Adresse" => "".$adress,
+        "CodeP" => "".$codeP,
+        "Ville" => "".$ville,
+        "Siret" => "".$siret,
         "Capital" => "".str_replace(" Euros", "", $summary->sharecapital));
+    //$return = $result;
     echo json_encode($return);
 }

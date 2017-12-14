a1de070f1189a9a018a8706eacf1271c73762d5f
tommy <tommy@192.168.1.104>
2017-12-14 15:22:53 +0100

    files bimpcore

---
+++ htdocs/bimpcore/test.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once("../main.inc.php");
+
+
+
+
+
+$modulepart = 'bimpcore';
+$modulepart2 = 'financement';
+
+$object = new stdClass();
+$object->ref = "ref156888";
+
+$permission = $user->rights->societe->creer;
+$permtoedit = $user->rights->societe->creer;
+
+$object->id = 156888;
+
+$action = GETPOST("action");
+$confirm = GETPOST("confirm");
+$upload_dir = DOL_DATA_ROOT."/".$modulepart."/".$modulepart2."/".$object->ref;
+require_once DOL_DOCUMENT_ROOT.'/core/lib/files.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/html.formfile.class.php';
+include_once DOL_DOCUMENT_ROOT . '/core/actions_linkedfiles.inc.php';
+
+//ATTENTION pas de données envoyé avant cela
+llxHeader();
+
+$relativepathwithnofile = $modulepart2."/".$object->ref."/";
+$filearray=dol_dir_list($upload_dir,"files",0,'','(\.meta|_preview.*\.png)$',$sortfield,(strtolower($sortorder)=='desc'?SORT_DESC:SORT_ASC),1);
+include_once DOL_DOCUMENT_ROOT . '/core/tpl/document_actions_post_headers.tpl.php';
+
+echo "nnnn";
+
+
+
+
+
+
+llxFooter();

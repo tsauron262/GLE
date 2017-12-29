2b1f3044e92d01530e509ef23d14ce376d9ce143
tommy <tommy@192.168.1.104>
2017-12-29 13:28:52 +0100

    Module BIMP PDF

--- htdocs/bimpcore/pdf/classes/BimpModelPDF.php
+++ htdocs/bimpcore/pdf/classes/BimpModelPDF.php
@@ -1,9 +1,8 @@
 <?php
 
-require_once __DIR__.'/BimpPDF.php';
+require_once __DIR__ . '/BimpPDF.php';
 
-Abstract class BimpModelPDF
-{
+Abstract class BimpModelPDF {
 
     protected $pdf = null;
     public $tpl_dir = DOL_DOCUMENT_ROOT . '/bimpcore/pdf/templates/invoice/';
@@ -13,91 +12,89 @@ Abstract class BimpModelPDF
         'footer_line_2' => 'NAF: 4741Z - RCS: Lyon B 320 387 483 - TVA intracommunautaire: FR 34 320387483'
     );
     public $prefName = "";
-    
     public $text = "";
     public $object;
     private $isInit = false;
 
-    public function __construct()
-    {
+    public function __construct() {
         global $conf, $mysoc;
         $this->header_vars = array(
-            'logo_img' => $conf->mycompany->dir_output.'/logos/'.$mysoc->logo,
+            'logo_img' => $conf->mycompany->dir_output . '/logos/' . $mysoc->logo,
             'logo_width' => '120'
         );
-        
-        
-        
+
+
+
         $this->pdf = new BimpPDF();
     }
-    
+
     abstract function initData();
-    
-    function init($object){
+
+    function init($object) {
         $this->object = $object;
-        
+
+        if (is_file($this->tpl_dir . '/style.css')) {
+            $css = $this->pdf->renderTemplate($this->tpl_dir . '/style.css', array());
+            $this->text .= "<style>" . $css . "</style>";
+        }
+
         $this->initData();
         $this->isInit = true;
     }
-    
-    
 
-    public function render($file_name, $display)
-    {
-        if(!$this->isInit)
+    public function render($file_name, $display) {
+        if (!$this->isInit)
             $this->init(null);
-        
+
         $header = $this->pdf->renderTemplate($this->tpl_dir . '/header.html', $this->header_vars);
         $this->pdf->createHeader($header);
-        
+
         $footer = $this->pdf->renderTemplate($this->tpl_dir . '/footer.html', $this->footer_vars);
         $this->pdf->createFooter($footer);
-        
-        
 
-        $this->pdf->writePage($this->text);
+
+
+        $this->printContent();
 
         return $this->pdf->render($file_name, $display);
     }
-    
-    
-    
-        
-    function getPath(){
+
+    public function printContent() {
+        $this->pdf->writePage($this->text);
+    }
+
+    function getPath() {
         global $conf;
-        
-        $path = DOL_DATA_ROOT. "/divers/";
+
+        $path = DOL_DATA_ROOT . "/divers/";
 
         $nObj = $this->typeObject;
-        if($nObj != ""){
-            if(isset($conf->$nObj) && isset($conf->$nObj->dir_output))
-                $path = $conf->$nObj->dir_output. "/";
+        if ($nObj != "") {
+            if (isset($conf->$nObj) && isset($conf->$nObj->dir_output))
+                $path = $conf->$nObj->dir_output . "/";
             else
-                $path .= $nObj."/";
+                $path .= $nObj . "/";
         }
 
-        if(isset($this->object) && isset($this->object->ref))
-            $path .= dol_sanitizeFileName($this->object->ref). "/";
+        if (isset($this->object) && isset($this->object->ref))
+            $path .= dol_sanitizeFileName($this->object->ref) . "/";
 
 
         return $path;
     }
-        
-    function getName(){
-        $name = "doc_".$this->typeObject;
-        if(isset($this->object)){
-            if(isset($this->object->ref))
+
+    function getName() {
+        $name = "doc_" . $this->typeObject;
+        if (isset($this->object)) {
+            if (isset($this->object->ref))
                 $name = dol_sanitizeFileName($this->object->ref);
-            elseif(isset($this->object->id))
-                $name .= "_".dol_sanitizeFileName($this->object->id);
+            elseif (isset($this->object->id))
+                $name .= "_" . dol_sanitizeFileName($this->object->id);
         }
-        return $this->prefName.$name;
+        return $this->prefName . $name;
     }
-        
-        
-    
-    
-	/**
+
+    /**
      *  Function to build pdf onto disk
      *
      *  @param		Object		$object				Object to generate
@@ -107,14 +104,14 @@ Abstract class BimpModelPDF
      *  @param		int			$hidedesc			Do not show desc
      *  @param		int			$hideref			Do not show ref
      *  @return     int             				1=OK, 0=KO
-	 */
-    function write_file($object,$outputlangs,$srctemplatepath='',$hidedetails=0,$hidedesc=0,$hideref=0)
-    {
+     */
+    function write_file($object, $outputlangs, $srctemplatepath = '', $hidedetails = 0, $hidedesc = 0, $hideref = 0) {
         $this->init($object);
-        $file = $this->getPath().$this->getName();
+        $file = $this->getPath() . $this->getName();
 
         $this->render($file, false);
 
         return 1;
     }
+
 }
--- htdocs/bimpcore/pdf/classes/PropalPDF.php
+++ htdocs/bimpcore/pdf/classes/PropalPDF.php
@@ -1,37 +1,109 @@
 <?php
 
-require_once __DIR__.'/BimpModelPDF.php';
+require_once __DIR__ . '/BimpModelPDF.php';
 
-class PropalPDF extends BimpModelPDF
-{
+class PropalPDF extends BimpModelPDF {
+
     public $mode = "normal";
 
-        function initData()
-	{
-            $this->typeObject = "propal";
-            $this->prefName = "loyer_";
-            
-            
-            $this->text  = "<style>h1{text-align:center;}</style>";
-            
-            
-            $this->text  .= "<h1>Gros titre</h1>";
-            
-            if(isset($this->object) && is_object($this->object)){
-                $this->text  .= "<h2>Propal ".$this->object->ref."</h2>";
+    function renderTable($arrayHead, $arrayData) {
+        $arrayUtil = array();
+        foreach ($arrayHead as $clef => $label) {
+            foreach ($arrayData as $ligne) {
+                $val = $ligne[$clef];
+                if (is_array($val))
+                    $val = $val[0];
+
+                if ($val !== null && $val !== "" && $val !== "0")
+                    $arrayUtil[$clef] = $label;
             }
-            
-            if($this->mode == "loyer")
-                $this->text .= "<h3>En mode Loyer</h3>"; 
-            
-            $espace = "";
-            for($i=0;$i<100;$i++){
-                $espace .= " - ";
-                $this->text .= "<br/>".$espace."Ligne n°".$i;
+        }
+
+        $html = "<table><tr style='background-color: green;'>";
+        foreach ($arrayUtil as $valT) {
+            if (is_array($valT))
+                $label = $valT[0];
+            else
+                $label = $valT;
+            $html .= "<th>" . $label . "</th>";
+        }
+        $html .= "</tr>";
+
+        foreach ($arrayData as $ligne) {
+            $html .= "<tr>";
+            foreach ($arrayUtil as $clef => $valT) {
+                $html .= "<td>";
+                if (isset($ligne[$clef])) {
+                    $unit = "";
+                    $val = $ligne[$clef];
+                    if (is_array($valT) && isset($valT[1]))
+                        $unit = " " . $valT[1];
+                    if ($unit == " €" || $unit == " %")
+                        $val = price($val);
+                    $html .= $val . $unit;
+                }
+                $html .= "</td>";
             }
-            
-            
-	}
-    
-    
+            $html .= "</tr>";
+        }
+        $html .= "</table>";
+
+        return $html;
+    }
+
+    function initData() {
+        $this->typeObject = "propal";
+        $this->prefName = "loyer_";
+
+
+        $titre = "";
+
+
+
+        $arrayHead = array("desc" => "Description", "prix" => array("Prix", "€"), "qty" => "Quantité", "remise" => array("Remise", " %"), "total_ht" => array("Total Ht", "€"), "tva" => array("Tva", " %"), "total_ttc" => array("Total Ttc", "€"));
+        $arrayData = array();
+        $arrayTot = array();
+
+        $this->object->fetch_lines();
+//            $line = new PropaleLigne;
+        foreach ($this->object->lines as $line) {
+            $arrayData[] = array("desc" => $line->desc,
+                "prix" => $line->subprice,
+                "qty" => $line->qty,
+                "remise" => $line->remise_percent,
+                "total_ht" => $line->total_ht,
+                "tva" => $line->tva_tx,
+                "total_ttc" => $line->total_ttc);
+
+            $arrayTot["prix"] += $line->subprice;
+        }
+        $tabHtml = $this->renderTable($arrayHead, $arrayData);
+
+
+
+        if (isset($this->object) && is_object($this->object)) {
+            $titre .= "<h2>" . get_class($this->object) . " " . $this->object->ref . "</h2>";
+        }
+
+
+
+
+
+
+        $this->text .= $this->pdf->renderTemplate($this->tpl_dir . '/table.html', array("titre" => $titre, "table" => $tabHtml));
+
+
+
+        //ci apres juste pour les test
+        if ($this->mode == "loyer")
+            $this->text .= "<h3>En mode Loyer</h3>";
+
+        $espace = "";
+        $this->text .= "<br/><br/><br/>";
+        for ($i = 0; $i < 100; $i++) {
+            $espace .= " - ";
+            $this->text .= "<br/>" . $espace . "Ligne n°" . $i;
+        }
+    }
+
 }

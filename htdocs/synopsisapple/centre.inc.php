b319523c2677da3eb06ceed84b8583618e2634f4
tommy <tommy@192.168.1.104>
2018-01-11 17:04:23 +0100

    bimp numerotation

--- htdocs/synopsisapple/centre.inc.php
+++ htdocs/synopsisapple/centre.inc.php
@@ -3,7 +3,7 @@
 global $tabCentre;
 $tabCentre = array("V" => array("04 75 81 81 54", "sav07@bimp.fr", "Saint Peray (valence)", 19, "1000566", "07130", "Saint-Peray", "739 Avenue Gross Umstadt"), 
     "M" => array("04 50 22 15 55", "sav74@bimp.fr", "Meythet", 58, "462140", "74960", "Meythet", "3 RUE DU VIEUX MOULIN"), 
-    "CB" => array("09 70 72 12 32", "sav73@bimp.fr", "Chambéry", 58, "1139941", "73000", "Chambéry", "150 rue croix d'or"), 
+    "CB" => array("09 70 72 12 32", "sav73@bimp.fr", "Chambéry", 213, "1139941", "73000", "Chambéry", "150 rue croix d'or"), 
     'L' => array("04 78 17 30 28", "sav69@bimp.fr", "Lyon", 21, "1000565", "69006", "Lyon", "67 rue Vendome"),
     "S" => array("04 77 81 58 12", "sav42@bimp.fr", "Saint-Etienne", 18, "1000483", "42000", "Saint Etienne", "14 rue gambetta"),
     "GA" => array("04 76 23 05 18", "sav38@bimp.fr", "Grenoble (Arts et Métiers)", 52, "494685", "38000", "Grenoble", "5, rue des arts et métiers"),
@@ -11,15 +11,15 @@ $tabCentre = array("V" => array("04 75 81 81 54", "sav07@bimp.fr", "Saint Peray 
     "MO" => array("03 81 95 19 20", "sav252@bimp.fr", "Montbeliard", 84, "484926", "25200", "Montbeliard", "37 place Denfert Rochereau"),
     "C" => array("03 44 200 200", "sav60@bimp.fr", "Compiegne", 106, "1040727", "60200", "Compiègne", "10 rue de l’étoile"),
     "MA" => array("04 96 11 29 40", "sav13@bimp.fr", "Marseille", 116, "1046076", "13011", "Marseille", "Route de la Sablière"),
-    "AB" => array("04 50 22 15 55", "sav74@bimp.fr", "Annecy Boutique", 58, "1046075", "74000", "Annecy", "7 rue de la poste"),
-    "CFC" => array("04 63 46 76 37", "sav63@bimp.fr", "Clermont-Ferrand", 58, "1111748", "63000", "Clermont-Ferrand", "Centre Jaude 2 (1er Etage) 7 Rue Giscard de la Tour Fondue"),
-    "CFB" => array("04 63 46 76 37", "sav63@bimp.fr", "Clermont-Ferrand", 58, "1111748", "63000", "Clermont-Ferrand", "Centre Jaude 2 (1er Etage) 7 Rue Giscard de la Tour Fondue"),
+    "AB" => array("04 50 22 15 55", "sav74@bimp.fr", "Annecy Boutique", 60, "1046075", "74000", "Annecy", "7 rue de la poste"),
+    "CFC" => array("04 63 46 76 37", "sav63@bimp.fr", "Clermont-Ferrand", 147, "1111748", "63000", "Clermont-Ferrand", "Centre Jaude 2 (1er Etage) 7 Rue Giscard de la Tour Fondue"),
+    "CFB" => array("04 63 46 76 37", "sav63@bimp.fr", "Clermont-Ferrand", 147, "1111748", "63000", "Clermont-Ferrand", "Centre Jaude 2 (1er Etage) 7 Rue Giscard de la Tour Fondue"),
     
     
-    "P" => array("09 71 002 450", "sav66@bimp.fr", "Perpignan", 58, "883234", "66000", "Perpignan", "9 Boulevard Georges Clemenceau"),
-    "N" => array("04 66 842 974", "sav30@bimp.fr", "Nîmes", 58, "459993", "30900", "Nîmes", "Centre commercial Cap Costière, 400 Avenue Claude Baillet"),
-    "MONTP" => array("04 67 555 111", "sav34@bimp.fr", "Montpellier", 58, "869780", "34000", "Montpellier", "36 Rue Saint-Guilhem"),
-    "MAU" => array("04 67 222 143", "sav341@bimp.fr", "Mauguio", 58, "579256", "34130", "Mauguio", "39 Rue René Fonck"),
+    "P" => array("09 71 002 450", "sav66@bimp.fr", "Perpignan", 215, "883234", "66000", "Perpignan", "9 Boulevard Georges Clemenceau"),
+    "N" => array("04 66 842 974", "sav30@bimp.fr", "Nîmes", 217, "459993", "30900", "Nîmes", "Centre commercial Cap Costière, 400 Avenue Claude Baillet"),
+    "MONTP" => array("04 67 555 111", "sav34@bimp.fr", "Montpellier", 218, "869780", "34000", "Montpellier", "36 Rue Saint-Guilhem"),
+    "MAU" => array("04 67 222 143", "sav341@bimp.fr", "Mauguio", 221, "579256", "34130", "Mauguio", "39 Rue René Fonck"),
     
     );
 

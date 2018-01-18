678fcb8e60fd3dd5440aa242240830680598a260
tommy <tommy@192.168.1.104>
2018-01-18 18:13:25 +0100

    revert modif

--- htdocs/bimpcontratauto/contrat.php
+++ htdocs/bimpcontratauto/contrat.php
@@ -108,7 +108,7 @@ if ($user->rights->contrat->creer) {
 
     print '<h5>Note</h5>';
 
-    print '<textarea id="noteP"></textarea><br>';
+    print '<textarea id="noteP">SN: </textarea><br>';
 
     print '<div class="buttonCustom">Valider</div>';
 

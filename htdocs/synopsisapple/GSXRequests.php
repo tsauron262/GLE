3f745d586a1784f41416f4a5eed67bb6ea52b39e
tommy <tommy@192.168.1.104>
2018-01-24 12:58:58 +0100

    new tier part

--- htdocs/synopsisapple/GSXRequests.php
+++ htdocs/synopsisapple/GSXRequests.php
@@ -635,7 +635,6 @@ class GSX_Request {
                     foreach ($tabT as $tabT2)
                         $tab3[] = $tabT2;
             }
-            echo "kk";print_r($tab3);
             $i = 100;
 
             $html .= "<div class='partDatasBlockHide'>";

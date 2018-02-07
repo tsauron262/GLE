19bcb59231b876215e06dc643b884f794383e6c7
tommy <tommy@192.168.1.104>
2018-02-07 16:27:54 +0100

    Grosse modifs module romain

--- htdocs/synopsistools/test.php
+++ htdocs/synopsistools/test.php
@@ -15,24 +15,40 @@ c.ref FROM `llx_synopsischrono` c, `llx_synopsis_apple_repair` r
 WHERE r.`chronoId` = c.`id` AND `closed` = 0 AND DATEDIFF(c.tms, now()) < 0  AND DATEDIFF(c.tms, now()) > -20
 AND serial_number is not null
 
-ORDER BY `nbJ` DESC, c.id LIMIT 0,500");
-echo "debut";
-
-$GSXdatas = new gsxDatas($ligne->serial_number);
-$repair = new Repair($db, $GSXdatas->gsx, false);
-        while ($ligne = $db->fetch_object($sql)){
-            if ($GSXdatas->connect){
-                $repair->rowId = $ligne->rid;
-                $repair->load();
-                if($repair->lookup())
-                    echo "Tentative de maj de ".$ligne->ref." statut ".$repair->repairComplete." num ".$repair->repairNumber.". num2 ".$repair->confirmNumbers['repair']."<br/>";
-                else
-                    echo "Echec de ma recup de ".$ligne->ref."<br/>";
-            }
-            else{
-                echo "Connexion GSX impossible";
-            }
-        }
+ORDER BY `nbJ` DESC, c.id");
         
         
         llxFooter();
+        
+        
+        
+function tentativeFermetureAuto(){
+            
+            $req = "SELECT -DATEDIFF(c.tms, now()) as nbJ, r.rowid as rid, `serial_number`, c.id,
+
+c.ref FROM `llx_synopsischrono` c, `llx_synopsischrono_chrono_105` cs, `llx_synopsis_apple_repair` r
+
+WHERE r.`chronoId` = c.`id` AND `closed` = 0 AND DATEDIFF(c.tms, now()) > -15
+AND serial_number is not null
+AND c.id = cs.id AND cs.Etat = 999
+
+ORDER BY `nbJ` DESC, c.id";
+            
+            $req .= " LIMIT 0,500";
+
+    $GSXdatas = new gsxDatas($ligne->serial_number);
+    $repair = new Repair($db, $GSXdatas->gsx, false);
+    while ($ligne = $db->fetch_object($sql)) {
+        if ($GSXdatas->connect) {
+            $repair->rowId = $ligne->rid;
+            $repair->load();
+            if ($repair->lookup())
+                echo "Tentative de maj de " . $ligne->ref . " statut " . $repair->repairComplete . " num " . $repair->repairNumber . ". num2 " . $repair->confirmNumbers['repair'] . "<br/>";
+            else
+                echo "Echec de ma recup de " . $ligne->ref . "<br/>";
+        }
+        else {
+            echo "Connexion GSX impossible";
+        }
+    }
+}

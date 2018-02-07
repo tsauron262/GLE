defc38fd70d6d5b1ae8ddea600179039f5e69d65
tommy <tommy@192.168.1.104>
2018-02-07 14:50:29 +0100

    Grosse modifs module romain

--- htdocs/synopsistools/test.php
+++ htdocs/synopsistools/test.php
@@ -13,13 +13,16 @@ $sql = $this->db->query("SELECT -DATEDIFF(c.tms, now()) as nbJ, `serial_number`,
 c.ref FROM `llx_synopsischrono` c, `llx_synopsis_apple_repair` r
 
 WHERE r.`chronoId` = c.`id` AND `ready_for_pick_up` = 0 AND DATEDIFF(c.tms, now()) < -15  AND DATEDIFF(c.tms, now()) > -60
-AND `serial_number` != null
+AND serial_number is not null
+
 ORDER BY `nbJ`  DESC");
         while ($ligne = $db->fetch_object($sql)){
             
             $GSXdatas = new gsxDatas($ligne->serial_number);
-            if ($GSXdatas->connect)
+            if ($GSXdatas->connect){
                 $response = $GSXdatas->gsx->lookup($ligne->serial_number, 'warranty');
+                echo "Tentative de maj de ".$ligne->ref." <br/>";
+            }
         }
         
         

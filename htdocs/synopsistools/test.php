8863b69d9aee046c2a68cbed64082bd5ba43538d
tommy <tommy@192.168.1.104>
2018-02-07 14:57:09 +0100

    Grosse modifs module romain

--- htdocs/synopsistools/test.php
+++ htdocs/synopsistools/test.php
@@ -8,17 +8,18 @@ require_once DOL_DOCUMENT_ROOT . '/synopsisapple/gsxDatas.class.php';
 
 llxHeader();
 
-$sql = $this->db->query("SELECT -DATEDIFF(c.tms, now()) as nbJ, `serial_number`, c.id,
+$sql = $db->query("SELECT -DATEDIFF(c.tms, now()) as nbJ, `serial_number`, c.id,
 
 c.ref FROM `llx_synopsischrono` c, `llx_synopsis_apple_repair` r
 
 WHERE r.`chronoId` = c.`id` AND `ready_for_pick_up` = 0 AND DATEDIFF(c.tms, now()) < -15  AND DATEDIFF(c.tms, now()) > -60
 AND serial_number is not null
 
-ORDER BY `nbJ`  DESC");
-        while ($ligne = $db->fetch_object($sql)){
-            
+ORDER BY `nbJ`  DESC LIMIT 0,10");
+echo "debut";
             $GSXdatas = new gsxDatas($ligne->serial_number);
+        while ($ligne = $db->fetch_object($sql)){
+            echo "dd";
             if ($GSXdatas->connect){
                 $response = $GSXdatas->gsx->lookup($ligne->serial_number, 'warranty');
                 echo "Tentative de maj de ".$ligne->ref." <br/>";

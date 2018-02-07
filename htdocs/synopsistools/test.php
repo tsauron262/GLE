2288a677d7011374c6e174eebfa17f5d38f45c52
tommy <tommy@192.168.1.104>
2018-02-07 16:42:44 +0100

    Grosse modifs module romain

--- htdocs/synopsistools/test.php
+++ htdocs/synopsistools/test.php
@@ -8,14 +8,8 @@ require_once DOL_DOCUMENT_ROOT . '/synopsisapple/gsxDatas.class.php';
 
 llxHeader();
 
-$sql = $db->query("SELECT -DATEDIFF(c.tms, now()) as nbJ, r.rowid as rid, `serial_number`, c.id,
 
-c.ref FROM `llx_synopsischrono` c, `llx_synopsis_apple_repair` r
-
-WHERE r.`chronoId` = c.`id` AND `closed` = 0 AND DATEDIFF(c.tms, now()) < 0  AND DATEDIFF(c.tms, now()) > -20
-AND serial_number is not null
-
-ORDER BY `nbJ` DESC, c.id");
+mailNonFerme();
         
         
         llxFooter();
@@ -23,7 +17,7 @@ ORDER BY `nbJ` DESC, c.id");
         
         
 function tentativeFermetureAuto(){
-            
+            global $db;
             $req = "SELECT -DATEDIFF(c.tms, now()) as nbJ, r.rowid as rid, `serial_number`, c.id,
 
 c.ref FROM `llx_synopsischrono` c, `llx_synopsischrono_chrono_105` cs, `llx_synopsis_apple_repair` r
@@ -35,6 +29,7 @@ AND c.id = cs.id AND cs.Etat = 999
 ORDER BY `nbJ` DESC, c.id";
             
             $req .= " LIMIT 0,500";
+            $sql = $db->query($req);
 
     $GSXdatas = new gsxDatas($ligne->serial_number);
     $repair = new Repair($db, $GSXdatas->gsx, false);
@@ -50,5 +45,21 @@ ORDER BY `nbJ` DESC, c.id";
         else {
             echo "Connexion GSX impossible";
         }
+    }
+}
+
+
+
+function mailNonFerme(){
+    global $db;
+        $nbJ = 15;
+$sql = $db->query("SELECT -DATEDIFF(c.tms, now()) as nbJ, c.id,
+
+c.ref FROM `llx_synopsischrono` c, llx_synopsischrono_chrono_105 cs
+
+WHERE c.id = cs.id AND cs.Etat != 999 AND DATEDIFF(c.tms, now()) < ".-$nbJ."");
+    
+    while ($ligne = $db->fetch_object($sql)) {
+        echo "SAV Non fermé depuis plus de : ".$nbJ." jours || ".$ligne->ref."</br>";
     }
 }

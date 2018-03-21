3e4dcbb48ff9e064a6b97cd9efa115571a81ea8b
tommy <tommy@192.168.1.104>
2018-03-22 00:08:45 +0100

    tabs comm client

--- htdocs/synopsissignature/signature.php
+++ htdocs/synopsissignature/signature.php
@@ -306,7 +306,7 @@ if ($selectedFile) {
             /* $tabFilePc[] = DOL_DATA_ROOT."/ficheinter/".$object->ref."/";
               $tabFilePc2[] = ".pdf";
               $tabFilePc3[] = "PC-" . $chrono->ref . ".pdf"; */
-            mailSyn2("FI Signé", "j.mazet@bimp.fr", null, "Bonjour la FI " . $object->getNomUrl(1) . " est Signé", array(), array(), array());
+            mailSyn2("FI Signé", "j.mazet@bimp.fr, v.gilbert@bimp.fr", null, "Bonjour la FI " . $object->getNomUrl(1) . " est Signé", array(), array(), array());
         } elseif (stripos($signeFile, 'SH') === 0) {
             $pdf->setXY(25, 240);
             $pdf->image($nomSign, 25, 245, 40);

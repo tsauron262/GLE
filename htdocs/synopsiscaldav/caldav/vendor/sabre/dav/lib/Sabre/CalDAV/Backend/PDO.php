52c00d5ace8549865f4d31a27263bfa971f87061
tommy <tommy@192.168.1.104>
2018-03-23 16:42:55 +0100

    annul erreur

--- htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/dav/lib/Sabre/CalDAV/Backend/PDO.php
@@ -494,7 +494,7 @@ global $conf;
         date_default_timezone_set("Europe/Paris");
         
         //DECODAGE
-        $calData = str_replace(array("<br />\r\n", "<br />\r\n", "\r", "\n"), "\n", $calData);
+        $calData = str_replace(array("<br />\r\n", "<br />\r", "<br />\n", "\r", "\n"), "\n", $calData);
         $calData = html_entity_decode($calData,ENT_QUOTES);
         
         

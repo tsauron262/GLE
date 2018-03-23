34c029a385fe9e0a695d02e56a1d5947bb5469d2
RomainPe <pelegrinromain@gmail.com>
2018-03-23 17:03:39 +0100

    init backup

---
+++ scripts/backup_server/create.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once 'param.inc.php';
+
+//$date = new DateTime();
+//
+//$return_var = NULL;
+//$output = NULL;
+//
+//$command = 'mysqldump '
+//        . '--user=\'' . DB_USER . '\' '
+//        . '--password=\'' . DB_PASSWORD . '\' '
+//        . '--host=\'' . DB_HOST . '\' '
+//        . '\'' . DB_NAME . '\' > '
+//        . '\'' . PATH . '/backups/backup-' . $date->getTimestamp() . '.sql\'';
+//
+//echo $command . "<br/>";
+//
+//$bak_result = exec($command, $errors, $ret);
+//print_r($errors);
+//echo "retval = ".$ret."\n\n";
---
+++ scripts/backup_server/param.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+define('LOGIN', 'a');
+define('PASSWORD', 'z');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '$mokinU2');
+define('DB_NAME', 'test');
+define('PATH', realpath(dirname(__FILE__)));
+define('URL_ROOT', '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));

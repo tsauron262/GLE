d659f1ba41e3fbc326357760b1435d59f05f8f14
tommy <tommy@192.168.1.104>
2018-05-08 14:24:10 +0200

    kbb

--- htdocs/bimpapple/objects/GSX_Repair.class.php
+++ htdocs/bimpapple/objects/GSX_Repair.class.php
@@ -297,8 +297,8 @@ class GSX_Repair extends BimpObject
         $update = false;
         
         
-        if($this->repairLookup['repairStatus'] == "Prêt pour enlèvement"){
             dol_syslog($this->repairLookup['repairStatus'],3);
+        if($this->repairLookup['repairStatus'] == "Prêt pour enlèvement"){
             $update = true;
             $this->set('ready_for_pick_up', 1);
         }

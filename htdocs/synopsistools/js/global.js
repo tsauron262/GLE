0719df13d19d6d8cb718207a06d5f7444a9a98fa
tommy <tommy@192.168.1.104>
2018-05-18 15:28:54 +0200

    ajout infos 8sens dans essentielle

--- htdocs/synopsistools/js/global.js
+++ htdocs/synopsistools/js/global.js
@@ -53,6 +53,18 @@ $(window).on("load", function () {
                 return false
         });
     }
+    
+    
+$("td:contains('Email de secours')").each(function () {   ///plus de mdp apple
+        $(this).next("td").each(function () {
+            if ($(this).find("input").length > 0) {
+                val = $(this).find("input").val();
+                $(this).parent().append("<input type='password' name='options_apple_mdp' value='" + val + "'/>");
+                $(this).remove();
+            } else
+                $(this).html("*******");
+        });
+    });
 
 
 

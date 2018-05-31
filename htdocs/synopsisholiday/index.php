e094606beb480aef99efddc3c38274387cbb835f
tommy <tommy@192.168.1.104>
2018-05-31 17:27:48 +0200

    stock facture + facturé commande devis

--- htdocs/synopsisholiday/index.php
+++ htdocs/synopsisholiday/index.php
@@ -434,7 +434,8 @@ if (!empty($holiday->holiday)) {
 
         $date = $infos_CP['date_create'];
 
-        print '<tr ' . $bc[$var] . '>';
+        $tabColor = array("16"=>"green", "17"=>"blue", "18"=>"orange", "19"=>"purple");
+        print '<tr ' . $bc[$var] . '  style=" color:'.$tabColor[dol_print_date($infos_CP['date_debut'], '%y')].'">';
         print '<td>';
         $holidaystatic->id = $infos_CP['rowid'];
         $holidaystatic->ref = $infos_CP['rowid'];
@@ -453,9 +454,8 @@ if (!empty($holiday->holiday)) {
         else
             print $groupstatic->name;
         print '</td>';
-        $tabColor = array("16"=>"green", "17"=>"blue", "18"=>"orange", "19"=>"purple");
         print '<td>' . $approbatorstatic->getNomUrl('1') . '</td>';
-        print '<td align="center" style=" color:'.$tabColor[dol_print_date($infos_CP['date_debut'], '%y')].'">' . dol_print_date($infos_CP['date_debut'], 'day') . '</td>';
+        print '<td align="center">' . dol_print_date($infos_CP['date_debut'], 'day') . '</td>';
         print '<td align="center">' . dol_print_date($infos_CP['date_fin'], 'day') . '</td>';
         print '<td align="right">';
         $nbopenedday = num_open_day($infos_CP['date_debut_gmt'], $infos_CP['date_fin_gmt'], 0, 1, $infos_CP['halfday']);

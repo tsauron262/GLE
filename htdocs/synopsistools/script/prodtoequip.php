2d00896467a09f5aac2ba7f9453c14d2df207e02
tommy <tommy@192.168.1.104>
2018-06-08 11:38:13 +0200

    Merge origin/DEV-FLO
    Conflicts:
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-brands-400.eot
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-brands-400.svg
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-brands-400.ttf
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-brands-400.woff
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-brands-400.woff2
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-regular-400.eot
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-regular-400.svg
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-regular-400.ttf
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-regular-400.woff
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-regular-400.woff2
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-solid-900.eot
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-solid-900.svg
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-solid-900.ttf
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-solid-900.woff
    	htdocs/bimpcore/views/fonts/fontawesome5/fa-solid-900.woff2
    Former-commit-id: 624f68628e03fe78bd1b10dd50547eb0a7577001

--- htdocs/synopsistools/script/prodtoequip.php
+++ htdocs/synopsistools/script/prodtoequip.php
@@ -19,6 +19,8 @@ $OK= 0;
 
 $newErrors = array("pas d'erreur");
 
+define('DONT_CHECK_SERIAL', true);
+
 if ($loadEquip == true) {
     $sql = $db->query("SELECT * FROM `llx_synopsischrono_chrono_101` ce, llx_synopsischrono c WHERE c.id = ce.id AND concat('OLD', ce.id) NOT IN (SELECT note FROM `llx_be_equipment` WHERE 1) AND `N__Serie` NOT LIKE '% %' AND `N__Serie` NOT LIKE '' ORDER BY c.id LIMIT  0,10");
 

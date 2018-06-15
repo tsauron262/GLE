2ec2b84810705d296dc024c277689a851fbf178b
tommy <tommy@192.168.1.104>
2018-06-15 13:46:17 +0200

    scroll pop recherche prod
    Former-commit-id: 341a4fd327b7b433e7a7eef7a0f17070450952c3

---
+++ htdocs/bimpcore/updates/5.9.sql
@@ -0,0 +1,9 @@
+
+ALTER TABLE `llx_facture`
+ ADD `codeCli8Sens` varchar(30);
+
+ALTER TABLE `llx_facture`
+ ADD `Collab8sens` varchar(30);
+
+ALTER TABLE `llx_facture`
+ ADD `Collab8sens` varchar(30);

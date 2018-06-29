aac9215c334434e9f057b32a16d432272f9c4b84
tommy <tommy@192.168.1.104>
2018-06-29 14:54:22 +0200

    scroll pop recherche prod
    Former-commit-id: 9d128f5e1a04ae56290f6bd406813b19bf6e9b37

--- htdocs/core/class/translate.class.php
+++ htdocs/core/class/translate.class.php
@@ -595,8 +595,9 @@ class Translate
 	function trans($key, $param1='', $param2='', $param3='', $param4='', $maxsize=0)
 	{
         global $conf;
-            
-                    setEventMessages("ii".$key."pp", null, 'errors');
+        
+        if($key == "")
+            return "";
 
 	    if (! empty($this->tab_translate[$key]))	// Translation is available
 		{

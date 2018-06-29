d559c64232f4772073d072005786ae09b315b49a
tommy <tommy@192.168.1.104>
2018-06-29 17:24:14 +0200

    scroll pop recherche prod
    Former-commit-id: 8337ac46d56948077abcb82ded8f52c3e6040468

---
+++ htdocs/bimpcore/updates/6.4.sql
@@ -0,0 +1,3 @@
+UPDATE `llx_user_extrafields` SET `maxpriceorder` = 50;
+UPDATE `llx_user_extrafields` SET `maxpriceorder` = 50000 WHERE fk_object = 62;
+UPDATE `llx_user_extrafields` SET `maxpriceorder` = 100000 WHERE fk_object = 7;

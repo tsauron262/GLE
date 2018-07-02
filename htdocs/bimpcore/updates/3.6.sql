351f509b6079a2d94be8c90a23bf1acf38a28270
tommy <tommy@192.168.1.104>
2018-07-02 21:04:38 +0200

    scroll pop recherche prod
    Former-commit-id: 218aa34f6b8c4998b5ab186c958c5eb3612a487f

--- htdocs/bimpcore/updates/3.6.sql
+++ htdocs/bimpcore/updates/3.6.sql
@@ -1,5 +1,5 @@
 
-ALTER TABLE `llx_br_reservation` ADD `ref` VARCHAR(128) NOT NULL AFTER `id`;
+    ALTER TABLE `llx_br_reservation` ADD `ref` VARCHAR(128) NOT NULL AFTER `id`;
 ALTER TABLE `llx_br_reservation_cmd_fourn` ADD `ref_reservation` VARCHAR(128) NOT NULL DEFAULT '' AFTER `id_reservation`;
 ALTER TABLE `llx_br_reservation_cmd_fourn` ADD `id_entrepot` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `id_commande_client`;
 ALTER TABLE `llx_br_reservation_cmd_fourn` ADD `special_price_tva_tx` FLOAT NOT NULL DEFAULT '0' AFTER `special_price`;

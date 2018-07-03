b23b9f77849317d58bc8325cdf8674e4843a7d9b
flo <flo@flo-micro>
2018-07-03 11:44:04 +0200

    Correction SAV + MAJ SAV / GSX / PDF
    Former-commit-id: c1ca91b7976403709496d96d1e93fe82251b469e

--- htdocs/bimpsupport/views/js/sav.js
+++ htdocs/bimpsupport/views/js/sav.js
@@ -92,7 +92,6 @@ function loadGSXView($button, id_sav) {
     var $gsxForm = $('#loadGSXForm');
     var $container = $('#gsxResultContainer');
     var serial = $gsxForm.find('#gsx_equipment_serial').val();
-//    serial = 'FFRW17TXHXR5';
     if (/^S?[A-Z0-9]{11,12}$/.test(serial) || /^S?[0-9]{15}$/.test(serial)) {
         BimpAjax('loadGSXView', {
             serial: serial,
@@ -150,57 +149,4 @@ $(document).ready(function () {
             }
         }
     });
-
-//    var $gsxForm = $('#loadGSXForm');
-//    if ($.isOk($gsxForm)) {
-//        $gsxForm.find('#loadGSXButton').click(function () {
-//            var $container = $('#gsxContainer');
-//
-//            var serial = $gsxForm.find('#gsx_equipment_serial').val();
-//            serial = 'C02QL8SEFVH5';
-//            var ok = false;
-//            if (/^[A-Z0-9]{11,12}$/.test(serial)) {
-//                GSX.loadProduct(serial, 'gsxResultContainer');
-////                        setRequest('newSerial', '&serial=' + serial);
-//                ok = true;
-//            }
-//            if (/^[0-9]{15}$/.test(serial)) {
-//                GSX.loadProduct(serial, 'gsxResultContainer');
-////                        setRequest('newSerial', '&serial=' + serial);
-//                ok = true;
-//            }
-//            if (!ok) {
-//                $('#gsxResultContainer').html('<p class="alert alert-danger">Pas de numéro de série correct</p>');
-//            }
-//        });
-//    }
 });
-//
-// if ($("textarea#Descr").length) {
-//        textarea2 = $("textarea#Descr");
-//        tabAccess = Array("Rayure", "Écran cassé", "Liquide");
-//        textarea2.parent().append(' <select name="sometext" multiple="multiple" class="grand" id="sometext2">    <option>' + tabAccess.join('</option><option>') + '</option></select>');
-//        $("#sometext2").click(function () {
-//            textarea2.val(textarea2.val() + $(this).val() + ', ');
-//        });
-//    }
-//    
-//
-//    if ($("textarea#Symptomes").length) {
-//        textarea3 = $("textarea#Symptomes");
-//        tabAccess = Array("Renouvellement anti virus et maintenance annuelle","Anti virus expiré","Virus ? Eradication? Nettoyage?", "Machine lente","Formatage","Réinstallation système.");
-//        textarea3.parent().append(' <select name="sometext" multiple="multiple" class="grand" id="sometext3">    <option>' + tabAccess.join('</option><option>') + '</option></select>');
-//        $("#sometext3").click(function () {
-//            textarea3.val(textarea3.val() + $(this).val() + ', ');
-//        });
-//    }
-//
-//
-//    if ($("textarea.choixAccess").length) {
-//        textarea = $("textarea.choixAccess");
-//        tabAccess = Array("Housse", "Alim", "Carton", "Clavier", "Souris", "Dvd", "Batterie", "Boite complet");
-//        textarea.parent().append(' <select name="sometext" style="height: '+(20 * tabAccess.length)+'px;" multiple="multiple" class="grand" id="sometext">    <option>' + tabAccess.join('</option><option>') + '</option></select>');
-//        $("#sometext").click(function () {
-//            textarea.val(textarea.val() + $(this).val() + ', ');
-//        });
-//    }

c001d376472c5eb6df195785fab3adb726e5a018
tommy <tommy@192.168.1.104>
2018-07-04 09:36:45 +0200

    scroll pop recherche prod
    Former-commit-id: 6be168e84108c9c9b31ed16a1b87a1b97b92c316

--- htdocs/bimpvalidateorder/class/bimpvalidateorder.class.php
+++ htdocs/bimpvalidateorder/class/bimpvalidateorder.class.php
@@ -33,9 +33,8 @@ class BimpValidateOrder {
             setEventMessages(null, $this->errors, 'errors');
             return -3;
         }
-        $tropRemise = ($order->array_options['options_type'] == "C" ? $this->checkRemise($order) : 0);
+        //$tropRemise = ($order->array_options['options_type'] == "C" ? $this->checkRemise($order) : 0);
 
-die($tropRemise);
         if ($max_price <= $price_order || $tropRemise) {
             $id_responsibles = $this->getResponsiblesIds($price_order, $order);
             $error = false;

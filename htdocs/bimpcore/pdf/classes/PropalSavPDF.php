2c9d6732da7bc04c02637ba4653c7dc6a8b1feb2
tommy <tommy@192.168.1.104>
2018-07-16 13:22:55 +0200

    mail sec
    Former-commit-id: 538fb7917d3adcc1390414e0a6751fe7697751bf

--- htdocs/bimpcore/pdf/classes/PropalSavPDF.php
+++ htdocs/bimpcore/pdf/classes/PropalSavPDF.php
@@ -60,19 +60,21 @@ class PropalSavPDF extends PropalPDF
     public function renderAfterBottom()
     {
         $html .= '<table cellpadding="20px"><tr><td>';
-        $html .= '<p style="font-size: 7px; color: #002E50">';
-        $html .= '      Les informations personnelles requises suivantes (nom, adresse, téléphone et adresse mail) sont nécessaires pour poursuivre la ';
-        $html .= 'demande de réparation.<br/>Si le service est requis conformément à une obligation de réparation d’un tiers, ces informations seront ';
+//        $html .= '<p style="font-size: 7px; color: #002E50">';
+        $html .= '<div style="text-indent: 15px; font-size: 7px; color: #002E50">';
+        $html .= 'Si le service est requis conformément à une obligation de réparation d’un tiers, ces informations seront ';
         $html .= 'transférées au tiers pour vérification et des objectifs de qualité, notamment la confirmation de la transaction de réparation et la ';
         $html .= 'soumission d’une enquéte client. En signant, vous acceptez ce transfert ainsi que l’utilisation de ces informations par un tiers.';
-        $html .= '</p>';
-        $html .= '<p style="font-size: 7px; color: #002E50">';
-        $html .= '      Les pièces de maintenance ou les produits utilisés pour la réparation de votre produit sont neufs ou d\'un état équivalent à neuf ';
+        $html .= '<br/>';
+        $html .= 'Les pièces de maintenance ou les produits utilisés pour la réparation de votre produit sont neufs ou d\'un état équivalent à neuf ';
         $html .= 'en termes de performance et de fiabilité. ';
-        $html .= '</p>';
-        $html .= '<p style="font-size: 7px; color: #002E50">';
-        $html .= '      Pour du matériel couvert par Apple, la garantie initiale s\'applique. Pour du matériel non couvert par Apple, la garantie est de 3 mois pour les pièces et la main d\'oeuvre. Les pannes logicielles ne sont pas couvertes par la garantie du fabricant. Une garantie de 30 jours est appliquée pour les réparations logicielles.';
-        $html .= '</p>';
+        $html .= '<br/>';
+        $html .= 'Pour du matériel couvert par Apple, la garantie initiale s\'applique. Pour du matériel non couvert par Apple, la garantie est de 3 mois pour les pièces et la main d\'oeuvre. Les pannes logicielles ne sont pas couvertes par la garantie du fabricant. Une garantie de 30 jours est appliquée pour les réparations logicielles.';
+        $html .= '<br/>';
+        $html .= 'Les informations personnelles requises suivantes (nom, adresse, téléphone et adresse mail) sont nécessaires pour poursuivre la ';
+        $html .= 'demande de réparation.';
+        $html .= '</div>';
+//        $html .= '</p>';
         $html .= '</td></tr></table>';
 
         $this->writeContent($html);

6d8034a66047d066cb393c4fec317ea69c18cde1
tommy <tommy@192.168.1.104>
2018-09-05 14:22:14 +0200

    export import
    Former-commit-id: 49df9a1da6dcd53de3e76127110ddd7833e91473

--- htdocs/synopsistools/class/cronSynopsis.class.php
+++ htdocs/synopsistools/class/cronSynopsis.class.php
@@ -299,7 +299,7 @@ class CronSynopsis {
         if (array_key_exists('options_date_s', $user->array_options)) {
             $mails = "tommy@bimp.fr, grh@bimp.fr";
             $mails2 = $mails .", f.poirier@bimp.fr, j.belhocine@bimp.fr";
-            $sql = $this->db->query("SELECT u.login, u.rowid, u2.email  FROM `" . MAIN_DB_PREFIX . "user_extrafields` ue, " . MAIN_DB_PREFIX . "user u LEFT JOIN llx_user u2 ON u2.rowid = u.fk_user  WHERE `date_s` <= now() AND fk_object = u.rowid AND statut = 1");
+            $sql = $this->db->query("SELECT u.login, u.rowid, u2.email  FROM `" . MAIN_DB_PREFIX . "user_extrafields` ue, " . MAIN_DB_PREFIX . "user u LEFT JOIN llx_user u2 ON u2.rowid = u.fk_user  WHERE `date_s` <= now() AND fk_object = u.rowid AND u.statut = 1");
             while ($result = $this->db->fetch_object($sql)) {
                 $userF = new User($this->db);
                 $userF->fetch($result->rowid);
@@ -310,7 +310,7 @@ class CronSynopsis {
             }
             
             foreach(array(14, 7) as $nbDay){
-                $sql = $this->db->query("SELECT u.login, u.rowid, u2.email  FROM `" . MAIN_DB_PREFIX . "user_extrafields` ue, " . MAIN_DB_PREFIX . "user u LEFT JOIN llx_user u2 ON u2.rowid = u.fk_user WHERE `date_s` = DATE(DATE_ADD(now(), INTERVAL ".$nbDay." DAY)) AND fk_object = u.rowid AND statut = 1");
+                $sql = $this->db->query("SELECT u.login, u.rowid, u2.email  FROM `" . MAIN_DB_PREFIX . "user_extrafields` ue, " . MAIN_DB_PREFIX . "user u LEFT JOIN llx_user u2 ON u2.rowid = u.fk_user WHERE `date_s` = DATE(DATE_ADD(now(), INTERVAL ".$nbDay." DAY)) AND fk_object = u.rowid AND u.statut = 1");
                 while ($result = $this->db->fetch_object($sql)) {
                     $str2 = "Bonjour le compte de " . $result->login . " sera fermé dans ".$nbDay." jours. Cordialement.";
                     $str .= $str2."<br/>";

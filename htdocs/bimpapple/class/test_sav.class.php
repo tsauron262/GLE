448172ad2c3db635f1a030bf3393d6ca80466a16
tommy <tommy@192.168.1.104>
2018-09-25 14:01:34 +0200

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/updates/6.9.sql
    Former-commit-id: 2bd93147cfb0f3b3fb5dc75ef2367530767b44a6

--- htdocs/bimpapple/class/test_sav.class.php
+++ htdocs/bimpapple/class/test_sav.class.php
@@ -159,7 +159,7 @@ AND s.status = " . ($statut == "closed" ? "999" : "9");
                                 }
                                 else {
                                     $this->nbErr++;
-                                    echo "N'arrive pas a être passé a RFPU dans GSX<br/> ";
+                                        echo "N'arrive pas a être passé a RFPU dans GSX<br/> ";
                                     if (isset($_GET['envoieMail']))
                                         mailSyn2("Sav non RFPU dans GSX", $mailTech, "gle_suivi@bimp.fr", "Bonjour le SAV " . $this->getNomUrlChrono($ligne->cid, $ligne->ref) . " avec comme code repa : " . $repair->confirmNumbers['repair'] . " n'est pas passé RFPU dans GSX. Reponse : " . $repair->repairLookUp['repairStatus']);
                                 }
@@ -192,8 +192,8 @@ AND s.status = " . ($statut == "closed" ? "999" : "9");
                 if (!$this->useCache || !isset($_SESSION['idRepairIncc'][$ligne->rid])) {
                     $repair->fetch($ligne->rid);
                     if (count($repair->lookup()) == 0) {
-                        echo "Tentative de maj de " . $ligne->ref . " statut " . $repair->repairComplete . " num " . $repair->repairNumber . ". num2 " . $repair->confirmNumbers['repair'] . " Reponse : " . $repair->repairLookUp['repairStatus'] . "<br/>";
-                        if ($repair->repairLookUp['repairStatus'] == "Prêt pour enlèvement" || $repair->repairComplete) {
+                        echo "Tentative de maj de " . $ligne->ref . " statut ready for pickup : " . $repair->getData('ready_for_pick_up') . " num " . $repair->repairNumber . ". num2 " . $repair->confirmNumbers['repair'] . " Reponse : " . $repair->repairLookUp['repairStatus'] . "<br/>";
+                        if ($repair->repairLookUp['repairStatus'] == "Prêt pour enlèvement" || $repair->getData('ready_for_pick_up')) {
                             echo "Passage dans GLE a RFPU<br/>";
                             $repair->readyForPickUp = 1;
                             $repair->update();

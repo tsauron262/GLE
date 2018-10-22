90dc3a685c0205159b200a0bf7866e55bfe0a3de
flo <flo@flo-micro>
2018-10-22 18:08:36 +0200

    maj sql 8.2
    Former-commit-id: 57d757690486b7bb4faf98f0f1de47090e337649

--- htdocs/bimpcore/updates/8.2.sql
+++ htdocs/bimpcore/updates/8.2.sql
@@ -1,2 +1,2 @@
 
--- ALTER TABLE `llx_bimp_list_config` ADD `sort_option` VARCHAR(128) NOT NULL DEFAULT '' AFTER `sort_field`;
+ALTER TABLE `llx_bimp_list_config` ADD `sort_option` VARCHAR(128) NOT NULL DEFAULT '' AFTER `sort_field`;

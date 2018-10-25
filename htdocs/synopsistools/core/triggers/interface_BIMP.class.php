d3080571f03c6310ae6f9a867b151c1166d24843
tommy <tommy@192.168.1.104>
2018-10-25 14:19:46 +0200

    bug entrepot label ref
    Former-commit-id: 0b595a3fcd35a22bf0bc84ad241c6519dc1f9381

--- htdocs/synopsistools/core/triggers/interface_BIMP.class.php
+++ htdocs/synopsistools/core/triggers/interface_BIMP.class.php
@@ -30,10 +30,9 @@ class InterfaceSpecialSoc extends DolibarrTriggers {
     public function runTrigger($action, $object, User $user, Translate $langs, Conf $conf) {
         global $conf, $user;
         
-        die('oo'.$action);
         
-        if($action == "USER_UPDATE"){
-            
+        
+        if($action == "USER_CREATE"){
             $msg = "Bonjour, un nouveau collaborateur : ".$object->getFullName($langs)." viens de rejoindre l'équipe";
             $msg .= "<br/>à ".$user->getFullAddress();
             $msg .= "<br/>en tant que ".$user->job;

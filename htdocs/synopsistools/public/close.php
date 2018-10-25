a21149f046d4d38bc25d80699849be11935fc21b
tommy <tommy@192.168.1.215>
2018-10-25 23:40:52 +0200

    DESACTIVE FIXE TABS
    Former-commit-id: 462562b5f803437139a5855c71d829c1435b988f

--- htdocs/synopsistools/public/close.php
+++ htdocs/synopsistools/public/close.php
@@ -4,6 +4,10 @@ require_once(DOL_DOCUMENT_ROOT.'/main.inc.php');
 
 echo "<img src='".DOL_URL_ROOT."/synopsistools/img/travaux.png'/>";
 echo "<h1>Application ferm&eacute;e pour cause de maintenance.</h1>";
-if(defined("CLOSE_DATE"))
+
+
+if(defined("REDIRECT_URL"))
+    echo "<script>window.location = '".REDIRECT_URL."';</script>";
+elseif(defined("CLOSE_DATE"))
     echo "<h2>Retour pr&eacute;vue : ". dol_print_date(CLOSE_DATE, "dayhourtext")."</h2>";
 

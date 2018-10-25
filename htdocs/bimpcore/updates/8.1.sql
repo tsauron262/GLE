1124f85204a49f56635c7ac56b1d955dc7f200f8
tommy <tommy@192.168.1.104>
2018-10-25 14:44:50 +0200

    DESACTIVE FIXE TABS
    Former-commit-id: 9885a75ccd91301808b673bbb883a29071c74949

--- htdocs/bimpcore/updates/8.1.sql
+++ htdocs/bimpcore/updates/8.1.sql
@@ -1,4 +1,14 @@
 
+UPDATE `llx_extrafields` SET param = 'a:1:{s:7:"options";a:1:{s:28:"entrepot:ref:rowid::statut=1";N;}}' WHERE `name` LIKE 'entrepot';
+
+
+UPDATE `llx_extrafields` SET list = 1
+
+UPDATE `llx_cronjob` SET entity = 1 WHERE label LIKE 'Get Reservations GSX'
+
+
+
+
 CREATE TABLE IF NOT EXISTS `llx_bimp_list_config` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `owner_type` int(10) UNSIGNED NOT NULL DEFAULT '2',

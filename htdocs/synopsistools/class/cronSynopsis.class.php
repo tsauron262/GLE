ba3486d79237f2cd44b74fc1b1d21973b5d634ac
tommy <tommy@192.168.1.104>
2018-10-26 19:44:36 +0200

    code client que a l'enregistrement
    Former-commit-id: 83e30bd6e7e305f5b9220e9f05e2cb5035f3215f

--- htdocs/synopsistools/class/cronSynopsis.class.php
+++ htdocs/synopsistools/class/cronSynopsis.class.php
@@ -37,7 +37,7 @@ class CronSynopsis {
 
 
 
-
+        $this->output .= "FIN";
         echo 1;
     }
 
@@ -48,11 +48,13 @@ class CronSynopsis {
     
     
     public function callTrigerPeter(){
-        $this->db->query("CALL refresh_llx_mat_view_propaldet();");
-        $this->db->query("CALL refresh_llx_mat_view_facturedet();");
-        $this->db->query("CALL refresh_llx_mat_view_commandedet();");
-        $this->db->query("CALL refresh_llx_mat_view_categorie();");
-        $this->db->query("CALL refresh_llx_mat_view_product_cat();");
+        $result1 = $this->db->query("CALL refresh_llx_mat_view_propaldet();");
+        $result2 = $this->db->query("CALL refresh_llx_mat_view_facturedet();");
+        $result3 = $this->db->query("CALL refresh_llx_mat_view_commandedet();");
+        $result4 = $this->db->query("CALL refresh_llx_mat_view_categorie();");
+        $result5 = $this->db->query("CALL refresh_llx_mat_view_product_cat();");
+        if($result1 && $resul2 && $resul3 && $result4 && $result5)
+            $this->output .= "OK";
     }
 
     public function extractFact($debug = false) {

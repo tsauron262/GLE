218c66134aba430b9ab6816fb5df8581efaa75e3
tommy <tommy@192.168.1.104>
2018-10-30 14:14:48 +0100

    fetch_optinals
    Former-commit-id: 8b253f5cef4576ae09c0475d42543e7bbfcc82e9

--- htdocs/synopsistools/class/importExport/exportfacture.class.php
+++ htdocs/synopsistools/class/importExport/exportfacture.class.php
@@ -33,7 +33,7 @@ class exportfacture extends export8sens {
             if(stripos($file, ".ER8"))
                     $nbFilesErr++;
         if($nbFiles > 5)
-            mailSyn2("Synchro 8Sens OFF", "tommy@bimp.fr", "admin@bimp.fr", "Dossier : ".$this->pathExport." <br/><br/>Nb files : ".$nbFiles);
+            mailSyn2("Synchro 8Sens OFF", "dev@bimp.fr, gsx@bimp.fr", "admin@bimp.fr", "Dossier : ".$this->pathExport." <br/><br/>Nb files : ".$nbFiles);
         if($nbFilesErr > 0)
             mailSyn2("Synchro 8Sens FICHIER ERREURS", "tommy@bimp.fr", "admin@bimp.fr", "Dossier : ".$this->pathExport." <br/><br/>Nb files : ".$nbFilesErr);
     }

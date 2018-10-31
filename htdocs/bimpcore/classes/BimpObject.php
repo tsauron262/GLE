18399f242a771526045890a0f4f73d2109e0debe
tommy <tommy@192.168.1.104>
2018-10-31 11:59:37 +0100

    fetch_optinals
    Former-commit-id: 9edda51fb23ddad97a6c46087c5dbe53fa1e2ade

--- htdocs/bimpcore/classes/BimpObject.php
+++ htdocs/bimpcore/classes/BimpObject.php
@@ -4303,4 +4303,11 @@ class BimpObject extends BimpCache
             'success_callback' => 'window.location.reload();'
         );
     }
+    
+    
+    public static function testMail($mail){
+        if(filter_var($mail, FILTER_VALIDATE_EMAIL))
+                return true;
+        return false;
+    }
 }

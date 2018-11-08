a78b877acb53c3154cbc3661a04a0d7c42ddaa10
tommy <tommy@192.168.1.104>
2018-11-08 17:01:25 +0100

    Merge origin/gle8
    Conflicts:
    	htdocs/bimpfinancement/objects/BF_Demande.yml
    Former-commit-id: 2ffb39e6bc76cbd6421dc075d80ebbd94b9d7e9a

--- htdocs/bimptask/objects/BIMP_Task.class.php
+++ htdocs/bimptask/objects/BIMP_Task.class.php
@@ -37,12 +37,59 @@ class BIMP_Task extends BimpObject
         //return array(0=>array('label'=>"cool", 'classes'=>array('info')));//sucess info dangerous important
         return self::$cache["status_list_task"];
     }
+    
+    public function actionSendMail($data, &$success){
+        $success = "impec";
+        $errors = $warnings = array();
+        $sep = "<br/>---------------------<br/>";
+        $idTask = "IDTASK:5467856456".$this->getData("id");
+        
+        
+//        $notes = $this->getChildrenObjects("bimp_note");
+        $notes = array("msg1", "msg2", "msg3");
+        
+        
+        $msg = $data['txt2'];
+        
+        
+        $msg .= "<br/>".$sep."Merci d'inclure ces lignes dans les prochaines conversations<br/>".$idTask.$sep;
+        
+        $msg .= "<br/>Fil de discution :";
+        foreach($notes as $note){
+            $msg .= $sep;
+            $msg .= $note;
+        }
+        
+        $sujet = "Re:".$this->getData("subj");
+        $to = $this->getData("src");
+        $from = $this->getData("dst");
+        
+        $success .= "<br/>to:".$to."<br/>from:".$from."<br/>sujet:".$sujet."<br/>msg : ".$msg;
+//        if(!mailSyn2($sujet, $to, $from, $msg))
+        $msg .= "Destinataire tronqué ".$to."<br/>";
+        if(!mailSyn2($sujet, "tommy@bimp.fr", $from, $msg))
+                $errors[] = "Envoie email impossible";
+        else{
+            $this->addNote($data['txt2'], 4);
+        }
+        
+        return array(
+            'errors'   => $errors,
+            'warnings' => $warnings
+        );
+    }
 
     
     public function getInfosExtraBtn()
     {
         global $langs;
         $buttons = array();
+        
+                $buttons[] = array(
+                    'label'   => 'Répondre par mail',
+                    'icon'    => 'send',
+                    'onclick' => $this->getJsActionOnclick('sendMail', array(), array('form_name' => 'newMail'))
+                );
        
         return $buttons;
     }

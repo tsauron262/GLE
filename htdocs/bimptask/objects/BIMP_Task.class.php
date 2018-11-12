3cb7e0c4406114ecd0db6b4238050717e7361edd
tommy <tommy@192.168.1.104>
2018-11-12 21:25:16 +0100

    bimpcache petit bug
    Former-commit-id: b90f09d5a38d794d241be495805a2cbb6733b9dc

--- htdocs/bimptask/objects/BIMP_Task.class.php
+++ htdocs/bimptask/objects/BIMP_Task.class.php
@@ -165,15 +165,13 @@ class BIMP_Task extends BimpObject
 
     public function actionSendMail($data, &$success)
     {
-        $success = "impec";
+        $success = "Message envoyé";
         $errors = $warnings = array();
         $sep = "<br/>---------------------<br/>";
         $idTask = "IDTASK:5467856456" . $this->getData("id");
         $data['email'] = str_replace("<br>", "<br/>", $data['email']);
 
 
-//        $notes = $this->getChildrenObjects("bimp_note");
-        $notes = array("msg1", "msg2", "msg3");
         $notes = $this->getNotes();
 
         $msg = $data['email'];
@@ -184,12 +182,8 @@ class BIMP_Task extends BimpObject
         if ($data['include_file']) {
             $msg .= "<br/>Fil de discution :";
             foreach ($notes as $note) {
-//                $msg .= get_class($note);
-//                if (is_a($note, "BimpNote")) {
-//                    $msg .= print_r($note);
                     $msg .= $sep;
                     $msg .= $note->getData("content");
-//                }
             }
 
             $msg .= $sep . "Message original :";
@@ -206,13 +200,10 @@ class BIMP_Task extends BimpObject
 //        $msg = str_replace("<br/>", "\n", $msg);
         
         
-        $success .= "<br/>dest:" . $to . "<br/>from:" . $from . "<br/>sujet:" . $sujet . "<br/>msg : " . $msg;
         if(!mailSyn2($sujet, $to, $from, $msg))
-//        $msg .= "Destinataire tronqué " . $to . " remplcé par tommy et peter<br/>";
-//        if(!mailSyn2($sujet, "tommy@bimp.fr, peter@bimp.fr", $from, $msg))
                 $errors[] = "Envoie email impossible";
         else{
-            $this->addNote($data['email'], 4);
+            $this->addNote($data['email'], 4, 1);
         }
 
         return array(

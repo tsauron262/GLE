033eeb3eb13b69449cd224337e2abf67750af00f
flo <flo@flo-micro>
2018-11-20 17:23:34 +0100

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/Bimp_Lib.php
    	htdocs/bimptask/task.php
    Former-commit-id: 045411c2de2c4c3f7d3dc02a083077db6bb20a90

--- htdocs/bimptask/task.php
+++ htdocs/bimptask/task.php
@@ -8,42 +8,42 @@ function runBimpTask()
 
     $task = BimpObject::getInstance('bimptask', 'BIMP_Task');
 
-    $tasks = $task->getList(array('id_user_owner' => (int) $user->id, 'status' => array(
-                    'operator' => '<',
-                    'value'    => 4
-                )));
-    $i=0;
+    $tasks = $task->getList(array('id_user_owner' => (int) $user->id, 'status'        => array(
+            'operator' => '<',
+            'value'    => 4
+    )));
+    $i = 0;
     BIMP_Task::$nbNonLu = 0;
     BIMP_Task::$nbAlert = 0;
-    foreach($tasks as $taskData){
+    foreach ($tasks as $taskData) {
         $task->fetch($taskData["id"]);
-        if($task->canView()){
+        if ($task->canView()) {
             $content .= $task->renderLight();
             $content .= "<br/>";
             $i++;
         }
-    } 
+    }
     $class = array();
-    if(BIMP_Task::$nbAlert > 0){
+    if (BIMP_Task::$nbAlert > 0) {
         $class[] = 'clignote';
         $alert = true;
     }
-    
-    $nonLu1 = BIMP_Task::$nbNonLu;
-    
 
-    
-    
-    
-    
+    $nonLu1 = BIMP_Task::$nbNonLu;
+
+
+
+
+
+
 
     $content2 = "";
-    $tasks = $task->getList(array('id_user_owner' => 0, 
-        'status' => array(
-                    'operator' => '<',
-                    'value'    => 4
-                )));
-    
+    $tasks = $task->getList(array('id_user_owner' => 0,
+        'status'        => array(
+            'operator' => '<',
+            'value'    => 4
+    )));
+
     $i2 = 0;
     BIMP_Task::$nbNonLu = 0;
     BIMP_Task::$nbAlert = 0;
@@ -55,30 +55,29 @@ function runBimpTask()
             $i2++;
         }
     }
-    
+
     $class2 = array();
-    if(BIMP_Task::$nbAlert > 0){
+    if (BIMP_Task::$nbAlert > 0) {
         $class2[] = 'clignote';
         $alert = true;
     }
-    
-    if($alert){
+
+    if ($alert) {
         $contentT .= "<script>playAlert();</script>";
-    }
-    else{
+    } else {
         $contentT .= "<script>stopAlert();</script>";
     }
-    
-    if($i2>0)
+
+    if ($i2 > 0)
         $content2 .= $contentT;
     else
         $content .= $contentT;
     $nonLu2 = BIMP_Task::$nbNonLu;
-    
-    
+
+
     if ($i > 0)
-        $bimp_fixe_tabs->addTab("mYtask", "<span class='".implode(" ", $class)."' >".$i . " tache(s) en attente" .($nonLu1 > 0 ? " <span class='red'>".$nonLu1." message non lu.</span>" : "")."</span>", $content);
-    
+        $bimp_fixe_tabs->addTab("mYtask", "<span class='" . implode(" ", $class) . "' >" . $i . " tâche(s) en attente" . ($nonLu1 > 0 ? " <span class='red'>" . $nonLu1 . " message" . ($nonLu1 > 1 ? 's' : '') . " non lu" . ($nonLu1 > 1 ? 's' : '') . ".</span>" : "") . "</span>", $content);
+
     if ($i2 > 0)
-        $bimp_fixe_tabs->addTab("taskAPersonne", "<span class='".implode(" ", $class2)."' >".$i2 . " tache(s) non attribué" .($nonLu2 > 0 ? " <span class='red'>".$nonLu2." message non lu.</span>" : "")."</span>", $content2);
+        $bimp_fixe_tabs->addTab("taskAPersonne", "<span class='" . implode(" ", $class2) . "' >" . $i2 . " tâche" . ($i2 > 1 ? 's' : '') . " non attribuée" . ($i2 > 1 ? 's' : '') . ($nonLu2 > 0 ? " <span class='red'>" . $nonLu2 . " message" . ($nonLu2 > 1 ? 's' : '') . " non lu" . ($nonLu2 > 1 ? 's' : '') . ".</span>" : "") . "</span>", $content2);
 }

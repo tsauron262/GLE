2749c5a7f744d3718f3c9fa99e4c152e9ffe539d
tommy <tommy@192.168.1.104>
2018-12-20 16:17:19 +0100

    aj log
    Former-commit-id: c86e61d5a7da05ba54e23c632faff7ccec02079f

--- htdocs/bimpsupport/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/objects/BS_SAV.class.php
@@ -1457,7 +1457,7 @@ class BS_SAV extends BimpObject
 
         $product = BimpObject::getInstance('bimpcore', 'Bimp_Product');
         foreach ($this->getChildrenObjects('propal_lines', array(
-            'type'               => BS_SavPropalLine::LINE_PRODUCT,
+            //'type'               => BS_SavPropalLine::LINE_PRODUCT,
             'linked_id_object'   => 0,
             'linked_object_name' => ''
         )) as $line) {

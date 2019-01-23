4f1c9b23c42ee88bb631d5ec949e420bc43bbd08
tommy <tommy@192.168.1.104>
2019-01-23 01:55:40 +0100

    pas de paiement exeptionelle quand production
    Former-commit-id: a359dcd430d9f67db3c993b5682000a461c0ed6d

--- htdocs/bimpfinancement/objects/BF_Demande.class.php
+++ htdocs/bimpfinancement/objects/BF_Demande.class.php
@@ -369,6 +369,19 @@ class BF_Demande extends BimpObject
         if($marge < -1)
             $this->warningsMsg['marge-'] = "Attention la marge est négative : ".$marge;
         
+        $asso = new BimpAssociation($this, 'factures');
+        $tot = 0;
+        if(count($asso->getAssociatesList()) > 0){
+            foreach ($asso->getAssociatesList() as $id_facture) {
+                $facture = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Facture', (int) $id_facture);
+                if (BimpObject::objectLoaded($facture)) {
+                    $tot += $facture->getData('total');
+                }
+            }
+            $diference =  ($this->getTotalFraisDiv()+$this->getTotalLoyerInter()) - $tot;
+            if($diference > 0.1 || $diference < -0.1)
+                $this->warningsMsg['fraisdivdif'] = "Attention les factures de frais divers + loyer intercalaires ne correspond pas diférence de : ".price($diference);
+        }
         
         
         

42d2c43af5e82574e93ec345d9bafae94249d9b4
Alexis BERNARD <al.bernard@bimp.fr>
2019-01-30 16:39:59 +0100

    Add FraisInvite bimpnotesfrais
    Former-commit-id: bbd5b3ba265072b7a615f1abd09d6cedb6db1a96

---
+++ htdocs/bimpnotesfrais/objects/BNF_FraisInvite.class.php
@@ -0,0 +1,39 @@
+<?php 
+
+	class BNF_FraisInvite extends BimpObject {
+
+		public static $typePersonne = array(
+			1 => array('label' => 'EXTERNE', 'classes' => array('danger'), 'icon' => ''),
+			2 => array('label' => 'INTERNE', 'classes' => array('warning'), 'icon' => ''),
+		);
+
+		public function showInvite() {
+			global $db;
+			$BimpDb = new BimpDb($db);
+			if(!$this->getData('name')) {
+			 	$lastname = $BimpDb->getValue('user', 'lastname', '`rowid` = ' . $this->getData('id_user'));
+			 	$firstname = $BimpDb->getValue('user', 'firstname', '`rowid` = ' . $this->getData('id_user'));
+			 	return $lastname . " " . $firstname;
+			 } else {
+			 	return $this->getData('name');
+			}
+		}
+
+		public function getListeMontant() {
+			global $db;
+			$return = array();
+			$BimpDb = new BimpDb($db);
+			$id_frais = $this->getData('id_frais');
+			$result = $BimpDb->getRows('bnf_frais_montant', '`id_frais` = ' . $id_frais);
+			foreach ($result as $amount) {
+				$return[$amount->id] = array('label' => "Frais <b>#" . $amount->id . ' d\'un montant de ' . $amount->amount . '€', 'classes' => array(''));
+			}
+			return $return;
+		}
+
+		public function showMontant() {
+			return '<b>#' . $this->getData('id_montant') . "</b>";
+		}
+
+
+	}

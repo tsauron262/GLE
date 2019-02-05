546438cd09ae519fdd03e27d42fa40b5dcff3bdb
tommy <tommy@192.168.1.104>
2019-02-05 16:32:51 +0100

    mail au comm quand comm valide
    Former-commit-id: 8f8af3f2f7922329c5393441ad0a6d6363d53d0a

---
+++ htdocs/synopsisficheinter/tabs/client.php
@@ -0,0 +1,22 @@
+<?php
+require_once("../../main.inc.php");
+require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
+
+llxHeader($js, "Intervnetion en rapport avec le client");
+
+$id = GETPOST("id");
+    $object = new Societe($db);
+    $object->fetch($id);
+    $head = societe_prepare_head($object);
+
+dol_fiche_head($head, 'inter', $langs->trans("Interventions"), 0, 'company');
+
+$fi = new Synopsisfichinter($db);
+$sql = $db->query("SELECT * FROM `llx_fichinter` WHERE `fk_soc` = ".$id);
+while($ln = $db->fetch_object($sql)){
+    $fi->fetch($ln->rowid);
+    echo $fi->getNomUrl(1);
+    echo "<br/><br/>";
+}
+
+llxFooter();
---
+++ htdocs/synopsisficheinter/tabs/order.php
@@ -0,0 +1,22 @@
+<?php
+require_once("../../main.inc.php");
+
+llxHeader($js, "Intervnetion en rapport avec la commande");
+
+$id = GETPOST("id");
+require_once(DOL_DOCUMENT_ROOT . '/commande/class/commande.class.php');
+require_once(DOL_DOCUMENT_ROOT . "/core/lib/order.lib.php");
+$commande = new Commande($db);
+$commande->fetch($id);
+$head = commande_prepare_head($commande);
+dol_fiche_head($head, 'inter', $langs->trans("Interventions"), 0, 'order');
+
+$fi = new Synopsisfichinter($db);
+$sql = $db->query("SELECT * FROM `llx_synopsisfichinter` WHERE `fk_commande` = ".$id);
+while($ln = $db->fetch_object($sql)){
+    $fi->fetch($ln->rowid);
+    echo $fi->getNomUrl(1);
+    echo "<br/><br/>";
+}
+
+llxFooter();

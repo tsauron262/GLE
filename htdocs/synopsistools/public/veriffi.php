2d38a9fd2f8db7996f99f50d8b7f393f5794d0b1
tommy <tommy@192.168.1.104>
2019-02-06 13:24:57 +0100

    fi page d'info orpheline pour mail
    Former-commit-id: 03ace7c548f319f609ecb0fc83e00bca6dac6e69

--- htdocs/synopsistools/public/veriffi.php
+++ htdocs/synopsistools/public/veriffi.php
@@ -8,7 +8,7 @@ if (!defined('NOREQUIREHTML'))
     define('NOREQUIREHTML', '1'); // If we don't need to load the html.form.class.php
 if (!defined('NOREQUIREAJAX'))
     define('NOREQUIREAJAX', '1');
-define("NOLOGIN", 1);  // This means this output page does not require to be logged.
+//define("NOLOGIN", 1);  // This means this output page does not require to be logged.
 define("NOCSRFCHECK", 1); // We accept to go on this page from external web site.
 
 require_once('../../main.inc.php');
@@ -23,6 +23,24 @@ require_once('../../main.inc.php');
 
 $soc = new Societe($db);
 $sql = $db->query("SELECT * FROM `". MAIN_DB_PREFIX ."fichinter` f LEFT JOIN ". MAIN_DB_PREFIX ."synopsisfichinter sf ON  f.rowid = sf.rowid  WHERE (f.fk_contrat < 1 || fk_contrat IS NULL) AND (fk_commande < 1 || fk_commande IS NULL) AND datec > '2018-01-01 00:00:00'");
+while($ln = $db->fetch_object($sql)){
+    echo "<br><br>".$ln->ref." ";
+    if($ln->fk_soc){
+        $soc->fetch($ln->fk_soc);
+        if($soc->id > 0){
+            $comms = $soc->getSalesRepresentatives($user);
+            foreach($comms as $comm){
+                echo " ".print_r($comm['email'],1);
+            }
+        }
+    }
+}
+
+
+
+echo '<br/><br/>Non validée<br/><br/>';
+
+$sql = $db->query("SELECT * FROM `". MAIN_DB_PREFIX ."fichinter` f WHERE fk_statut = 0 AND datec > '2018-01-01 00:00:00'");
 while($ln = $db->fetch_object($sql)){
     echo "<br><br>".$ln->ref." ";
     if($ln->fk_soc){

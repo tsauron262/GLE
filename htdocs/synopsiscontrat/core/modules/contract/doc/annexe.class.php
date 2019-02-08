d2b50d7e5e2420e9838c6961a0ac0b24573c885a
Alexis BERNARD <al.bernard@bimp.fr>
2019-02-08 15:42:47 +0100

    Modif justifier annexe
    Former-commit-id: d099f59f4770c7b6c50035c079347a24302d9af5

--- htdocs/synopsiscontrat/core/modules/contract/doc/annexe.class.php
+++ htdocs/synopsiscontrat/core/modules/contract/doc/annexe.class.php
@@ -120,9 +120,9 @@ class annexe {
                 $this->pdf->SetY($this->model->marge_haute + $this->model->hauteurHeader);
             }
 
-            $this->pdf->SetFont('', $style, 7); // 8
-            if(strlen($ligne) > 1)
-                $this->pdf->multicell(180, 5, utf8_encodeRien(utf8_encodeRien($ligne)), 0, 'L');
+            $this->pdf->SetFont('', $style, 6); // 8
+            if(strlen($ligne) > 1) 
+                $this->pdf->multicell($this->model->page_largeur - $this->model->marge_droite - ($this->model->marge_gauche), 5, utf8_encodeRien(utf8_encodeRien($ligne."\n")), 0, (empty($style) ? 'J' : 'L'));
         }
     }
 

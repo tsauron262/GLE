439cc729d170f8748ccb479d68dee32acacc461d
tommy <tommy@192.168.1.104>
2019-02-12 12:45:58 +0100

    pdf line piece add force_qty_1
    Former-commit-id: 370a93a90ef7a4f0296b2166b383ec5c1192675f

---
+++ htdocs/bimpcore/updates/tommy/1.2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `llx_bimp_commande_line` ADD `force_qty_1` BOOLEAN NOT NULL;
+ALTER TABLE `llx_bs_sav_propal_line` ADD `force_qty_1` BOOLEAN NOT NULL;
+ALTER TABLE `llx_bimp_propal_line` ADD `force_qty_1` BOOLEAN NOT NULL;

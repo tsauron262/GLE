ad5dc7c56d50282e5d61d4c56448e83526f32904
tommy <tommy@192.168.1.104>
2019-02-13 11:04:20 +0100

    pdf line piece add force_qty_1
    Former-commit-id: 14ab8ec1907d90e1159fe1ba6f890a6d267693c3

--- htdocs/bimpstatsfacture/core/modules/modBimpStatsFacture.class.php
+++ htdocs/bimpstatsfacture/core/modules/modBimpStatsFacture.class.php
@@ -169,7 +169,7 @@ class modBimpStatsFacture extends DolibarrModules {
         $r = 0;
         $this->rights[$r][0] = $this->numero + $r; // Permission id (must not be already used)
         $this->rights[$r][1] = 'Acces au module'; // Permission label
-        $this->rights[$r][3] = 0;      // Permission by default for new user (0/1)
+        $this->rights[$r][3] = 1;      // Permission by default for new user (0/1)
         $this->rights[$r][4] = 'read';    // In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
         $this->rights[$r][5] = '';        // In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
         $r++;

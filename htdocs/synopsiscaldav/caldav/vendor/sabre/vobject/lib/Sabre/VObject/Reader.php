44e6d52a38775af9445bd195c1a5191ff993638c
tommy <tommy@192.168.1.104>
2019-02-14 19:01:23 +0100

    Merge origin/master
    Conflicts:
    	htdocs/bimpsupport/objects/BS_SAV.class.php
    Former-commit-id: 96845f62bf75b7fa660bb3a90994bc81c89ad144

--- htdocs/synopsiscaldav/caldav/vendor/sabre/vobject/lib/Sabre/VObject/Reader.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/vobject/lib/Sabre/VObject/Reader.php
@@ -140,8 +140,8 @@ class Reader {
             if ($options & self::OPTION_IGNORE_INVALID_LINES) {
                 return null;
             } else {
-                global $dataOrig;
-                dol_syslog("Probléme de parsage caldav ".print_r($lines,1)." ligne original ". $dataOrig, 3);
+                global $dataOrig, $dataOrig2;
+                dol_syslog("Probléme de parsage caldav ".print_r($lines,1)." ligne original ". $dataOrig." apres modifs ". $dataOrig2, 3);
                 throw new ParseException('Invalid VObject, line ' . ($lineNr+1) . ' did not follow the icalendar/vcard format');
             }
         }

01a65c400e6d9705318f65b522c8a6b3007571e3
tommy <tommy@192.168.1.104>
2019-03-11 13:26:41 +0100

    grosse modifs des updates viré tout table bimpcore
    Former-commit-id: 85e4a9cf448691132f898081801974aa27df9962

--- htdocs/bimptask/core/modules/modBimptask.class.php
+++ htdocs/bimptask/core/modules/modBimptask.class.php
@@ -301,6 +301,26 @@ class modBimptask extends DolibarrModules {
      */
     public function init($options = '') {
         $sql = array();
+        
+        
+        
+        $sql[] = 'CREATE TABLE `llx_bimp_task` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `src` varchar(127) NOT NULL,
+  `dst` varchar(127) NOT NULL,
+  `subj` varchar(254) NOT NULL,
+  `txt` text NOT NULL,
+  `comment` text DEFAULT NULL,
+  `id_user_owner` int(11) DEFAULT NULL,
+  `user_create` int(11) NOT NULL,
+  `user_update` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT 0,
+  `prio` int(11) NOT NULL,
+  `date_create` datetime NOT NULL DEFAULT current_timestamp,
+  `date_update` datetime NOT NULL,
+  `prio` int(11) NOT NULL,
+  test_ferme text
+);';
 
         // Add restrictions to all categories son of root
 //        initRestrictions();

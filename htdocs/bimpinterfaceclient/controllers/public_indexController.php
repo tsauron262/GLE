3d3a152aa24878cc651805b7c3498d2cd9b15d68
Alexis BERNARD <al.bernard@bimp.fr>
2019-04-03 17:40:23 +0200

    Begin indexController
    Former-commit-id: 7c37967c5db84a7395d6bdd2e92dad6a4e5e2d65

--- htdocs/bimpinterfaceclient/controllers/public_indexController.php
+++ htdocs/bimpinterfaceclient/controllers/public_indexController.php
@@ -2,12 +2,16 @@
 
 class public_indexController extends BimpController {
     public function renderHtml() {
-        global $langs;
+        global $langs, $userClient;
+        
+     $valide_contrat = $userClient->getContratVisible(true);
+     $all_contrat = $userClient->getContratVisible();
      $html = "";
      
      $html .= '<div class="row"><div class="col-md-12"><div class="header"><h3 class="title" data-color="bimp">'.$langs->trans('bonjour').'</h3></div>';
      $html .= '<div class="content" ><div class="col-md-12">';
      
+     $html .= '</div></div>';
      $html .= '</div></div>';
       
       return $html;

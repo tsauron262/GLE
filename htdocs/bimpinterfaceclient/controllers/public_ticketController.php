38592e1e3089b89bd84ef609cf499862eda2474d
tommy <tommy@192.168.1.104>
2019-04-04 15:27:55 +0200

    ticket controller filtre soc
    Former-commit-id: 7b01affe74f6d8f9446dce581c79d9e985c38e50

--- htdocs/bimpinterfaceclient/controllers/public_ticketController.php
+++ htdocs/bimpinterfaceclient/controllers/public_ticketController.php
@@ -2,10 +2,10 @@
 
 class public_ticketController extends BimpController {
     public function renderHtml() {
-        global $connected_client;
+        global $userClient;
         $tickets = BimpObject::getInstance('bimpinterfaceclient', 'BIC_UserTickets');
         $list = new BC_ListTable($tickets, 'interface_client', 1, null, 'Tickets', 'wrench');
-        $list->addFieldFilterValue('id_client', (int) $connected_client);
+        $list->addFieldFilterValue('id_client', (int) $userClient->getData("attached_societe"));
         return $list->renderHtml();
     }
 }

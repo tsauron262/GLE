57242c4bb35f64d734e07056ed6bdf24b5ef2038
tommy <tommy@192.168.1.104>
2019-04-11 17:20:17 +0200

    sav timoté
    Former-commit-id: fd78abd0639c46568cadf8246e0652f7fb5d8cf2

--- htdocs/bimpsupport/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/objects/BS_SAV.class.php
@@ -741,7 +741,7 @@ class BS_SAV extends BimpObject
         }
 
         global $user;
-        if (($user->admin || $user->id == 60) && BimpObject::objectLoaded($propal)) {
+        if (($user->admin || $user->id == 60 || $user->id == 282) && BimpObject::objectLoaded($propal)) {
             $propal->module = 'bimpcommercial';
             $buttons[] = array(
                 'label'   => 'Fiche Propale ' . $propal->id,

649e6010e1cad32071622717d145d2205627659a
Alexis BERNARD <al.bernard@bimp.fr>
2019-04-12 14:45:33 +0200

    accessforbiden() quand module pas installer
    Former-commit-id: ae0ec98a7c9fab4598dd76575bbaaba27b4acf1a

--- htdocs/bimpinterfaceclient/index.php
+++ htdocs/bimpinterfaceclient/index.php
@@ -7,7 +7,9 @@ require_once '../bimpcore/main.php';
 
 require_once DOL_DOCUMENT_ROOT.'/bimpcore/Bimp_Lib.php';
 
-
+if(!BimpCore::getConf('module_version_bimpinterfaceclient')) {
+    accessforbidden();
+}
 
 if(BimpTools::getContext() == 'public' || $user->id < 1)
     require 'client.php';

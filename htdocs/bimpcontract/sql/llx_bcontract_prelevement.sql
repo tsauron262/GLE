3005fb6e5e1e7a152aedec78ab4161284256f752
Alexis BERNARD <al.bernard@bimp.fr>
2019-04-18 15:02:40 +0200

    prelevement
    Former-commit-id: 5b903795005564176807f776a6421ab8342ed2c3

---
+++ htdocs/bimpcontract/sql/llx_bcontract_prelevement.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `llx_bcontract_prelevement` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id_contrat` int(11) NOT NULL,
+  `next_facture_date` datetime NOT NULL,
+  `next_facture_amount` double NOT NULL,
+  `validate` int(1) DEFAULT NULL,
+  `user_create` int(10) UNSIGNED NOT NULL DEFAULT "0",
+  `date_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_update` int(10) UNSIGNED NOT NULL DEFAULT "0",
+  `date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;

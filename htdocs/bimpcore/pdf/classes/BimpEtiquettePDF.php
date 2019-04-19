bf78d89408344d18620c316d8f722c844f713219
tommy <tommy@192.168.1.104>
2019-04-19 16:15:19 +0200

    etiquette
    Former-commit-id: 29df2c0f02e0b17359c894f57066f59992cec61a

---
+++ htdocs/bimpcore/pdf/classes/BimpEtiquettePDF.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/BimpModelPDF.php';
+
+class BimpEtiquettePDF extends BimpModelPDF {
+
+    public function __construct($db, $orientation = 'L', $format = array(89, 36)) {
+        parent::__construct($db, $orientation, $format);
+        $this->pdf->addCgvPages = false;
+        $this->pdf->headerMargin = 2;
+        $this->pdf->topMargin = 2;
+        $this->pdf->sideMargin = 6;
+        $this->pdf->footerMargin = 1;
+    }
+
+    
+}

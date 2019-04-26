15d150f1952d8cea5018969ff937917b0c6a57d5
flo <flo@mbp-de-florian>
2019-04-26 15:20:28 +0200

    Maj
    Former-commit-id: 83c80d4ee8fbb42849a862c3be0ec1abe86f44a4

--- htdocs/bimpcore/classes/BimpObject.php
+++ htdocs/bimpcore/classes/BimpObject.php
@@ -4494,6 +4494,11 @@ class BimpObject extends BimpCache
         }
         return BimpRender::renderAlerts('Erreur de configuration: objet "' . $object_name . '" non défini');
     }
+    
+    public function renderHeaderBtnRedir()
+    {
+        return $this->processRedirect(false);
+    }
 
     // Générations javascript: 
 
@@ -5543,11 +5548,6 @@ class BimpObject extends BimpCache
         }
         if (isset($user->array_options["options_defaultentrepot"]))
             return $user->array_options["options_defaultentrepot"];
-    }
-
-    public function renderHeaderBtnRedir()
-    {
-        return $this->processRedirect(false);
     }
 
     public function processRedirect($newVersion = true)

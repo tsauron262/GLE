073d4f15e3d2773940fae1af62f54ca774591784
tommy <tommy@192.168.1.104>
2019-04-26 15:18:43 +0200

    chois jour de congé
    Former-commit-id: af8ad5be00bcabd83f7c84be35a26b972367fd9e

--- htdocs/bimpticketrestaurant/class/BimpTicketRestaurant.class.php
+++ htdocs/bimpticketrestaurant/class/BimpTicketRestaurant.class.php
@@ -11,33 +11,47 @@ class BimpTicketRestaurant {
         $this->db = $db;
     }
 
-    public function getTicket() {
+    public function getTicket($id_user) {
 
         // Now
         $row_date = new DateTime('now');
         $month = $row_date->format('m');
         $year = $row_date->format('Y');
+        
 
-        $year -= 1; // TODO remove
+//        $year -= 1; // TODO remove
         // Get date
         try {
-            $date_work_plus_1 = new DateTime($year . '-' . ((int) $month + 2));
-            $date_work = new DateTime($year . '-' . ((int) $month + 1));
-            $date_holiday = new DateTime($year . '-' . (int) $month);
-//            $date_expense_report = new DateTime($year . '-' . ((int) $month -1));
+            $date_debut = new DateTime($year . '-' . ((int) $month-1). '-01');
+            $nb_day_of_month = cal_days_in_month(CAL_GREGORIAN, $date_debut->format('m'), $date_debut->format('Y'));
+            $date_fin = new DateTime($year . '-' . ((int) $month-1). '-01');
+            $date_fin ->add(new DateInterval('P1M'));
+            $date_fin ->sub(new DateInterval('P1D'));
         } catch (Exception $e) {
             $this->errors[] = $e->getMessage();
             return -1;
         }
 
-        $days_worked = num_open_day($date_work->getTimestamp(), $date_work_plus_1->getTimestamp());
 
-        $holidays = $this->getHolidays($date_holiday);
+        $days_worked = num_open_dayUser($id_user, $date_debut->getTimestamp(), $date_fin->getTimestamp(),0,1);
 
-        echo '<br/>Entre le ' . $date_work->format('d/m/Y') . ' et le ' . $date_work_plus_1->format('d/m/Y') . ' il y a ' . $days_worked . ' jour de travail';
+        $holidays = getNbHolidays($date_debut, $date_fin, $id_user);
 
-        echo '<pre>';
-        print_r($holidays);
+        
+        $nbTicket = $days_worked - $holidays - $nbNotefrais;
+        
+        if(1){//debug
+            echo '<br/>Entre le ' . $date_debut->format('d/m/Y') . ' et le ' . $date_fin->format('d/m/Y') . ' il y a :';
+            echo '<br/>' . $days_worked . ' jour de travail';
+            echo '<br/>' . $holidays . ' congé';
+            echo '<br/>' . $nbNotefrais . ' note de frais';
+
+
+            echo '<br/>Soit ' . $nbTicket . ' tickets restaurant';
+        }
+        
+//        echo '<pre>';
+//        print_r($holidays);
 
 //        echo $date_target->getTimestamp();
 //        echo '<br/>$date_work_plus_1' . $date_work_plus_1->format('d/m/Y');
@@ -46,68 +60,10 @@ class BimpTicketRestaurant {
 //        die('ok');
         // TODO create csv
         // TODO send email
+        
+        return $nbTicket;
     }
 
-    private function getHolidays($date_month) {
-
-        $holidays = array();
-        $month_to_check = (int) $date_month->format('m');
-
-        $nb_day_of_month = cal_days_in_month(CAL_GREGORIAN, $date_month->format('m'), $date_month->format('Y'));
-
-
-        $sql = 'SELECT fk_user, date_debut, date_fin, type_conges';
-        $sql .= ' FROM ' . MAIN_DB_PREFIX . 'holiday';
-        $sql .= ' WHERE statut=6';
-        $sql .= ' AND  (';
-        $sql .= '("' . $date_month->format('Y-m-1') . '" BETWEEN date_debut AND date_fin)';
-        $sql .= ' OR ("' . $date_month->format('Y-m-' . $nb_day_of_month) . '" BETWEEN date_debut AND date_fin)';
-        $sql .= ')';
-
-
-        // Retrieve holy days
-        $result = $this->db->query($sql);
-        while ($obj = $this->db->fetch_object($result)) {
-            $holidays[] = $obj;
-//            if (isset($holidays[$obj->fk_user]))
-//                $holidays[$obj->fk_user] = $obj;
-//            else
-//                $holidays[$obj->fk_user] = array($obj);
-        }
-
-
-        foreach ($holidays as $i => $h) {
-            $date_start = new DateTime($h->date_debut);
-            $date_end = new DateTime($h->date_fin);
-
-            $h_month_start = (int) $date_start->format('m');
-            $h_month_end = (int) $date_end->format('m');
-
-            // Start and end are in the same month
-            if ($h_month_start == $h_month_end) {
-                $holidays[$i]->ticket_to_remove = num_open_day($date_start->getTimestamp(), $date_end->getTimestamp());
-                $holidays[$i]->id = 1;
-            }
-            // Overlap with previous month
-            elseif ($h_month_start < $month_to_check and ( $h_month_start + 1) == $month_to_check) {
-                $holidays[$i]->ticket_to_remove = num_open_day($date_month->getTimestamp(), $date_end->getTimestamp());
-                $holidays[$i]->id = 2;
-            }
-            // Overlap with next month
-            elseif ($month_to_check < $h_month_end and ( $month_to_check + 1) == $h_month_end) {
-                $date_end_month = new DateTime($date_start->format('Y-m-' . $nb_day_of_month));
-                $holidays[$i]->ticket_to_remove = num_open_day($date_start->getTimestamp(), $date_end_month->getTimestamp());
-                $holidays[$i]->id = 3;
-            }
-            // start and end in other month
-            else {
-                $date_end_month = new DateTime($date_start->format('Y-m-' . $nb_day_of_month));
-                $holidays[$i]->ticket_to_remove = num_open_day($date_month->getTimestamp(), $date_end_month->getTimestamp());
-                $holidays[$i]->id = 4;
-            }
-        }
-
-        return $holidays;
-    }
+    
 
 }

a9a47db0e2445b30ef34294108102f18ce46ed68
tommy <tommy@192.168.1.104>
2019-05-03 15:23:02 +0200

    filtre soc for commandes
    Former-commit-id: 1fae37a98c74cc4ff9ad5e0b1ffee056ef56e74e

---
+++ htdocs/bimpcommercial/controllers/commandesController.php
@@ -0,0 +1,50 @@
+<?php
+
+class commandesController extends BimpController
+{
+    var $socid = "";
+
+    public function displayHead()
+    {
+        global $db, $langs, $user;
+        
+        if(BimpTools::getValue("socid") > 0){
+            $this->socid = BimpTools::getValue("socid");
+            require_once DOL_DOCUMENT_ROOT . '/core/lib/company.lib.php';
+            $soc = new Societe($db);
+            $soc->fetch($this->socid);
+            $head = societe_prepare_head($soc);
+            dol_fiche_head($head, 'bimpcomm', '');
+            
+            $linkback = '<a href="' . DOL_URL_ROOT . '/societe/list.php?restore_lastsearch_values=1">' . $langs->trans("BackToList") . '</a>';
+
+            dol_banner_tab($soc, 'id', $linkback, ($user->societe_id ? 0 : 1), 'rowid', 'nom', '', '&fc=commandes');
+        }
+    }
+
+    public function renderHtml()
+    {
+        $list = 'default';
+        $titre = 'Commandes';
+        if($this->socid){
+            $societe = BimpCache::getBimpObjectInstance('bimpcore', 'Bimp_Client', $this->socid);
+
+            if (!BimpObject::objectLoaded($societe)) {
+                return BimpRender::renderAlerts('ID du client invalide');
+            }
+            $list = 'client';
+            $titre .= ' du client "' . $societe->getData('code_client') . ' - ' . $societe->getData('nom');
+        }
+
+        $propal = BimpObject::getInstance('bimpcommercial', 'Bimp_Commande');
+
+        $list = new BC_ListTable($propal, $list, 1, null, $titre);
+        
+        
+        if($this->socid){
+            $list->addFieldFilterValue('fk_soc', (int) $societe->id);
+            $list->params['add_form_values']['fields']['fk_soc'] = (int) $societe->id;
+        }
+            return $list->renderHtml();
+    }
+}

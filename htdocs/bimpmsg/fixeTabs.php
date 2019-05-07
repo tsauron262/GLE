b911dee7b82d28af4727b4e7c9952808127127f9
tommy <tommy@192.168.1.104>
2019-05-07 19:18:07 +0200

    messages
    Former-commit-id: e48f66a37a2f73c3e6e310aad1d8a23d63ffb980

--- htdocs/bimpmsg/fixeTabs.php
+++ htdocs/bimpmsg/fixeTabs.php
@@ -7,7 +7,7 @@ class FixeTabs_bimpmsg extends FixeTabs_module {
     function init() {
         $content = "";
         $alert = false;
-        $maxTaskView = 20;
+        $maxTaskView = 30;
         
         
         $notes = BimpObject::getInstance("bimpcore", "BimpNote");
@@ -17,31 +17,45 @@ class FixeTabs_bimpmsg extends FixeTabs_module {
         $content = "";
         $nonLu = 0;
         foreach($tabFils as $fil){
-            $i++;
-            $content .= "<br/>";
-            $content .= "<div class='lnMsg' onclick='loadModalObjectNotes($(this), \"".$fil['obj']->module."\", \"".$fil['obj']->object_name."\", \"".$fil['obj']->id."\", \"chat\", true);'>";
-            $content .= "<span class='titreFilMsg'>";
-            if(method_exists($fil['obj'], "getNomUrl"))
-                $content .= $fil['obj']->getNomUrl();
-            else
-                $content .=  "<br/>".$fil['obj']->getName();
-            if(!$fil['lu']){
-                $content .=  " (non lu)";
-                $nonLu++;
-                $notes->fetch($fil['idNoteRef']);
+            if($fil['obj']->id > 0){
+                $i++;
+                $content .= "<br/>";
+                $content .= "<div class='lnMsg' onclick='loadModalObjectNotes($(this), \"".$fil['obj']->module."\", \"".$fil['obj']->object_name."\", \"".$fil['obj']->id."\", \"chat\", true);'>";
+                $content .= "<span class='titreFilMsg'>";
+                if(method_exists($fil['obj'], "getNomUrl"))
+                    $content .= $fil['obj']->getNomUrl();
+                else
+                    $content .=  "<br/>".$fil['obj']->getName();
+
+    //                    $m = new BimpObject();
+    //            $m->getChildObject("societe")
+
+                if(!$fil['lu']){
+                    $content .=  " (non lu)";
+                    $nonLu++;
+                    $notes->fetch($fil['idNoteRef']);
+                }
+                else {
+                    global $db;
+                    $sql = $db->query('SELECT MAX(id) max FROM `llx_bimpcore_note` WHERE `obj_type` = "bimp_object" AND `obj_module` = "'.$fil['obj']->module.'" AND `obj_name` = "'.$fil['obj']->object_name.'" AND `id_obj` = '.$fil['obj']->id);
+                    $ln = $db->fetch_object($sql);
+                    $notes->fetch($ln->max);
+                }
+                $content .= "</span>";
+                if(method_exists($fil['obj'], "getChildObject")){
+                        $soc = $fil['obj']->getChildObject("societe");
+                        if(!$soc || !$soc->isLoaded()){
+                            $soc = $fil['obj']->getChildObject("client");
+                        }
+                        if($soc && $soc->isLoaded())
+                            $content .= " - ".$soc->getNomUrl(1);
+                }
+                $content .= $notes->displayChatmsg("petit", false);
+                $content .= "</div>";
+
+
+                //echo $fil['obj']->renderNotesList(true, "chat", $i);
             }
-            else {
-                global $db;
-                $sql = $db->query('SELECT MAX(id) max FROM `llx_bimpcore_note` WHERE `obj_type` = "bimp_object" AND `obj_module` = "'.$fil['obj']->module.'" AND `obj_name` = "'.$fil['obj']->object_name.'" AND `id_obj` = '.$fil['obj']->id);
-                $ln = $db->fetch_object($sql);
-                $notes->fetch($ln->max);
-            }
-            $content .= "</span>";
-            $content .= $notes->displayChatmsg("petit", false);
-            $content .= "</div>";
-            
-            
-            //echo $fil['obj']->renderNotesList(true, "chat", $i);
         }
 
         

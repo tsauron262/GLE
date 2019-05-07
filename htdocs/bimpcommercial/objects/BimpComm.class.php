9e40252908115c123b19527db919bba23b6e4767
tommy <tommy@192.168.1.104>
2019-05-07 17:42:27 +0200

    messages
    Former-commit-id: 65443f8262258a1e7a2a0673ab6c72b170813ab5

--- htdocs/bimpcommercial/objects/BimpComm.class.php
+++ htdocs/bimpcommercial/objects/BimpComm.class.php
@@ -125,6 +125,14 @@ class BimpComm extends BimpDolObject
                 ))
             );
         }
+        
+        
+        $note = BimpObject::getInstance("bimpcore", "BimpNote");
+        $buttons[] = array(
+            'label'   => 'Message logistique ',
+            'icon'    => 'far fa-paper-plane',
+            'onclick' => $note->getJsActionOnclick('repondre', array("obj_type" => "bimp_object", "obj_module" => $this->module, "obj_name" => $this->object_name, "id_obj" => $this->id, "type_dest"=>$note::BN_DEST_GROUP, "fk_group_dest"=>$note::BN_GROUPID_LOGISTIQUE, "content"=>""), array('form_name' => 'rep'))
+        );
 
         return $buttons;
     }

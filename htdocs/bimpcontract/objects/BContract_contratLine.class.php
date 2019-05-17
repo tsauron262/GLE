cebdadb2abc3a3a891c7fd4a5baa9e84d2a56ebc
Alexis BERNARD <al.bernard@bimp.fr>
2019-05-17 11:54:42 +0200

    Create lines of contrat
    Former-commit-id: dc7aa6b88bb5c47086a1e8d55dc99e1236dcf17c

--- htdocs/bimpcontract/objects/BContract_contratLine.class.php
+++ htdocs/bimpcontract/objects/BContract_contratLine.class.php
@@ -3,14 +3,20 @@
 class BContract_contratLine extends BimpObject {
     
      protected function createDolObject(&$errors) {
-         $contrat = $this->getParentInstance();
+         global $db;
          $data = $this->getDataArray();
-         if(BimpObject::objectLoaded($contrat)) {
+         $contrat = $this->getParentInstance();
+         BimpTools::loadDolClass('product');
+         BimpTools::loadDolClass('contrat');
+         $produit = new Product($db);
+         $produit->fetch($data['fk_product']);
+         
+         if(!BimpObject::objectLoaded($contrat)) {
              $errors[] = 'id du contrat ' . $contrat->id;
              return 0;
          }
-         if($contrat->dol_object->addLine() <= 0) {
-             $errors[] = BimpTools::getMsgFromArray(BimpTools::getErrorsFromDolObject($contrat->dol_object));
+         if($contrat->dol_object->addLine($data['description'], $produit->price, $data['qty'], $produit->tva_tx, 0, 0, $produit->id, 0 /* a changer */, date('Y-m-d'), date('Y-m-d')) <= 0) {
+             $errors[] = BimpTools::getMsgFromArray(BimpTools::getErrorsFromDolObject($contrat));
              return 0;
          }
          

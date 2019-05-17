55cfded0f4d510a217e09e9b294f489d34670f15
Alexis BERNARD <al.bernard@bimp.fr>
2019-05-17 14:41:09 +0200

    Init updateDolObject
    Former-commit-id: abe4c55194e5c5504f1c409184c237dc574af487

--- htdocs/bimpcontract/objects/BContract_contratLine.class.php
+++ htdocs/bimpcontract/objects/BContract_contratLine.class.php
@@ -1,42 +1,45 @@
 <?php
 
 class BContract_contratLine extends BimpObject {
+
+    protected function createDolObject(&$errors) {
+        global $db;
+        $data = $this->getDataArray();
+        $contrat = $this->getParentInstance();
+        $produit = $this->getInstance('bimpcore', 'Bimp_Product', $data['fk_product']);
+        if (!BimpObject::objectLoaded($contrat)) {
+            $errors[] = 'L\'id du contrat ' . $contrat->id . ' n\'éxiste pas';
+            return 0;
+        }
+        if ($contrat->dol_object->addLine($data['description'], $produit->getData('price'), $data['qty'], $produit->getData('tva_tx'), 0, 0, $produit->id, $data['remise_percent'], date('Y-m-d'), date('Y-m-d')) <= 0) {
+            $errors[] = BimpTools::getMsgFromArray(BimpTools::getErrorsFromDolObject($contrat));
+            return 0;
+        }
+
+        return 1;
+    }
     
-     protected function createDolObject(&$errors) {
-         global $db;
-         $data = $this->getDataArray();
-         $contrat = $this->getParentInstance();
-         BimpTools::loadDolClass('product');
-         BimpTools::loadDolClass('contrat');
-         $produit = new Product($db);
-         $produit->fetch($data['fk_product']);
-         
-         if(!BimpObject::objectLoaded($contrat)) {
-             $errors[] = 'id du contrat ' . $contrat->id;
-             return 0;
-         }
-         if($contrat->dol_object->addLine($data['description'], $produit->price, $data['qty'], $produit->tva_tx, 0, 0, $produit->id, 0 /* a changer */, date('Y-m-d'), date('Y-m-d')) <= 0) {
-             $errors[] = BimpTools::getMsgFromArray(BimpTools::getErrorsFromDolObject($contrat));
-             return 0;
-         }
-         
-         return 1;
-     }
-     
-     public function canCreate() {
-         $contrat = $this->getParentInstance();
-         if($contrat->getData('statut') > 0) {
-             return 0;
-         }
-         return 1;
-     }
-     
-     public function canDelete() {
-         return $this->canCreate();
-     }
-     
-     public function canEdit() {
-         return $this->canCreate();
-     }
-    
+    protected function updateDolObject(&$errors) {
+        $data = $this->getDataArray();
+        $contrat = $this->getParentInstance();
+        
+        return 0;
+    }
+
+    public function canCreate() {
+        $contrat = $this->getParentInstance();
+        if ($contrat->getData('statut') > 0) {
+            return 0;
+        }
+        return 1;
+    }
+
+    public function canDelete() {
+        return $this->canCreate();
+    }
+
+    public function canEdit() {
+        return $this->canCreate();
+    }
+
 }

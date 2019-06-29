ad1d6fc2e5cf072632afe5199e917c3f8677c53b
tommy <tommy@192.168.1.21>
2019-06-29 18:28:45 +0200

    Merge origin/BIMP_ERP_FLO_DEV_2 into BIMP_ERP_FLO_DEV_2
    Conflicts:
    	htdocs/synopsistools/class/importExport/importCommande.class.php
    	htdocs/synopsistools/class/importExport/importCommandeFourn.class.php
    Former-commit-id: c7970754d252defc1c353b94b78cfaad4c4a5e11

--- htdocs/bimpequipment/manageequipment/index.php
+++ htdocs/bimpequipment/manageequipment/index.php
@@ -59,40 +59,40 @@ print '<br/>';
 
 print '<br/><div id="allTheFiche" class="fadeInOut">';
 print '<div id="ph_links"></div>';
-
-print '<h4><strong>Transferts en cours</strong></h4>';
-print '<div id="alertPlaceHolder" style="clear : left"></div>';
-
-print '<div class="object_list_table">';
-print '<table id="table_transfer" class="noborder objectlistTable" style="margin-top:10px">';
-print '<thead>';
-print '<th>Ref</th>';
-print '<th>Responsable</th>';
-print '<th>Statut</th>';
-print '<th>Date d\'ouverture</th>';
-print '<th>Date de réception</th>';
-print '<th>Nombre de produit envoyés</th>';
-print '<th>Entrepot de départ</th>';
-print '<th>Lien</th>';
-print '</thead>';
-print '<tbody></tbody>';
-print '</table>';
-print '</div>';
-
-print '<h4><strong>Commandes fournisseur en attentes</strong></h4>';
-
-print '<div class="object_list_table">';
-print '<table id="table_order" class="noborder objectlistTable" style="margin-top:10px">';
-print '<thead>';
-print '<th>Fournisseur</th>';
-print '<th>Statut</th>';
-print '<th>Date d\'ouverture</th>';
-//print '<th>Nombre de produit commandé</th>';
-print '<th>Lien livraison</th>';
-print '</thead>';
-print '<tbody></tbody>';
-print '</table>';
-print '</div>';
+//
+//print '<h4><strong>Transferts en cours</strong></h4>';
+//print '<div id="alertPlaceHolder" style="clear : left"></div>';
+//
+//print '<div class="object_list_table">';
+//print '<table id="table_transfer" class="noborder objectlistTable" style="margin-top:10px">';
+//print '<thead>';
+//print '<th>Ref</th>';
+//print '<th>Responsable</th>';
+//print '<th>Statut</th>';
+//print '<th>Date d\'ouverture</th>';
+//print '<th>Date de réception</th>';
+//print '<th>Nombre de produit envoyés</th>';
+//print '<th>Entrepot de départ</th>';
+//print '<th>Lien</th>';
+//print '</thead>';
+//print '<tbody></tbody>';
+//print '</table>';
+//print '</div>';
+//
+//print '<h4><strong>Commandes fournisseur en attentes</strong></h4>';
+//
+//print '<div class="object_list_table">';
+//print '<table id="table_order" class="noborder objectlistTable" style="margin-top:10px">';
+//print '<thead>';
+//print '<th>Fournisseur</th>';
+//print '<th>Statut</th>';
+//print '<th>Date d\'ouverture</th>';
+////print '<th>Nombre de produit commandé</th>';
+//print '<th>Lien livraison</th>';
+//print '</thead>';
+//print '<tbody></tbody>';
+//print '</table>';
+//print '</div>';
 
 print '</div>';
 $db->close();

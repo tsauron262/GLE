f0a76131538ce85cdfe7f4797a3b7994f86a8d54
tommy <tommy@192.168.1.21>
2019-07-18 11:23:09 +0200

    droit facture
    Former-commit-id: 19b20d608da1413dca856c52607b01e9144e5fbc

--- htdocs/bimpcore/scripts/check_sav_avoir_factures.php
+++ htdocs/bimpcore/scripts/check_sav_avoir_factures.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../Bimp_Lib.php';
 //llxHeader();
 
 $i=0;
-$sql = $db->query("SELECT rowid FROM `llx_facture` WHERE `date_valid` > '2019-06-30 00:00:00' AND fk_user_comm = 0 AND `facnumber` LIKE 'AVS%'");
+$sql = $db->query("SELECT rowid FROM `llx_facture` WHERE `date_valid` > '2019-06-30 00:00:00' AND fk_user_comm = 0 AND `facnumber` LIKE 'AV%'");
 while($ln = $db->fetch_object($sql)){
     
     

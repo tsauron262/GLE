7ca502d34fc57f5f8e6e52d0bfac66156ccf9ef7
tommy <tommy@192.168.1.21>
2019-07-19 10:22:53 +0200

    droit facture
    Former-commit-id: 6f085501dd99185fbb1b5d2fba32d435cb737e70

--- htdocs/bimpcommercial/core/modules/modBimpCommercial.class.php
+++ htdocs/bimpcommercial/core/modules/modBimpCommercial.class.php
@@ -231,6 +231,16 @@ class modBimpCommercial extends DolibarrModules
 		//$this->rights[$r][5] = '';				    // In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
 
 		$r++;
+                
+                $this->rights[$r][0] = $this->numero + $r;	// Permission id (must not be already used)
+		$this->rights[$r][1] = 'Changer entrepot';	// Permission label
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
+		$this->rights[$r][4] = 'changeEntrepot';				// In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
+		//$this->rights[$r][5] = '';				    // In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
+
+		$r++;
+                
+                
 
 		// Main menu entries
 		$this->menu = array();			// List of menus to add

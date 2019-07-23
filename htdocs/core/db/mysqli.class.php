aa75a36e50e152078543de585d37aa44470b326c
tommy <tommy@192.168.1.21>
2019-07-23 12:10:58 +0200

    stats ligne objet
    Former-commit-id: d4c2c534959f10a9b2438a183e073e74161b12ee

--- htdocs/core/db/mysqli.class.php
+++ htdocs/core/db/mysqli.class.php
@@ -262,9 +262,9 @@ class DoliDBMysqli extends DoliDB
         
         if(defined('BDD_2_HOST') && !defined('OFF_MULTI_SQL') && BDD_2_HOST !=  $this->database_host){
             if(stripos(trim($query), "SELECT") === 0){
-                if(stripos(trim($query), "MAXnnnnn") !== false){
-                    $testPlusPetitQueDix = rand(2,12);//rand(6,15);
-                    if(1){
+                if(stripos(trim($query), "MAX") === false){
+                    $testPlusPetitQueDix = rand(5,12);//rand(6,15);
+                    if($testPlusPetitQueDix < 10){
                         global $dbRead;
                         if(!$dbRead){
                             $servRead = BDD_2_HOST;

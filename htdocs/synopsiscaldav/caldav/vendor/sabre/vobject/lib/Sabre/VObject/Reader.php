c02bc0b8a1bc2da8735492821201092d13e83bd8
tommy <tommy@192.168.1.21>
2019-07-31 10:26:58 +0200

    vv
    Former-commit-id: 7def96563ede0b52f0ee39d3d52198d4c9e3d891

--- htdocs/synopsiscaldav/caldav/vendor/sabre/vobject/lib/Sabre/VObject/Reader.php
+++ htdocs/synopsiscaldav/caldav/vendor/sabre/vobject/lib/Sabre/VObject/Reader.php
@@ -145,7 +145,7 @@ class Reader {
                     file_put_contents("/data/synchro/test.txt", $dataOrig);
                 if(!is_file("/data/synchro/test2.txt"))
                     file_put_contents("/data/synchro/test2.txt", $dataOrig2);
-                dol_syslog("Probléme de parsage caldav ".print_r($lines,1)." ligne original ". $dataOrig." apres modifs ". $dataOrig2." apres modifs ". $dataOrig3, 3);
+                dol_syslog("Probléme de parsage caldav ".print_r($lines,1)." ligne original ". $dataOrig." apres modifs ". $dataOrig2." apres modifs ". $dataOrig3, 3, 0, "_caldav");
                 throw new ParseException('Invalid VObject, line ' . ($lineNr+1) . ' did not follow the icalendar/vcard format');
             }
         }

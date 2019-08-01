f6663c4c1b96e9886f462777b658be3d0b58bc6a
tommy <tommy@192.168.1.21>
2019-08-01 17:42:52 +0200

    style
    Former-commit-id: 80bf292154578856d418d45334b8183f6daf783f

--- htdocs/bimpsupport/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/objects/BS_SAV.class.php
@@ -1891,7 +1891,7 @@ class BS_SAV extends BimpObject
                     $tabFile2[] = "application/pdf";
                     $tabFile3[] = $ref_propal . ".pdf";
                 } elseif (in_array((int) $this->getData('status'), self::$need_propal_status)) {
-                    $errors[] = 'Attention: PDF du devis non trouvé et donc non envoyé au client';
+                    $errors[] = 'Attention: PDF du devis non trouvé et donc non envoyé au client File : '.$fileProp;
                     dol_syslog('SAV "' . $this->getRef() . '" - ID ' . $this->id . ': échec envoi du devis au client '.print_r($errors,1), LOG_ERR);
                 }
             } else {

7f2ab3e1e26590d021dff339ff3bd4618dda7980
tommy <tommy@192.168.1.21>
2019-09-28 16:28:54 +0200

    new inventaire
    Former-commit-id: bbeb0b552e025c33548d8ed74f76ec519716b134

--- htdocs/bimplogistique/objects/Inventory.class.php
+++ htdocs/bimplogistique/objects/Inventory.class.php
@@ -383,7 +383,7 @@ class Inventory extends BimpDolObject
         $diff = $this->getDiffEquipment();
 
         $equipment = BimpObject::getInstance('bimpequipment', 'Equipment');
-        $list = new BC_ListTable($equipment, 'default', 1, null, 'Équipements manquants');
+        $list = new BC_ListTable($equipment, 'inventaireManquant', 1, null, 'Équipements manquants');
         if (!empty($diff['ids_manquant']))
             $list->addFieldFilterValue('id IN(' . implode(',', $diff['ids_manquant']) . ') AND 1', $filters);
         else
@@ -391,7 +391,7 @@ class Inventory extends BimpDolObject
         $html .= $list->renderHtml();
 
         $equipment = BimpObject::getInstance('bimpequipment', 'Equipment');
-        $list = new BC_ListTable($equipment, 'default', 1, null, 'Équipements en trop');
+        $list = new BC_ListTable($equipment, 'inventaire', 1, null, 'Équipements en trop');
         if (!empty($diff['ids_en_trop']))
             $list->addFieldFilterValue('id IN(' . implode(',', $diff['ids_en_trop']) . ') AND 1', $filters);
         else

d26e5ef9b649a357cbfbd1a6666527f3d2de1e8d
tommy <tommy@192.168.1.21>
2019-09-30 17:33:08 +0200

    new inventaire
    Former-commit-id: af26af4e4e26bf1c58b8f56685a1ff77fbaf256b

--- htdocs/bimplogistique/objects/Inventory.class.php
+++ htdocs/bimplogistique/objects/Inventory.class.php
@@ -503,9 +503,9 @@ class Inventory extends BimpDolObject
         $ids_place = array();
 
         $sql = 'SELECT id_equipment';
-        $sql .= ' FROM ' . MAIN_DB_PREFIX . 'be_equipment_place';
+        $sql .= ' FROM ' . MAIN_DB_PREFIX . 'be_equipment_place p, ' . MAIN_DB_PREFIX . 'be_equipment e';
         $sql .= ' WHERE id_entrepot=' . $this->getData('fk_warehouse');
-        $sql .= ' AND position=1 AND type=2';
+        $sql .= ' AND p.id_equipment = e.id AND position=1 AND type=2';
 
         $result = $this->db->db->query($sql);
         if ($result and mysqli_num_rows($result) > 0) {

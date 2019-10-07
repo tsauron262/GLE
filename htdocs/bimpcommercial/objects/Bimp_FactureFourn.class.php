bf84b5c3d9b2c5ba63d018160c5ea3e4882d78f3
tommy <tommy@192.168.1.21>
2019-10-07 21:50:22 +0200

    desactive wising
    Former-commit-id: 4dc54c7a59fd6369ffe0261a826fd82a1b5252af

--- htdocs/bimpcommercial/objects/Bimp_FactureFourn.class.php
+++ htdocs/bimpcommercial/objects/Bimp_FactureFourn.class.php
@@ -2,6 +2,7 @@
 
 require_once DOL_DOCUMENT_ROOT . '/bimpcommercial/objects/BimpComm.class.php';
 require_once DOL_DOCUMENT_ROOT . '/fourn/class/fournisseur.product.class.php';
+require_once DOL_DOCUMENT_ROOT . '/fourn/class/fournisseur.facture.class.php';
 
 class Bimp_FactureFourn extends BimpComm
 {

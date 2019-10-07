0efb266dcd47df68ea503e6e891a14d297a765b1
tommy <tommy@192.168.1.21>
2019-10-07 22:34:01 +0200

    desactive wising
    Former-commit-id: 11bfc62f6c51a83e21d72c8f8c287de8cb2385f4

--- htdocs/bimpcommercial/objects/Bimp_FactureFourn.class.php
+++ htdocs/bimpcommercial/objects/Bimp_FactureFourn.class.php
@@ -561,6 +561,7 @@ class Bimp_FactureFourn extends BimpComm
                 $user->fetch((int) $this->getData('fk_user_valid'));
                 $html .= '<div class="object_header_infos">';
                 $html .= 'Validée par ' . $user->getNomUrl(1);
+                $html .= ' le '.$this->displayData('date_valid', 'default', false, true);
                 $html .= '</div>';
             }
         }

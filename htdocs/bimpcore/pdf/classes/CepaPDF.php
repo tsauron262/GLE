895b6394ff0bf786fc0bccb347da4823cec5981f
tommy <tommy@192.168.1.21>
2019-10-31 12:34:49 +0100

    hortho
    Former-commit-id: 6e1a1130a83fbffa57d39cebcd00ae4367743deb

--- htdocs/bimpcore/pdf/classes/CepaPDF.php
+++ htdocs/bimpcore/pdf/classes/CepaPDF.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/BimpDocumentPDF.php';
 require_once DOL_DOCUMENT_ROOT . '/comm/propal/class/propal.class.php';
+require_once DOL_DOCUMENT_ROOT . '/core/lib/pdf.lib.php';
 
 ini_set('display_errors', 1);
 
@@ -32,19 +33,24 @@ class CepaPDF extends BimpDocumentPDF
         require_once DOL_DOCUMENT_ROOT . '/includes/tcpdi/tcpdi.php';
         require_once DOL_DOCUMENT_ROOT . '/bimpcore/pdf/src/fpdf2.php';
         require_once DOL_DOCUMENT_ROOT . '/bimpcore/pdf/src/autoload.php';
-        $this->pdf2 = new BimpConcatPdf();
+        //$this->pdf2 = new BimpConcatPdf();
+                $this->pdf2 = pdf_getInstance($this->format);
         $this->pdf2->addPage();
         $this->pdf2->SetFont('Times');
         $pagecountTpl = $this->pdf2->setSourceFile(DOL_DOCUMENT_ROOT . '/bimpcore/pdf/templates/SEPA-2.pdf');
         $tplidx = $this->pdf2->importPage(1, "/MediaBox");
-        $size = $this->pdf2->getTemplateSize($tplIdx);
-        $this->pdf2->useTemplate($tplidx, null, null, $size['w'], $size['h'], true);
+                $this->pdf2->useTemplate($tplidx, 0, 0, 0, 0, true);
+//        $size = $this->pdf2->getTemplateSize($tplidx);
+//        $this->pdf2->useTemplate($tplidx, null, null, $size['w'], $size['h'], true);
         $file = $this->getFilePath() . $this->getFileName().'_sepa.pdf';
         
         $soc = BimpCache::getBimpObjectInstance("bimpcore", "Bimp_Societe", $this->object->id);
         
         $this->pdf2->setXY(105,118.7);
         $this->pdf2->Cell(70,8, $soc->getNumSepa(), 0);
+        
+        
+        $this->pdf2->Close();
             
         $this->pdf2->Output($file, 'F');
 

f42090374f74e7d8b28c6cf7cf3448afdc121294
tommy <tommy@192.168.1.21>
2019-11-25 17:51:33 +0100

    fixtabs
    Former-commit-id: 340ae4d6c22e66c89665f305e730adc03f787a40

--- htdocs/bimpcore/objects/Bimp_Societe.class.php
+++ htdocs/bimpcore/objects/Bimp_Societe.class.php
@@ -74,7 +74,7 @@ class Bimp_Societe extends BimpObject
              * Entreprise onf fait les verifs...
              */
             if($this->getData('fk_pays') == 1 || $this->getData('fk_pays') < 1)
-                if(strlen($this->getData("siret")) != 14 && strlen($this->getData("siren")) != 9){
+                if(strlen($this->getData("siret")) != 14 || !$this->Luhn($this->getData("siret"), 14)){
                     $errors[] = "Siren/siret client invalide :".$this->getData("siren")."/".$this->getData("siret");
                 }
         }
@@ -91,6 +91,57 @@ class Bimp_Societe extends BimpObject
             return false;
         return true;
     }
+    
+    
+    public function Luhn($numero,$longueur){ 
+    // On passe à la fonction la variable contenant le numéro à vérifier
+                     // et la longueur qu'il doit impérativement avoir
+
+        if ((strlen($numero)==$longueur) && preg_match("#[0-9]{".$longueur."}#i", 
+    $numero)){
+    // si la longueur est bonne et que l'on n'a que des chiffres
+
+            /* on décompose le numéro dans un tableau  */
+            for ($i=0;$i<$longueur;$i++){
+                $tableauChiffresNumero[$i]=substr($numero,$i,1);
+            }
+
+            /* on parcours le tableau pour additionner les chiffres */
+            $luhn=0; // clef de luhn à tester
+            for ($i=0;$i<$longueur;$i++){
+                if ($i%2==0){ // si le rang est pair (0,2,4 etc.)
+                    if(($tableauChiffresNumero[$i]*2) > 9){ 
+    // On regarde si son double est > à 9
+                        $tableauChiffresNumero[$i]=($tableauChiffresNumero[$i]*2)-9;
+     //si oui on lui retire 9
+                        // et on remplace la valeur
+                        // par ce double corrigé
+                    }
+                    else{
+
+                        $tableauChiffresNumero[$i]=$tableauChiffresNumero[$i]*2; 
+    // si non on remplace la valeur
+                        // par le double
+                    }
+                }
+                $luhn=$luhn+$tableauChiffresNumero[$i]; 
+    // on additionne le chiffre à la clef de luhn
+            }
+
+            /* test de la divition par 10 */
+            if($luhn%10==0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false; 
+    // la valeur fournie n'est pas conforme (caractère non numérique ou mauvaise
+    // longueur)
+        }
+      }
 
     public function getTypes_entArray()
     {

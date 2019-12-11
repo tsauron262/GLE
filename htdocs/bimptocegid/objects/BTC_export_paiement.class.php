9f23f7c2f2f138a14446e581df2ca2d2d0d5d594
Alexis BERNARD <al.bernard@bimp.fr>
2019-12-11 12:05:33 +0100

    Modif ecart cts + return error PAY
    Former-commit-id: 7cc33b1a0a5cc4a48c8518b9c79b9c53f3971196

--- htdocs/bimptocegid/objects/BTC_export_paiement.class.php
+++ htdocs/bimptocegid/objects/BTC_export_paiement.class.php
@@ -6,9 +6,9 @@ class BTC_export_paiement extends BTC_export {
     const RETURNED_STATUS_OK = 1;
 
     public function export($id, $paiement_id, $forced) {
-
-        $errors = [];
-        $success = [];
+        
+        $error = 0;
+        $ecritures = '';
         $liste_transactions = $this->db->getRows('paiement_facture', 'fk_paiement = ' . $id);
         $paiement = $this->getInstance('bimpcommercial', 'Bimp_Paiement', $id);
         foreach ($liste_transactions as $transaction) {
@@ -186,7 +186,7 @@ class BTC_export_paiement extends BTC_export {
                     'fin' => ['0', 2]
                 ];
 
-                $ecritures = $this->struct($structure);
+                $ecritures .= $this->struct($structure);
 
                 $structure['compte'] = [$compte_general_411, 17];
                 $structure['code_compta'] = [$auxiliaire_client, 16];
@@ -196,10 +196,9 @@ class BTC_export_paiement extends BTC_export {
                 $structure['lettrage'] = ['-XAL', 4];
                 $ecritures .= $this->struct($structure);
 
-                $this->write_tra($ecritures, $this->create_daily_file('paiement'));
-
         }
-        $paiement->updateField('exported', self::RETURNED_STATUS_OK);
+        
+        return $this->write_tra($ecritures, $this->create_daily_file('paiement'));
     }
 
 }

8115dfee668ab9462fa6835529fc73da738964d4
tommy <tommy@192.168.1.21>
2019-12-13 13:45:49 +0100

    restricted paiement
    Former-commit-id: f6d15f62cb18b94aadf3219d6e75adf4c3e564ab

--- htdocs/bimpcommercial/core/modules/modBimpCommercial.class.php
+++ htdocs/bimpcommercial/core/modules/modBimpCommercial.class.php
@@ -296,6 +296,15 @@ class modBimpCommercial extends DolibarrModules
 
 		$r++;
                 
+                $this->rights[$r][0] = $this->numero + $r;	// Permission id (must not be already used)
+		$this->rights[$r][1] = 'Edite zone de Vente';	// Permission label
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
+		$this->rights[$r][4] = 'edit_zone_vente';				// In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
+//		$this->rights[$r][5] = 'valid';				    // In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
+
+		$r++;
+                
+                
                 
 
 		// Main menu entries

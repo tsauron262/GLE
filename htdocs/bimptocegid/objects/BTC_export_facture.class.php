47ebed7858fb4ae4852760999dfa276367fa7b96
Alexis BERNARD <al.bernard@bimp.fr>
2019-12-16 16:52:56 +0100

    export way
    Former-commit-id: 34f6c528ae2669bc3b0e7c63399cfa99c2e542d8

--- htdocs/bimptocegid/objects/BTC_export_facture.class.php
+++ htdocs/bimptocegid/objects/BTC_export_facture.class.php
@@ -3,7 +3,7 @@
 class BTC_export_facture extends BTC_export {
     
     public function export($id_facture, $forced) {
-        $this->create_daily_file('vente');
+        $file = $this->create_daily_file('vente');
         $facture = $this->getInstance('bimpcommercial', 'Bimp_Facture', $id_facture);
         if($contact = $this->db->getRow('element_contact', 'element_id = ' . $facture->getData('fk_soc') . ' AND fk_c_type_contact = 60')) {
             $id_client_facturation = $contact->fk_socpeople;
@@ -295,7 +295,7 @@ class BTC_export_facture extends BTC_export {
             $ecritures .= $this->struct($structure);
         }
         
-         if($this->write_tra($ecritures, "")) {
+         if($this->write_tra($ecritures, $file)) {
             $facture->updateField('exported', 1);
             return 1;
         } else {

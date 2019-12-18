c8ca15ea44e43a6f03d9253affb9956f42ad5c1d
tommy <tommy@192.168.1.21>
2019-12-18 18:24:03 +0100

    serial with S
    Former-commit-id: 38416fa33c0b0dfb9a400705fd4067afd539d32e

--- htdocs/bimpcore/scripts/majCodeConfigurationProd.php
+++ htdocs/bimpcore/scripts/majCodeConfigurationProd.php
@@ -5,7 +5,7 @@ require_once("../../main.inc.php");
 
 $sql = $db->query("SELECT COUNT(*) as nbSerial, SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) as fin, MIN(id) as minEquipment, MIN(id_product) as minProd, COUnT(DISTINCT(id_product)) as nbProd FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product > 0 GROUP BY fin ORDER BY COUNT(*) DESC");
 
-$erreurs = $ok = array();
+$erreurs = $info = $ok = array();
 
 while($ln = $db->fetch_object($sql)){
     if($ln->nbSerial > 50){
@@ -13,17 +13,31 @@ while($ln = $db->fetch_object($sql)){
             $erreurs[] = $ln->fin." plusieurs prod ...";
         }
         else{
+            if(!isset($ln->fin))
+                $ok[$ln->fin]= 0;  
+            else
+                $erreurs[] = $ln->fin.' plusieurs fois ATTENTION......';
+            $ok[$ln->fin]++;
+            
             $sql2 = $db->query("SELECT COUNT(*) as nbSerial  FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product = 0 AND serial LIKE '%".$ln->fin."'");
             $ln2 = $db->fetch_object($sql2);
             
-            $ok[] = "OK prod ".$ln->minProd.' code config '.$ln->fin. '   corrigera '.$ln2->nbSerial. ' equipement SAV';
+            $info[] = "OK prod ".$ln->minProd.' code config '.$ln->fin. '   corrigera '.$ln2->nbSerial. ' equipement SAV';
         }
     }
 }
 
 
+$sql3 = $db->query("SELECT COUNT(*) as nbIdentique, serial FROM `llx_be_equipment` WHERE `id_product` > 0 AND ( LENGTH(serial) = 13 || LENGTH(serial) = 12) GROUP BY `serial`, id_product HAVING nbIdentique > 1  
+ORDER BY COUNT(*)  DESC");
+while($ln3 = $db->fetch_object($sql3)){
+    $erreurs[] = $ln3->serial." plusieurs foix ... grave";
+}
+
+
+
 echo '<pre>';
 
-print_r($ok);
+print_r($info);
 
 print_r($erreurs);

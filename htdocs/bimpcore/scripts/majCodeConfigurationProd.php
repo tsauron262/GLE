f909d284d618c1a575b0580dd7fd963d3d879e05
tommy <tommy@192.168.1.21>
2019-12-19 13:29:40 +0100

    serial with S
    Former-commit-id: a4d18f1941fa786c86714c16ae9d858720f3243d

--- htdocs/bimpcore/scripts/majCodeConfigurationProd.php
+++ htdocs/bimpcore/scripts/majCodeConfigurationProd.php
@@ -3,29 +3,39 @@
 require_once("../../main.inc.php");
 
 
-$sql = $db->query("SELECT COUNT(*) as nbSerial, SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) as fin, MIN(id) as minEquipment, MIN(id_product) as minProd, COUnT(DISTINCT(id_product)) as nbProd FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product > 0 GROUP BY fin ORDER BY COUNT(*) DESC");
+$sql = $db->query("SELECT COUNT(*) as nbSerial, SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) as fin, MIN(id) as minEquipment, MIN(id_product) as minProd, MAX(id_product) as maxProd, COUnT(DISTINCT(id_product)) as nbProd FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product > 0 AND SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) NOT IN (SELECT code_config FROM llx_product_extrafields WHERE code_config IS NOT NULL) GROUP BY fin ORDER BY COUNT(*) DESC");
 
 $erreurs = $info = $ok = array();
+
+$go = (isset($_REQUEST['action']) && $_REQUEST['action'] == 'go')? 1 : 0;
 
 while($ln = $db->fetch_object($sql)){
     if($ln->nbSerial > 50){
         if($ln->nbProd > 1){
-            $erreurs[] = $ln->fin." plusieurs prod ...";
+            $erreurs[] = $ln->fin." plusieurs prod (".$ln->minProd.", ".$ln->maxProd.", ...)";
         }
         else{
-            if(!isset($ok[$ln->fin]))
-                $ok[$ln->fin]= 0;  
+            if(!isset($ok[$ln->fin])){
+                $ok[$ln->fin]= 0; 
+                $sql2 = $db->query("SELECT COUNT(*) as nbSerial  FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product = 0 AND serial LIKE '%".$ln->fin."'");
+                $ln2 = $db->fetch_object($sql2);
+
+                $info[] = "OK prod ".$ln->minProd.' code config '.$ln->fin. '   corrigera '.$ln2->nbSerial. ' equipement SAV';
+
+                if($go)
+                    $db->query("UPDATE llx_product_extrafields SET code_config = '".$ln->fin."' WHERE fk_object = '".$ln->minProd."'"); 
+            }
             else
                 $erreurs[] = $ln->fin.' plusieurs ('.($ok[$ln->fin]+1).') fois ATTENTION......';
             $ok[$ln->fin]++;
             
-            $sql2 = $db->query("SELECT COUNT(*) as nbSerial  FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product = 0 AND serial LIKE '%".$ln->fin."'");
-            $ln2 = $db->fetch_object($sql2);
             
-            $info[] = "OK prod ".$ln->minProd.' code config '.$ln->fin. '   corrigera '.$ln2->nbSerial. ' equipement SAV';
         }
     }
 }
+
+
+
 
 
 $sql3 = $db->query("SELECT COUNT(*) as nbIdentique, serial FROM `llx_be_equipment` WHERE `id_product` > 0 AND ( LENGTH(serial) = 13 || LENGTH(serial) = 12) GROUP BY `serial`, id_product HAVING nbIdentique > 1  
@@ -34,6 +44,16 @@ while($ln3 = $db->fetch_object($sql3)){
     $erreurs[] = $ln3->serial." plusieurs foix ... grave";
 }
 
+
+
+
+
+if($go){
+    $sql4 = $db->query("SELECT code_config, fk_object FROM llx_product_extrafields WHERE code_config IS NOT NULL");
+    while($ln4 = $db->fetch_object($sql4)){
+        $db->query("UPDATE llx_be_equipment SET id_product = ".$ln4->fk_object." WHERE serial LIKE '%".$ln4->code_config."' and ( LENGTH(serial) = 13 || LENGTH(serial) = 12)");
+    }
+}
 
 
 echo '<pre>';

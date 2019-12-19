446533884199a6560f27b93b0b77a1efb5acecb3
tommy <tommy@192.168.1.21>
2019-12-19 14:45:04 +0100

    serial with S
    Former-commit-id: ec76fea11e0e82a750eea9b0bdda37df1812e876

--- htdocs/bimpcore/scripts/majCodeConfigurationProd.php
+++ htdocs/bimpcore/scripts/majCodeConfigurationProd.php
@@ -24,8 +24,18 @@ class majCodeConfigurationnProd{
     private $info = array();
     private $ok = array();
 
-    function getSN($fin, $id_prod = null){
-        $this->db->query();
+    function getSN($fin, $id_prod = null, $max = null){
+        $return = array();
+        $req = "SELECT `serial` FROM `llx_be_equipment` WHERE ".$this->whereTaille." AND `serial` LIKE '%".$fin."'";
+        if($id_prod > 0)
+            $req .= " AND `id_product` = ".$id_prod;
+        if($max > 0)
+            $req .= " LIMIT 0,".$max;
+        $sql = $this->db->query($req);
+        while($ln = $this->db->fetch_object($sql)){
+            $return[] = $ln->serial;
+        }
+        return implode(" | ", $return);
     }
     
     function __construct($db) {
@@ -35,12 +45,12 @@ class majCodeConfigurationnProd{
     function updateEquipmentOrfellin(){
         $sql4 = $this->db->query("SELECT code_config, fk_object FROM llx_product_extrafields WHERE code_config IS NOT NULL");
         while($ln4 = $this->db->fetch_object($sql4)){
-            $this->db->query("UPDATE llx_be_equipment SET id_product = ".$ln4->fk_object." WHERE serial LIKE '%".$ln4->code_config."' and ( LENGTH(serial) = 13 || LENGTH(serial) = 12)");
+            $this->db->query("UPDATE llx_be_equipment SET id_product = ".$ln4->fk_object." WHERE serial LIKE '%".$ln4->code_config."' and ".$this->whereTaille);
         }
     }
     
     function testSerialDouble(){
-        $sql3 = $this->db->query("SELECT COUNT(*) as nbIdentique, serial FROM `llx_be_equipment` WHERE `id_product` > 0 AND ( LENGTH(serial) = 13 || LENGTH(serial) = 12) GROUP BY `serial`, id_product HAVING nbIdentique > 1  
+        $sql3 = $this->db->query("SELECT COUNT(*) as nbIdentique, serial FROM `llx_be_equipment` WHERE `id_product` > 0 AND ".$this->whereTaille." GROUP BY `serial`, id_product HAVING nbIdentique > 1  
         ORDER BY COUNT(*)  DESC");
         while($ln3 = $this->db->fetch_object($sql3)){
             $this->erreurs[] = $ln3->serial." plusieurs foix ... grave";
@@ -48,16 +58,16 @@ class majCodeConfigurationnProd{
     }
     
     function updateCodeConfigProd($go){
-        $sql = $this->db->query("SELECT COUNT(*) as nbSerial, SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) as fin, MIN(id) as minEquipment, MIN(id_product) as minProd, MAX(id_product) as maxProd, COUnT(DISTINCT(id_product)) as nbProd FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product > 0 AND SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) NOT IN (SELECT code_config FROM llx_product_extrafields WHERE code_config IS NOT NULL) GROUP BY fin ORDER BY COUNT(*) DESC");
+        $sql = $this->db->query("SELECT COUNT(*) as nbSerial, SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) as fin, MIN(id) as minEquipment, MIN(id_product) as minProd, MAX(id_product) as maxProd, COUnT(DISTINCT(id_product)) as nbProd FROM `llx_be_equipment` WHERE ".$this->whereTaille." AND  id_product > 0 AND SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) NOT IN (SELECT code_config FROM llx_product_extrafields WHERE code_config IS NOT NULL) GROUP BY fin ORDER BY COUNT(*) DESC");
         while($ln = $this->db->fetch_object($sql)){
             if($ln->nbSerial > 30){
                 if($ln->nbProd > 1){
-                    $this->erreurs[] = $ln->fin." plusieurs prod (".$ln->nbProd.")  ".$ln->minProd.", ".$ln->maxProd.($ln->nbProd > 2? ", ..." :"");
+                    $this->erreurs[] = $ln->fin." plusieurs prod (".$ln->nbProd.")  <br/>".$ln->minProd."(".$this->getSN($ln->fin, $ln->minProd, 4).") <br/>".$ln->maxProd."(".$this->getSN($ln->fin, $ln->maxProd, 4).")".($ln->nbProd > 2? "<br/> ..." :"")."<br/>";
                 }
                 else{
                     if(!isset($this->ok[$ln->fin])){
                         $this->ok[$ln->fin]= 0; 
-                        $sql2 = $this->db->query("SELECT COUNT(*) as nbSerial  FROM `llx_be_equipment` WHERE ( LENGTH(serial) = 13 || LENGTH(serial) = 12) AND id_product = 0 AND serial LIKE '%".$ln->fin."'");
+                        $sql2 = $this->db->query("SELECT COUNT(*) as nbSerial  FROM `llx_be_equipment` WHERE ".$this->whereTaille." AND  id_product = 0 AND serial LIKE '%".$ln->fin."'");
                         $ln2 = $this->db->fetch_object($sql2);
 
                         $this->totCorrection += $ln2->nbSerial;

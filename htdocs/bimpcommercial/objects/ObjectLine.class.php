a0c589c81bcf0122c8474f2f700152f6e8936976
tommy <tommy@192.168.1.21>
2019-12-23 18:11:20 +0100

    serial with S
    Former-commit-id: 8a853b77df1abe0e4a272988a6a045d2280c2665

--- htdocs/bimpcommercial/objects/ObjectLine.class.php
+++ htdocs/bimpcommercial/objects/ObjectLine.class.php
@@ -2825,10 +2825,10 @@ class ObjectLine extends BimpObject
             if (is_null($this->remise) || (float) $this->remise !== (float) $remises_infos['total_percent'] ||
                     $remises_infos['total_percent'] !== (float) $this->getData('remise') ||
                     $remises_infos['total_percent'] !== (float) $this->getInitData('remise') ) {
-                if($this->nbCalcremise < 90){
+//                if($this->nbCalcremise < 90){
                     $this->nbCalcremise++;
                     $this->update($warnings, true);
-                }
+//                }
             }
         }
     }

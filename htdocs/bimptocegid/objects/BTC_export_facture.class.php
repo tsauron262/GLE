ee4e2ae9cc531f1b2011270d3f7b5c88762c39a5
Alexis BERNARD <al.bernard@bimp.fr>
2019-12-26 15:44:13 +0100

    Ecrats centimes?
    Former-commit-id: a79232ff6d56d45db8c7ddd0502fb9b2523117c3

--- htdocs/bimptocegid/objects/BTC_export_facture.class.php
+++ htdocs/bimptocegid/objects/BTC_export_facture.class.php
@@ -296,6 +296,7 @@ class BTC_export_facture extends BTC_export {
                 
         if(round($total_lignes, 2) != round($total_ttc_facture, 2)) {            
             $montant_ecart = round($total_ttc_facture, 2) - (round($total_lignes, 2));
+            echo 'MONTANT : ' . round($montant_ecart,2);
 
             $lignes = $this->rectifications_ecarts($lignes, round($montant_ecart,2), 'vente');
            

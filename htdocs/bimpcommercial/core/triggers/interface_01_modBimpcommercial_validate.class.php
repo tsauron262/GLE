5f12d4e89d58d38d7d481b9273d35a1f2353692a
tommy <tommy@192.168.1.21>
2020-01-06 11:31:27 +0100

    new serveur
    Former-commit-id: 9c71060dd5608154de4b67b5bef328f8b7a41048

--- htdocs/bimpcommercial/core/triggers/interface_01_modBimpcommercial_validate.class.php
+++ htdocs/bimpcommercial/core/triggers/interface_01_modBimpcommercial_validate.class.php
@@ -66,18 +66,19 @@ class Interfacevalidate extends DolibarrTriggers
             if ($action == 'ORDER_VALIDATE') {
                 $bimp_object = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Commande', $object->id);
 
-                if (is_object($bimp_object) && $bimp_object->isLoaded()) {
-                    $client = $bimp_object->getChildObject('client');
-                    if (is_object($client) && $client->isLoaded()) {
-                        if (!$client->canBuy($errors)) {
-                            $object->errors = array_merge($object->errors, $errors);
-                            return -1;
-                        }
-                    }
-                }
             } else {
                 $bimp_object = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Facture', $object->id);
                 $bimp_object->onValidate();
+            }
+            
+            if (is_object($bimp_object) && $bimp_object->isLoaded()) {
+                $client = $bimp_object->getChildObject('client');
+                if (is_object($client) && $client->isLoaded()) {
+                    if (!$client->canBuy($errors)) {
+                        $object->errors = array_merge($object->errors, $errors);
+                        return -1;
+                    }
+                }
             }
 
 

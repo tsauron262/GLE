6952a713f3ef8c8f9ef3a608e80fbdd5eb26ef8b
tommy <tommy@192.168.1.21>
2020-01-07 11:13:20 +0100

    edit datef
    Former-commit-id: ea880de165c81c1cab1dcf8eb8fdac930691d696

--- htdocs/bimpcore/scripts/majCodeConfigurationProd.php
+++ htdocs/bimpcore/scripts/majCodeConfigurationProd.php
@@ -132,6 +132,8 @@ class majCodeConfigurationnProd{
                 . "AND SUBSTRING(`serial`, LENGTH(`serial`)-3, 4) NOT IN (SELECT code_config FROM llx_product_extrafields WHERE code_config IS NOT NULL) "
                 . "AND id_product =p.rowid AND ref LIKE 'APP-%' "
                 . "AND label  NOT LIKE '%(Demo)%' "
+                . "AND label  NOT LIKE '%Demo' "
+                . "AND label  NOT LIKE '%Recondtionné%' "
                 . "AND ref  NOT LIKE 'APP-Z0%' "
                 . "AND label  NOT LIKE '%WaTcH%'  "
                 . "AND (label  NOT LIKE '%GRAVURE%' || label  NOT LIKE '%C2C%')  "

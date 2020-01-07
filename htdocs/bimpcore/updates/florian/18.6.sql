17399c4918ccd8f76f24a098875fc7e6836b377d
flo <flo@192.168.0.11>
2020-01-07 15:48:34 +0100

    Majs bimpComm
    Former-commit-id: f2a6db30a1b1ef539855702092e67bac9d7a1d5c

---
+++ htdocs/bimpcore/updates/florian/18.6.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `llx_bimp_propal_line` ADD `hide_in_pdf` BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE `llx_bs_sav_propal_line` ADD `hide_in_pdf` BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE `llx_bimp_commande_line` ADD `hide_in_pdf` BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE `llx_bimp_facture_line` ADD `hide_in_pdf` BOOLEAN NOT NULL DEFAULT FALSE;

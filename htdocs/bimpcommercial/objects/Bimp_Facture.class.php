fcfb3f7d9825951e1199bce1dac4011dc37813e3
Alexis BERNARD <al.bernard@bimp.fr>
2020-01-14 11:45:26 +0100

    Orthographe
    Former-commit-id: 6100d0ccd4d8515e756e3b92f9ae4b91562506f3

--- htdocs/bimpcommercial/objects/Bimp_Facture.class.php
+++ htdocs/bimpcommercial/objects/Bimp_Facture.class.php
@@ -2086,8 +2086,8 @@ class Bimp_Facture extends BimpComm
         $id_contrat = $this->db->getValue('element_element', 'fk_source', 'sourcetype = "contrat" and targettype = "facture" and fk_target = ' . $this->id);
         if($id_contrat && $this->getData('fk_statut') == 0) {
                 $html .= '<div class="object_header_infos">';
-                $msg = "<h5>Cette facture est une facture de contrat. Pour le moment, merci de supprimer cette PROV à partir de l'échéancier et non depuis le bouton supprimer de la facture. <b><i class='fas fa-exclamation-triangle'></i> Très important</b></h5>";
-                $msg .= '<h5><b><a href="'.DOL_URL_ROOT.'/bimpcontract?fc=contrat&id='.$id_contrat.'&navtab-maintabs=echeancier">Page de l\'échéancier du contrat</a></b></h5>';
+                $msg = "<h5>Cette facture est une facture de contrat. Pour le moment, merci de supprimer cette PROV à partir de l'échéancier et non <b>pas</b> depuis le bouton \"poubelle\" habituel. <b><i class='fas fa-exclamation-triangle'></i> TR&Egrave;S IMPORTANT</b></h5>";
+                $msg .= '<h5><b><a href="'.DOL_URL_ROOT.'/bimpcontract?fc=contrat&id='.$id_contrat.'&navtab-maintabs=echeancier">Aller vers la page de l\'échéancier du contrat <i class="fas fa-arrow-right" ></i></a></b></h5>';
                 $html .= BimpRender::renderAlerts($msg, 'danger', false);
                 $html .= '</div>';
             }

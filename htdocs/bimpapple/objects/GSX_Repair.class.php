ed0ab108189fd5dc1e4a02d3ca2e2647e9d0ec4c
tommy <tommy@192.168.1.21>
2020-01-22 19:30:59 +0100

    bug deprecié
    Former-commit-id: 5d38da896a65f43c72a18868ab4061ff6b5118ed

--- htdocs/bimpapple/objects/GSX_Repair.class.php
+++ htdocs/bimpapple/objects/GSX_Repair.class.php
@@ -42,11 +42,11 @@ class GSX_Repair extends BimpObject
         parent::__construct($module, $object_name);
     }
 
-    public function initGsx(&$errors = array())
+    public function initGsx(&$errors = array(), $force = false)
     {
         if ($this->use_gsx_v2) {
             if (is_null($this->gsx_v2) || !$this->gsx_v2->logged) {
-                $this->gsx_v2 = GSX_v2::getInstance((is_null($this->gsx_v2)?false:true));
+                $this->gsx_v2 = GSX_v2::getInstance($force);
             }
             $this->gsx_v2->resetErrors();
             if (!$this->gsx_v2->logged) {

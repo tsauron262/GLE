a9f9de288cf5cdcc26cc9d9fd5dc8c295270d1b8
tommy <tommy@192.168.1.21>
2020-01-22 20:12:54 +0100

    bug deprecié
    Former-commit-id: 77b36a006c8898d18e9b65732f50bb31beeaea6f

--- htdocs/bimpapple/class/test_sav.class.php
+++ htdocs/bimpapple/class/test_sav.class.php
@@ -56,7 +56,7 @@ class test_sav
             $user = new User($db);
             $user->fetch(242);
             $user->fetch_optionals();
-            $this->output .= "userId".$user->id.$user->login;
+            $this->output .= "userId".$user->id.$user->login."|".$user->array_options['options_apple_id'];
             if(!$this->repair->initGsx($error, true)){
                 $this->output .= " Non authentifié sur GSX ! ";
             }

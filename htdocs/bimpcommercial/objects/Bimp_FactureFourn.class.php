613824746c9ed3bba7adc25d97dea4b70eeb91a4
tommy <tommy@192.168.1.21>
2020-01-27 16:36:20 +0100

    divers bug
    Former-commit-id: a729403b4bc729f0ea18e1b4eca1aded81ccfacb

--- htdocs/bimpcommercial/objects/Bimp_FactureFourn.class.php
+++ htdocs/bimpcommercial/objects/Bimp_FactureFourn.class.php
@@ -1481,7 +1481,7 @@ class Bimp_FactureFourn extends BimpComm
             $mail = $userCreate->email;
 //            if ($mail == '')
                 $mail = "tommy@bimp.fr";
-                echo $obj->getNomUrl();
+            require_once(DOL_DOCUMENT_ROOT."/synopsistools/SynDiversFunction.php");
             if (mailSyn2('Facture fournisseur brouillon à régulariser', $mail, 'admin@bimp.fr', 'Bonjour, vous avez laissé une facture fournisseur en l’état de brouillon depuis plus de ' . $nbDay . ' jour(s) : ' . $obj->getNomUrl() . ' <br/>Merci de bien vouloir la régulariser au plus vite.'))
                 $i++;
         }

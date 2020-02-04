cda0310854e0b6effbd83360d17eb5c4e05014ac
tommy <tommy@192.168.1.21>
2020-02-04 12:50:44 +0100

    doc cgv
    Former-commit-id: fd6a1df6ba1f91b142f119b488744b6418e3ba9a

--- htdocs/bimpapple/class/test_sav.class.php
+++ htdocs/bimpapple/class/test_sav.class.php
@@ -326,11 +326,11 @@ AND s.status = " . ($statut == "closed" ? "999" : "9");
             $equipment = BimpObject::getInstance('bimpequipment', 'Equipment');
 
             $rows = $equipment->getList(array(
-                'imei' => '',
-                'serial'         => array(
-                                    'operator' => '!=',
-                                    'value'    => '0'
-                                )
+                'imei'    => '',
+                'serial'  => array(
+                            'operator' => '!=',
+                            'value'    => '0'
+                          )
                     ), $nb, 1, 'id', 'desc', 'array', array('id', 'serial'));
 
             if (!empty($rows)) {

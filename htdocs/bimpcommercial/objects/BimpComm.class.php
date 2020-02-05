538fb089dc96054b9ceedbe76540fcbc1bb0de92
tommy <tommy@192.168.1.23>
2020-02-05 17:35:02 +0100

    CHECKALLLINES
    Former-commit-id: 8f80f2535bec9b89fce4433d0cf9a32eca3114aa

--- htdocs/bimpcommercial/objects/BimpComm.class.php
+++ htdocs/bimpcommercial/objects/BimpComm.class.php
@@ -4059,4 +4059,44 @@ class BimpComm extends BimpDolObject
 
         $this->processRemisesGlobales();
     }
+    
+    
+    
+    public static function checkAllObjectLine($id_product, &$sortie = '', $nbMax = 10){
+        global $db;
+        $tabInfo = $errors = array();
+        $sortie = '';
+
+        $tabInfo[] = array('fk_propal', 'llx_propaldet', array('llx_bimp_propal_line', 'llx_bs_sav_propal_line'), 'Bimp_Propal');
+        $tabInfo[] = array('fk_commande', 'llx_commandedet', 'llx_bimp_commande_line', 'Bimp_Commande');
+        $tabInfo[] = array('fk_facture', 'llx_facturedet', 'llx_bimp_facture_line', 'Bimp_Facture');
+        $tabInfo[] = array('fk_commande', 'llx_commande_fournisseurdet', 'llx_bimp_commande_fourn_line', 'Bimp_CommandeFourn');
+        $tabInfo[] = array('fk_facture_fourn', 'llx_facture_fourn_det', 'llx_bimp_facture_fourn_line', 'Bimp_FactureFourn');
+
+        foreach($tabInfo as $info){
+            $i = 0;
+            if(!is_array($info[2]))
+                $info[2] = array($info[2]);
+            $where = array();
+            foreach($info[2] as $table)
+                $where[] = 'rowid NOT IN (SELECT id_line FROM '.$table.')';
+            $req = 'SELECT DISTINCT(`'.$info[0].'`) as id FROM `'.$info[1].'` WHERE '.implode(' AND ', $where).' ';
+
+            if($id_product > 0)
+                $req .= ' AND fk_product = '.$id_product;
+
+            $sql = $db->query($req);
+            $tot= $db->num_rows($sql);
+            $sql = $db->query($req.' LIMIT 0,'.$nbMax);
+            while ($ln = $db->fetch_object($sql)){
+                $comm = BimpCache::getBimpObjectInstance('bimpcommercial', $info[3], $ln->id);
+                if($comm->isLoaded()){
+                    $errors = array_merge($errors, $comm->checkLines());
+                    $i++;
+                }
+            }
+            $sortie .= '<br/>fin '.$i.' / '.$tot.' corrections de '.$info[1];
+        }
+        return $errors;
+    }
 }

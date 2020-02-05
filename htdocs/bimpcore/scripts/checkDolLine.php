ae380d02de76b85610a844cc72fbbbf275cf7c79
tommy <tommy@192.168.1.23>
2020-02-05 17:35:27 +0100

    CHECKALLLINES
    Former-commit-id: bf60d5c62ec355cf66339a899aaf842bc15892f1

--- htdocs/bimpcore/scripts/checkDolLine.php
+++ htdocs/bimpcore/scripts/checkDolLine.php
@@ -16,38 +16,9 @@ echo 'DEBUT <br/><br/>';
 
 global $db;
 
+$html = '';
+$bimpComm = BimpCache::getBimpObjectInstance('bimpcommercial', 'BimpComm');
+$bimpComm->checkAllObjectLine(0, $html, 500);
 
-
-
-$tabInfo = $errors = array();
-
-$tabInfo[] = array('fk_propal', 'llx_propaldet', array('llx_bimp_propal_line', 'llx_bs_sav_propal_line'), 'Bimp_Propal');
-$tabInfo[] = array('fk_commande', 'llx_commandedet', 'llx_bimp_commande_line', 'Bimp_Commande');
-$tabInfo[] = array('fk_facture', 'llx_facturedet', 'llx_bimp_facture_line', 'Bimp_Facture');
-$tabInfo[] = array('fk_commande', 'llx_commande_fournisseurdet', 'llx_bimp_commande_fourn_line', 'Bimp_CommandeFourn');
-$tabInfo[] = array('fk_facture_fourn', 'llx_facture_fourn_det', 'llx_bimp_facture_fourn_line', 'Bimp_FactureFourn');
-
-foreach($tabInfo as $info){
-    $i = 0;
-    if(!is_array($info[2]))
-        $info[2] = array($info[2]);
-    $where = array();
-    foreach($info[2] as $table)
-        $where[] = 'rowid NOT IN (SELECT id_line FROM '.$table.')';
-    $req = 'SELECT DISTINCT(`'.$info[0].'`) as id FROM `'.$info[1].'` WHERE '.implode(' AND ', $where).' ORDER BY`rowid` ASC';
-    $sql = $db->query($req);
-    $tot= $db->num_rows($sql);
-    $sql = $db->query($req.' LIMIT 0,1000');
-    while ($ln = $db->fetch_object($sql)){
-        $comm = BimpCache::getBimpObjectInstance('bimpcommercial', $info[3], $ln->id);
-        if($comm->isLoaded()){
-            $errors = array_merge($errors, $comm->checkLines());
-            $i++;
-        }
-//        echo $ln->id."<br/>";
-    }
-    echo '<br/>fin '.$i.' / '.$tot.' corrections de '.$info[1];
-}
-
-echo "<br/>";
+echo $html."<br/>";
 print_r($errors);

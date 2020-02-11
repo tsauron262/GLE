f1d908e381cb7e62f3d43e8319df4f7021564d70
tommy <tommy@192.168.1.23>
2020-02-11 10:15:11 +0100

    marge
    Former-commit-id: 270e2ad4f72a6ebd86839c040581fe63bc0863a0

--- htdocs/bimpcore/objects/BimpProductMouvement.class.php
+++ htdocs/bimpcore/objects/BimpProductMouvement.class.php
@@ -14,6 +14,54 @@ class BimpProductMouvement extends BimpObject {
     
     public static $originetypes = array('' => 'Aucun', 'facture' => 'Facture', 'commande' => 'Commande', 'societe' => 'Vente en caisse ou SAV', 'order_supplier' => 'Commande fournisseur', 'user' => 'Utilisateur');
     
+    
+    public function getInfosOrigine(){
+            $objet = '';
+            $module = '';
+            $label = '';
+            switch($this->getData('origintype')) {
+                
+                case 'facture':
+                    $objet = 'Bimp_Facture';
+                    $module = 'bimpcommercial';
+                    $label = 'Vente';
+                    break;
+                
+                case 'commande':
+                    $objet = 'Bimp_Commande';
+                    $module = 'bimpcommercial';
+                    $label = 'Vente';
+                    break;
+                
+                case 'order_supplier':
+                    $objet = 'Bimp_CommandeFourn';
+                    $module = 'bimpcommercial';
+                    $label = 'Achat';
+                    break;
+                
+                case 'user':
+                    $objet = 'Bimp_User';
+                    $module = 'bimpcore';
+                    $label = 'Immo';
+                    break;  
+                
+                case 'societe':
+                    $objet = 'Bimp_Societe';
+                    $module = 'bimpcore';
+                    $label = 'Vente';
+                    break;
+                
+                case 'transfert':
+                    $objet = 'Transfert';
+                    $module = 'bimptransfer';
+                    $label = 'Transfert';
+                    break;
+                
+            }
+            
+            return array($objet, $module, $label);
+    }
+    
     /**
      * function displayOriginMvt
      * Rôle: Afficher l'origine du mouvement
@@ -23,48 +71,38 @@ class BimpProductMouvement extends BimpObject {
         
         // Si fk_origin > 0
         if($this->getData('fk_origin') > 0) {
+            $infos = $this->getInfosOrigine();
             
-            // On parcours origintype
-            switch($this->getData('origintype')) {
-                
-                case 'facture':
-                    $objet = 'Bimp_Facture';
-                    $module = 'bimpcommercial';
-                    break;
-                
-                case 'commande':
-                    $objet = 'Bimp_Commande';
-                    $module = 'bimpcommercial';
-                    break;
-                
-                case 'order_supplier':
-                    $objet = 'Bimp_CommandeFourn';
-                    $module = 'bimpcommercial';
-                    break;
-                
-                case 'user':
-                    $objet = 'Bimp_User';
-                    $module = 'bimpcore';
-                    break;  
-                
-                case 'societe':
-                    $objet = 'Bimp_Societe';
-                    $module = 'bimpcore';
-                    break;
-                
+            
+            if($infos[0] != "" && $infos[1] != ""){
+                // OPn load l'instance en fonction de l'élément 
+                $instance = $this->getInstance($infos[1], $infos[0], $this->getData('fk_origin'));
+
+                if($instance->isLoaded()) // Si l'instance est bien loader
+                    return $instance->getNomUrl(); // On affiche le nom de l'éléméent 
+                else
+                    return "L'objet n'existe plus : ".$infos[1]."/".$infos[0]; // Autrement on dit que l'objet est inexistant
             }
-            
-            // OPn load l'instance en fonction de l'élément 
-            $instance = $this->getInstance($module, $objet, $this->getData('fk_origin'));
-            
-            if($instance->isLoaded()) // Si l'instance est bien loader
-                return $instance->getNomUrl(); // On affiche le nom de l'éléméent 
-            else
-                return "L'objet n'existe plus"; // Autrement on dit que l'objet est inexistant
+            return 'Object inconnnue'; // Si fk_origin = 0 alors on met aucun
         }
         
         return 'Aucun'; // Si fk_origin = 0 alors on met aucun
         
+    }
+    
+    public function displayReasonMvt() {
+        $infos = $this->getInfosOrigine();
+        $reason = $infos[2];
+        if($reason == ''){
+            $reason = 'Inconnue';
+            
+            if(stripos($this->getData("label"), "Transfert de stock") === 0
+                    || stripos($this->getData("label"), "TR-") === 0)
+                  $reason = 'Transfert';
+            elseif(stripos($this->getData("inventorycode"),'inventory-id-') === 0)
+                $reason = 'Inventaire';
+        }
+        return $reason;
     }
     
     public function displayProduct()

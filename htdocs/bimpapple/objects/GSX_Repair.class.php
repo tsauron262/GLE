0e62eff886ea33ea237eba4504e48101b1187858
tommy <tommy@192.168.1.23>
2020-02-13 11:17:04 +0100

    mail grouper
    Former-commit-id: c8d07744da1570b5d4c7c4ba74936ec36967d98b

--- htdocs/bimpapple/objects/GSX_Repair.class.php
+++ htdocs/bimpapple/objects/GSX_Repair.class.php
@@ -619,14 +619,14 @@ class GSX_Repair extends BimpObject
         switch ($status) {
             case 'RFPU':
                 if ((int) $this->getData('ready_for_pick_up')) {
-                    mailSyn2('Passage a RFPU deja RFPU', 'tommy@bimp.fr, jc.cannet@bimp.fr', null, 'Attention SAV : '.$this->getData('id_sav'));
+                    BimpTools::mailGrouper('tommy@bimp.fr, jc.cannet@bimp.fr', null, 'Passage a RFPU deja RFPU<br/>Attention SAV : '.$this->getData('id_sav'));
                     return array();
                 }
                 break;
 
             case 'SPCM':
                 if ((int) $this->getData('repair_complete')) {
-                    mailSyn2('Passage a Fermé deja Fermé', 'tommy@bimp.fr, jc.cannet@bimp.fr', null, 'Attention SAV : '.$this->getData('id_sav'));
+                    BimpTools::mailGrouper('tommy@bimp.fr, jc.cannet@bimp.fr', null, 'Passage a Fermé deja Fermé<br/>Attention SAV : '.$this->getData('id_sav'));
                     return array();
                 }
                 break;
@@ -735,7 +735,8 @@ class GSX_Repair extends BimpObject
             }
         }
         else
-            mailSyn2('Changement de statut erreur', 'tommy@bimp.fr, jc.cannet@bimp.fr', null, 'Attention SAV : '.$this->getData('id_sav'). ' changement de statut erreur '.print_r($errors,1));
+            BimpTools::mailGrouper('tommy@bimp.fr, jc.cannet@bimp.fr', null, 'Changement de statut erreur<br/>Attention SAV : '.$this->getData('id_sav'). ' changement de statut erreur '.print_r($errors,1));
+
 
         return $errors;
     }

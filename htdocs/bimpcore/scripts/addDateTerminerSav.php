9a8eeeedffb8359215b06cae2d02fe88cc65141e
tommy <tommy@192.168.1.23>
2020-02-17 16:16:15 +0100

    date_terminer SAV
    Former-commit-id: 7f3dc004db5ff452d72e1f896fca1004ddf06a83

---
+++ htdocs/bimpcore/scripts/addDateTerminerSav.php
@@ -0,0 +1,13 @@
+<?php
+require_once("../../main.inc.php");
+
+$sql = $db->query("SELECT s.id as idSav, n.date_create as date  FROM `llx_bs_sav` s, `llx_bimpcore_note` n WHERE `date_terminer` IS NULL
+AND `obj_name` = 'BS_SAV' AND `id_obj` = s.id AND `content` LIKE 'Réparation terminée le \"%\" par%'");
+
+$i=0;
+while ($ln = $db->fetch_object($sql)){
+    $db->query("UPDATE llx_bs_sav SET date_terminer = '".$ln->date."' WHERE id = ".$ln->idSav);
+    $i++;
+}
+
+echo 'fin '.$i;

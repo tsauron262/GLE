e736c9750eb304b92e39ffc40e85abd4e71418d7
tommy <tommy@192.168.1.23>
2020-02-21 21:11:50 +0100

    bug fi
    Former-commit-id: 344e0ac8d5c77ed75752adfb916119556d760bf4

--- htdocs/bimpcore/scripts/addExtraPrimeFactureDevis.php
+++ htdocs/bimpcore/scripts/addExtraPrimeFactureDevis.php
@@ -6,4 +6,5 @@ require_once __DIR__ . '/../Bimp_Lib.php';
 
 $extrafields = new ExtraFields($db);
 $extrafields->addExtraField('prime', 'Prime', 'double', 100, '24,8', 'facture', 0, 0, 0, '', 1, '', 1);
+$extrafields->addExtraField('prime', 'Prime', 'double', 100, '24,8', 'propal', 0, 0, 0, '', 1, '', 1);
 echo 'Fin';

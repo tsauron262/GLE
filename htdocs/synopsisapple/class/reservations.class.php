e53c028e8e404ba680ebab380701c2abf2633b1b
tommy <tommy@192.168.1.23>
2020-02-28 15:01:35 +0100

    bug contrat
    Former-commit-id: 0f6709d797b839e495634092a568bfddcd2f41d5

--- htdocs/synopsisapple/class/reservations.class.php
+++ htdocs/synopsisapple/class/reservations.class.php
@@ -514,12 +514,13 @@ Merci d’avoir pris rendez-vous dans notre Centre de Services Agrée Apple, nou
 Afin de préparer au mieux votre prise en charge, nous souhaitons attirer votre attention sur les points suivants :
 - Vous devez sauvegarder vos données car nous serons peut-être amenés à les effacer de votre appareil.
 
+- Vous devez désactiver la fonction « localiser » dans le menu iCloud avec votre mot de passe.
+
 - Le délai de traitement des réparations est habituellement de 7 jours.
 
 
 Conditions particulières aux iPhones
 
-- Vous devez désactiver la fonction « localiser mon iPhone » dans le menu iCloud de votre téléphone avec votre mot de passe iCloud.
 
 - Pour certains types de pannes sous garantie, un envoi de l’iPhone dans un centre Apple peut être nécessaire, entrainant un délai plus long (jusqu’à 10 jours ouvrés), dans ce cas un téléphone de prêt est possible (sous réserve de disponibilité). Si cela vous intéresse, merci de vous munir d’un chèque de caution.
 

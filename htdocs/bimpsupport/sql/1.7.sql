80110142749f7755bb2f66053628f8c2a9ff3737
tommy <tommy@192.168.1.23>
2020-03-27 16:36:08 +0100

    horth
    Former-commit-id: 3736af697054d28ac93739854586cdea6b99e3d2

---
+++ htdocs/bimpsupport/sql/1.7.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `ERP_TEST_BIMP_2019_11`.`llx_bs_remote_token` 
+( `id` INT NOT NULL AUTO_INCREMENT , 
+`id_user` INT NOT NULL , 
+`user_create` INT NOT NULL , 
+`user_update` INT NOT NULL , 
+`id_client` INT NOT NULL , 
+`token` text(30) NOT NULL default '' , 
+`port` int NOT NULL default 0 , 
+`date_valid` DATETIME NOT NULL , 
+`date_create` DATETIME NOT NULL , 
+`date_update` DATETIME NOT NULL , 
+
+PRIMARY KEY (`id`)) ENGINE = InnoDB;

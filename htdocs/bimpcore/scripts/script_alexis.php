c35048cef303eaf918a02228e7edbb6a8b97b4d2
Alexis BERNARD <al.bernard@bimp.fr>
2020-04-06 15:10:38 +0200

    Start change statut contrat
    Former-commit-id: 5a625ecc17291734c97094cbdab1cc891a11cba6

--- htdocs/bimpcore/scripts/script_alexis.php
+++ htdocs/bimpcore/scripts/script_alexis.php
@@ -9,26 +9,48 @@ require_once("../../main.inc.php");
 require_once __DIR__ . '/../Bimp_Lib.php';
 //$extrafields = new ExtraFields($db);
 //$extrafields->addExtraField('entrepot', 'Entrepot', 'varchar', 104, 8, 'contrat');
+ini_set('display_errors', 1);
+$can_execute = ($user->admin) ? true : false;
 
-/* 
- * Vendredi 13 Février 2020 
- * Insertion de la date de fin des contrats dans le champ: end_date_contrat
- */
+if($can_execute) {
+    /* 
+     * Vendredi 13 Février 2020 
+     * Insertion de la date de fin des contrats dans le champ: end_date_contrat
+     */
 
-$contrat = BimpObject::getInstance('bimpcontract', "BContract_contrat");
-$liste_contrats = $contrat->getList(['statut' => 11]);
+    //$contrat = BimpObject::getInstance('bimpcontract', "BContract_contrat");
+    //$liste_contrats = $contrat->getList(['statut' => 11]);
+    //
+    //foreach ($liste_contrats as $nb => $crt) {
+    //    $contrat->fetch($crt['rowid']);
+    //    
+    //    $errors = $contrat->updateField('end_date_contrat', $contrat->getEndDate()->format('Y-m-d'));
+    //    
+    //    if(!count($errors)) {
+    //        $validation = "<b style='color:green'>OUI</b>";
+    //    } else {
+    //        $validation = "<b style='color:red'>NON</b>";
+    //    }
+    //    
+    //    
+    //    echo '<b>' . $contrat->getData('ref') . ': '.$validation.'</b><br />';
+    //}
 
-foreach ($liste_contrats as $nb => $crt) {
-    $contrat->fetch($crt['rowid']);
+    /*
+     * Lundi 6 Avril 2020
+     * Passage de tous les anciens contrats validé en actif
+     */
     
-    $errors = $contrat->updateField('end_date_contrat', $contrat->getEndDate()->format('Y-m-d'));
     
-    if(!count($errors)) {
-        $validation = "<b style='color:green'>OUI</b>";
-    } else {
-        $validation = "<b style='color:red'>NON</b>";
+    $contrats = BimpObject::getInstance('bimpcontract', 'BContract_contrat');
+    $liste = $contrats->getList(['statut' => 1]);
+    foreach($liste as $nb => $ctr) {
+        
+        $contrats->fetch($ctr['rowid']);
+        if(!$contrats->getData('date_start')) { // Il n'on pas de date de début donc c'est des anciens contrat
+            echo $contrats->getData('ref') . '<br />';
+        }
+        
     }
     
-    
-    echo '<b>' . $contrat->getData('ref') . ': '.$validation.'</b><br />';
 }

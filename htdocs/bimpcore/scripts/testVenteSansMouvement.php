7577546d793a125f5e4bdfc3bd095887998e436f
tommy <tommy@192.168.1.23>
2020-05-05 18:08:56 +0200

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/scripts/import_products.php
    Former-commit-id: aa61fc5292a6b559e59f507977f3c309ffaa28b3

--- htdocs/bimpcore/scripts/testVenteSansMouvement.php
+++ htdocs/bimpcore/scripts/testVenteSansMouvement.php
@@ -19,7 +19,7 @@ while ($ln = $db->fetch_object($sql)){
 $sql = $db->query("SELECT va.*, v.id_entrepot FROM `llx_bc_vente_article` va, llx_bc_vente v WHERE v.status = 2 AND `date_create` > '2019-10-01 00:00:00')");
 while ($ln = $db->fetch_object($sql)){
     if(isset($idProdOk[$ln->id_product])){
-        $sql2 = $db->query("SELECT * FROM `llx_stock_mouvement` WHERE inventorycode LIKE 'VENTE".$ln->id_vente."_ART".$ln->id."' AND fk_product = ".$ln->id_product. " AND value = -".$ln->qty);
+        $sql2 = $db->query("SELECT * FROM `llx_stock_mouvement` WHERE inventorycode LIKE 'VENTE".$ln->id_vente."_ART".$ln->id."' AND fk_entrepot = ".$ln->id_entrepot." AND fk_product = ".$ln->id_product. " AND value = -".$ln->qty);
         if($db->num_rows($sql2) != 1){
             echo ("<br/>probléme vente : ".$ln->id_vente." prod : ".$ln->id_product. " qty : ".$ln->qty);
             

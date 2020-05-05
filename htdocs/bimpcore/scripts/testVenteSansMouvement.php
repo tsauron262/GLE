8b796f20ecd0e07c58bbee7b104ffac8e23e27bf
tommy <tommy@192.168.1.23>
2020-05-05 16:57:25 +0200

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/scripts/import_products.php
    Former-commit-id: 9608001502eec2a700360fe99af6ae41ca2ed8ac

---
+++ htdocs/bimpcore/scripts/testVenteSansMouvement.php
@@ -0,0 +1,27 @@
+<?php
+
+
+require_once("../../main.inc.php");
+
+
+require_once __DIR__ . '/../Bimp_Lib.php';
+
+
+
+$sql = $db->query("SELECT rowid FROM `llx_product` WHERE fk_product_type = 0");
+$idProdOk[] = array();
+while ($ln = $db->fetch_object($sql)){
+    $idProdOk[$ln->rowid] = $ln->rowid;
+}
+
+
+
+$sql = $db->query("SELECT * FROM `llx_bc_vente_article` WHERE `id_vente` IN (SELECT id FROM llx_bc_vente v WHERE v.status = 2)");
+while ($ln = $db->fetch_object($sql)){
+    if(isset($idProdOk[$ln->id_product])){
+        $sql2 = $db->query("SELECT * FROM `llx_stock_mouvement` WHERE inventorycode LIKE 'VENTE".$ln->id_vente."_ART".$ln->id."' AND fk_product = ".$ln->id_product. " AND value = -".$ln->qty);
+        if($db->num_rows($sql2) != 1){
+            echo ("<br/>probléme vente ".$ln->id_vente." prod : ".$ln->id_product. "     SELECT * FROM `llx_stock_mouvement` WHERE 'VENTE".$ln->id_vente."_ART".$ln->id."' AND fk_product = ".$ln->id_product. " AND value = -".$ln->qty);
+        }
+    }
+}

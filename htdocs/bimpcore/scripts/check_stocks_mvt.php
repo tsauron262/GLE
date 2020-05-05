d70f953e6d467f3d13a83a96cb99084021585c59
flo <flo@192.168.0.11>
2020-05-06 00:24:10 +0200

    Check stocks
    Former-commit-id: cab5f1700a9ff97470eb26ac4c5040bb3a40cb93

---
+++ htdocs/bimpcore/scripts/check_stocks_mvt.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once("../../main.inc.php");
+
+ini_set('display_errors', 1);
+require_once __DIR__ . '/../Bimp_Lib.php';
+set_time_limit(0);
+
+ignore_user_abort(0);
+
+top_htmlhead('', 'CHECK MVTS STOCKS', 0, 0, array(), array());
+
+echo '<body>';
+
+BimpCore::displayHeaderFiles();
+
+global $db, $user;
+
+if (!BimpObject::objectLoaded($user)) {
+    echo BimpRender::renderAlerts('Aucun utilisateur connecté');
+    exit;
+}
+
+if (!$user->admin) {
+    echo BimpRender::renderAlerts('Seuls les admin peuvent exécuter ce script');
+}
+
+$bdb = new BimpDb($db);
+
+// Articles ventes: 
+//$codes = $bdb->getRows('stock_mouvement', 'inventorycode LIKE \'VENTE%_ART%\'', null, 'array', array('inventorycode'));
+//
+//echo $bdb->db->lasterror();
+//
+//echo '<pre>';
+//print_r($codes);
+//exit;
+//
+//$sql = 'SELECT va.id as id_art, v.id as id_vente FROM llx_bc_vente_article va';
+//$sql .= ' LEFT JOIN llx_bc_vente v ON v.id = va.id_vente';
+//$sql .= ' WHERE v.status = 2';
+//$sql .= ' AND (SELECT COUNT(sm.rowid) FROM llx_stock_mouvement sm WHERE CONCAT(\'VENTE\', va.id_vente, \'_ART\', va.id) = sm.inventorycode) = 0';
+//
+//$rows = $bdb->executeS($sql, 'array');
+//
+//foreach ($rows as $r) {
+//    $mvt = $bdb->getRow('stock_mouvement', 'inventorycode LIKE \'VENTE' . $r['id_vente'] . '_ART' . $r['id_art'] . '\'');
+//
+//    if (is_null($mvt)) {
+//        echo 'MOUVEMENT absent pour ART #' . $r['id_art'] . ' - VENTE #' . $r['id_vente'] . '<br/>';
+//    }
+//}
+// Expés commandes: 
+
+$rows = $bdb->getRows('stock_mouvement', 'inventorycode LIKE \'CO%_EXP%\'', null, 'array', array('inventorycode'));
+$codes = array();
+foreach ($rows as $r) {
+    $codes[] = $r['inventorycode'];
+}
+
+$sql = 'SELECT s.id as ids, c.rowid as idc FROM llx_bl_commande_shipment s';
+$sql .= ' LEFT JOIN llx_commande c ON c.rowid = s.id_commande_client';
+$sql .= ' WHERE s.status > 1';
+//$sql .= ' AND (SELECT COUNT(sm.rowid) FROM llx_stock_mouvement sm WHERE CONCAT(\'VENTE\', va.id_vente, \'_ART\', va.id) = sm.inventorycode) = 0';
+
+$rows = $bdb->executeS($sql, 'array');
+
+$txt = '';
+
+$n = 0;
+$ok = 0;
+
+foreach ($rows as $r) {
+    if (!in_array('CO' . $r['idc'] . '_EXP' . $r['ids'], $codes)) {
+        $txt .= 'MVT ABSENT pour comm #' . $r['idc'] . ' - EXP #' . $r['ids'] . '<br/>';
+        $n++;
+    } else {
+        $ok++;
+    }
+}
+
+echo 'OK: ' . $ok . '<br/><br/>';
+echo 'NB ERREURS: ' . $n . '<br/><br/>';
+echo $txt;
+echo '<br/>FIN';
+echo '</body></html>';
+
+//llxFooter();

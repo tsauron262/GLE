bb97e7436bca9a267cf991bade8cbc8267a86a43
tommy <tommy@192.168.1.23>
2020-05-15 11:53:18 +0200

    restriction majedi
    Former-commit-id: 2bdf83c769033de63f087d22f706914fbc5d5fb8

--- htdocs/bimpcore/scripts/check_tot_comm_tot_fact.php
+++ htdocs/bimpcore/scripts/check_tot_comm_tot_fact.php
@@ -26,8 +26,10 @@ while ($ln = $db->fetch_object($sql)){
         $sql2 = $db->query("SELECT * FROM `llx_commandedet` WHERE `fk_commande` = ".$ln->crowid. ' AND fk_product > 0 AND fk_product NOT IN (SELECT `fk_product` FROM `llx_facturedet` WHERE `fk_facture` = '.$ln->frowid.')');
         while ($ln2 = $db->fetch_object($sql2)){
             
-            
-            echo $ln->frowid."<br/>";
+            $fact = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Facture', $ln->frowid);
+            echo $fact->getLink()."<br/>";
+            $comm = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Commande', $ln->crowid);
+            echo $comm->getLink()."<br/>";
             print_r($ln2);
             echo "<br/><br/>";
         }

182acc4a751ec8b9e75e3d307e0b0399d57cb6cb
tommy <tommy@192.168.1.23>
2020-06-08 15:37:58 +0200

    addhook
    Former-commit-id: 672661abe5776c6894804ab63acff5e227c685d1

--- htdocs/bimptheme/switch.php
+++ htdocs/bimptheme/switch.php
@@ -18,5 +18,6 @@ else{
     $db->query("INSERT INTO `llx_user_param`(`fk_user`, `entity`, `param`, `value`) VALUES (".$user->id.", ".$conf->entity.", 'MAIN_THEME', 'BimpTheme')");
 }
 
-
+if($_SERVER['HTTP_REFERER'] == "")
+    $_SERVER['HTTP_REFERER'] = '/';
 header("location:".  $_SERVER['HTTP_REFERER']); 

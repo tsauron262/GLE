a99f4c4c567f6d361a0a8da79998c18ce439b783
flo <flo@192.168.0.11>
2020-06-11 01:57:08 +0200

    Maj relances
    Former-commit-id: 0f8fc47660b3d11b3df21fad73635e5c107f5c54

--- htdocs/bimpcommercial/core/modules/modBimpCommercial.class.php
+++ htdocs/bimpcommercial/core/modules/modBimpCommercial.class.php
@@ -353,7 +353,7 @@ class modBimpCommercial extends DolibarrModules
 		$r++;
                 
                 $this->rights[$r][0] = $this->numero + $r;	// Permission id (must not be already used)
-		$this->rights[$r][1] = 'Désactivation des relances auto (1 mois)';	// Permission label
+		$this->rights[$r][1] = 'Désactivation des relances auto 1 mois';	// Permission label
 		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'deactivate_relances_one_month';				// In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)
 //		$this->rights[$r][5] = 'valid';				    // In php code, permission will be checked by test if ($user->rights->mymodule->level1->level2)

cc0b23d4c6aedeb973fcf2eb245629c8fead6dac
flo <flo@192.168.0.11>
2020-07-29 14:43:56 +0200

    Maj AddLog
    Former-commit-id: 466c8af5ebc584a6f16b72dd3081b0ae86bbf935

--- htdocs/bimpcore/classes/BimpCore.php
+++ htdocs/bimpcore/classes/BimpCore.php
@@ -326,32 +326,32 @@ class BimpCore
 
     public static function addlog($msg, $level = 1, $type = 'bimpcore', $object = null, $extra_data = array())
     {
-        global $user;
-
-        $mod = '';
-        $obj = '';
-        $id = 0;
-
-        if (is_a($object, 'BimpObject')) {
-            $mod = $object->module;
-            $obj = $object->object_name;
-            $id = (int) $object->id;
-        }
-
-        $errors = array();
-
-        $log = BimpObject::createBimpObject('bimpcore', 'Bimp_Log', array(
-                    'id_user'    => (BimpObject::objectLoaded($user) ? (int) $user->id : 1),
-                    'type'       => $type,
-                    'level'      => $level,
-                    'msg'        => $msg,
-                    'obj_module' => $mod,
-                    'obj_name'   => $obj,
-                    'id_object'  => $id,
-                    'extra_data' => $extra_data
-                        ), true, $errors);
-
-        return $errors;
+//        global $user;
+//
+//        $mod = '';
+//        $obj = '';
+//        $id = 0;
+//
+//        if (is_a($object, 'BimpObject')) {
+//            $mod = $object->module;
+//            $obj = $object->object_name;
+//            $id = (int) $object->id;
+//        }
+//
+//        $errors = array();
+//
+//        $log = BimpObject::createBimpObject('bimpcore', 'Bimp_Log', array(
+//                    'id_user'    => (BimpObject::objectLoaded($user) ? (int) $user->id : 1),
+//                    'type'       => $type,
+//                    'level'      => $level,
+//                    'msg'        => $msg,
+//                    'obj_module' => $mod,
+//                    'obj_name'   => $obj,
+//                    'id_object'  => $id,
+//                    'extra_data' => $extra_data
+//                        ), true, $errors);
+//
+//        return $errors;
     }
 
     // Chargements librairies: 

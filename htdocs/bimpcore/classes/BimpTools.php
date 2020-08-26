f560e44177cfc7d72c3c7e6b6782db1d96f08752
flo <flo@192.168.0.11>
2020-08-26 16:28:28 +0200

    Correctifs Logs
    Former-commit-id: ffbca2260720717ade8012329bd5905866b90a51

--- htdocs/bimpcore/classes/BimpTools.php
+++ htdocs/bimpcore/classes/BimpTools.php
@@ -1557,7 +1557,7 @@ class BimpTools
             }
         }
 
-        if (preg_match('/^(\d{4}\-\d{2}\-\d{2}).?(\d{2}:\d{2}:\d{2})?.*$/', $value, $matches)) {
+        if (is_string($value) && preg_match('/^(\d{4}\-\d{2}\-\d{2}).?(\d{2}:\d{2}:\d{2})?.*$/', $value, $matches)) {
             if (preg_match('/^1970\-01\-01.*$/', $value)) {
                 return '';
             }

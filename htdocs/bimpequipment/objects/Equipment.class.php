d3996bc25d036c1ef1fb96b10fe0bd53e5dda344
tommy <tommy@192.168.1.23>
2020-09-04 11:45:54 +0200

    cache
    Former-commit-id: f727debcb8d2100c87e23e39344913f8f036f002

--- htdocs/bimpequipment/objects/Equipment.class.php
+++ htdocs/bimpequipment/objects/Equipment.class.php
@@ -101,8 +101,8 @@ class Equipment extends BimpObject
 
         // Check de la présence dans l\'entrepôt. 
         if ((int) $id_entrepot) {
-            $errors[] = 'Pas dans l\'entrepot';
             if (!$this->isInEntrepot($id_entrepot, $errors)) {
+                $errors[] = 'Pas dans l\'entrepot';
                 return 0;
             }
         }

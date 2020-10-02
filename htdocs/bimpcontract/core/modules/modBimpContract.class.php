7b63dcad59d89b61415a81b5653ff71881d3769e
Alexis BERNARD <al.bernard@bimp.fr>
2020-10-02 15:02:54 +0200

    Rights change periodicity groupe contrat
    Former-commit-id: a8aa21e643d120ef3965cf6c5c0399fc3b0a86d8

--- htdocs/bimpcontract/core/modules/modBimpContract.class.php
+++ htdocs/bimpcontract/core/modules/modBimpContract.class.php
@@ -94,6 +94,12 @@ class modBimpContract extends DolibarrModules {
         $this->rights[$r][3] = 0;
         $this->rights[$r][4] = 'auto_billing'; 
         $this->menu = array();   // List of menus to add
+        $r++;
+        $this->rights[$r][0] = $this->numero + $r;
+        $this->rights[$r][1] = 'Changer la périodicité de facturation';
+        $this->rights[$r][3] = 0;
+        $this->rights[$r][4] = 'change_periodicity'; 
+        $this->menu = array();   // List of menus to add
         $r = 1;
 
     }

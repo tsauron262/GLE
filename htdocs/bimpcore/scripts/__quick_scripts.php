6f2ba49df9079ba702900b1c411d19438018547a
tommy <tommy@192.168.1.23>
2020-10-20 16:31:37 +0200

    log déplacment paiement
    Former-commit-id: 22671d5b9c571ce06296dc238dad96ed95af502f

--- htdocs/bimpcore/scripts/__quick_scripts.php
+++ htdocs/bimpcore/scripts/__quick_scripts.php
@@ -57,7 +57,7 @@ if (!$action) {
 switch ($action) {
     case 'traite_obsolete':
         global $db;
-        $sql = $db->query("SELECT DISTINCT (a.rowid) FROM llx_product a LEFT JOIN llx_product_extrafields ef ON a.rowid = ef.fk_object WHERE (a.stock BETWEEN '0' AND '0') AND a.tosell IN ('1') AND (ef.famille = 3097) ORDER BY a.ref DESC LIMIT 0, 50");
+        $sql = $db->query("SELECT DISTINCT (a.rowid) FROM llx_product a LEFT JOIN llx_product_extrafields ef ON a.rowid = ef.fk_object WHERE (a.stock BETWEEN '0' AND '0') AND a.tosell IN ('1') AND (ef.famille = 3097) ORDER BY a.ref DESC");
         while($ln = $db->fetch_object($sql))
                 $db->query("UPDATE `llx_product` SET `tosell` = 0, `tobuy` = 0 WHERE rowid = ".$ln->rowid);
     case 'correct_prod_cur_pa':

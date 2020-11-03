623ad56cbeb2a3eee00e450a14db2692f69ecb5d
tommy <tommy@192.168.1.23>
2020-11-03 14:15:21 +0100

    rataché fact
    Former-commit-id: f244fd453a4c0ae891e3229c2b900040c077d9ad

--- htdocs/bimpcore/classes/components/BC_ListTable.php
+++ htdocs/bimpcore/classes/components/BC_ListTable.php
@@ -1595,9 +1595,17 @@ class BC_ListTable extends BC_List
                     if ($row['cols'][$col_name]['col_style']) {
                         $html .= $row['cols'][$col_name]['col_style'];
                     }
+                    
                     $html .= '"' . ($row['params']['single_cell'] ? ' colspan="' . count($this->cols) . '"' : '') . '>';
+                    
                     if ((int) $row['cols'][$col_name]['show']) {
+                        if($col_name == $this->object->getRefField() && (int) $item_params['page_btn'] && $row['params']['url']) {
+                            $html .= '<a href=\'' . $row['params']['url'] . '\'>';
+                        }
                         $html .= (isset($row['cols'][$col_name]['content']) ? $row['cols'][$col_name]['content'] : '');
+                        if($col_name == $this->object->getRefField() && (int) $item_params['page_btn'] && $row['params']['url']) {
+                            $html .= '</a>';
+                        }
                     }
                     $html .= '</td>';
                 }

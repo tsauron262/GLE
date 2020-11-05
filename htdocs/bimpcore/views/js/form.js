8b2a03c9d8c1e6c3abc21e3df75b6f8e008a1ef1
flo <flo@192.168.0.11>
2020-11-05 19:00:14 +0100

    Revals en série lignes fac.
    Former-commit-id: e5bb96b82aca31879d705731fd13c345159d1412

--- htdocs/bimpcore/views/js/form.js
+++ htdocs/bimpcore/views/js/form.js
@@ -3113,23 +3113,23 @@ function setSearchListOptionsEvents($container) {
 }
 
 function setSelectDisplayHelpEvents($container, $input) {
-    if ($input.tagName() === 'select') {
-        if (!$input.data('select_help_event_init')) {
-            $input.change(function () {
-                var $inputContainer = $input.findParentByClass('inputContainer');
-                if ($.isOk($inputContainer)) {
-                    var field_name = $inputContainer.data('field_name');
-                    $inputContainer.find('div.selectOptionHelp').stop().hide().removeAttr('style');
-                    var $div = $inputContainer.find('div.' + field_name + '_' + $input.val() + '_help');
-                    if ($.isOk($div)) {
-                        $div.slideDown(250);
-                    }
-                }
-            });
-            $input.data('select_help_event_init', 1);
-            $input.change();
-        }
-    }
+//    if ($input.tagName() === 'select') {
+//        if (!$input.data('select_help_event_init')) {
+//            $input.change(function () {
+//                var $inputContainer = $input.findParentByClass('inputContainer');
+//                if ($.isOk($inputContainer)) {
+//                    var field_name = $inputContainer.data('field_name');
+//                    $inputContainer.find('div.selectOptionHelp').stop().hide().removeAttr('style');
+//                    var $div = $inputContainer.find('div.' + field_name + '_' + $input.val() + '_help');
+//                    if ($.isOk($div)) {
+//                        $div.slideDown(250);
+//                    }
+//                }
+//            });
+//            $input.data('select_help_event_init', 1);
+//            $input.change();
+//        }
+//    }
 }
 
 function setSortableMultipleValuesHandlesEvents($container) {

f542354fec656faa9271d33b9d4a292de176d63b
flo <flo@192.168.0.11>
2020-11-05 18:58:10 +0100

    Revals en série lignes fac.
    Former-commit-id: 1e5fecb5d9d6e0a28d6538222f7f785a70b64683

--- htdocs/bimpcore/views/js/form.js
+++ htdocs/bimpcore/views/js/form.js
@@ -3115,10 +3115,10 @@ function setSearchListOptionsEvents($container) {
 function setSelectDisplayHelpEvents($container, $input) {
     if ($input.tagName() === 'select') {
         if (!$input.data('select_help_event_init')) {
-            var field_name = $input.attr('name');
             $input.change(function () {
                 var $inputContainer = $input.findParentByClass('inputContainer');
                 if ($.isOk($inputContainer)) {
+                    var field_name = $inputContainer.data('field_name');
                     $inputContainer.find('div.selectOptionHelp').stop().hide().removeAttr('style');
                     var $div = $inputContainer.find('div.' + field_name + '_' + $input.val() + '_help');
                     if ($.isOk($div)) {

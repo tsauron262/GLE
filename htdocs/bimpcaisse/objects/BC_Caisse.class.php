0a4337dcc747b1eb7ca2bd1e725bd77fb1ed28e4
tommy <tommy@192.168.1.23>
2020-11-13 14:22:00 +0100

    Merge origin/master
    Conflicts:
    	htdocs/bimpcommercial/objects/Bimp_Commande.class.php
    Former-commit-id: fa90de56e02496d2640916ae27263f1e30286c96

--- htdocs/bimpcaisse/objects/BC_Caisse.class.php
+++ htdocs/bimpcaisse/objects/BC_Caisse.class.php
@@ -2,7 +2,7 @@
 
 class BC_Caisse extends BimpObject
 {
-    public static $useYounited = true;
+    public static $useYounited = false;
 
     public static $states = array(
         0 => array('label' => 'Fermée', 'icon' => 'times', 'classes' => array('danger')),

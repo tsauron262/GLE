cfb1cb27b6914159a88b58bead570f09c0de2884
tommy <tommy@192.168.1.23>
2020-12-01 17:33:58 +0100

    add association reverse
    Former-commit-id: a150da2f3b04b9ebed6c97dbfcd3226c3a7ef782

--- htdocs/synopsissignature/signature.php
+++ htdocs/synopsissignature/signature.php
@@ -267,7 +267,7 @@ if ($selectedFile) {
         echo "<br/><h4>Lien : <a href='" . $lien . "'>" . $lien . "</a></h4>";
     } else if (isset($_REQUEST['img'])) {
         if (!is_dir($dirTmp . "/temp"))
-            mkdir($dirTmp . "/temp",'0777',true);
+            mkdir($dirTmp . "/temp",null,true);
         $nomSign = $dirTmp . "/temp/signature.png";
         base64_to_jpeg($_REQUEST['img'], $nomSign);
 

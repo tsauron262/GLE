65d7d1adc014d81a1ba8d7a3b26fd343a1923b3c
tommy <tommy@192.168.1.23>
2020-12-01 17:26:11 +0100

    add association reverse
    Former-commit-id: f164ab93e67a65c6ade170a6d6511d11784d140b

--- htdocs/synopsissignature/signature.php
+++ htdocs/synopsissignature/signature.php
@@ -226,6 +226,7 @@ if ($object != $soc && $socid > 0) {
 
 
 $dir = DOL_DATA_ROOT . "/" . $dirFile . "/" . $object->$clef;
+$dirTmp = PATH_TMP . "/" . $dirFile . "/" . $object->$clef;
 if (!$selectedFile) {
     $filearray = dol_dir_list($dir, "files");
     $filearray2 = array();
@@ -265,9 +266,9 @@ if ($selectedFile) {
         $lien = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . DOL_URL_ROOT . "/synopsissignature/signature.php?code=" . $code;
         echo "<br/><h4>Lien : <a href='" . $lien . "'>" . $lien . "</a></h4>";
     } else if (isset($_REQUEST['img'])) {
-        if (!is_dir($dir . "/temp"))
-            mkdir($dir . "/temp");
-        $nomSign = $dir . "/temp/signature.png";
+        if (!is_dir($dirTmp . "/temp"))
+            mkdir($dirTmp . "/temp");
+        $nomSign = $dirTmp . "/temp/signature.png";
         base64_to_jpeg($_REQUEST['img'], $nomSign);
 
         $afficheSign = false;

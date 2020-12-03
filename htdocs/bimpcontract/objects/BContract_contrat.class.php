d2bc62c5595463cfed0a7bca32b64e8887c083ba
tommy <tommy@192.168.1.23>
2020-12-03 16:08:52 +0100

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/classes/components/BC_ListTable.php
    Former-commit-id: 351c8fd7f4bd772bcb27a93f52dd4c8d609b0743

--- htdocs/bimpcontract/objects/BContract_contrat.class.php
+++ htdocs/bimpcontract/objects/BContract_contrat.class.php
@@ -2478,8 +2478,12 @@ class BContract_contrat extends BimpDolObject {
     }
 
     public function renderHeaderStatusExtra() {
-
         $extra = '';
+        $notes = $this->getNotes();
+        $nb = count($notes);
+        if($nb > 0)
+            $extra .= '<br/><span class="warning"><span class="badge badge-warning">'.$nb.'</span> Note'.($nb>1 ? 's' : '').'</span>';
+
         if (!is_null($this->getData('date_contrat'))) {
             $extra .= '<br/><span class="important">' . BimpRender::renderIcon('fas_signature', 'iconLeft') . 'Contrat signé</span>';
         }

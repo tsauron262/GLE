0803abeaa69d9060378e672bab25d9b1f4d0d8b9
tommy <tommy@192.168.1.23>
2020-12-04 10:49:30 +0100

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/classes/components/BC_ListTable.php
    Former-commit-id: 9b314bba6a5dcf7321000ba88e308116e9ca9179

--- htdocs/bimpcontract/objects/BContract_contrat.class.php
+++ htdocs/bimpcontract/objects/BContract_contrat.class.php
@@ -1744,11 +1744,11 @@ class BContract_contrat extends BimpDolObject {
                 dol_syslog(get_class($this)."::actionValidation Renomer => ".$dirsource." => ".$dirdest);
                 if (rename($dirsource, $dirdest)){
                     dol_syslog("Renomer avec succès");
-                    if(file_exists($dirsource . '/Contrat_' . $oldref . '_Ex_OLYS.pdf')) {
-                        unlink($dirsource . '/Contrat_' . $oldref . '_Ex_OLYS.pdf');
+                    if(file_exists($dirdest . '/Contrat_' . $dirdest . '_Ex_OLYS.pdf')) {
+                        unlink($dirdest . '/Contrat_' . $dirdest . '_Ex_OLYS.pdf');
                     }
-                    if(file_exists($dirsource . '/Contrat_' . $oldref . '_Ex_Client.pdf')){
-                        unlink($dirsource . '/Contrat_' . $oldref . '_Ex_Client.pdf');
+                    if(file_exists($dirdest . '/Contrat_' . $dirdest . '_Ex_Client.pdf')){
+                        unlink($dirdest . '/Contrat_' . $dirdest . '_Ex_Client.pdf');
                     }
                     
                     $listoffiles=dol_dir_list($conf->contract->dir_output.'/'.$newref, 'files', 1, '^'.preg_quote($oldref,'/'));

df02d95d34ff022fdc264f9d7a63c80b992c31bc
tommy <tommy@192.168.1.23>
2020-12-04 19:32:03 +0100

    cache serveur
    Former-commit-id: 4728c953f929285d0336cb17bc65911a31fcbc40

--- htdocs/bimpcore/classes/BimpCacheRedis.php
+++ htdocs/bimpcore/classes/BimpCacheRedis.php
@@ -1,18 +1,29 @@
 <?php
 
 class BimpCacheRedis{
-    static $REDIS_LOCALHOST_SOCKET = "/var/run/redis/redis.sock";
+    static $REDIS_LOCALHOST_SOCKET = "/var/run/redis/redis.socks";
     static $redisObj = null;
+    static $isActif = true;
     
     public static function init(){
         if(is_null(static:: $redisObj)){
             static::$redisObj = new Redis();
-            static::$redisObj->connect(static::$REDIS_LOCALHOST_SOCKET);
+            
+            try {
+                static::$redisObj->connect(static::$REDIS_LOCALHOST_SOCKET);
+            }
+            catch( Exception $e ){
+                static::$isActif = false;
+            }
         }
     }
     
     public static function getCacheServeur($key){
+        if(!static::$isActif)
+            return null;
         static::init();
+        if(!static::$isActif)
+            return null;
         $result = static::$redisObj->get($key);
         if($result == '')
             return null;
@@ -26,6 +37,8 @@ class BimpCacheRedis{
     }
     
     public static function setCacheServeur($key, $value){
+        if(!static::$isActif)
+            return false;
         if(is_null($value))
             $value = "valnull";
         if($value == '')

822e4e378ae307463d29dadfa377639cd4c3da07
tommy <tommy@192.168.1.23>
2020-12-08 10:24:14 +0100

    cache serveur
    Former-commit-id: f05f55724749f000f73f78fb76c7d2129cd0a9aa

--- htdocs/bimpsupport/objects/BS_ApplePart.class.php
+++ htdocs/bimpsupport/objects/BS_ApplePart.class.php
@@ -299,7 +299,7 @@ class BS_ApplePart extends BimpObject
         $tabException1 = array("F661", "AB661", "SF661","B661",  "J661", "E661");
         foreach($tabException1 as $exception1){
             if(stripos($ref, $exception1) !== false && $prix == 130.63)
-                    return 209;
+                    return (209/1.2);
         }
         
         

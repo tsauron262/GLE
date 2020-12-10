0dea3da4dce0323fceb606962aef155a945cd3cc
tommy <tommy@192.168.1.23>
2020-12-10 12:59:08 +0100

    cache serveur
    Former-commit-id: ae7f6f6f8c64915cfeec6f1d17579aa1319f9edc

--- htdocs/bimpcore/classes/FixeTabs.php
+++ htdocs/bimpcore/classes/FixeTabs.php
@@ -69,47 +69,47 @@ class FixeTabs
     {
         $html = '';
 
-//        if (!$content_only) {
-//            $html .= '<div id="bimp_fixe_tabs">';
-//        }
-//
-//        $html .= '<div id="bimp_fixe_tabs_captions">';
-//
-//        $html .= '<div class="fixe_tabs_loading">';
-//        $html .= '<i class="fa fa-spinner fa-spin"></i>';
-//        $html .= '</div>';
-//
-//        $html .= '<div class="fixe_tabs_refresh_btn">';
-//        $html .= '<span onclick="bimpFixeTabs.hold = false;bimpFixeTabs.active = true;bimpFixeTabs.reload();">';
-//        $html .= BimpRender::renderIcon('fas_sync');
-//        $html .= '</span>';
-//        $html .= '</div>';
-//
-//        foreach ($this->tabs as $tab) {
-//            $html .= '<div class="fixe_tab_caption';
-//            if (count($tab['classes'])) {
-//                foreach ($tab['classes'] as $class) {
-//                    $html .= ' ' . $class;
-//                }
-//            }
-//            $html .= '" id="fixe_tab_caption_' . $tab['id'] . '" data-id_tab="' . $tab['id'] . '">';
-//
-//            $html .= '<span class="title">' . $tab['caption'] . '</span>';
-//            $html .= '</div>';
-//        }
-//        $html .= '</div>';
-//
-//        $html .= '<div id="bimp_fixe_tabs_contents">'; // Ne pas mettre de styles dans cette balise (conflit js). 
-//        foreach ($this->tabs as $tab) {
-//            $html .= '<div class="fixe_tab_content" id="fixe_tab_content_' . $tab['id'] . '" data-id_tab="' . $tab['id'] . '">';
-//            $html .= $tab['content'];
-//            $html .= '</div>';
-//        }
-//        $html .= '</div>';
-//
-//        if (!$content_only) {
-//            $html .= '</div>';
-//        }
+        if (!$content_only) {
+            $html .= '<div id="bimp_fixe_tabs">';
+        }
+
+        $html .= '<div id="bimp_fixe_tabs_captions">';
+
+        $html .= '<div class="fixe_tabs_loading">';
+        $html .= '<i class="fa fa-spinner fa-spin"></i>';
+        $html .= '</div>';
+
+        $html .= '<div class="fixe_tabs_refresh_btn">';
+        $html .= '<span onclick="bimpFixeTabs.hold = false;bimpFixeTabs.active = true;bimpFixeTabs.reload();">';
+        $html .= BimpRender::renderIcon('fas_sync');
+        $html .= '</span>';
+        $html .= '</div>';
+
+        foreach ($this->tabs as $tab) {
+            $html .= '<div class="fixe_tab_caption';
+            if (count($tab['classes'])) {
+                foreach ($tab['classes'] as $class) {
+                    $html .= ' ' . $class;
+                }
+            }
+            $html .= '" id="fixe_tab_caption_' . $tab['id'] . '" data-id_tab="' . $tab['id'] . '">';
+
+            $html .= '<span class="title">' . $tab['caption'] . '</span>';
+            $html .= '</div>';
+        }
+        $html .= '</div>';
+
+        $html .= '<div id="bimp_fixe_tabs_contents">'; // Ne pas mettre de styles dans cette balise (conflit js). 
+        foreach ($this->tabs as $tab) {
+            $html .= '<div class="fixe_tab_content" id="fixe_tab_content_' . $tab['id'] . '" data-id_tab="' . $tab['id'] . '">';
+            $html .= $tab['content'];
+            $html .= '</div>';
+        }
+        $html .= '</div>';
+
+        if (!$content_only) {
+            $html .= '</div>';
+        }
 
         return $html;
     }

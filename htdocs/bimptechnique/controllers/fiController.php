0b701a3b9cb583c49736cbdb95b34ee071726583
Alexis BERNARD <al.bernard@bimp.fr>
2020-12-22 14:34:19 +0100

    FI test
    Former-commit-id: 866ce247f45467c1328b84f794e21280765a4c92

---
+++ htdocs/bimptechnique/controllers/fiController.php
@@ -0,0 +1,75 @@
+<?php
+
+class fiController extends BimpController {
+    
+    protected function ajaxProcessSignFi() {
+        global $user, $db;
+        $controlle = BimpTools::getPostFieldValue('controlle');
+        $base64 = BimpTools::getPostFieldValue('base64');
+        $nom = BimpTools::getPostFieldValue('nom');
+        $isChecked = BimpTools::getPostFieldValue('isChecked');
+        $itsOkForSign = (is_null($controlle)) ? false : true;
+        $email = BimpTools::getPostFieldValue('email');
+        $success = "";
+        $errors = array();
+        if(!$itsOkForSign && $isChecked == 'false') { $errors[] = "Il doit y avoir une signature"; }
+        if(empty($nom)) { $errors[] = "Il doit y avoir un nom de signataire"; }
+        if(empty($email)) { $errors[] = "Il doit y avoir un email"; }
+        
+        if(!empty($email)) {
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "Email invalide";
+            }
+        }
+        
+        $instance = BimpObject::getInstance('bimptechnique', 'BT_ficheInter', $_REQUEST['id']);
+        if(!count($errors)) {
+            if($instance->isLoaded()) {
+                $instance->updateField('signed', 1);
+                
+                if($isChecked == 'false') {
+                    $instance->updateField('base_64_signature', $base64);
+                }
+                $errors = $instance->updateField('signataire', $nom);
+                if(!count($errors)) {
+                    $instance->dol_object->setValid($user);
+                    
+                    $today = date('Y-m-d');
+                    
+                    $sql_where = 'code = "RC_RDV" AND fk_user_author = ' . $user->id . ' AND datep LIKE "'.$today.'%"';
+                    
+                    $bdd = new BimpDb($db);
+                    
+                    $id_actionComm = $bdd->getValue('actioncomm', 'id', $sql_where);
+                    
+//                    if(count(json_decode($instance->getData('tickets'))) > 0) {
+//                        foreach(json_decode($instance->getData('tickets')) as $id_ticket) {
+//                            $ticket = BimpObject::getInstance('bimpsupport', "BS_Ticket", $id_ticket);
+//                            if($ticket->getData('')) {
+//                                
+//                            }
+//                        }
+//                    }
+                    
+                    if($id_actionComm > 0) {
+                        $actionComm = $this->getInstance("bimpcore", "Bimp_ActionComm", $id_actionComm);
+                        $actionComm->updateField("percent", 100);
+                    }
+                    
+                    $success = "Rapport signé avec succès";
+                }
+            } else {
+                $errors[] = "Erreur lors du load de la fiche d'intervention";
+            }
+        }
+        
+        die(json_encode(array(
+            'success_callback'    => "window.location.href = '".DOL_URL_ROOT."/bimptechnique/?fc=fi&id=".$instance->id."'",
+            'request_id' => BimpTools::getValue('request_id', 0), // Imporant
+            'errors' => $errors
+        )));
+    }
+    
+    
+    
+}

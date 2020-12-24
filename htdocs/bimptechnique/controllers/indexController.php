2578c35a9f27337098feadc95806b44506f2ca67
Alexis BERNARD <al.bernard@bimp.fr>
2020-12-24 15:49:20 +0100

    Panel index texh fi
    Former-commit-id: beb490913bf40ff101fcebc22bace687dd63c4d6

--- htdocs/bimptechnique/controllers/indexController.php
+++ htdocs/bimptechnique/controllers/indexController.php
@@ -16,23 +16,34 @@ class indexController extends BimpController {
         global $user;
         $html = "";
         
+        if(isset($_REQUEST['specialTech']) && $_REQUEST['specialTech'] > 0)
+            $userId = $_REQUEST['specialTech'];
+        else
+            $userId = $user->id;
         
-        
-        
-        $tech = BimpObject::getInstance('bimpcore', "Bimp_User", $user->id);
+        $tech = BimpObject::getInstance('bimpcore', "Bimp_User", $userId);
         
         $your_fiches = BimpObject::getInstance('bimptechnique', 'BT_ficheInter');
         $list = new BC_ListTable($your_fiches, "tech");
-        
+        $list->setParam('n', 5);
         $buttons[] = array(
-                            'label' => 'Plannifier une intervention',
-                            'icon' => 'fas_calendar',
-                            'onclick' => $your_fiches->getJsActionOnclick('createFromRien', array(), array(
-                                'form_name' => 'createFiche'
-                            ))
-                    );
+            'label' => 'Plannifier une intervention',
+            'icon' => 'fas_calendar',
+            'onclick' => $your_fiches->getJsActionOnclick('createFromRien', array(), array(
+                'form_name' => 'createFiche'
+            ))
+        );
+        
+        if($user->id != $tech->id) {
+            $html .= "<h3><strong class='warning' >".BimpRender::renderIcon('warning')."</strong><strong>Vous regardez les intervention de </strong><strong class='warning'>".$tech->getName()."</strong></h3>";
+        }
         
         $html .= BimpRender::renderButtonsGroup($buttons, []);
+        $html .= $list->renderHtml();
+        
+        $list = new BC_ListTable($your_fiches, 'historiqueTech');
+        $list->setParam('title', "Liste des fiches d'interventions de l'ancien module");
+        $list->setParam('n', 5);
         $html .= $list->renderHtml();
         
         if(isset($_REQUEST['annee']) && $_REQUEST['annee'] != "") {

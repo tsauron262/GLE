535c102023217689d852e1e06cf70dcace6f6bdf
Alexis BERNARD <al.bernard@bimp.fr>
2021-01-06 15:00:12 +0100

    Comment all in script_alexis.php
    Former-commit-id: 97df4549593415a56aea0a4b6e0913d84b510dd8

--- htdocs/bimpcore/scripts/script_alexis.php
+++ htdocs/bimpcore/scripts/script_alexis.php
@@ -4,46 +4,27 @@
  *  Script Alexis
  * 
  */
-
-require_once("../../main.inc.php");
-require_once __DIR__ . '/../Bimp_Lib.php';
-//$extrafields = new ExtraFields($db);
-//$extrafields->addExtraField('entrepot', 'Entrepot', 'varchar', 104, 8, 'contrat');
-ini_set('display_errors', 1);
-$can_execute = ($user->admin) ? true : false;
-
-if($can_execute) {
-    
-//    $echeancier = BimpObject::getInstance('bimpcontract', 'BContract_echeancier');
-//    
-//    $list = $echeancier->getList();
-//    foreach($list as $i => $infos) {
-//        $echeancier->fetch($infos['id']);
-//        $echeancier->switch_statut();
-//        
-//        $contrat = BimpObject::getInstance('bimpcontract', 'BContract_contrat', $echeancier->getData('id_contrat'));
-//        $echeancier->updateField('commercial', $contrat->getData('fk_commercial_suivi'));
-//        
-//        $client = BimpObject::getInstance('bimpcore', 'Bimp_Societe', $contrat->getData('fk_soc'));
-//        $echeancier->updateField('client', $client->id);
-//        
+//
+//require_once("../../main.inc.php");
+//require_once __DIR__ . '/../Bimp_Lib.php';
+////$extrafields = new ExtraFields($db);
+////$extrafields->addExtraField('entrepot', 'Entrepot', 'varchar', 104, 8, 'contrat');
+//ini_set('display_errors', 1);
+//$can_execute = ($user->admin) ? true : false;
+//
+//if($can_execute) {
+//
+//    $contrat = BimpObject::getInstance('bimpcontract', 'BContract_contrat');
+//    $list = $contrat->getList(['statut' => 11]);
+//    foreach($list as $id) {
+//        $contrat->fetch($id['rowid']);
+//        if(!$contrat->getData('initial_renouvellement')) {
+//            $contrat->updateField('initial_renouvellement', $contrat->getData('tacite'));
+//            echo "MAJ<br />";
+//        }
 //    }
-
-    
-    $contrats = BimpObject::getInstance('bimpcontract', 'BContract_contrat');
-    
-    $list = $contrats->getList(['statut' => 11]);
-    foreach($list as $i => $infos) {
-        
-        $contrats->fetch($infos['rowid']);
-       // echo '<i>'.$contrats->getData('ref').'</i><br />';
-        $lines = BimpObject::getInstance('bimpcontract', 'BContract_contratLine');
-        $lines_list = $lines->getList(['statut' => 0, 'fk_contrat' => $contrats->id]);
-        if(count($lines_list)) {
-            echo $contrats->getData('ref') . '<br />';
-        }
-    }
-    
-    
-    
-}
+//    
+//    
+//} else {
+//    
+//}

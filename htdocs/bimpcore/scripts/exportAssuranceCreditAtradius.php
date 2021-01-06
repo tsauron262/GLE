01f4310c2ea83216ab4955c3e31b7189d8c826e6
Alexis BERNARD <al.bernard@bimp.fr>
2021-01-06 19:43:17 +0100

    societe
    Former-commit-id: 5a50ca6d6c1c9f253e8211a27240c75c19554a1a

--- htdocs/bimpcore/scripts/exportAssuranceCreditAtradius.php
+++ htdocs/bimpcore/scripts/exportAssuranceCreditAtradius.php
@@ -8,7 +8,6 @@ require_once __DIR__ . '/../Bimp_Lib.php';
 ini_set('display_errors', 1);
 set_time_limit(0);
 ini_set('memory_limit', "20000MB");
-
 $bdd = new BimpDb($db);
 
 $moinTroisAns = new DateTime();
@@ -21,11 +20,41 @@ $allSociete = [];
 
 $client = BimpCache::getBimpObjectInstance("bimpcore", "Bimp_Societe");
 $tt = 0;
+
+BimpCore::loadPhpExcel();
+$excel = new PHPExcel();
+if (!class_exists('ZipArchive')) {
+            echo 'La classe "ZipArchive" n\'existe pas';
+            return 0;
+        }
+        
+$firstLoop = true;
+
 foreach($res as $index => $array) {
+    
+    if ($firstLoop) { 
+        $sheet = $excel->createSheet(); $sheet->setTitle("Export");
+        $sheet->setCellValueByColumnAndRow(0, 1, 'Nom du client');
+        $sheet->setCellValueByColumnAndRow(1, 1, 'Référence client');
+    } else {$sheet = $excel->getActiveSheet();$firstLoop = false;}
+    
     $client->fetch($array->fk_soc);
     if($client->getData('is_subsidiary') != 1 && $client->getData('fk_typent') != 5 && $client->getData('fk_typent') != 8) {
         $tt++;
         echo $client->getData('nom') . " <br />" ;
     }
 }
+
+$file_name = 'exportAssurance';
+$file_path = DOL_DATA_ROOT . '/bimpcore/lists_excel/' . $file_name . '.xlsx';
+
+$writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+$writer->save($file_path);
+
+$url = DOL_URL_ROOT . '/document.php?modulepart=bimpcore&file=' . htmlentities('lists_excel/' . $file_name . '.xlsx');
+
+echo '<script>';
+echo 'window.open(\'' . $url . '\')';
+echo '</script>';
+
 echo "<br />" . $tt;

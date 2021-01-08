5a3de0e58e89cecb6a393399b25c1d1d451a7178
flo <flo@192.168.0.11>
2021-01-08 16:48:50 +0100

    Maj
    Former-commit-id: 8fa13d41200f17e3377da556bcedd2beb80a42e6

--- htdocs/bimpcore/controllers/productsController.php
+++ htdocs/bimpcore/controllers/productsController.php
@@ -10,9 +10,8 @@ class productsController extends BimpController
         $obj = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Product_Entrepot');
         $list = new BC_ListTable($obj);
         
-        
         if(isset($_GET['date_for_stock']))
-            $list->addExtraData ('date_for_stock', $_GET['date_for_stock']);
+            $list->addExtraData('date_for_stock', $_GET['date_for_stock']);
         
         $html .= $list->renderHtml();
         

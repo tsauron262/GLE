72b017828f99dc8fc9be1ce0bca6b6280dda1a96
flo <flo@192.168.0.11>
2021-02-16 17:08:39 +0100

    Maj suivis recouvrement
    Former-commit-id: 8a68c1fae7f89e1bc0ee63744efa8dc04a74d32e

--- htdocs/bimpcore/objects/Bimp_Client_Suivi_Recouvrement.class.php
+++ htdocs/bimpcore/objects/Bimp_Client_Suivi_Recouvrement.class.php
@@ -1,6 +1,15 @@
 <?php
 
-class Bimp_Client_Suivi_Recouvrement extends BimpObject{
+class Bimp_Client_Suivi_Recouvrement extends BimpObject
+{
+
     public $mode_list = array("Courrier", "Email", "Sms", "Téléphone", "Interne");
     public $sens_list = array("Entrant", "Sortant", "Interne");
+
+    public function canDelete()
+    {
+        global $user;
+
+        return (int) $user->admin;
+    }
 }

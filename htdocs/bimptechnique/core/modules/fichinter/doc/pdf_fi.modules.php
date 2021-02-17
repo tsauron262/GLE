5cdcb8180e15b7e151382a84599133583ec86626
tommy <tommy@192.168.1.23>
2021-02-17 14:56:20 +0100

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/objects/BimpObject.yml
    Former-commit-id: 22c3cbda07524988755abf885b3bd73dddbc89aa

--- htdocs/bimptechnique/core/modules/fichinter/doc/pdf_fi.modules.php
+++ htdocs/bimptechnique/core/modules/fichinter/doc/pdf_fi.modules.php
@@ -294,7 +294,7 @@ class pdf_fi {
                 $pdf->ln();
                 $pdf->ln();
                 
-                $dir_output = $conf->bimptechnique->dir_output . '/fi/pdf/';
+                $dir_output = DOL_DOCUMENT_ROOT . '/bimptechnique/views/images/';
                 if(count($comm) > 0) {
                     $fileName = 'commande_fi.png';
                     $pdf->Image($dir_output . $fileName, /* x */ 38, /* y */ 80, 0, 15, '', '', '', false, 250, '');
@@ -307,37 +307,47 @@ class pdf_fi {
                     $fileName = 'ticket_fi.png';
                     $pdf->Image($dir_output . $fileName, /* x */ 158, /* y */ 80, 0, 15, '', '', '', false, 250, '');
                 }
+                $title = '';
                 if(count($comm) > 0) {
                     $title = (count($commandes) > 1) ? "Références commandes" : "Référence commande";
-                    $pdf->Cell($W, 4, $title, 0, null, 'C', true);
                 }
+                $pdf->Cell($W, 4, $title, 0, null, 'C', true);
+                $title = '';
                 if($fiche->getData('fk_contrat')) 
-                    $pdf->Cell($W, 4, "Référence contrat", 0, null, 'C', true);
+                    $title = "Référence contrat";
+                $pdf->Cell($W, 4, $title, 0, null, 'C', true);
+                $title = '';
                 if(count($tickets) > 0) {
                     $title = (count($tickets) > 1) ? "Références tickets" : "Référence ticket";
-                    $pdf->Cell($W, 4, $title, 0, null, 'C', true);
                 }
+                $pdf->Cell($W, 4, $title, 0, null, 'C', true);
                 $pdf->ln();
                 $pdf->SetFont('', '', 9); 
                 $refY = $pdf->getY();
                 
-                $pdf->SetFont('', '', 7); 
+                $pdf->SetFont('', '', 7);
+                $text = '';
                 if(count($comm) > 0) {
-                    $pdf->Cell($W, 4, implode(',', $comm), 0, null, 'C', 0);
+                    $text = implode(',', $comm);
                 } else {
 //                    $pdf->Cell($W, 4, "Il n'y à pas de commandes liées à ce rapport", 0, null, 'C', 0);
                 }
+                $pdf->Cell($W, 4, $text, 0, null, 'C', 0);
+                $text = '';
                 if($fiche->getData('fk_contrat')) {
                     $contrat = BimpObject::getInstance('bimpcontract', 'BContract_contrat', $fiche->getData('fk_contrat'));
-                    $pdf->Cell($W, 4, $contrat->getRef(), 0, null, 'C', 0);
+                     $text = $contrat->getRef();
                 } else {
 //                    $pdf->Cell($W, 4, "Il n'y à pas de contrat lié à ce rapport", 0, null, 'C', 0);
                 }
+                $pdf->Cell($W, 4, $text, 0, null, 'C', 0);
+                $text = '';
                 if(count($tickets) > 0) {
-                    $pdf->Cell($W, 4, implode(',', $tick), 0, null, 'C', 0);
+                     $text = implode(',', $tick);
                 } else {
 //                    $pdf->Cell($W, 4, "Il n'y à pas de tickets support liés à ce rapport", 0, null, 'C', 0);
                 }
+                $pdf->Cell($W, 4, $text, 0, null, 'C', 0);
                 $pdf->Ln();
                 
                 // Tableau des conditions du contrat

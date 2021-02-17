6b17c32ed3db9856f81c5397e4bb005e94afd7d5
Alexis BERNARD <al.bernard@bimp.fr>
2021-02-17 14:52:49 +0100

    facturation contrat
    Former-commit-id: ab285d2d2afb4a84faf6d12fe713cdf0f96b969d

--- htdocs/bimptechnique/core/modules/fichinter/doc/pdf_fi.modules.php
+++ htdocs/bimptechnique/core/modules/fichinter/doc/pdf_fi.modules.php
@@ -429,7 +429,7 @@ class pdf_fi {
                     if($str == ", ") {
                         $pdf->MultiCell($this->page_largeur - $this->marge_droite - ($this->marge_gauche), 4, "Il n'y à pas de note supplémentaire", 0, 'L');
                     } else {
-                        $pdf->MultiCell($this->page_largeur - $this->marge_droite - ($this->marge_gauche), 4, $str, 0, 'L');
+                        $pdf->MultiCell($this->page_largeur - $this->marge_droite - ($this->marge_gauche), 4, strip_tags($str), 0, 'L');
                     }
 
                     $pdf->Ln();

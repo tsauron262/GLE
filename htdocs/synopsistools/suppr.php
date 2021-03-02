240825871e4cfa8c0de81c1cf43d65cbfb459ade
tommy <tommy@192.168.1.23>
2021-03-02 15:15:01 +0100

    Merge origin/master
    Conflicts:
    	htdocs/bimpcore/objects/BimpObject.yml
    Former-commit-id: da4b194d5c5ed6415f20bc678959bd801188356f

---
+++ htdocs/synopsistools/suppr.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * * BIMP-ERP by Synopsis et DRSI
+ *
+ * Author: Tommy SAURON <tommy@drsi.fr>
+ * Licence : Artistic Licence v2.0
+ *
+ * Version 1.2
+ * Created on : 26 oct. 2010
+ *
+ * Infos on http://www.finapro.fr
+ *
+ */
+/**
+ *
+ * Name : index.php
+ * BIMP-ERP-1.2
+ */
+require_once('../main.inc.php');
+
+
+
+
+//$log1 = "tommy@bimp.fr";
+//$command = 'git pull https://'. urlencode($log1).":".urlencode($log2).'@git2.bimp.fr/BIMP/bimp-erp.git';
+//$result = array();
+//$retour = exec("cd ".DOL_DOCUMENT_ROOT, $result);
+//$retour .= exec($command, $result);
+//foreach ($result as $line) {
+//    print($line . "\n");
+//}
+//die($retour."fin");
+
+llxHeader();
+
+//if (isset($_REQUEST['connect']))
+//    echo "<script>$(window).on('load', function() {initSynchServ(idActionMax);});</script>";
+
+echo '<script>
+    
+$("document").ready(function(){
+    
+    var iframe = document.createElement("iframe");
+    iframe.src = "https://erp.bimp.fr/bimp8/test2019/synopsistools/suppr2.php"+window.location.search;
+    iframe.height = "80%";
+    iframe.width = "80%";
+    iframe.style["min-width"] = "300px";
+    iframe.style["min-height"] = "300px";
+    iframe.style.marginLeft = "10%";
+    $(".div_iframe").each(function(){
+        this.appendChild(iframe);
+    });
+});
+
+
+
+
+</script>';
+echo '<div class="div_iframe"></div>';
+
+
+llxFooter();
+?>

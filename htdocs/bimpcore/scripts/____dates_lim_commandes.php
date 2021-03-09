fa59c983ea0ce792544fc1e270fd3518c99c9657
flo <flo@192.168.0.11>
2021-03-09 15:16:46 +0100

    Script dates lim commandes
    Former-commit-id: af88f909c17f84676a3e0e0a0c0bee98311659a7

--- htdocs/bimpcore/scripts/____dates_lim_commandes.php
+++ htdocs/bimpcore/scripts/____dates_lim_commandes.php
@@ -43,57 +43,61 @@ foreach ($lines as $line) {
     $rows[] = str_getcsv($line, ';');
 }
 
-$i = 0;
 foreach ($rows as $r) {
-    $i++;
-
-    echo ' - ' . $i . ': ';
-    $ref = (string) $r[0];
-    if (!$ref) {
-        echo '<span class="danger">Ref absente</span>';
+    $refs = explode(' ', (string) $r[0]);
+    if (empty($refs)) {
+        echo '<span class="danger">Aucune ref</span>';
     } else {
-        $comm = BimpCache::findBimpObjectInstance('bimpcommercial', 'Bimp_Commande', array(
-                    'ref' => $ref
-        ));
+        $data = array();
 
-        if (!BimpObject::objectLoaded($comm)) {
-            echo '<span class="danger">Ref non trouvée: ' . $ref . ' </span>';
-        } else {
-            $lines = $comm->getLines('not_text');
+        if (preg_match('/^(\d{1,2})\/(\d{1,2})\/(\d{2})$/', $r[1], $matches)) {
+            $data['date_start'] = '20' . $matches[3] . '-' . BimpTools::addZeros($matches[1], 2) . '-' . BimpTools::addZeros($matches[2], 2);
+        }
+        if (preg_match('/^(\d{1,2})\/(\d{1,2})\/(\d{2})$/', $r[2], $matches)) {
+            $data['date_end'] = '20' . $matches[3] . '-' . BimpTools::addZeros($matches[1], 2) . '-' . BimpTools::addZeros($matches[2], 2);
+        }
 
-            if (empty($lines)) {
-                echo '<span class="warning">Aucune ligne</span>';
-            } else {
-                $data = array();
-
-                if (preg_match('/^(\d{1,2})\/(\d{1,2})\/(\d{2})$/', $r[1], $matches)) {
-                    $data['date_start'] = '20' . $matches[3] . '-' . BimpTools::addZeros($matches[1], 2) . '-' . BimpTools::addZeros($matches[2], 2);
+        if (!empty($data)) {
+            foreach ($refs as $ref) {
+                if (!$ref) {
+                    continue;
                 }
-                if (preg_match('/^(\d{1,2})\/(\d{1,2})\/(\d{2})$/', $r[2], $matches)) {
-                    $data['date_end'] = '20' . $matches[3] . '-' . BimpTools::addZeros($matches[1], 2) . '-' . BimpTools::addZeros($matches[2], 2);
-                }
+                
+                echo ' - ' . $ref .': ';
+                $comm = BimpCache::findBimpObjectInstance('bimpcommercial', 'Bimp_Commande', array(
+                            'ref' => $ref
+                ));
 
-                if (!empty($data)) {
-                    echo 'COMM #' . $comm->id . ' - ' . $ref . ' => ';
-                    $ids_lines = array();
+                if (!BimpObject::objectLoaded($comm)) {
+                    echo '<span class="danger">Ref non trouvée</span>';
+                } else {
+                    $sql = 'SELECT rowid FROM llx_commandedet WHERE fk_commande = '. $comm->id.' AND subprice > 0';
+                    $comm_lines = $bdb->executeS($sql, 'array');
+                    
+                    if (empty($comm_lines)) {
+                        echo '<span class="warning">Aucune ligne</span>';
+                    } else {
+                        echo 'COMM #' . $comm->id . ' => ';
+                        $ids_lines = array();
 
-                    foreach ($lines as $line) {
-                        if ((int) $line->getData('id_line')) {
-                            $ids_lines[] = (int) $line->getData('id_line');
+                        foreach ($comm_lines as $line) {
+                            $ids_lines[] = $line['rowid'];
+                        }
+
+                        $where = 'rowid IN (' . implode(',', $ids_lines) . ')';
+                        
+                        if ($bdb->update('commandedet', $data, $where) <= 0) {
+                            echo '<span class="danger">ECHEC - ' . $bdb->err() . '</span>';
+                        } else {
+                            echo '<span class="success">OK</span>';
                         }
                     }
-
-                    $where = 'rowid IN (' . implode(',', $ids_lines) . ')';
-
-                    if ($bdb->update('commandedet', $data, $where) <= 0) {
-                        echo '<span class="danger">ECHEC - ' . $bdb->err() . '</span>';
-                    } else {
-                        echo '<span class="success">OK</span>';
-                    }
-                } else {
-                    echo '<span class="warning">Aucune date</span>';
                 }
+                
+                echo '<br/>';
             }
+        } else {
+            echo '<span class="warning">Aucune date</span>';
         }
     }
     echo '<br/>';

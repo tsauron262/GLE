a998efb232ee8530edc10b585a5cd1a0d419d6ae
Alexis BERNARD <al.bernard@bimp.fr>
2021-03-09 09:01:47 +0100

    Start changeTech + Start signature distance
    Former-commit-id: b49377becbe86466036674f5fd23cd3bf0d7e989

--- htdocs/bimptechnique/controllers/indexController.php
+++ htdocs/bimptechnique/controllers/indexController.php
@@ -2,12 +2,18 @@
 
 class indexController extends BimpController {
     
-    public function getDataUser($attr) {
+    public function getDataUser($attr, $if_right = "") {
         global $user;
+        $u = BimpObject::getInstance('bimpcore', 'Bimp_User', $user->id);
         switch($attr) {
             case "name": 
-                $u = BimpObject::getInstance('bimpcore', 'Bimp_User', $user->id);
                 return $u->getName();
+                break;
+            case "right":
+                if($if_right != "") {
+                    return $user->rights->bimptechnique->$if_right;
+                }
+                return 0;
                 break;
         }
     }
@@ -66,6 +72,56 @@ class indexController extends BimpController {
         $html .= "$annee";
         $html .= "<a style='margin-left:50px' href='".$callback_next."'>".BimpRender::renderIcon('arrow-right')."</a></h3></center>";
         return $html;
+    }
+    
+    public function renderStats() {
+        $html = "";
+        // &navtab-maintabs=NOM_TAB
+        if($this->getDataUser("right", "view_stats")) {
+            $mois_rapport = date('m');
+            $annee_rapport = date('Y');
+            if(isset($_REQUEST['moisRapport']) && $_REQUEST['moisRapport'] > 0) {
+                $mois_rapport = $_REQUEST['moisRapport'];
+            }
+            if(isset($_REQUEST['anneeRapport']) && $_REQUEST['anneeRapport'] > 0) {
+                $annee_rapport = $_REQUEST['anneeRapport'];
+            }
+            
+            $current = new DateTime($annee_rapport . "-" . $mois_rapport . "-01");
+            
+            $html .= "<h3><b style='color:#EF7D00'>BIMP</b><b>technique</b> <sup style='color:grey' >Statistiques</sup>";
+            
+            switch($current->format('m')) {
+                case "01":$m = "Janvier";break;
+                case "02":$m = "Février";break;
+                case "03":$m = "Mars";break;
+                case "04":$m = "Avril";break;
+                case "05":$m = "Mai";break;
+                case "06":$m = "Juin";break;
+                case "07":$m = "Juillet";break;
+                case "08":$m = "Aout";break;
+                case "09":$m = "Septembre";break;
+                case "10":$m = "Octobre";break;
+                case "11":$m = "Novembre";break;
+                case "12":$m = "Décembre";break;
+            }
+
+            $m_moin_un = (int) $current->format('m') - 1;
+            $m_plus_un = (int) $current->format('m') + 1;
+                        
+            $html .= "<b style='margin-left:50px; font-size:15px'>".BimpRender::renderIcon("arrow-left", "backButton")."<b style='font-size:20px; margin-left:20px; margin-right: 20px'>".$m . " " . $current->format('Y') . "</b>".BimpRender::renderIcon('arrow-right')."</b>";
+            
+            $html .= "</h3><hr>";
+            
+        } else {
+            $html .= BimpRender::renderAlerts("Vous n'avez pas la permission de voir les statistiques des fiches d'interventions - EN DEV", "danger", false);
+        }
+
+        return $html;
+    }
+    
+    protected function ajaxProcessChargeStat() {
+        
     }
     
 }

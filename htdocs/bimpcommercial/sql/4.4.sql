c4b6942198abe6ba4a9e076d6ad7e36d776f0fad
flo <flo@192.168.0.11>
2021-03-12 14:47:11 +0100

    Maj refs remplacées pièces perdues
    Former-commit-id: 7f350321d307579b8ca2ec984fa05a8febca5e7f

--- htdocs/bimpcommercial/sql/4.4.sql
+++ htdocs/bimpcommercial/sql/4.4.sql
@@ -1,4 +1,4 @@
--- ALTER TABLE `llx_propal` ADD `replaced_ref` VARCHAR(255) NOT NULL DEFAULT ''; 
+ALTER TABLE `llx_propal` ADD `replaced_ref` VARCHAR(255) NOT NULL DEFAULT ''; 
 ALTER TABLE `llx_commande` ADD `replaced_ref` VARCHAR(255) NOT NULL DEFAULT ''; 
 ALTER TABLE `llx_facture` ADD `replaced_ref` VARCHAR(255) NOT NULL DEFAULT ''; 
 ALTER TABLE `llx_commande_fournisseur` ADD `replaced_ref` VARCHAR(255) NOT NULL DEFAULT ''; 

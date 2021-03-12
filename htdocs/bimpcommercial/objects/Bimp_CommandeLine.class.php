5779cb4e487f485f09d03785acb9ec3509cb6575
flo <flo@192.168.0.11>
2021-03-12 15:07:58 +0100

    Maj refs remplacées pièces perdues
    Former-commit-id: 0f1dbd4435c2c76ab7e12780c33f0b3f416e564b

--- htdocs/bimpcommercial/objects/Bimp_CommandeLine.class.php
+++ htdocs/bimpcommercial/objects/Bimp_CommandeLine.class.php
@@ -29,6 +29,18 @@ class Bimp_CommandeLine extends ObjectLine
         12 => 'Annuel'
     );
 
+    // droits users: 
+    
+    public function canSetAction($action)
+    {
+        switch ($action) {
+            case 'addToCommandeFourn':
+                $comm = BimpObject::getInstance('bimpcommercial', 'Bimp_CommandeFourn');
+                return $comm->can('create');
+        }
+        return parent::canSetAction($action);
+    }
+    
     // Getters booléens:
 
     public function isRemiseEditable()
@@ -327,7 +339,7 @@ class Bimp_CommandeLine extends ObjectLine
                 $reserved_qties = $this->getReservedQties();
                 $product = $this->getProduct();
 
-                if ($type === self::LINE_PRODUCT) {
+                if ($type === self::LINE_PRODUCT && $this->canSetAction('addToCommandeFourn')) {
                     if (BimpObject::objectLoaded($product)) {
                         if ((int) $product->getData('fk_product_type') !== 0 || (isset($reserved_qties['status'][0]) && $reserved_qties['status'][0] > 0))
                             $buttons[] = array(

28c30baa986637669b1799a881d3ea52ec887d6c
tommy <tommy@192.168.1.23>
2021-03-22 10:49:07 +0100

    warning
    Former-commit-id: d50e0a47e76903d613415c6830d8dd21a6672c8e

--- htdocs/bimpcore/objects/BimpTest.class.php
+++ htdocs/bimpcore/objects/BimpTest.class.php
@@ -7,42 +7,47 @@ class BimpTest extends BimpObject{
         
         $timeDeb = $this->microtime_float();
         
-        $timeGoogle = 0;
         $commande = '/usr/local/bin/curl -o /dev/null -s -w %{time_total}\  http://google.fr';
-        $timeGoogle += exec($commande);
-        $timeGoogle += exec($commande);
-        $timeGoogle += exec($commande);
+        exec($commande);
+        exec($commande);
+        exec($commande);
+        $timeFinGoogle = $this->microtime_float();
+        $timeGoogle = $timeFinGoogle - $timeDeb;
         
-        
-        $timePhpDeb = $this->microtime_float();
+        $str = '';
         for($i=0;$i< 10000000;$i++){
             for($j=0;$j< 3;$j++){
-                //echo 'o';
+                $str .= 'pppppjjjjjjj';
             }
         }
-        $timePhp = $this->microtime_float() - $timePhpDeb;
+        $timeFinPhp = $this->microtime_float();
+        $timePhp = $timeFinPhp - $timeFinGoogle;
         
+        file_put_contents(PATH_TMP.'/testFile.txt', $str);//=<> 360Mo
+        file_get_contents(PATH_TMP.'/testFile.txt');
+        $timeFinFile = $this->microtime_float();
+        $timeFile = $timeFinFile - $timeFinPhp;
         
         
         global $db;
-        $timeMySqlDeb = $this->microtime_float();
         $db->query("SELECT * FROM llx_facture LIMIT 0,100000");
-        $timeMySql = $this->microtime_float() - $timeMySqlDeb;
+        $timeMySql = $this->microtime_float() - $timeFinFile;
         
         
         
-        $html .= $this->getMsgTime('google', $timeGoogle,1);
-        $html .= $this->getMsgTime('PHP', $timePhp);
-        $html .= $this->getMsgTime('MySql', $timeMySql);
-        $time = $this->microtime_float() - $timeDeb;
-        $html .= $this->getMsgTime('Total', $time, 3);
+//        $time = $this->microtime_float() - $timeDeb;
+//        $html .= $this->getMsgTime('google', $timeGoogle,1);
+//        $html .= $this->getMsgTime('file', $timeFile,1);
+//        $html .= $this->getMsgTime('PHP', $timePhp);
+//        $html .= $this->getMsgTime('MySql', $timeMySql);
+//        $html .= $this->getMsgTime('Total', $time, 3);
         
         return array(
             'errors'           => $errors,
             'warnings'         => $warnings,
             'success_callback' => ''
                 . "timeFin = new Date().getTime();"
-                . 'displayResult('.$timeGoogle.','.$timePhp.','.$timeMySql.',(timeFin - timeDeb)/1000);'
+                . 'displayResult('.$timeGoogle.','.$timePhp.','.$timeMySql.','.$timeFile.',(timeFin - timeDeb)/1000);'
         );
     }
     function microtime_float()
@@ -50,14 +55,14 @@ class BimpTest extends BimpObject{
         list($usec, $sec) = explode(" ", microtime());
         return ((float)$usec + (float)$sec);
     }
-    public function getMsgTime($nom, $time, $max = 1.5){
-        $html = '<br/><br/>';
-        
-        $class = '';
-        if($time > $max)
-            $class .= ' error';
-        $html .= "<span class='".$class."'> Time ".$nom." : ".$time."</span>";
-        return $html;
-    }
+//    public function getMsgTime($nom, $time, $max = 1.5){
+//        $html = '<br/><br/>';
+//        
+//        $class = '';
+//        if($time > $max)
+//            $class .= ' error';
+//        $html .= "<span class='".$class."'> Time ".$nom." : ".$time."</span>";
+//        return $html;
+//    }
 }
 

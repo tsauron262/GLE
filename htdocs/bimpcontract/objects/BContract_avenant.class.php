90cfe62036482f5936e7d510fa7310c98f966f5d
Alexis BERNARD <al.bernard@bimp.fr>
2021-03-25 16:52:37 +0100

    no message
    Former-commit-id: 0f016206c4b7ef1057a36557fd6ef0310ea80d34

--- htdocs/bimpcontract/objects/BContract_avenant.class.php
+++ htdocs/bimpcontract/objects/BContract_avenant.class.php
@@ -199,7 +199,7 @@ class BContract_avenant extends BContract_contrat {
                         $lineContrat = BimpCache::getBimpObjectInstance('bimpcontract', 'BContract_contratLine', $infos['id_line_contrat']);
 
                         $qty1 = count(json_decode($lineContrat->getdata('serials')));
-                        $qty2 = count(json_decode($infos('serials_in')));
+                        $qty2 = count(json_decode($infos['serials_in']));
                         $new_qty = $qty2 - $qty1;
 
                         $new = [

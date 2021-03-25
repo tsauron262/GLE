0560b6a616eb7af8fba9609055ac7865e2b9f3ec
flo <flo@192.168.0.11>
2021-03-25 14:23:14 +0100

    Debug cache serveur
    Former-commit-id: 38e51a44d10ecc68d50396301361d92f945b447e

--- htdocs/bimpcore/classes/BimpCache.php
+++ htdocs/bimpcore/classes/BimpCache.php
@@ -115,17 +115,7 @@ class BimpCache
         }
 
         if (is_a(self::$cache_server, 'BimpCacheServer')) {
-            $result = self::$cache_server->setCacheServeur($key, $value);
-
-            if (!$result) {
-                $msg = 'URL: ' . DOL_URL_ROOT . "\n";
-                $msg .= 'KEY: ' . $key;
-                mailSyn2('CACHE SERVER FAIL', 'f.martinez@bimp.fr', '', $msg);
-            }
-        } else {
-            $msg = 'URL: ' . DOL_URL_ROOT . "\n";
-            $msg .= 'KEY: ' . $key;
-            mailSyn2('NO CACHE SERVER', 'f.martinez@bimp.fr', '', $msg);
+            return self::$cache_server->setCacheServeur($key, $value);
         }
 
         return false;

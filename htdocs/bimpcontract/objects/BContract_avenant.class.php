20c2ed114614648283e073a6edaef7354f8c008c
Alexis BERNARD <al.bernard@bimp.fr>
2021-03-25 16:35:50 +0100

    no message
    Former-commit-id: b3940317b243de1e3c0bb4572356978633a1edd1

--- htdocs/bimpcontract/objects/BContract_avenant.class.php
+++ htdocs/bimpcontract/objects/BContract_avenant.class.php
@@ -210,7 +210,7 @@ class BContract_avenant extends BContract_contrat {
                             $lineContrat->set('qty', $new['qty']);
                             $lineContrat->set('serials', $new['serials']);
 
-                            $errors = $lineContrat->update($warnings, true);
+                            $errors = $lineContrat->updateDolObject($errors, $warnings);
 
                         }
 

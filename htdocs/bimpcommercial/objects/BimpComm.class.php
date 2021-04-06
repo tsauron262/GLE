210d9c948081114cabfdfa7f6c352b6e44803b50
flo <flo@192.168.0.11>
2021-04-06 12:27:33 +0200

    Ajout log checklines
    Former-commit-id: c8dbeb42ebdd6c505ccef10c39cb5f23bca43b0d

--- htdocs/bimpcommercial/objects/BimpComm.class.php
+++ htdocs/bimpcommercial/objects/BimpComm.class.php
@@ -2253,6 +2253,11 @@ class BimpComm extends BimpDolObject
                 if (!array_key_exists($id_dol_line, $bimp_lines) && method_exists($bimp_line, 'createFromDolLine')) {
                     $objectLine = BimpObject::getInstance($bimp_line->module, $bimp_line->object_name);
                     $objectLine->parent = $this;
+                    
+                    BimpCore::addlog('Ajout ligne absente ' . $this->getLabel('of_the'), Bimp_Log::BIMP_LOG_ERREUR, 'bimpcore', $this, array(
+                        'ID DOL LINE' => $id_dol_line
+                    ));
+                    
                     $line_errors = $objectLine->createFromDolLine((int) $this->id, $dol_line);
                     if (count($line_errors)) {
                         $errors[] = BimpTools::getMsgFromArray($line_errors, 'Des erreurs sont survenues lors de la récupération des données pour la ligne n° ' . $i);

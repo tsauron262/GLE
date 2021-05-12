1847afd807fd429ea833a035d73eb829c45a2d65
flo <flo@localhost>
2021-05-12 16:19:01 +0200

    Maj GSX
    Former-commit-id: 0e4eac03a31ade9699a2b73c1fd68822979deda0

--- htdocs/bimpapple/controllers/gsxController.php
+++ htdocs/bimpapple/controllers/gsxController.php
@@ -1642,8 +1642,7 @@ class gsxController extends BimpController
             $id_sav = (isset($params['id_sav']) ? (int) $params['id_sav'] : 0);
             $repairType = (isset($params['repairType']) ? $params['repairType'] : '');
             $coverageOption = (isset($params['coverageOption']) ? $params['coverageOption'] : '');
-//            $consumerLaw = (isset($params['consumerLaw']) ? $params['consumerLaw'] : '');
-            $consumerLaw = '';
+            $consumerLaw = (isset($params['consumerLaw']) ? $params['consumerLaw'] : '');
 
             if (!$id_sav) {
                 $errors[] = 'ID du SAV absent 63';

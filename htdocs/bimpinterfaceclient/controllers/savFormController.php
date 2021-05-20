fb65d54603fad55bc459bb8fe8d879b07d04222f
tommy <tommy@192.168.1.23>
2021-05-20 13:19:27 +0200

    virer placeholder
    Former-commit-id: 2902be81bdea2722c1edb09d6d8221a5a616ba95

--- htdocs/bimpinterfaceclient/controllers/savFormController.php
+++ htdocs/bimpinterfaceclient/controllers/savFormController.php
@@ -1551,7 +1551,7 @@ class savFormController extends BimpPublicController
 Conditions particulières aux iPhones
 
 
-- Pour certains types de pannes sous garantie, un envoi de l’iPhone dans un centre Apple peut être nécessaire, entrainant un délai plus long (jusqu’à 10 jours ouvrés), dans ce cas un téléphone de prêt est possible (sous réserve de disponibilité). Si cela vous intéresse, merci de vous munir d’un chèque de caution.
+- Pour certains types de pannes sous garantie, un envoi de l’iPhone dans un centre Apple peut être nécessaire, entrainant un délai plus long (jusqu’à 10 jours ouvrés).
 
 La plupart de nos centres peuvent effectuer une réparation de votre écran d’iPhone sous 24h00. Pour savoir si votre centre SAV est éligible à ce type de réparation consultez nottre site internet.
 
@@ -1599,7 +1599,7 @@ L’équipe BIMP";
                             $success_html .= '- Le délai de traitement des réparations est habituellement de 7 jours.<br/><br/>';
 
                             $success_html .= 'Conditions particulières aux iPhones: <br/>';
-                            $success_html .= '- Pour certains types de pannes sous garantie, un envoi de l’iPhone dans un centre Apple peut être nécessaire, entrainant un délai plus long (jusqu’à 10 jours ouvrés), dans ce cas un téléphone de prêt est possible (sous réserve de disponibilité). Si cela vous intéresse, merci de vous munir d’un chèque de caution.<br/><br/>';
+                            $success_html .= '- Pour certains types de pannes sous garantie, un envoi de l’iPhone dans un centre Apple peut être nécessaire, entrainant un délai plus long (jusqu’à 10 jours ouvrés).<br/><br/>';
 
                             $success_html .= 'La plupart de nos centres peuvent effectuer une réparation de votre écran d’iPhone sous 24h00. Pour savoir si votre centre SAV est éligible à ce type de réparation consultez nottre site internet.<br/><br/>';
 

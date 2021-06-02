b911dc6b4e4692f70550126a6b1f4c7dc954d8b5
flo <flo@localhost>
2021-06-02 16:29:11 +0200

    Maj
    Former-commit-id: 67090e8ff99b9810d1a7b61e903c1b4532b34b08

--- htdocs/bimpcore/scripts/__quick_scripts.php
+++ htdocs/bimpcore/scripts/__quick_scripts.php
@@ -48,7 +48,8 @@ if (!$action) {
         'check_facs_margin'                         => 'Vérifier les marges + revals OK factures',
         'change_sn'                                 => 'Changement de SN',
         'secteur_facture_fourn_with_commande_fourn' => 'Secteur fact fourn with comm fourn',
-        'correct_sav_dates_rdv'                     => 'Corriger dates RDV SAV'
+        'correct_sav_dates_rdv'                     => 'Corriger dates RDV SAV',
+        'correct_tickets_serials'                   => 'Récupérer serials tickets depuis sujet'
     );
 
     $path = pathinfo(__FILE__);
@@ -263,6 +264,11 @@ switch ($action) {
     case 'correct_sav_dates_rdv':
         BimpObject::loadClass('bimpsupport', 'BS_SAV');
         BS_SAV::correctDateRdvAll(true);
+        break;
+
+    case 'correct_tickets_serials':
+        BimpObject::loadClass('bimpsupport', 'BS_Ticket');
+        BS_Ticket::correctSerialsAll(true);
         break;
 
     default:

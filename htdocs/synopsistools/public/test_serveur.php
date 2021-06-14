2aefc85dc51235e80350b5b9fe1e232c1c8e31fa
tommy <tommy@192.168.1.23>
2021-06-14 20:29:47 +0200

    mail sav
    Former-commit-id: 2e7b684801110619e72b7c26999601a815f6b438

--- htdocs/synopsistools/public/test_serveur.php
+++ htdocs/synopsistools/public/test_serveur.php
@@ -12,7 +12,7 @@ foreach($files as $file){
     if(!file_get_contents($file)){
         $error[] = 'Pas de lecture '.$file;
     }
-
+    $file = str_replace('.txt', '_eciture.txt', $file);
     if(!file_put_contents($file, 'ok'))
         $error[] = 'Pas d\'ecriture '.$file;
         

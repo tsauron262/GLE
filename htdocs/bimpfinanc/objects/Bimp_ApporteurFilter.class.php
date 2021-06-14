84932c58d08e349e0e69e7383608a11f7d40fad8
romain <romain@romain-MacBookPro>
2021-06-14 12:03:24 +0200

    apporteur
    Former-commit-id: 78eb02e7cd67ee1f4cb87aa93f71ab45c596e39b

--- htdocs/bimpfinanc/objects/Bimp_ApporteurFilter.class.php
+++ htdocs/bimpfinanc/objects/Bimp_ApporteurFilter.class.php
@@ -16,4 +16,36 @@ class Bimp_ApporteurFilter extends BimpObject{
         else
             return 'all';
     }
+    
+    
+    public function getLabel($param= '') {
+        $libelle = $this->getData('libelle');
+        if($libelle != '') {
+            
+            // Contient un pourcentage
+            if(preg_match("/\d+\s?%/", $libelle) != 0)
+                return $libelle;
+
+            // Ne contient pas de pourcentage => on le rajoute
+            return $libelle . " " . $this->getData('commition') . "%";
+
+        } else
+            return "Commissionnée à " . $this->getData('commition') .  "  %";
+    }
+    
+    public function isFieldEditable($field, $force_edit = false) {
+                
+        return parent::isFieldEditable($field, $force_edit);
+    }
+    
+    
+    public function updateField($field, $value, $id_object = null, $force_update = true, $do_not_validate = false) {
+        
+        $errors = parent::updateField($field, $value, $id_object, $force_update, $do_not_validate);
+        
+//        if(empty($errors)) {
+//            
+//        }
+        
+    }
 }

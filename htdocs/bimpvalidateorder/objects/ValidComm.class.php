359edc428f0e4031777aaf3e2baf611174d04965
tommy <tommy@192.168.1.23>
2021-06-21 10:37:37 +0200

    mail sav
    Former-commit-id: 9152dc1f560b4dda02e2122f5435bddbc5a5e515

--- htdocs/bimpvalidateorder/objects/ValidComm.class.php
+++ htdocs/bimpvalidateorder/objects/ValidComm.class.php
@@ -471,12 +471,12 @@ class ValidComm extends BimpObject
                 'in' => array($object, self::OBJ_ALL)
             ),
             'val_max' => array(
-                'operator' => '>=',
-                'value'    => intval($val)
+                'operator' => '>',
+                'value'    => $val
             ),
             'val_min' => array(
                 'operator' => '<=',
-                'value'    => intval($val)
+                'value'    => $val
             ),
         );
         

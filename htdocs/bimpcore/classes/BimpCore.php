f775e6a565d04233e4a69e63c202acd054c01485
tommy <tommy@192.168.1.23>
2021-06-25 15:04:29 +0200

    logs
    Former-commit-id: bcfd42b35718cf3fcd744f4631094158c871f282

--- htdocs/bimpcore/classes/BimpCore.php
+++ htdocs/bimpcore/classes/BimpCore.php
@@ -451,6 +451,9 @@ class BimpCore
         
         if(defined('ID_ERP'))
             $extra_data['id_erp'] = ID_ERP;
+        global $user;
+        if(is_object($user) && $user->id > 0)
+            $extra_data['user'] = $user->getNomUrl(1);
 
         if (!$bimp_logs_locked) {
             $bimp_logs_locked = 1;

0428696f56105a61160dc6d0a268c8d23248b7b2
tommy <tommy@192.168.1.23>
2021-07-06 17:56:32 +0200

    apporteur
    Former-commit-id: ebaaab5da97e4037f2f16b816f4306457d52f63a

--- htdocs/bimpfinanc/objects/BimpRevalorisation.class.php
+++ htdocs/bimpfinanc/objects/BimpRevalorisation.class.php
@@ -90,7 +90,7 @@ class BimpRevalorisation extends BimpObject
     public function actionSetStatus($data, &$success)
     {
         $success = 'Maj status OK';
-        $errors = array();
+        $errors = $warnings = array();
         if ($this->canSetAction('process')) {
             if ($data['status'] == 1 || $data['status'] == 2) {
                 foreach ($data['id_objects'] as $nb => $idT) {
@@ -114,7 +114,7 @@ class BimpRevalorisation extends BimpObject
         } else {
             $errors[] = 'Vous n\'avez pas la permission';
         }
-        return $errors;
+        return array('errors' => $errors, 'warnings' => $warnings);
     }
 
     // Getters booléens: 

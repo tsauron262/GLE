f83b402d87d3c98a497f897bd6750e34431ca638
tommy <tommy@192.168.1.23>
2021-07-06 16:56:43 +0200

    apporteur
    Former-commit-id: 1d5b334cd7021882311e465feb7691699fde0548

--- htdocs/bimpcore/classes/BimpCore.php
+++ htdocs/bimpcore/classes/BimpCore.php
@@ -43,10 +43,11 @@ class BimpCore
     public static $filesInit = false;
     public static $config = null;
     public static $dev_mails = array(
-        'tommy'   => 'tommy@bimp.fr',
+        'tommy'   => 't.sauron@bimp.fr',
         'florian' => 'f.martinez@bimp.fr',
         'alexis'  => 'al.bernard@bimp.fr',
-        'romain'  => 'r.PELEGRIN@bimp.fr'
+        'romain'  => 'r.PELEGRIN@bimp.fr',
+        'peter'   => 'p.tkatchenko@bimp.fr'
     );
 
     public static function displayHeaderFiles($echo = true)
@@ -437,7 +438,7 @@ class BimpCore
 
     public static function addlog($msg, $level = 1, $type = 'bimpcore', $object = null, $extra_data = array(), $force = false)
     {
-//        die('LOG : '.$msg." ".print_r($extra_data,1));
+        die('LOG : '.$msg." ".print_r($extra_data,1));
         $extra_data = BimpTools::merge_array(static::$logs_extra_data, $extra_data);
         if (!$force && $level < Bimp_Log::BIMP_LOG_ERREUR && (int) BimpCore::getConf('bimpcore_mode_eco', 0)) {
             return array();

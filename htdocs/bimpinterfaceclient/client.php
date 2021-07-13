800d064135522878973870b1f14a32f16945d53b
tommy <tommy@192.168.1.23>
2021-07-13 17:02:39 +0200

    logs
    Former-commit-id: bc31a59f478e32136b2d0216f12f4e00ed562540

--- htdocs/bimpinterfaceclient/client.php
+++ htdocs/bimpinterfaceclient/client.php
@@ -11,7 +11,7 @@ if(!isset($_COOKIE[$sessionname])){
     setcookie($sessionname, session_id(), array('SameSite' => 'None', 'Secure'=>true));
 }
 if(!isset($_COOKIE['SERVID']) && defined('ID_ERP'))
-    setcookie('SERVID', 'erp'.ID_ERP.'|hjkhhkh|ghghjg', array('SameSite' => 'None', 'Secure'=>true));
+    setcookie('SERVID', urlencode('erp'.ID_ERP.''), array('SameSite' => 'None', 'Secure'=>true));
 
 BimpCore::setContext("public");
 

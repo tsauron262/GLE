f2fb40b5050a84ed66ebb5b58e9ee6b31008fb6d
tommy <tommy@192.168.1.23>
2021-07-22 16:06:27 +0200

    apporteur
    Former-commit-id: 96c2ca0a69991fcf61afbb63b23442954ad922b4

--- htdocs/bimpcontract/objects/BContract_echeancier.class.php
+++ htdocs/bimpcontract/objects/BContract_echeancier.class.php
@@ -701,7 +701,7 @@ class BContract_echeancier extends BimpObject {
                 }
             }
         }
-        if ($this->getData('next_facture_date') == 0 && $parent->getTotalContrat() - $parent->getTotalDejaPayer() > 0) {
+        if ($this->getData('next_facture_date') == 0 && (intval($parent->getTotalContrat()) - intval($parent->getTotalDejaPayer())) > 0) {
             $this->updateField('next_facture_date', $dateFin->add(new DateInterval('P1D'))->format('Y-m-d 00:00:00'));
             die('Echéancier corrigé, rafraichir la page');
         }

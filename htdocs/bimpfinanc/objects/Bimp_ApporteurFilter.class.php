b35d23f59b124cc1a26b491ac3d6f8b90c1350b6
tommy <tommy@192.168.1.23>
2021-08-31 15:41:56 +0200

    filtre apporteur
    Former-commit-id: ab7696adb04bc03711cf298104a5a301214f5c0a

--- htdocs/bimpfinanc/objects/Bimp_ApporteurFilter.class.php
+++ htdocs/bimpfinanc/objects/Bimp_ApporteurFilter.class.php
@@ -4,13 +4,13 @@ class Bimp_ApporteurFilter extends BimpObject{
     function getNbRsult(){
         $list = $this->getProductIds();
         if($list == 'all')
-            return 'All';
+            return 'Tous';
         return count($list);
     }
     
     public function getProductIds(){
-        $filters_array = json_decode($this->getData('filter'), true);
-        if(is_array($filters_array)){
+        $filters_array = BimpTools::json_decode_array($this->getData('filter'));
+        if(is_array($filters_array) && count($filters_array)){
             $object = BimpObject::getInstance('bimpcore', 'Bimp_Product');
             $list = BC_FiltersPanel::getObjectListIdsFromFilters($object, $filters_array);
             return $list;

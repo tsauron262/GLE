b77cb10f0aead38533f11d18c3894c44b3dda3fc
tommy <tommy@192.168.1.23>
2021-09-15 10:36:32 +0200

    bug
    Former-commit-id: ace89ca2ca3136363cd862d39cb73a6143686807

--- htdocs/bimpcontract/objects/BContract_contrat.class.php
+++ htdocs/bimpcontract/objects/BContract_contrat.class.php
@@ -339,7 +339,7 @@ class BContract_contrat extends BimpDolObject
 
         $total_fis = $ficheInter->time_to_qty($ficheInter->timestamp_to_time($total_tms)) * BimpCore::getConf("bimptechnique_coup_horaire_technicien");
         $previsionelle = 0;
-        if($this->getJourTotal() > 0)
+        if($this->getJourTotal() > 0 && $this->getJourTotal() > $this->getJourRestant())
             $previsionelle = $total_fis / ($this->getJourTotal() - $this->getJourRestant()) * $this->getJourRestant();
 
         $marge = ($this->getTotalContrat() - $total_fis);

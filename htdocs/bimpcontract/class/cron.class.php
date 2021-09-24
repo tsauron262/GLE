89326e93f909e84a2b5b450c0b8e8e8153698041
Alexis BERNARD <al.bernard@bimp.fr>
2021-09-24 09:43:48 +0200

    cron contrat ?  erreur?
    Former-commit-id: 1977ba53150da3ba6c88a8dbcaa09323d0c4421c

--- htdocs/bimpcontract/class/cron.class.php
+++ htdocs/bimpcontract/class/cron.class.php
@@ -325,12 +325,16 @@
                             $diff = $now->diff($endDate);
                             if($diff->y == 0 && $diff->m == 0 && $diff->d <= 30 && $diff->d > 0 && $diff->invert == 0) {
                                 if($c->getData('relance_renouvellement') && in_array($c->getData('tacite'), $not_tacite)){
-                                    $this->sendMailCommercial('ECHEANCE - Contrat ' . $c->getData('ref') . "[".$client->getData('code_client')."]", $c->getData('fk_commercial_suivi'), $message, $c);
+                                    $message = "Contrat " . $c->getNomUrl(). "<br />Client ".$client->dol_object->getNomUrl()." <br /> dont vous êtes le commercial arrive à expiration dans <b>$diff->d jour.s</b>";
+                                    $sujet = "Echéance contrat - " . $c->getRef() . " - " . $client->getData('code_client');
+                                    $bimp_mail = new BimpMail($sujet, $email_comm, '', $message, '', '', 'at.bernard@bimp.fr');
+                                    $bimp_mail->send();
+                                    $this->output .= "Mail envoyé à <b>$email_comm</b> pour le contrat <b>".$c->getRef()."</b><br />" ;
+                                    //$this->sendMailCommercial('ECHEANCE - Contrat ' . $c->getData('ref') . "[".$client->getData('code_client')."]", $c->getData('fk_commercial_suivi'), $message, $c);
                                 }
-                                    
 
                                 $nombre_relance++;
-                                $message = "Contrat " . $c->getNomUrl(). "<br />Client ".$client->dol_object->getNomUrl()." <br /> dont vous êtes le commercial arrive à expiration dans <b>$diff->d jour.s</b>";
+                                
                         }  else {
                             $nombre_pas_relance++;
                         }

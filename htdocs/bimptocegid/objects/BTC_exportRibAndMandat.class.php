ada361b8e59583bd02794090f7048db5beaf250a
Alexis BERNARD <al.bernard@bimp.fr>
2021-10-01 09:00:59 +0200

    date creation mandat export compta
    Former-commit-id: a1c6153919735405418a7cbae8182023cfcdc9fd

--- htdocs/bimptocegid/objects/BTC_exportRibAndMandat.class.php
+++ htdocs/bimptocegid/objects/BTC_exportRibAndMandat.class.php
@@ -84,7 +84,7 @@ class BTC_exportRibAndMandat extends BTC_export {
      */
     
     private function printMANDATtra(Bimp_SocBankAccount $rib, Bimp_Societe $client, Bimp_Facture $facture):string {
-        $now = new DateTime();
+        $date = new DateTime($rib->getData('datec'));
         $this->structure_mandat = Array(
             "FIXE" => $this->sizing("***", 3),
             "IDENTIFIANT" => $this->sizing("MDT", 3),
@@ -98,7 +98,7 @@ class BTC_exportRibAndMandat extends BTC_export {
             "PAIEMENT" => $this->sizing($this->recurrentORponctuel($facture->getData('ef_type')), 3),
             "TYPE" => $this->sizing($this->parORpro($client), 3),
             "STATUT" => $this->sizing("1FI", 3),
-            "DATECREATION" => $this->sizing($now->format('dmY'), 8),
+            "DATECREATION" => $this->sizing($date->format('dmY'), 8),
             "DATEENVOICLI" => $this->sizing("", 8),
             "DATESIGNATURE" => $this->sizing("", 8),
             "DATEMVTENVOI" => $this->sizing("", 8),

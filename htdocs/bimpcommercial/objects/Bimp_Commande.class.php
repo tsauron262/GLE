255dbacac054add9d35f44bea05867cd223b3c71
tommy <tommy@192.168.1.23>
2021-10-07 12:34:48 +0200

    sav
    Former-commit-id: 63e7c34bbf5e2e6b973ed957fab500e26187def0

--- htdocs/bimpcommercial/objects/Bimp_Commande.class.php
+++ htdocs/bimpcommercial/objects/Bimp_Commande.class.php
@@ -109,7 +109,7 @@ class Bimp_Commande extends BimpComm
                 return 1;
 
             case 'forceStatus':
-                if ((int) $user->admin || $user->bimpcommercial->forcerStatus) {
+                if ((int) $user->admin || $user->rights->bimpcommercial->forcerStatus) {
                     return 1;
                 }
                 return 0;

6c3e63127e0a714f86ce616837225b87e404401c
tommy <tommy@192.168.1.23>
2021-10-28 17:14:07 +0200

    tra
    Former-commit-id: e2f3cef3eb31dbc0c4687acc33120cadefde6b94

--- htdocs/bimpcore/scripts/script_alexis.php
+++ htdocs/bimpcore/scripts/script_alexis.php
@@ -18,11 +18,10 @@ $file = '4_'."BIMP".'_(RIBS)_' . "UNIQUE" . "_" . "Y2" . ".tra";
 $export_dir = PATH_TMP  ."/" . 'exportCegid' . '/' . $complementDirectory . '/';
 $export_project_dir = PATH_TMP . "/" . 'exportCegid' . '/';
 
-if(!file_exists($export_dir . $file)) {
-    $create_file = fopen($export_dir . $file, 'a+');
+//if(!file_exists($export_dir . $file)) {
+    $create_file = fopen($export_dir . $file, 'w');
     fwrite($create_file, $export->head_tra());
-    fclose($create_file);
-}
+//}
 
 
 $list = $bdd->getRows("societe_rib", "exported = 1");
@@ -30,5 +29,11 @@ $list = $bdd->getRows("societe_rib", "exported = 1");
 foreach($list as $rib) {
     $ecriture .= $export->export_rib_exported($rib->rowid);
 }
+echo 'Fichier : '.$export_dir . $file;
 echo "<pre>";
 echo $ecriture;
+    fwrite($create_file, $ecriture);
+    fclose($create_file);
+
+
+echo BimpTools::sendMailGrouper();

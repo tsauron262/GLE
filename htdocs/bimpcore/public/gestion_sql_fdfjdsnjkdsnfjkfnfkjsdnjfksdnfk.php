d7c9bdd4ea7362c8a0ab0aa4567e08cccaca1ae1
tommy <tommy@192.168.1.23>
2021-11-02 14:54:43 +0100

    tra
    Former-commit-id: 2681f1b8d196fdbbbbb5e4272210ef0ab3be9147

--- htdocs/bimpcore/public/gestion_sql_fdfjdsnjkdsnfjkfnfkjsdnjfksdnfk.php
+++ htdocs/bimpcore/public/gestion_sql_fdfjdsnjkdsnfjkfnfkjsdnjfksdnfk.php
@@ -28,42 +28,45 @@ else{
     $maxWsrep_last_committed = 0;
     $bestServ = '';
     $result = array();
+    $datasServ = array();
     foreach(unserialize(CONSUL_SERVERS) as $serv){
         $consulAdd = $serv.':8500/v1/catalog/service/'.CONSUL_SERVICE_DATABASE;
         $datas = json_decode(file_get_contents($consulAdd));
         foreach($datas as $data){
             $dataSql = $data->ServiceTaggedAddresses->lan_ipv4;
-            $dbT = new DoliDBMysqli('mysql', $dataSql->Address, $db->database_user, $db->database_pass, $db->database_name, $dataSql->Port);
-            if(!$dbT->connected){
-                echo 'Attention probléme de connexion '.$dbT->error;
-            }
-            $sql = $dbT->query("SHOW GLOBAL STATUS LIKE 'wsrep_%';");
-            while($ln = $db->fetch_object($sql)){
-                if(in_array($ln->Variable_name, $variables))
-                    $result[$serv][$dataSql->Address][$ln->Variable_name] = $ln->Value;
-                if($ln->Variable_name == 'wsrep_last_committed' && $ln->Value > $maxWsrep_last_committed){
-                    $maxWsrep_last_committed = $ln->Value;
-                    $bestServ = $dataSql->Address;
-                }
-            }
+            $datasServ[$dataSql->Address] = $dataSql->Port;
         }
     }
 
-
-    foreach($result as $serv => $datasServ){
-        echo '<h1>Consul : '.$serv.'</h1>';
-        foreach($datasServ as $servSql => $dataSql){
-            echo '<h2>Mysql : '.$servSql.'</h2>';
-            foreach($dataSql as $var => $val){
-                echo $var.' => '.$val.'<br/>';
+    foreach($datasServ as $servSql => $port){
+        $dbT = new DoliDBMysqli('mysql', $servSql, $db->database_user, $db->database_pass, $db->database_name, $port);
+        if(!$dbT->connected){
+            echo 'Attention probléme de connexion '.$dbT->error;
+        }
+        $sql = $dbT->query("SHOW GLOBAL STATUS LIKE 'wsrep_%';");
+        while($ln = $db->fetch_object($sql)){
+            if(in_array($ln->Variable_name, $variables))
+                $result[$servSql][$ln->Variable_name] = $ln->Value;
+            if($ln->Variable_name == 'wsrep_last_committed' && $ln->Value > $maxWsrep_last_committed){
+                $maxWsrep_last_committed = $ln->Value;
+                $bestServ = $servSql;
             }
-
-            $style = 'color: red;';
-            if($servSql == $bestServ)
-                $style = 'color: green;';
-            echo '<a style="'.$style.'"  href="?active='.$servSql.'">Activer</a>';
         }
     }
+        
+    foreach($result as $servSql => $dataSql){
+        echo '<div style="float:left;"><h2>Mysql : '.$servSql.'</h2>';
+        foreach($dataSql as $var => $val){
+            echo $var.' => '.$val.'<br/>';
+        }
+
+        $style = 'color: red;';
+        if($servSql == $bestServ)
+            $style = 'color: green;';
+        echo '<a style="'.$style.'"  href="?active='.$servSql.'">Activer</a>';
+        echo '</div>';
+    }
+    echo '</div>';
 }
 
 

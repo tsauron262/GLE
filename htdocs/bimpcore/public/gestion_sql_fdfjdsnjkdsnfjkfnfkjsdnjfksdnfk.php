0196d7c5312a7eab78e798db71b9f66bdcbc974b
tommy <tommy@192.168.1.23>
2021-11-02 14:39:30 +0100

    tra
    Former-commit-id: 906bec9be58b9fb8b17615471db5ae3638f2dc16

--- htdocs/bimpcore/public/gestion_sql_fdfjdsnjkdsnfjkfnfkjsdnjfksdnfk.php
+++ htdocs/bimpcore/public/gestion_sql_fdfjdsnjkdsnfjkfnfkjsdnjfksdnfk.php
@@ -7,27 +7,61 @@ require_once('../../main.inc.php');
 ini_set("display_errors", 1);
 
 
-if(!defined('CONSUL_SERVERS') || !defined('CONSUL_SERVICE_DATABASE'))
-    die('Consul mal configurée');
 
-$variables = array('wsrep_ready', 'wsrep_connected', 'wsrep_cluster_status', 'wsrep_local_state_comment', 'wsrep_cluster_state_uuid', 'wsrep_cluster_conf_id', 'wsrep_cluster_size', 'wsrep_last_committed');
+            require_once(DOL_DOCUMENT_ROOT.'/core/db/mysqli.class.php');
 
-foreach(unserialize(CONSUL_SERVERS) as $serv){
-    echo '<h1>Consul : '.$serv.'</h1>';
-    $consulAdd = $serv.':8500/v1/catalog/service/'.CONSUL_SERVICE_DATABASE;
-    $datas = json_decode(file_get_contents($consulAdd));
-    foreach($datas as $data){
-        $dataSql = $data->ServiceTaggedAddresses->lan_ipv4;
-        echo '<h2>Mysql : '.$dataSql->Address.'</h2>';
-        require_once(DOL_DOCUMENT_ROOT.'/core/db/mysqli.class.php');
-        $dbT = new DoliDBMysqli('mysql', $dataSql->Address, $db->database_user, $db->database_pass, $db->database_name, $dataSql->Port);
+if(isset($_REQUEST['active'])){
+        $dbT = new DoliDBMysqli('mysql', $_REQUEST['active'], $db->database_user, $db->database_pass, $db->database_name, $db->database_port);
         if(!$dbT->connected){
             echo 'Attention probléme de connexion '.$dbT->error;
         }
-        $sql = $dbT->query("SHOW GLOBAL STATUS LIKE 'wsrep_%';");
-        while($ln = $db->fetch_object($sql)){
-            if(in_array($ln->Variable_name, $variables))
-                echo $ln->Variable_name.' => '.$ln->Value.'<br/>';
+        $req = "SET GLOBAL wsrep_provider_options='pc.bootstrap=YES';";
+//        $sql = $dbT->query($req);
+        echo $req.' sur '.$_REQUEST['active'];
+}
+else{
+    if(!defined('CONSUL_SERVERS') || !defined('CONSUL_SERVICE_DATABASE'))
+        die('Consul mal configurée');
+
+    $variables = array('wsrep_ready', 'wsrep_connected', 'wsrep_cluster_status', 'wsrep_local_state_comment', 'wsrep_cluster_state_uuid', 'wsrep_cluster_conf_id', 'wsrep_cluster_size', 'wsrep_last_committed');
+
+    $maxWsrep_last_committed = 0;
+    $bestServ = '';
+    $result = array();
+    foreach(unserialize(CONSUL_SERVERS) as $serv){
+        $consulAdd = $serv.':8500/v1/catalog/service/'.CONSUL_SERVICE_DATABASE;
+        $datas = json_decode(file_get_contents($consulAdd));
+        foreach($datas as $data){
+            $dataSql = $data->ServiceTaggedAddresses->lan_ipv4;
+            $dbT = new DoliDBMysqli('mysql', $dataSql->Address, $db->database_user, $db->database_pass, $db->database_name, $dataSql->Port);
+            if(!$dbT->connected){
+                echo 'Attention probléme de connexion '.$dbT->error;
+            }
+            $sql = $dbT->query("SHOW GLOBAL STATUS LIKE 'wsrep_%';");
+            while($ln = $db->fetch_object($sql)){
+                if(in_array($ln->Variable_name, $variables))
+                    $result[$serv][$dataSql->Address][$ln->Variable_name] = $ln->Value;
+                if($ln->Variable_name == 'wsrep_last_committed' && $ln->Value > $maxWsrep_last_committed){
+                    $maxWsrep_last_committed = $ln->Value;
+                    $bestServ = $dataSql->Address;
+                }
+            }
+        }
+    }
+
+
+    foreach($result as $serv => $datasServ){
+        echo '<h1>Consul : '.$serv.'</h1>';
+        foreach($datasServ as $servSql => $dataSql){
+            echo '<h2>Mysql : '.$servSql.'</h2>';
+            foreach($dataSql as $var => $val){
+                echo $var.' => '.$val.'<br/>';
+            }
+
+            $style = 'color: red;';
+            if($servSql == $bestServ)
+                $style = 'color: green;';
+            echo '<a style="'.$style.'"  href="?active='.$servSql.'">Activer</a>';
         }
     }
 }

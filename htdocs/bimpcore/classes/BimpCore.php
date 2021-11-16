fc8da2aa508e2bb47044178f975c9c90b0e38962
tommy <tommy@192.168.1.23>
2021-11-16 17:13:52 +0100

    version git
    Former-commit-id: 800c30bd5409f5c80c3eb8c5f81c4f004aa1e3ac

--- htdocs/bimpcore/classes/BimpCore.php
+++ htdocs/bimpcore/classes/BimpCore.php
@@ -446,51 +446,52 @@ class BimpCore
 
     public static function addlog($msg, $level = 1, $type = 'bimpcore', $object = null, $extra_data = array(), $force = false)
     {
-        if (BimpCore::isModeDev() && (int) self::getConf('bimpcore_print_logs', 1)) {
-            $bt = debug_backtrace(null, 30);
-
-            $html = 'LOG ' . Bimp_Log::$levels[$level]['label'] . '<br/><br/>';
-            $html .= 'Message: ' . $msg . '<br/><br/>';
-
-            if (is_a($object, 'BimpObject') && BimpObject::objectLoaded($object)) {
-                $html .= 'Objet: ' . $object->getLink() . '<br/><br/>';
-            }
-
-            if (!empty($extra_data)) {
-                $html .= 'Données: <pre>';
-                $html .= print_r($extra_data, 1);
-                $html .= '</pre>';
-            }
-
-            $html .= 'Backtrace: <br/>';
-            $html .= BimpRender::renderBacktrace(BimpTools::getBacktraceArray($bt));
-
-            die($html);
-        }
-
-        $extra_data = BimpTools::merge_array(static::$logs_extra_data, $extra_data);
-        if (!$force && $level < Bimp_Log::BIMP_LOG_ERREUR && (int) BimpCore::getConf('bimpcore_mode_eco', 0)) {
-            return array();
-        }
-
-        if (!(int) BimpCore::getConf('bimpcore_use_logs', 0) && !(int) BimpTools::getValue('use_logs', 0)) {
-            return array();
-        }
-
-        $errors = array();
-
         // $bimp_logs_locked: Eviter boucles infinies 
         global $bimp_logs_locked, $user;
 
         if (is_null($bimp_logs_locked)) {
             $bimp_logs_locked = 0;
         }
-
-        if (defined('ID_ERP'))
-            $extra_data['id_erp'] = ID_ERP;
-
         if (!$bimp_logs_locked) {
             $bimp_logs_locked = 1;
+            if (BimpCore::isModeDev() && (int) self::getConf('bimpcore_print_logs', 1)) {
+                $bt = debug_backtrace(null, 30);
+
+                $html = 'LOG ' . Bimp_Log::$levels[$level]['label'] . '<br/><br/>';
+                $html .= 'Message: ' . $msg . '<br/><br/>';
+
+                if (is_a($object, 'BimpObject') && BimpObject::objectLoaded($object)) {
+                    $html .= 'Objet: ' . $object->getLink() . '<br/><br/>';
+                }
+
+                if (!empty($extra_data)) {
+                    $html .= 'Données: <pre>';
+                    $html .= print_r($extra_data, 1);
+                    $html .= '</pre>';
+                }
+
+                $html .= 'Backtrace: <br/>';
+                $html .= BimpRender::renderBacktrace(BimpTools::getBacktraceArray($bt));
+
+                die($html);
+            }
+
+            $extra_data = BimpTools::merge_array(static::$logs_extra_data, $extra_data);
+            if (!$force && $level < Bimp_Log::BIMP_LOG_ERREUR && (int) BimpCore::getConf('bimpcore_mode_eco', 0)) {
+                return array();
+            }
+
+            if (!(int) BimpCore::getConf('bimpcore_use_logs', 0) && !(int) BimpTools::getValue('use_logs', 0)) {
+                return array();
+            }
+
+            $errors = array();
+
+
+
+            if (defined('ID_ERP'))
+                $extra_data['id_erp'] = ID_ERP;
+
 
             $check = true;
             foreach (Bimp_Log::$exclude_msg_prefixes as $prefixe) {

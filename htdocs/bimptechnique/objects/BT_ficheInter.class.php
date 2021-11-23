ce42b593ced9a6badd6fddd2bcab8810953b0d3c
Alexis BERNARD <al.bernard@bimp.fr>
2021-11-23 17:39:49 +0100

    admin can generate pdf fi
    Former-commit-id: 90fa34bc6704f16ce78aaece1c072920b42523f4

--- htdocs/bimptechnique/objects/BT_ficheInter.class.php
+++ htdocs/bimptechnique/objects/BT_ficheInter.class.php
@@ -165,6 +165,9 @@ class BT_ficheInter extends BimpDolObject
                 return 1;
 
             case 'generatePdf':
+                if($user-admin) {
+                    return 1;
+                }
                 if ($status !== self::STATUT_BROUILLON) {
                     $errors[] = BimpTools::ucfirst($this->getLabel('this') . 'n\'est plus au statut "brouilon"');
                     return 0;

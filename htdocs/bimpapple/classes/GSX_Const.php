595b365d4c84dff04f188a6a60ee9981ac6ddd33
tommy <tommy@192.168.1.23>
2021-11-29 12:32:50 +0100

    bug
    Former-commit-id: b436ffb78bd1f9a6793a5d49f5f63196673ba0ab

--- htdocs/bimpapple/classes/GSX_Const.php
+++ htdocs/bimpapple/classes/GSX_Const.php
@@ -101,8 +101,8 @@ class GSX_Const
             'prod' => array('proditrb.pem', '')
         ),
         1442050 => array(
-            'test' => array('AppleCare-Partner-0001442050.Test.apple.com.key', ''),
-            'prod' => array('AppleCare-Partner-0001442050.Prod.apple.com.key', '')
+            'test' => array('AppleCare-Partner-0001442050.Test.apple.com.chain.pem', '', 'AppleCare-Partner-0001442050.Test.apple.com.key'),
+            'prod' => array('AppleCare-Partner-0001442050.Prod.apple.com.chain.pem', '', 'AppleCare-Partner-0001442050.Prod.apple.com.key')
         )
     );
     public static $test_ids = array(

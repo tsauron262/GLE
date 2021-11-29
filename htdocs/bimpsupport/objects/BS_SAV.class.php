c345062821b960512be2971064948f7b0f2d7f24
tommy <tommy@192.168.1.23>
2021-11-29 12:36:16 +0100

    bug
    Former-commit-id: ed213117d90c840667af5b01b17fbc168015e0d2

--- htdocs/bimpsupport/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/objects/BS_SAV.class.php
@@ -3845,7 +3845,7 @@ class BS_SAV extends BimpObject
         if(count($result2))
             $html .= '<div style="float: left; padding:5px">'.BimpRender::renderBimpListTable($result2, array('centre' => 'Centre', 'time' => 'Temps moyen en  J')).'</div>';
         
-        $html = BimpRender::renderPanel('Temps max diagnostic sur '.$time.' jours '.($ios? '(iOs)' : '(hors iOs)'), $html);
+        $html = BimpRender::renderPanel('Temps max diagnostic '.($ios? '(iOs)' : '(hors iOs)'), $html);
         
         
         return $html;

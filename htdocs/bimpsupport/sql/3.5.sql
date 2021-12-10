dacd698802b8cf0d50b85a206e4ef24feda11171
flo <flo@192.168.0.11>
2021-12-10 16:29:38 +0100

    Maj signatures
    Former-commit-id: 811466de99f7871f0968c258d8d346f24ab01995

---
+++ htdocs/bimpsupport/sql/3.5.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `llx_bs_sav` ADD `id_signature_pc` INT NOT NULL DEFAULT '0';
+ALTER TABLE `llx_bs_sav` ADD `signature_pc_params` TEXT NOT NULL DEFAULT '';
+ALTER TABLE `llx_bs_sav` ADD `id_signature_resti` INT NOT NULL DEFAULT '0';
+ALTER TABLE `llx_bs_sav` ADD `signature_resti_params` TEXT NOT NULL DEFAULT '';

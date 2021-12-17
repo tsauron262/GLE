61812b2f21072f99ce07989d913941ca4bfc6c9f
romain <romain@romain-MacBookPro>
2021-12-17 09:43:46 +0100

    limite utilisation crédit safe
    Former-commit-id: da3b4ecd942997dbac85bb99c62bfb7a9516e5e1

--- htdocs/bimpcore/objects/Bimp_Societe.class.php
+++ htdocs/bimpcore/objects/Bimp_Societe.class.php
@@ -1928,7 +1928,7 @@ class Bimp_Societe extends BimpDolObject
             return array('Siren de la Poste, trop de résultats');
 
         $errors = array();
-
+        
         $siret = '';
         $siren = '';
 
@@ -2090,6 +2090,11 @@ class Bimp_Societe extends BimpDolObject
 //                    BimpCore::addlog('Echec connexion SOAP pour Credit SAFE', Bimp_Log::BIMP_LOG_ERREUR, 'bimpcore', $this);
 //                }
             }
+        }
+        
+        if($this->field_exists('date_check_credit_safe')) {
+            $this->updateField('date_check_credit_safe', date('Y-m-d H:i:s'));
+            return $errors;
         }
 
         return $errors;

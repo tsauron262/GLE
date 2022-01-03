085d259ef1bb5017f225f9d59370c8ea3156970c
Alexis BERNARD <al.bernard@bimp.fr>
2022-01-03 10:24:47 +0100

    Avenant prolongation dates
    Former-commit-id: 53b4221ed0345b59b6840d396e090bf7ef8f6368

--- htdocs/bimpcontract/objects/BContract_avenant.class.php
+++ htdocs/bimpcontract/objects/BContract_avenant.class.php
@@ -168,7 +168,7 @@ class BContract_avenant extends BContract_contrat {
                 //$date_de_fin->sub(new DateInterval('P1D'));
                 
                 $date_effect = new DateTime($parent->displayRealEndDate("Y-m-d"));
-                $date_de_fin->add(new DateInterval("P1D"));
+                $date_effect->add(new DateInterval("P1D"));
                 
                 BimpTools::merge_array($errors, $this->updateField('want_end_date', $date_de_fin->format('Y-m-d')));
                 BimpTools::merge_array($errors, $this->updateField('date_effect', $date_effect->format("Y-m-d")));

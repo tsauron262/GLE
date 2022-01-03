19a5e5cf87c29c5f385332f59c06157f765fec95
tommy <tommy@192.168.1.23>
2022-01-03 14:56:51 +0100

    sav sac
    Former-commit-id: 8f6314dd2de3c0db75daa579fbe291f291313bad

--- htdocs/bimpcore/objects/Bimp_Client.class.php
+++ htdocs/bimpcore/objects/Bimp_Client.class.php
@@ -2667,7 +2667,7 @@ class Bimp_Client extends Bimp_Societe
             $buttons[] = array(
                 'label'   => 'Demander encours',
                 'icon'    => 'far_paper-plane',
-                'onclick' => $note->getJsActionOnclick('repondre', array("obj_type" => "bimp_object", "obj_module" => $this->module, "obj_name" => $this->object_name, "id_obj" => $this->id, "type_dest" => $note::BN_DEST_GROUP, "fk_group_dest" => 680, "content" => "Bonjour, ".$user->getFullName($langs)." sollicite pour le client N°CLIENT - NOM CLIENT un encours de XX XXX  €"), array('form_name' => 'rep'))
+                'onclick' => $note->getJsActionOnclick('repondre', array("obj_type" => "bimp_object", "obj_module" => $this->module, "obj_name" => $this->object_name, "id_obj" => $this->id, "type_dest" => $note::BN_DEST_GROUP, "fk_group_dest" => 680, "content" => "Bonjour, ".$user->getFullName($langs)." sollicite pour ce client un encours de XX XXX  €"), array('form_name' => 'rep'))
             );
             foreach ($buttons as $button) {
                 $html .= BimpRender::renderButton($button).'<br/>';

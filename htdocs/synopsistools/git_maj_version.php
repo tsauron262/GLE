5f03395c2e5ad36e06c8818152c6ec574e885afa
tommy <tommy@192.168.1.23>
2022-01-24 16:18:05 +0100

    version
    Former-commit-id: b3a8fa5d1b73da26b0d9a3f7755df1620989912b

--- htdocs/synopsistools/git_maj_version.php
+++ htdocs/synopsistools/git_maj_version.php
@@ -2,7 +2,6 @@
 
 require_once('../main.inc.php');
 
-llxHeader();
 
 
 sleep(5);
@@ -10,7 +9,3 @@ sleep(5);
 $version = BimpCore::getConf('git_version', 1)+1;
 echo 'version '.$version;
 BimpCore::setConf('git_version', $version);
-
-
-
-llxFooter();

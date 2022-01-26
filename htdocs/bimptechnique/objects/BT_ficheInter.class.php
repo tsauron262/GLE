71157e53562e9c8701a65998479bfe9d1bbd40a7
Alexis BERNARD <al.bernard@bimp.fr>
2022-01-26 10:09:13 +0100

    faute fi
    Former-commit-id: e5a47fb432f9e4e74389d5ce786c4b5355e48ada

--- htdocs/bimptechnique/objects/BT_ficheInter.class.php
+++ htdocs/bimptechnique/objects/BT_ficheInter.class.php
@@ -1381,7 +1381,7 @@ class BT_ficheInter extends BimpDolObject
 
             return 0;
         } else {
-            return BimpRender::renderAlerts("Calcule de la rentabilitée sur les anciennes FI en attente", "danger", false);
+            return BimpRender::renderAlerts("Calcul de la rentabilité sur les anciennes FI en attente", "danger", false);
         }
     }
 
@@ -1551,7 +1551,7 @@ class BT_ficheInter extends BimpDolObject
             $html .= '</tbody>';
             $html .= '</table>';
         } else {
-            $html .= BimpRender::renderAlerts("Il n'y à pas de commandes liées sur cette fiche d'intervention", "info", false);
+            $html .= BimpRender::renderAlerts("Il n'y a pas de commandes liées sur cette fiche d'intervention", "info", false);
         }
 
 

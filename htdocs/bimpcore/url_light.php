574f8e55d244f82075583c842dcf8962cebb5dd2
tommy <tommy@192.168.1.23>
2022-02-02 15:12:33 +0100

    expertise
    Former-commit-id: d082b4c69eeb1c9e6a94de527dd7779b80f99a35

--- htdocs/bimpcore/url_light.php
+++ htdocs/bimpcore/url_light.php
@@ -3,11 +3,8 @@
 if($_GET['a'] == 'df'){
     require '../bimpcommercial/duplicata.php';
 }
-elseif($_GET['a'] == 'p'){
-    require '../bimpinterfaceclient/client.php';
-}
 else{
-    echo 'hello world';
+    require '../bimpinterfaceclient/client.php';
 }
 
 

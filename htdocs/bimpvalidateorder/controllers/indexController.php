8cdb54bf9aea3f47fc306ec2c8cb67978d586cae
tommy <tommy@192.168.1.23>
2022-02-07 14:56:24 +0100

    Voir validations
    Former-commit-id: b25ebbf8b20068d565922c0b236da9125f351363

--- htdocs/bimpvalidateorder/controllers/indexController.php
+++ htdocs/bimpvalidateorder/controllers/indexController.php
@@ -4,13 +4,19 @@
 class indexController extends BimpController
 {
 
-    public function renderMyValidations() {
+    public function renderMyValidations($valideur = true) {
 
         global $user;
         
         $demande = BimpObject::getInstance('bimpvalidateorder', 'DemandeValidComm');
-        $list = new BC_ListTable($demande, 'my_demand', 1, null, 'Mes validations en attente');
-        $list->addFieldFilterValue('id_user_affected', (int) $user->id);
+        if($valideur){
+            $list = new BC_ListTable($demande, 'my_demand', 1, null, 'Mes validations en attente (valideur)');
+            $list->addFieldFilterValue('id_user_affected', (int) $user->id);
+        }
+        else{
+            $list = new BC_ListTable($demande, 'my_demand2', 1, null, 'Mes validations en attente (demandeur)');
+            $list->addFieldFilterValue('id_user_ask', (int) $user->id);
+        }
         $list->addFieldFilterValue('status', (int) DemandeValidComm::STATUS_PROCESSING);
 
         return $list->renderHtml();;

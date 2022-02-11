a2667e2c144f913c521d1ce1a5915144790f00d7
tommy <tommy@192.168.1.23>
2022-02-11 15:09:26 +0100

    acompte import paiement
    Former-commit-id: bb6db42bba35238e58c45a0ac53bf07807247f86

--- htdocs/bimpfinanc/objects/Bimp_ImportPaiementLine.class.php
+++ htdocs/bimpfinanc/objects/Bimp_ImportPaiementLine.class.php
@@ -246,7 +246,7 @@ class Bimp_ImportPaiementLine extends BimpObject
         }
         if(!count($errors)){
             $client = $obj->getChildObject('client');
-            if($obj->getData('total_ttc') < $this->getData('price'))
+            if($obj->getData('total_ttc')+0.1 < $this->getData('price'))
                 $errors[] = 'Montant plus grand que le total de la piéce';
             if(!count($errors))
                 $errors = $obj->createAcompte($this->getData('price'), $parent->id_mode_paiement, $this->getData('banque'), 1, $this->getData('date'), false, '', '', '', $warnings, 0, $this->getData('num'), $idFacture);

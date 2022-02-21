714d881e008b1fcb0310044a32840f20ee8f5cdb
Alexis BERNARD <al.bernard@bimp.fr>
2022-02-21 11:55:24 +0100

    relance FI brouillon CRON
    Former-commit-id: dbd1c359624e04bc260b729aeb7be52035db0937

--- htdocs/bimptechnique/class/cron.class.php
+++ htdocs/bimptechnique/class/cron.class.php
@@ -7,7 +7,10 @@ class Cron
 {
 
     public $output = "";
-
+    
+    public $idTechForVanina = [157,632];
+    public $sendEmailDefault = 'v.gilbert@bimp.fr';
+    
     public function start()
     {
         $this->relanceTechBrouillonJplus1etPlus();
@@ -46,7 +49,17 @@ class Cron
                 $mail .= $fi->getLink() . " retard de " . $i['days'] . " jours<br />";
             }
             $mail .= "<br />Si la régularisation a été faite entre temps, merci de ne pas tenir compte de cet email.<br />Cordialement.";
-            mailSyn2("Fiches d'intervention en brouillon", BimpTools::cleanEmailsStr($tech->getData('email')), null, $mail);
+            
+            $emailSendTo = BimpTools::cleanEmailsStr($tech->getData('email'));
+            $sujet = 'Fiches d\'intervention en brouillon';
+            
+            if(in_array($id_tech, $this->idTechForVanina)) {
+                $sujet = 'Fiche d\'intervention en brouillon de ' . $tech->getName();
+                $emailSendTo = $this->sendEmailDefault;
+                $email .= '<br /><br />Ceci est un mail de redirection de ' . $tech->getData('email') . ' vers ' . $this->sendEmailDefault;
+            }
+            
+            mailSyn2($sujet, $emailSendTo, null, $mail);
         }
     }
 }

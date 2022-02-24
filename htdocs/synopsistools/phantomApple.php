ede8e82de715da18531e1a0c166dcda3837f354b
tommy <tommy@192.168.1.23>
2022-02-24 12:02:28 +0100

    centre inactif
    Former-commit-id: b1da5e14fbb47aa2969c14f2144570f10f7d80f7

--- htdocs/synopsistools/phantomApple.php
+++ htdocs/synopsistools/phantomApple.php
@@ -6,6 +6,6 @@ llxHeader();
 
 
 require_once DOL_DOCUMENT_ROOT.'/bimpapple/classes/GSX_v2.php';
-echo GSX_v2::phantomAuth();
+echo GSX_v2::phantomAuth(GSX_v2::$default_ids['apple_id'], GSX_v2::$default_ids['apple_pword']);
 
 die('fin');

3f463443da01401a4368766b09d91a8c61c107c5
tommy <tommy@192.168.1.23>
2022-03-04 12:30:25 +0100

    stat facture contrat
    Former-commit-id: 723cdb735f7d02511ae4a525d751489171b79ebf

--- htdocs/bimpcore/url_light.php
+++ htdocs/bimpcore/url_light.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 if ($_GET['a'] == 'df') {
     require '../bimpcommercial/duplicata.php';
 } elseif ($_GET['a'] == 'ss') {
+    require_once('../main.inc.php');
+    require_once DOL_DOCUMENT_ROOT . "/bimpcore/Bimp_Lib.php";
     require '../bimpsupport/public/page.php';
 } else {
     require '../bimpinterfaceclient/client.php';

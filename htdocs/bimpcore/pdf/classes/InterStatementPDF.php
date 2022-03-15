c576b3448f4fb6cec7e7846b5dde0be3d4768ed3
Alexis BERNARD <al.bernard@bimp.fr>
2022-03-15 11:58:18 +0100

    pdf relever inter darte en premier
    Former-commit-id: 344b21f3739be854a810c71c7a5fa9f3ae2fc276

--- htdocs/bimpcore/pdf/classes/InterStatementPDF.php
+++ htdocs/bimpcore/pdf/classes/InterStatementPDF.php
@@ -42,7 +42,7 @@ class InterStatementPDF extends BimpDocumentPDF
         if($this->object->date_start_relever || $this->object->date_stop_relever) {
             $this->date_start = new DateTime($this->object->date_start_relever);
             $this->date_stop = new DateTime($this->object->date_stop_relever);
-            $this->string_filters .= 'Du ' . $this->date_start->format('d/m/Y') . ' au ' . $this->date_stop->format('d/m/Y') . '<br />';
+            $this->string_filters .= 'Du ' . $this->date_start->format('d/m/Y') . ' au ' . $this->date_stop->format('d/m/Y') . '<br /><br />';
             $this->filters['datei'] = array('min' => $this->object->date_start_relever, 'max' => $this->object->date_stop_relever);
         }
         
@@ -62,7 +62,7 @@ class InterStatementPDF extends BimpDocumentPDF
         if($this->object->id_contrat > 0) {
             $this->want_contrat = BimpCache::getBimpObjectInstance('bimpcontract', 'BContract_contrat', $this->object->id_contrat);
             $this->client = BimpCache::getBimpObjectInstance('bimpcore', 'Bimp_Societe', $this->want_contrat->getData('fk_soc'));
-            $this->string_filters .= 'Contrat: ' . $this->want_contrat->getRef() . '<br /><br />';
+            $this->string_filters .= '<br />Contrat: ' . $this->want_contrat->getRef() . '<br /><br />';
             $this->filters['fk_contrat'] = $this->want_contrat->id;
         }
         

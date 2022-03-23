3f3b86be59df3713cecdc8fbf44c9b224db9a236
tommy <tommy@192.168.1.23>
2022-03-23 13:26:32 +0100

    Bimp Doc
    Former-commit-id: fc566e3ad364b45345f1abcf50b17dd3ac934b41

--- htdocs/bimpcore/classes/BimpDoc.php
+++ htdocs/bimpcore/classes/BimpDoc.php
@@ -7,12 +7,13 @@ class BimpDoc {
     var $type = '';
     var $name = '';
     static $lang = 'fr_FR';
-    static $menu = array();
+    var $menu = array();
     static $nbInstance = 0;
     
-    function __construct($type, $name) {
+    function __construct($type, $name, &$menu = array()) {
         $this->name = $name;
         $this->type = $type;
+        $this->menu = &$menu;
         static::$nbInstance++;
         if(static::$nbInstance > 20){
             BimpCore::addlog('Attention boucle dans DOC');
@@ -38,29 +39,41 @@ class BimpDoc {
             if (preg_match('#\{code}([^\[]*) ?#', $ln, $matches)) {
                 $ln = '<xmp>' . $matches[1] . '</xmp>';
             } elseif (preg_match('#([^\[]*)?{{([^\[]*)?}}#U', $ln, $matches) && isset($matches[2])) {
-                $child = new BimpDoc('doc', $matches[2]);
+                $child = new BimpDoc('doc', $matches[2], $this->menu);
                 $child->initLines($niveau);
                 $this->lines = BimpTools::merge_array($this->lines, $child->lines);
                 $this->errors = BimpTools::merge_array($this->errors, $child->errors);
                 continue;
+            } elseif (preg_match('#([^\[]*)?{{([^\[]*)?}#U', $ln, $matches) && isset($matches[2])) {
+                $datas = explode(':', $matches[2]);
+                
+                $html = '<img src="'.DOL_URL_ROOT.'/bimpcore/docs/img/'.$this->convertTag($datas[0]).'"';
+                if(isset($datas[1]))
+                    $html .= ' width="'.$datas[1].'"';
+                if(isset($datas[2]))
+                    $html .= ' height="'.$datas[2].'"';
+                $html .= '/>';
+                $ln = str_replace('{{'.$matches[2].'}', $html, $ln);
             } elseif (preg_match('#(={2,4}) ?([^\[]*) ?#', $ln, $matches)) {
                 $ln = static::traiteLn($matches[2]);
                 $niveau = (strlen($matches[1]) - 1) + $initNiveau;
                 $type = 'h' . $niveau;
                 
                 /* gestion menu */
-                static::$menu[$niveau][] = $ln;
-                $pref = '';
-                for($i=1;$i<=10;$i++){//10 niveau de titres
-                    if($i<=$niveau)
-                    $pref .= count(static::$menu[$i]).($i<$niveau ? '.' : '');
-                    else
-                        unset(static::$menu[$i]);
+                if($this->type == 'doc'){
+                    $this->menu[$niveau][] = $ln;
+                    $pref = '';
+                    for($i=1;$i<=10;$i++){//10 niveau de titres
+                        if($i<=$niveau)
+                        $pref .= count($this->menu[$i]).($i<$niveau ? '.' : '');
+                        else
+                            unset($this->menu[$i]);
+                    }
+                    $ln = $pref . ' '.$ln;
+                    $hash = 'menu'.$pref;
+                    if ($niveau > 1)
+                        $styleMenu[] = 'text-indent: ' . ($niveau * 10) . 'px;';
                 }
-                $ln = $pref . ' '.$ln;
-                $hash = 'menu'.$pref;
-                if ($niveau > 1)
-                    $styleMenu[] = 'text-indent: ' . ($niveau * 10) . 'px;';
                 
             } else {
                 $ln = static::traiteLn($ln);
@@ -68,6 +81,12 @@ class BimpDoc {
 
             $this->lines[] = array('balise' => $type, 'value' => $ln, 'styleMenu' => $styleMenu, 'hash' => $hash);
         }
+    }
+    
+    static function convertTag($in){
+        $in = strtolower($in);
+        $in = str_replace(' ', '_', $in);
+        return $in;
     }
 
     function getEditFormDoc() {
@@ -164,8 +183,7 @@ class BimpDoc {
             if (isset($matches[2])) {
                 foreach ($matches[2] as $replace) {
                     $replaceInit = $replace;
-                    $replace = strtolower($replace);
-                    $replace = str_replace(' ', '_', $replace);
+                    $replace = static::convertTag($replace);
                     $new = static::traitePopOver($replace, $replaceInit);
                     $new = static::traiteLink($replace, $new);
                     $chaine = str_replace('{' . $replaceInit . '}', $new, $chaine);
@@ -179,7 +197,7 @@ class BimpDoc {
     }
 
     static function traitePopOver($str, $text) {
-        if (file_exists(DOL_DOCUMENT_ROOT . '/bimpcore/docs/files/' . $str . '_pop.txt')) {
+        if (file_exists(static::getPathFile('pop', $str))) {
             $bimpDoc = new BimpDoc('pop', $str);
             $text = '<span  class="bs-popover" ' . BimpRender::renderPopoverData($bimpDoc->displayInfoPop(), 'right', true) . '>' . $text . '</span>';
         }
@@ -188,7 +206,7 @@ class BimpDoc {
 
     static function traiteLink($str, $chaine) {
         $lienDoc = DOL_URL_ROOT . '/bimpcore/docs/test.php?name=';
-        if (file_exists(DOL_DOCUMENT_ROOT . '/bimpcore/docs/files/' . $str . '_doc.txt'))
+        if (file_exists(static::getPathFile('doc', $str)))
             $chaine = '<a href="' . $lienDoc . $str . '">' . $chaine . '</a>';
         return $chaine;
     }

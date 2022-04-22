213567a369e9d01a90bdc733f9480a3a6f517ef0
flo <flo@mbp-de-flo>
2022-04-22 16:46:24 +0200

    Maj triggers BimpComm
    Former-commit-id: 9144ca57da4336ef42c936f87f62a2630cf1717f

--- htdocs/bimpcommercial/core/triggers/interface_49_modBimpcommercial_PostProcess.class.php
+++ htdocs/bimpcommercial/core/triggers/interface_49_modBimpcommercial_PostProcess.class.php
@@ -45,13 +45,8 @@ class InterfacePostProcess extends BimpCommTriggers
                         break;
                 }
                 
-                if($action_name == 'VALIDATE'){
-                    $object->fetch($object->id);
-                }
-                else{
-                    $bimpObject->dol_object = $object;
-                    $bimpObject->hydrateDolObject();
-                }
+                
+                $bimpObject->hydrateDolObject();
 
                 if (count($warnings)) {
                     setEventMessages(BimpTools::getMsgFromArray($warnings), null, 'warnings');

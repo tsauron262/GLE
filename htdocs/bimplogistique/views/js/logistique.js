84f5755ee745f03bfd8fc4a1ae5faedb4ab9471c
flo <flo@mbp-de-flo>
2022-04-25 18:02:09 +0200

    Ajout auto équipements logistique
    Former-commit-id: 7c47b0b706dccb23f960b157ae2ae40efedaf1f7

--- htdocs/bimplogistique/views/js/logistique.js
+++ htdocs/bimplogistique/views/js/logistique.js
@@ -183,6 +183,29 @@ function onAddReturnsFromLinesFormSubmit($form, extra_data) {
     return extra_data;
 }
 
+function addReceivedEquipmentsToReservation($btn, equipments) {
+    var $container = $btn.findParentByClass('equipments_inputContainer');
+
+    if ($.isOk($container)) {
+        var $add_button = $container.find('.addValueBtn');
+        var $select = $container.find('select[name="equipments_add_value"]');
+
+        if ($select.length && $add_button.length) {
+            for (var i in equipments) {
+                var $option = $select.find('option[value="' + equipments[i] + '"]');
+
+                if ($option.length && $option.css('display') !== 'none') {
+                    $select.val(equipments[i]);
+                    $add_button.click();
+                }
+            }
+            return;
+        }
+    }
+
+    bimp_msg('Une erreur est survenue. Impossible d\'effectuer cette opération', 'danger');
+}
+
 // Expéditions commande client: 
 
 function addSelectedCommandeLinesToShipment($button, list_id, id_commande) {

f65b174a59e4e7b06d28f51a11cd10812db078fb
flo <flo@mbp-de-flo>
2022-04-26 14:35:21 +0200

    Debug BimpComm Triggers
    Former-commit-id: cbe019650fde5fa031ccf59df7b1db75499e2d94

--- htdocs/bimpcommercial/classes/BimpCommTriggers.php
+++ htdocs/bimpcommercial/classes/BimpCommTriggers.php
@@ -28,7 +28,7 @@ abstract class BimpCommTriggers extends DolibarrTriggers
 
             if (BimpObject::objectLoaded($bimpObject)) {
                 if (!$bimpObject->noFetchOnTrigger) { // noFetchOnTrigger : true lorsqu'on fait un create / update depuis le bimpObject => dans ce cas ne pas re-fetcher l'objet sinon écrasement des données. 
-//                    $bimpObject->fetch((int) $object->id);
+                    $bimpObject->fetch((int) $object->id);
                     
                     // On alimente $bimpObject avec les données de $object: 
                     $bimpObject->dol_object = $object;

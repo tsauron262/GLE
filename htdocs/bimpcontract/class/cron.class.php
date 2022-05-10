c97500c68f61fe2a2c3ad428937753401f5d5075
Alexis BERNARD <al.bernard@bimp.fr>
2022-05-10 15:51:08 +0200

    renouvellement tacite durée indéterminé
    Former-commit-id: 0b8c5cbf98b40e823f53d38e0c3637df7d9ce97c

--- htdocs/bimpcontract/class/cron.class.php
+++ htdocs/bimpcontract/class/cron.class.php
@@ -29,10 +29,12 @@
         CONST CONTRAT_RENOUVELLEMENT_5_FOIS = 5;
         CONST CONTRAT_RENOUVELLEMENT_6_FOIS = 7;
         CONST CONTRAT_RENOUVELLEMENT_SUR_PROPOSITION = 12;
+        CONST CONTRAT_RENOUVELLEMENT_AD_VITAM_ETERNAM = 666;
         
         public $arrayTacite = [
             self::CONTRAT_RENOUVELLEMENT_1_FOIS, self::CONTRAT_RENOUVELLEMENT_2_FOIS, self::CONTRAT_RENOUVELLEMENT_3_FOIS,
-            self::CONTRAT_RENOUVELLEMENT_4_FOIS, self::CONTRAT_RENOUVELLEMENT_5_FOIS, self::CONTRAT_RENOUVELLEMENT_6_FOIS
+            self::CONTRAT_RENOUVELLEMENT_4_FOIS, self::CONTRAT_RENOUVELLEMENT_5_FOIS, self::CONTRAT_RENOUVELLEMENT_6_FOIS,
+            self::CONTRAT_RENOUVELLEMENT_AD_VITAM_ETERNAM
         ];
         
         function zu_gehen() {
@@ -307,7 +309,7 @@
             $filters = [
                 'statut' => 11,
                 'tacite' => Array(
-                    'in' => Array(1,3,6,4,5,7)
+                    'in' => Array(1,3,6,4,5,7,666)
                 )
             ];
             

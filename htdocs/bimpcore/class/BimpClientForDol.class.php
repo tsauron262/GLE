ff7402be7dc6e2770ccbb2bc5bbc373337d50b63
tommy <tommy@localhost>
2022-06-03 17:05:18 +0200

    synchro atradius
    Former-commit-id: d778066d6018f8f2b86a5d224dcefaf8a0da8f30

--- htdocs/bimpcore/class/BimpClientForDol.class.php
+++ htdocs/bimpcore/class/BimpClientForDol.class.php
@@ -174,7 +174,7 @@ class BimpClientForDol extends Bimp_Client{
         
         $from = new DateTime();
 //        $from->sub(new DateInterval('PT1H')); TODO
-        $from->sub(new DateInterval('P1Y'));
+        $from->sub(new DateInterval('P2D'));
                 
         $nb_update = Bimp_Client::updateAllAtradius($from->format('Y-m-d\TH:i:s'), $errors, $warnings, $success);
         $this->output .= "Nombre de clients mis à jour : " . $nb_update . "<br/><br/>" . $success;

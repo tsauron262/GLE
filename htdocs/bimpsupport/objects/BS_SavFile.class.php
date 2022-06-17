3b80d438f531cf7d78d45487c655349ee080bfd8
flo <flo@mbp-de-flo>
2022-06-17 14:58:44 +0200

    Maj files sav
    Former-commit-id: daaf853b803a3b0f123faf826791fa94b5cda302

--- htdocs/bimpsupport/objects/BS_SavFile.class.php
+++ htdocs/bimpsupport/objects/BS_SavFile.class.php
@@ -5,6 +5,52 @@ require_once DOL_DOCUMENT_ROOT . '/bimpcore/objects/BimpFile.class.php';
 class BS_SavFile extends BimpFile
 {
 
+    public function getDefaultListExtraButtons()
+    {
+        $buttons = parent::getDefaultListExtraButtons();
+
+        if ($this->isLoaded()) {
+            $sav = $this->getParentInstance();
+
+            if (BimpObject::objectLoaded($sav) && is_a($sav, 'BS_SAV')) {
+                $files = $sav->getData('in_fac_emails_files');
+
+                if (!in_array((int) $this->id, $files)) {
+                    $buttons[] = array(
+                        'label'   => 'Ajouter aux fichiers à jondre à la facture',
+                        'icon'    => 'fas_plus-circle',
+                        'onclick' => $this->getJsActionOnclick('addToSavFacEmail')
+                    );
+                }
+            }
+        }
+
+        return $buttons;
+    }
+
+    public function actionAddToSavFacEmail($data, &$success)
+    {
+        $errors = array();
+        $warnings = array();
+        $success = 'Fichier ajouté à la liste des fichiers joints à la facture';
+
+        if ($this->isLoaded($errors)) {
+            $sav = $this->getParentInstance();
+
+            if (BimpObject::objectLoaded($sav) && is_a($sav, 'BS_SAV')) {
+                $errors = $sav->addFacEmailFile($this->id);
+            } else {
+                $errors[] = 'SAV lié absent';
+            }
+        }
+
+
+        return array(
+            'errors'   => $errors,
+            'warnings' => $warnings
+        );
+    }
+
     public function create(&$warnings = [], $force_create = false)
     {
         $errors = parent::create($warnings, $force_create);

95f6ffdd59c9c6857d2db9db055ba2f54147b5e9
romain <romain@romain-MacBookPro>
2022-06-23 15:02:49 +0200

    Notification, retrait débug
    Former-commit-id: 92b5411014b2652c2b30ccec4e58d31708f2ed61

--- htdocs/bimpcore/views/js/notification.js
+++ htdocs/bimpcore/views/js/notification.js
@@ -383,7 +383,6 @@ function BimpNotification() {
                     bn.$refreshBtn.show();
                     
                     if (result.notifications) {
-                        console.log(result.notifications);
                         for (const [nom, value] of Object.entries(result.notifications)) {
                             eval('bn.notificationActive.' + nom + '.obj.addElement(value);');
                         }                     

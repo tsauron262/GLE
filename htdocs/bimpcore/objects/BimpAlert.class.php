bead1e201a1a70938e14b9ca3685494bdb97bd1c
tommy <tommy@localhost>
2022-07-05 13:17:46 +0200

    debug time
    Former-commit-id: 54342afb0cfe6cf1d27d34f5394a8b6b53efcf2a

--- htdocs/bimpcore/objects/BimpAlert.class.php
+++ htdocs/bimpcore/objects/BimpAlert.class.php
@@ -18,10 +18,36 @@ class BimpAlert extends BimpObject
         return $user->admin;
     }
     
+    public static function getNextAlert($id = 0, $type = 'popup'){
+        if($id == 0){
+            global $user;
+            print_r($user->array_options['options_popup_alert_id']); 
+            $id = $user->array_options['options_popup_alert_id'];
+        }
+        
+        $filtre = array('status'=>1, "type" => 1);
+        if($id > 0)
+            $filtre['id'] = array('operator' => ">", "value" => $id);
+        $objs = BimpCache::getBimpObjectObjects('bimpcore', 'BimpAlert', $filtre, 'id', 'asc');
+        $html = '';
+        foreach($objs as $alert){
+            if($alert->getData('conditionExec') != '')
+                $exec = 0;
+            else
+                $exec = 1;
+            eval($alert->getData('conditionExec'));
+            if($exec){
+                return $alert;
+            }
+
+        }
+        return null;
+    }
+    
     
     public static function getMsgs(){
         if(!BimpTools::getValue('ajax')){
-            $objs = BimpCache::getBimpObjectObjects('bimpcore', 'BimpAlert', array('status'=>1), 'position');
+            $objs = BimpCache::getBimpObjectObjects('bimpcore', 'BimpAlert', array('status'=>1, 'type'=>0), 'position');
             $html = '';
             foreach($objs as $alert){
                 if($alert->getData('conditionExec') != '')
@@ -43,4 +69,49 @@ class BimpAlert extends BimpObject
             return $html;
         }
     }
+    
+    public function getMsg(){
+        $msg = $this->getData('msg');
+        if($this->getData('execution') != '')
+            eval($this->getData('execution'));
+        $msg = nl2br($msg);
+        return $msg;
+    }
+    
+    public function getPopup($id, $titre){
+//        return "<script>".$this->getJsActionOnclick('loadAlert').'</script>';
+        
+        $onclick = $this->getJsActionOnclick('isViewed', array('id' => $id));
+        $return = "bimpModal.loadAjaxContent($(this), 'loadAlertModal', {id: '$id'}, '".$titre."', 'Chargement', function (result, bimpAjax) {});bimpModal.show();";
+        $return .= "bimpModal.addButton('Ok', \"".$onclick."\", 'primary', 'is_viewed', modal_idx);";
+        return $return;
+        return '<script>$(document).ready(function () {'.$return.'});</script>';
+    }
+    
+    public function loadAlert(){
+        die('rfrfrfrfrf');
+    }
+    
+    public function actionIsViewed($data, &$success){
+        
+        $success = 'Vue';
+        
+        global $user;
+        $user->array_options['options_popup_alert_id'] = $data['id'];
+        $user->update($user);
+        
+        $obj = $this->getNextAlert($data['id']);
+        
+        if($obj)
+            $callback = $this->getPopup($obj->id, $obj->getData('label'));
+        else
+            $callback = 'bimpModal.clearCurrentContent()';
+        
+        return array(
+            'errors'   => $errors,
+            'warnings' => $warnings,
+            'success_callback' => $callback
+        );
+    }
+    
 }

66825f12057003f054649a345756e0c581aeb669
romain <romain@romain-MacBookPro>
2022-08-02 10:19:39 +0200

    commande Marc
    Former-commit-id: 3c73829949cb27cadcce4275d549af65f9e3a7e4

--- htdocs/bimpcommercial/objects/Bimp_Commande.class.php
+++ htdocs/bimpcommercial/objects/Bimp_Commande.class.php
@@ -408,7 +408,7 @@ class Bimp_Commande extends BimpComm
 
                 $id_cond_a_la_commande = self::getBdb()->getValue('c_payment_term', 'rowid', '`active` > 0 and code = "RECEPCOM"');
                 if ($client_facture->getData('outstanding_limit') < 1 and (int) $id_cond_a_la_commande != (int) $this->getData('fk_cond_reglement')) {
-                    if (in_array($user->id, array(232, 97))) {
+                    if (!in_array($user->id, array(232, 97))) {
                         $available_discounts = (float) $client_facture->getAvailableDiscountsAmounts();
                         if($available_discounts < $this->getData('total_ttc'))
                             $errors[] = "Les clients sans encours doivent régler à la commande";

26af376fd13dc25e4d229358eb5c6d3d6528a87e
Alexis BERNARD <al.bernard@bimp.fr>
2022-08-09 11:29:26 +0200

    reopen contrat
    Former-commit-id: 5646e42a6e2b17ef0f231f89721552ce6e354d97

--- htdocs/bimpcontract/objects/BContract_contrat.class.php
+++ htdocs/bimpcontract/objects/BContract_contrat.class.php
@@ -1527,7 +1527,7 @@ class BContract_contrat extends BimpDolObject
         $errors = Array();
         if (!count($errors)) {
             $success = "Contrat ré-ouvert avec succès";
-            $this->updateField('statut', self::CONTRAT_STATUS_WAIT);
+            $this->updateField('statut', self::CONTRAT_STATUT_WAIT_ACTIVER);
             $this->addLog('Contrat ré-ouvert');
             foreach ($this->dol_object->lines as $line) {
                 $the_line = $this->getInstance('bimpcontract', 'BContract_contratLine', $line->id);

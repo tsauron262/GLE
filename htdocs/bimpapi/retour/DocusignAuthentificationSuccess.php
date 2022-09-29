099fa963de44831c4370f96a08ecd8f5308b83d6
tommy <tommy@localhost>
2022-09-29 14:33:38 +0200

    virer echo
    Former-commit-id: 5748ccec0851b5eb0cee9296ebc7713e89c0f71c

--- htdocs/bimpapi/retour/DocusignAuthentificationSuccess.php
+++ htdocs/bimpapi/retour/DocusignAuthentificationSuccess.php
@@ -6,9 +6,16 @@ require_once("../../main.inc.php");
 //$api = BimpAPI::getApiInstance('Docusign');
 //$api->saveToken('code', $_GET['code']);
 
-
 $userAcompte = BimpCache::getBimpObjectInstance('bimpapi', 'API_UserAccount', $_SESSION['id_user_docusign']);
 unset($_SESSION['id_user_docusign']);
 $userAcompte->saveToken('code', $_GET['code']);
+$error = $warning = array();
+$userAcompte->connect($error, $warning);
 
-echo 'Authentification réussi, veuillez réitéré votre requête sur l\'ERP.';
+if(!count($error) && !count($warning))
+    echo 'Authentification réussi, veuillez réitéré votre requête sur l\'ERP.';
+else{
+    print_r($error);
+    
+    print_r($warning);
+}

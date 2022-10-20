bcc2d9233af7ffa20dfd8ee788868053996c1269
tommy <tommy@localhost>
2022-10-20 11:10:02 +0200

    optimisation operation massive
    Former-commit-id: 4feea7c6ca11c7b24416e4fc64219dea81b69c10

--- htdocs/bimpcore/pdf/classes/BimpModelPDF.php
+++ htdocs/bimpcore/pdf/classes/BimpModelPDF.php
@@ -284,8 +284,8 @@ Abstract class BimpModelPDF
             global $mysoc;
             if ($this->fromCompany->zip != $mysoc->zip || $this->fromCompany->town != $mysoc->town) {
                 $html .= '<span style="font-size: 6px; font-style: italic; color: #5A5959"><br/>';
-                $html .= 'NB: ne pas envoyer de réglement à cette adresse<br/>';
-                $html .= 'mais uniquement à : <b>OLYS 2 rue des Erables</b><br/>';
+                $html .= 'NB: les règlements ne doivent être envoyés qu\'à<br/>';
+                $html .= 'notre siège social : <b>OLYS 2 rue des Erables</b><br/>';
                 $html .= '<b>CS 21055 69760 LIMONEST</b>';
                 $html .= '</span>';
             }

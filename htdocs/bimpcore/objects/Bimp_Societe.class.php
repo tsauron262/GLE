b9c86aef36de9df7d4a41c6bdb028bb13162af9e
tommy <tommy@localhost>
2022-11-14 10:51:21 +0100

    duplicate FI
    Former-commit-id: e1e2431ef073ff40f7dba36cfe3fc43e07e66eff

--- htdocs/bimpcore/objects/Bimp_Societe.class.php
+++ htdocs/bimpcore/objects/Bimp_Societe.class.php
@@ -3823,12 +3823,12 @@ class Bimp_Societe extends BimpDolObject
         if ($limit > -1 && $limit != $this->getInitData('outstanding_limit'))
             $this->set('outstanding_limit', $limit);
 
-        if ($this->getInitData('fk_typent') != $this->getData('fk_typent') && !$this->canEditField('status')) {
-//            if (stripos($this->getData('code_compta'), 'P') === 0 && $this->getData('fk_typent') != 8)
-//                return array("Code compta particulier, le type de tiers ne peut être différent.");
-            if (stripos($this->getData('code_compta'), 'E') === 0 && $this->getData('fk_typent') == 8)
-                return array("Code compta entreprise, le type de tiers ne peut être différent.");
-        }
+//        if ($this->getInitData('fk_typent') != $this->getData('fk_typent') && !$this->canEditField('status')) {
+////            if (stripos($this->getData('code_compta'), 'P') === 0 && $this->getData('fk_typent') != 8)
+////                return array("Code compta particulier, le type de tiers ne peut être différent.");
+////            if (stripos($this->getData('code_compta'), 'E') === 0 && $this->getData('fk_typent') == 8)
+////                return array("Code compta entreprise, le type de tiers ne peut être différent.");
+//        }
 
         if ($init_solv != $this->getData('solvabilite_status') && (int) $this->getData('solvabilite_status') === self::SOLV_A_SURVEILLER_FORCE) {
             global $user;

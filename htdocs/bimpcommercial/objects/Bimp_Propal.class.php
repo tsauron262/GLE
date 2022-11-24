8e0552a7571b5668d1931b3e1d6d43d15ed1820a
flo <flo@mbp-de-flo>
2022-11-24 14:43:48 +0100

    Maj
    Former-commit-id: 07955537f63f866fca512e8b2522b27bd2ad1471

--- htdocs/bimpcommercial/objects/Bimp_Propal.class.php
+++ htdocs/bimpcommercial/objects/Bimp_Propal.class.php
@@ -1856,7 +1856,7 @@ class Bimp_Propal extends Bimp_PropalTemp
         return $this->getFilesDir();
     }
 
-    public function getSignatureDocFileName($doc_type = 'propal', $signed = false)
+    public function getSignatureDocFileName($doc_type = 'devis', $signed = false)
     {
         switch ($doc_type) {
             case 'devis':

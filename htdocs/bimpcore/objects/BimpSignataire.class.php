9539cf11474929ad4fcfbdb7617940444e321af9
tommy <tommy@localhost>
2022-12-01 16:13:44 +0100

    signature
    Former-commit-id: b976984f93cd8363feb866401808aca650e75a06

--- htdocs/bimpcore/objects/BimpSignataire.class.php
+++ htdocs/bimpcore/objects/BimpSignataire.class.php
@@ -1874,6 +1874,7 @@ class BimpSignataire extends BimpObject
 
     public function validate()
     {
+        $errors = array();
         switch ($this->getData('type')) {
             case self::TYPE_CLIENT:
                 $client = $this->getChildObject('client');

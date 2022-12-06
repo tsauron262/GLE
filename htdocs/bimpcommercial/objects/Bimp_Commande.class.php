a8ad86d7ed7a78139662175c71e453a7803bfa4f
tommy <tommy@localhost>
2022-12-06 13:57:52 +0100

    pas de valid commande si pas d'encours
    Former-commit-id: 26666ede8c49b83320897de26e779fdd94e66841

--- htdocs/bimpcommercial/objects/Bimp_Commande.class.php
+++ htdocs/bimpcommercial/objects/Bimp_Commande.class.php
@@ -447,7 +447,7 @@ class Bimp_Commande extends Bimp_CommandeTemp
                 if ($client_facture->getData('outstanding_limit') < 1 /*and (int) $id_cond_a_la_commande != (int) $this->getData('fk_cond_reglement')*/) {
                     if (!in_array($user->id, array(232, 97))) {
                         $available_discounts = (float) $client_facture->getAvailableDiscountsAmounts();
-                        if ($available_discounts < $this->getData('total_ttc'))
+                        if ($available_discounts < $this->getData('total_ttc') && $this->getData('total_ttc') > 2)
                             $errors[] = "Les clients sans encours doivent régler à la commande";
                     }
                 }

1e35b0b5d2febe1cf56593106497719072100908
flo <flo@localhost>
2022-12-16 10:51:59 +0100

    Maj
    Former-commit-id: 415fc3b0b0ffdc7999bcfc7c3f78ce90b50cd97a

--- htdocs/bimpcommercial/objects/BimpComm.class.php
+++ htdocs/bimpcommercial/objects/BimpComm.class.php
@@ -675,12 +675,13 @@ class BimpComm extends BimpDolObject
 
         return $buttons;
     }
-    
-    public function canSetAction($action) {
+
+    public function canSetAction($action)
+    {
         global $user;
-        if($action == 'checkTotal' && !$user->admin)
-            return 0;
-        if($action == 'checkMarge' && !$user->admin)
+//        if ($action == 'checkTotal' && !$user->admin)
+//            return 0;
+        if ($action == 'checkMarge' && !$user->admin)
             return 0;
         return parent::canSetAction($action);
     }
@@ -4454,53 +4455,52 @@ class BimpComm extends BimpDolObject
 
         return $data;
     }
-    
+
     //graph
-    
+
     public function getInfoGraph($graphName)
     {
         $data = parent::getInfoGraph($graphName);
-        $arrondirEnMinuteGraph = 60*12;
-        $data["data1"] = array("name"=>'Nb', "type" => "column");
-        $data["data2"] = array("name"=>'Total HT', "type" => "column");
+        $arrondirEnMinuteGraph = 60 * 12;
+        $data["data1"] = array("name" => 'Nb', "type" => "column");
+        $data["data2"] = array("name" => 'Total HT', "type" => "column");
         $data["axeX"] = array("title" => "Date", "valueFormatString" => 'DD MMM YYYY');
 //        $data["axeY"] = array("title" => 'Nb');
-        $data["params"] = array('minutes'=>$arrondirEnMinuteGraph);
-        $data["title"] = ucfirst($this->getLabel('name_plur')).' par jour';
+        $data["params"] = array('minutes' => $arrondirEnMinuteGraph);
+        $data["title"] = ucfirst($this->getLabel('name_plur')) . ' par jour';
 
         return $data;
     }
-    
+
     public function getGraphDatasPoints($params)
     {
         $result = array();
-        
+
         $fieldTotal = 'total_ht';
-        if($this->object_name == 'Bimp_Propal')
+        if ($this->object_name == 'Bimp_Propal')
             $dateStr = "UNIX_TIMESTAMP(datep)";
-        elseif($this->object_name == 'Bimp_Facture'){
+        elseif ($this->object_name == 'Bimp_Facture') {
             $dateStr = "UNIX_TIMESTAMP(datef)";
             $fieldTotal = 'total';
-        }
-        else
+        } else
             $dateStr = "UNIX_TIMESTAMP(date_commande)";
-        
-        
-        $req = 'SELECT count(*) as nb, SUM('.$fieldTotal.') as total_ht, '.$dateStr.' as timestamp FROM '.MAIN_DB_PREFIX.$this->params['table'].' a ';
+
+
+        $req = 'SELECT count(*) as nb, SUM(' . $fieldTotal . ') as total_ht, ' . $dateStr . ' as timestamp FROM ' . MAIN_DB_PREFIX . $this->params['table'] . ' a ';
         $filter = array();
-        foreach(json_decode(BimpTools::getPostFieldValue('param_list_filters'), true) as $filterT){
-            if(isset($filterT['filter']) && is_array($filterT['filter']))
+        foreach (json_decode(BimpTools::getPostFieldValue('param_list_filters'), true) as $filterT) {
+            if (isset($filterT['filter']) && is_array($filterT['filter']))
                 $filter[] = $filterT['filter'];
-            elseif(isset($filterT['filter']) && isset($filterT['name']))
+            elseif (isset($filterT['filter']) && isset($filterT['name']))
                 $filter[$filterT['name']] = $filterT['filter'];
         }
         $req .= BimpTools::getSqlWhere($filter);
-        $req .= ' GROUP BY '.$dateStr;
+        $req .= ' GROUP BY ' . $dateStr;
         $sql = $this->db->db->query($req);
-        while($ln = $this->db->db->fetch_object($sql)){
+        while ($ln = $this->db->db->fetch_object($sql)) {
             $tabDate = array($ln->annee, $ln->month, $ln->day, $ln->hour, $ln->minute);
-            $result[1][] = array("x" => "new Date(" . $ln->timestamp*1000 . ")", "y" => (int)$ln->nb);
-            $result[2][] = array("x" => "new Date(" . $ln->timestamp*1000 . ")", "y" => (int)$ln->total_ht);
+            $result[1][] = array("x" => "new Date(" . $ln->timestamp * 1000 . ")", "y" => (int) $ln->nb);
+            $result[2][] = array("x" => "new Date(" . $ln->timestamp * 1000 . ")", "y" => (int) $ln->total_ht);
         }
 
         return $result;

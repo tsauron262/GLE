478994d1ace7a94bd7c96a3fb1769db4e8b82b9c
tommy <tommy@localhost>
2022-12-27 12:52:00 +0100

    Merge git2.bimp.fr/master
    Conflicts:
    	htdocs/bimpsupport/objects/BS_SAV.yml
    Former-commit-id: 4d2a52c1eb81260f2e9e521f09ec2924dedad83a

--- htdocs/bimpwebservice/extends/entities/bimp/classes/BWSApi.php
+++ htdocs/bimpwebservice/extends/entities/bimp/classes/BWSApi.php
@@ -177,7 +177,6 @@ class BWSApi_ExtEntity extends BWSApi
                     ),
                     'logs'      => array(),
                 );
-                $response['missing_serials'] = $demande->getMissingSerials();
 
                 $user_resp = $demande->getChildObject('user_resp');
                 if (BimpObject::objectLoaded($user_resp)) {

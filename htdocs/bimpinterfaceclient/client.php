273590d4c1362b855b74a3702417afafdca1d45f
tommy <tommy@localhost>
2023-01-05 10:20:01 +0100

    year Year
    Former-commit-id: e84062f562aa2a985c3286f68c8e34fb90b5b3d7

--- htdocs/bimpinterfaceclient/client.php
+++ htdocs/bimpinterfaceclient/client.php
@@ -4,24 +4,29 @@ header('x-frame-options: ALLOWALL',false);
 define('ALLOW_ALL_IFRAME', true);
 $_REQUEST['bimp_context'] = 'public';
 
+//echo '<pre>';
+//print_r($_SERVER);
+//echo '</pre>';
 
-if(isset($_SERVER['HTTP_REFERER'])){
-    $result = parse_url($_SERVER['HTTP_REFERER']);
-    if(isset($result['host']) && stripos($result['host'],'ldlc.com') !== false){
-            echo '<h1>Votre navigateur n\'est pas compatible.</h1><h2> <a href="'.$url.'" target="popup">Merci de cliquer ici</a></h2>';
-            die;
-        if(stripos($_SERVER['HTTP_USER_AGENT'], 'Chrome') < 1 && stripos($_SERVER['HTTP_USER_AGENT'], 'Firefox') < 1){
-            if((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))   
-                 $url = "https://";   
-            else  
-                $url = "http://";     
-            $url.= $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];   
-
-            echo '<h1>Votre navigateur n\'est pas compatible.</h1><h2> <a href="'.$url.'" target="popup">Merci de cliquer ici</a></h2>';
-            die;
-        }
-    }
-}
+$url = "https://";       
+$url.= $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];  
+//if(isset($_SERVER['HTTP_REFERER'])){
+//    $result = parse_url($_SERVER['HTTP_REFERER']);
+//    if(isset($result['host']) && stripos($result['host'],'ldlc.com') !== false){
+//            echo '<h1>Votre navigateur n\'est pas compatible.</h1><h2> <a href="'.$url.'" target="popup">Merci de cliquer ici</a></h2>';
+//            die;
+//        if(stripos($_SERVER['HTTP_USER_AGENT'], 'Chrome') < 1 && stripos($_SERVER['HTTP_USER_AGENT'], 'Firefox') < 1){
+//            if((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))   
+//                 $url = "https://";   
+//            else  
+//                $url = "http://";     
+//            $url.= $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];   
+//
+//            echo '<h1>Votre navigateur n\'est pas compatible.</h1><h2> <a href="'.$url.'" target="popup">Merci de cliquer ici</a></h2>';
+//            die;
+//        }
+//    }
+//}
 
 require_once '../bimpcore/main.php';
 require_once DOL_DOCUMENT_ROOT . '/bimpcore/Bimp_Lib.php';
@@ -31,6 +36,23 @@ if(!isset($_COOKIE[$sessionname])){
 }
 
 BimpCore::setContext("public");
+
+
+
+echo "<script>function testCookie(){"
+. "setTimeout(function() {"
+    . "if(document.cookie.match('DOLSESSID_')){ "
+    . "}else{ "
+            . "window.open('".$url."', '_blank'); "
+            . "cells = document.getElementsByTagName('form');"
+            . "for (var i = 0; i < cells.length; i++) {"
+                . "cells[i].innerHTML = '<h1>Votre navigateur n\'est pas compatible.</h1><h2> <a href=\"".$url."\" target=\"popup\">Merci de cliquer ici</a></h2>';"
+            . "}"
+    . "}"
+. "}, 500)}; "
+. "document.onload=\"testCookie()\";testCookie();"
+. "</script>";
+
 
 $controllerName = BimpTools::getValue('fc', 'InterfaceClient');
 
@@ -63,7 +85,6 @@ switch ($controllerName) {
 
 $controller = BimpController::getInstance('bimpinterfaceclient', $controllerName);
 $controller->display();
-
 
 
 ?>

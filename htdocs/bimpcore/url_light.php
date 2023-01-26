3e1d6a79f16c4c70b7a96e4598a60e3a31d11ed1
tommy <tommy@localhost>
2023-01-26 10:09:30 +0100

    taches
    Former-commit-id: 221d604d8727b415d75632491e91fe023652f430

--- htdocs/bimpcore/url_light.php
+++ htdocs/bimpcore/url_light.php
@@ -1,9 +1,9 @@
 <?php
 ini_set('display_errors', 0);
 
-if ($_GET['a'] == 'df') {
+if (isset($_GET['a']) && $_GET['a'] == 'df') {
     require '../bimpcommercial/duplicata.php';
-} elseif ($_GET['a'] == 'ss') {
+} elseif (isset($_GET['a']) && $_GET['a'] == 'ss') {
     require '../bimpsupport/public/page.php';
 } else {
     require '../bimpinterfaceclient/client.php';

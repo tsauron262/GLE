76bfbcce4ecb47a6949a5141f899824f67759af3
tommy <tommy@localhost>
2023-02-06 12:35:17 +0100

    cron 131
    Former-commit-id: 9305c655aaefb730866a3680d289e92295aabe9c

--- htdocs/bimpdatasync/classes/process_overrides/BDS_ImportsLdlcProcess.php
+++ htdocs/bimpdatasync/classes/process_overrides/BDS_ImportsLdlcProcess.php
@@ -158,7 +158,7 @@ class BDS_ImportsLdlcProcess extends BDSImportFournCatalogProcess
                     }
                     if(count($tabHtml)){
                         ksort($tabHtml);
-                        mailSyn2('Produit LDLC marge négative', 'tommy@bimp.fr'/*, a.pfeffer@ldlc.com, j.viales@ldlc.com'*/, null, '<h3>Bonjour, voici la liste des produits avec une marge négative ('.$i.')</h3><br/><br/>'.implode('<br/><br/><br/>', $tabHtml));
+                        mailSyn2('Produit LDLC marge négative', 'tommy@bimp.fr'/*, a.pfeffer@ldlc.com, j.viales@ldlc.com'*/, null, '<h3>Bonjour, voici la liste des produits avec une marge négative ('.$i.')</h3><br/><br/>'.implode('<br/><br/><br/>------------------------- ', $tabHtml));
                     }
                 }
                 break;

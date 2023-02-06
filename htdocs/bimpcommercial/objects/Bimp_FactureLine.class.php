55300c1b06f23a30b032073b35a6f19f553508e8
tommy <tommy@localhost>
2023-02-06 12:10:37 +0100

    cron 131
    Former-commit-id: e2b8dd08e3efc4d3a902094f3fd3183fbc07205a

--- htdocs/bimpcommercial/objects/Bimp_FactureLine.class.php
+++ htdocs/bimpcommercial/objects/Bimp_FactureLine.class.php
@@ -22,6 +22,41 @@ class Bimp_FactureLine extends ObjectLine
 
         return 0;
     }
+    
+    
+    public function getTotalMarge(){
+        $margin = (float) $this->getMargin();
+        $total_reval = 0;
+
+        $done = false;
+        if ($this->object_name === 'Bimp_FactureLine') {
+            $facture = $this->getParentInstance();
+
+            if (BimpObject::objectLoaded($facture)) {
+                if ((int) $facture->getData('fk_statut')) {
+                    $done = true;
+                    $revals = BimpCache::getBimpObjectObjects('bimpfinanc', 'BimpRevalorisation', array(
+                                'id_facture_line' => (int) $this->id
+                    ));
+
+                    foreach ($revals as $reval) {
+                        if (in_array((int) $reval->getData('status'), array(0, 1))) {
+                            $reval_amount = $reval->getTotal();
+                            $margin += $reval_amount;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!$done) {
+            $remises_arrieres = $this->getTotalRemisesArrieres(false);
+
+            $total_reval += $remises_arrieres;
+            $margin += $remises_arrieres;
+        }
+        return $margin;
+    }
 
     public function canSetAction($action)
     {

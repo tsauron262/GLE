a221d2bd2d40ddd55888a3ced33a1451b45295db
tommy <tommy@localhost>
2023-02-09 14:53:14 +0100

    Redirect
    Former-commit-id: 3e80d6fb94dc3091cba30849b2004b52d31d8b72

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -6249,7 +6249,7 @@ if($obj->up == null)
 						 break;*/
 					case 'password':
 						$algo = '';
-						if ($this->array_options[$key] != '' /*moddrsi*//*&& isset($extrafields->attributes[$this->table_element]['param'][$attributeKey]['options']) *//*fmoddrsi*/&& is_array($extrafields->attributes[$this->table_element]['param'][$attributeKey]['options'])) {
+						if ($this->array_options[$key] != '' && isset($extrafields->attributes[$this->table_element]['param'][$attributeKey]['options']) && is_array($extrafields->attributes[$this->table_element]['param'][$attributeKey]['options'])) {
 							// If there is an encryption choice, we use it to crypt data before insert
 							$tmparrays = array_keys($extrafields->attributes[$this->table_element]['param'][$attributeKey]['options']);
 							$algo = reset($tmparrays);

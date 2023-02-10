da53e398fee590abc37f6caf9e889823ac31c9b2
tommy <tommy@localhost>
2023-02-10 14:29:38 +0100

    hotho
    Former-commit-id: b0b4ee1122f95edda83cad7428c786d852ea5f61

--- htdocs/synopsistools/git_pull.php
+++ htdocs/synopsistools/git_pull.php
@@ -16,41 +16,48 @@ if(defined('ID_ERP'))
 
 curl_init();
 
+$ok = (isset($_REQUEST['go']) && $_REQUEST['go']);
 
-$tabHook = array();
-$tabHook[] = array(
-    'url' => WEBHOOK_SERVER.WEBHOOK_PATH_GIT_PULL,
-    'secret' => WEBHOOK_SECRET_GIT_PULL
-);
-//$tabHook[] = array(
-//    'url' => WEBHOOK_SERVER.WEBHOOK_PATH_REDIS_RESTART,//"http://10.192.20.5:9000/hooks/bimp8";
-//    'secret' => WEBHOOK_SECRET_REDIS_RESTART
-//);
+echo '<form><input type="submit" value="Go"/><input type="hidden" name="go" value="1"/></form>';
 
-foreach($tabHook as $hook){
-    echo '<textarea style="whidth=\'200px\'; height=\'auto\'">';
-    $ch = curl_init($hook['url']);
-    $file_name = PATH_TMP.'/secret.json';
-    $secretJson = json_encode(array("secret" => $hook['secret']));
+if($ok){
+    $tabHook = array();
+    $tabHook[] = array(
+        'url' => WEBHOOK_SERVER.WEBHOOK_PATH_GIT_PULL,
+        'data'=> array(
+            'secret' => WEBHOOK_SECRET_GIT_PULL
+        )
+    );
+    //$tabHook[] = array(
+    //    'url' => WEBHOOK_SERVER.WEBHOOK_PATH_REDIS_RESTART,//"http://10.192.20.5:9000/hooks/bimp8";
+    //    'secret' => WEBHOOK_SECRET_REDIS_RESTART
+    //);
+
+    foreach($tabHook as $hook){
+        echo '<textarea style="whidth=\'200px\'; height=\'auto\'">';
+        $ch = curl_init($hook['url']);
+        $file_name = PATH_TMP.'/secret.json';
+        $secretJson = json_encode($hook['data']);
 
 
 
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,$secretJson);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$secretJson);
 
 
-    curl_exec($ch);
+        curl_exec($ch);
 
-    if(curl_error($ch)) {
-        print_r(curl_error($ch));
+        if(curl_error($ch)) {
+            print_r(curl_error($ch));
+        }
+        curl_close($ch);
+
+        echo '</textarea>';
+        echo '<br/><br/>';
+        echo 'Hook : '.$hook['url'].' OK';
+        echo '<br/><br/>';
     }
-    curl_close($ch);
-    
-    echo '</textarea>';
-    echo '<br/><br/>';
-    echo 'Hook : '.$hook['url'].' OK';
-    echo '<br/><br/>';
 }
 
 

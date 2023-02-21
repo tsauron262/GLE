3cd59cf2522fdee56aec5bbbb00e526b2e11597e
tommy <tommy@localhost>
2023-02-21 09:32:58 +0100

    total_ht facture dans contrat
    Former-commit-id: 82fc605061640214ff328c2b8ad501c7891bd857

--- htdocs/bimpcontract/objects/BContract_contrat.class.php
+++ htdocs/bimpcontract/objects/BContract_contrat.class.php
@@ -1504,7 +1504,7 @@ class BContract_contrat extends BimpDolObject
             $facture = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Facture', $infos['d']);
             if ($facture->getData('fk_statut') == 1 || $facture->getData('fk_statut') == 2) {
                 if ($facture->getData('type') == 0) {
-                    $total += $facture->getData('total');
+                    $total += $facture->getData('total_ht');
                 }
             }
         }
@@ -1540,7 +1540,7 @@ class BContract_contrat extends BimpDolObject
             foreach ($facture_delivred as $link) {
                 $instance = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Facture', $link['d']);
                 if ( $instance->getData('type') != 3)
-                    $montant += $instance->getData('total');
+                    $montant += $instance->getData('total_ht');
             }
             $return = $this->getTotalContrat() - $montant;
         } else {
@@ -1742,7 +1742,7 @@ class BContract_contrat extends BimpDolObject
         return $montant;
     }
 
-    public function getTotalDejaPayer($paye_distinct = false, $field = 'total')
+    public function getTotalDejaPayer($paye_distinct = false, $field = 'total_ht')
     {
         $element_factures = getElementElement('contrat', 'facture', $this->id);
         $montant = 0;
@@ -1752,16 +1752,16 @@ class BContract_contrat extends BimpDolObject
                 if ($instance->getData('type') != 3) {
                     if ($paye_distinct) {
                         if ($instance->getData('paye')) {
-                            if ($field == 'total')
-                                $montant += $instance->getData('total');
+                            if ($field == 'total_ht')
+                                $montant += $instance->getData('total_ht');
                             elseif ($field == 'pa')
 //                                $montant += $instance->getData('total_achat_reval_ok');
                                 $montant += $instance->getTotalMargeWithReval(array('"correction_pa"'));
                         }
                     } else {
                         if (/* $instance->getData('type') == 0 */1) {
-                            if ($field == 'total')
-                                $montant += $instance->getData('total');
+                            if ($field == 'total_ht')
+                                $montant += $instance->getData('total_ht');
                             elseif ($field == 'pa')
 //                                $montant += $instance->getData('total_achat_reval_ok');
                                 $montant += $instance->getTotalMargeWithReval(array('"correction_pa"'));
@@ -1770,6 +1770,7 @@ class BContract_contrat extends BimpDolObject
                 }
             }
         }
+//        echo 'ooo'.$montant;
         $montant = round($montant, 4);
         return $montant;
     }

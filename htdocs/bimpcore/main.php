d89e7766682f5586019cebf269e148e29da69506
tommy <tommy@localhost>
2023-02-27 12:24:38 +0100

    imports
    Former-commit-id: 4d8f34e1e769b7c980ddade5ef4dbf8c62224c65

--- htdocs/bimpcore/main.php
+++ htdocs/bimpcore/main.php
@@ -4,6 +4,34 @@ $context = (isset($_REQUEST['bimp_context']) && $_REQUEST['bimp_context'] ? $_RE
 
 if ($context == 'public') {
     define("NOLOGIN",1);
+    define("NOSESSION", 1);
+    
+    define('XFRAMEOPTIONS_ALLOWALL', true);
+    
+    $sessionname = 'publicerp';
+        session_set_cookie_params(array('SameSite' => 'None', 'Secure' => true, 'path' => '/'));
+	session_name($sessionname);
+        
+        $test = session_id();
+        
+//        echo '<pre>'.$test;
+//        print_r($_COOKIE);
+        
+//        echo '<br/>'.session_id().' finfinfin';
+//        session_id('testssessionid');
+//        session_id($test);
+	session_start();
+        
+        
+//        if(isset($_COOKIE[$sessionname]) && $_COOKIE[$sessionname] != $test && $test != ''){
+//            $_COOKIE[$sessionname] = $test;
+//            echo 'oui';
+//        }
+        
+//        echo '<pre>';
+//        echo session_id().'<br/>fin<br/>';
+//        print_r($_COOKIE);
+//        print_r($_SESSION);
 }
 
 if (isset($_REQUEST['ajax']) && $_REQUEST['ajax']) {

e064e045bf0e05112e42b2e1972d7cacb980d25d
tommy <tommy@localhost>
2023-02-27 09:36:39 +0100

    imports
    Former-commit-id: 498d5dbfd80210de3a7401e70a5776e1b687bb4c

--- htdocs/bimpinterfaceclient/client.php
+++ htdocs/bimpinterfaceclient/client.php
@@ -40,9 +40,9 @@ if (isset($_REQUEST['nav_not_compatible'])) {
 require_once '../bimpcore/main.php';
 require_once DOL_DOCUMENT_ROOT . '/bimpcore/Bimp_Lib.php';
 
-//if (!isset($_COOKIE[$sessionname])) {
-//    setcookie($sessionname, session_id(), array('SameSite' => 'None', 'Secure' => true, 'path' => '/'));
-//}
+if (!isset($_COOKIE[$sessionname])) {
+    setcookie($sessionname, session_id(), array('SameSite' => 'None', 'Secure' => true, 'path' => '/'));
+}
 
 BimpCore::setContext("public");
 

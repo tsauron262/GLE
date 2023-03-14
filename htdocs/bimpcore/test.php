7cc18e8ca5ae6472515e7dd37cf6e44d6875200f
flo <flo@mbpmartzflorian>
2023-03-14 01:20:40 +0100

    Maj comm applecare
    Former-commit-id: 68f97ef96b336e0f31bb1f1da7d573d6cbc6cdf8

---
+++ htdocs/bimpcore/test.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once("../main.inc.php");
+
+ini_set('display_errors', 1);
+require_once __DIR__ . '/Bimp_Lib.php';
+set_time_limit(0);
+
+ignore_user_abort(0);
+
+top_htmlhead('', 'TESTS', 0, 0, array(), array());
+
+echo '<body>';
+
+BimpCore::displayHeaderFiles();
+
+global $db, $user;
+
+if (!BimpObject::objectLoaded($user)) {
+    echo BimpRender::renderAlerts('Aucun utilisateur connecté');
+    exit;
+}
+
+if (!$user->admin) {
+    echo BimpRender::renderAlerts('Seuls les admin peuvent exécuter ce script');
+    exit;
+}
+
+if ((int) BimpTools::getValue('test_serials', 0)) {
+    echo 'CHECK SERIALS <br/>';
+    BimpObject::loadClass('bimpfinanc', 'BimpRevalorisation');
+    BimpRevalorisation::checkAppleCareSerials();
+    echo 'OK';
+}
+
+if ((int) BimpTools::getValue('test_revals', 0)) {
+    echo 'CHECKS REVALS: <br/>';
+    BimpObject::loadClass('bimpfinanc', 'BimpRevalorisation');
+    $errors = BimpRevalorisation::checkBilledApplecareReval();
+
+    echo 'ERRORS<pre>';
+    print_r($errors);
+    exit;
+}
+
+echo '<br/>FIN';
+echo '</body></html>';
+
+//llxFooter();

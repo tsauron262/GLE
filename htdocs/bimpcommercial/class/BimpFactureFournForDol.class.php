e7a44f5c0ebe2582c0756d6fdef28f3155e2803f
flo <flo@192.168.63.71>
2023-03-22 00:39:44 +0100

    Maj cron
    Former-commit-id: aad971418c5bc1c5d840a211f85b52ee0fc6cb5f

--- htdocs/bimpcommercial/class/BimpFactureFournForDol.class.php
+++ htdocs/bimpcommercial/class/BimpFactureFournForDol.class.php
@@ -1,20 +1,24 @@
 <?php
-require_once(DOL_DOCUMENT_ROOT.'/bimpcommercial/objects/Bimp_FactureFourn.class.php');
 
-class BimpFactureFournForDol extends Bimp_FactureFourn{
-    public function __construct($db) {
-        
+require_once(DOL_DOCUMENT_ROOT . '/bimpcommercial/objects/Bimp_FactureFourn.class.php');
+
+class BimpFactureFournForDol extends Bimp_FactureFourn
+{
+
+    public function __construct($db)
+    {
+
         require_once __DIR__ . '/../../bimpcore/Bimp_Lib.php';
-        
+
         return parent::__construct('bimpcommercial', 'Bimp_FactureFourn');
     }
-    
-    
-    public function sendRelanceExpertiseBimp(){
-        
-        
+
+    public function sendRelanceExpertiseBimp()
+    {
+
+
         $sends = array();
-        $filtres = array('expertise'=>0, 'datef' => array('operator'=> '>=', 'value'=> '2022-04-01'), 'a__ef.type' => array('operator'=>'!=', 'value'=>'S'), 'exported'=>1);
+        $filtres = array('expertise' => 0, 'datef' => array('operator' => '>=', 'value' => '2022-04-01'), 'a__ef.type' => array('operator' => '!=', 'value' => 'S'), 'exported' => 1);
         $joins = array();
 //        $joins['ef'] = array(
 //                        'alias' => 'ef',
@@ -24,55 +28,56 @@ class BimpFactureFournForDol extends Bimp_FactureFourn{
 //        die('count : '.count(BimpObject::getBimpObjectList('bimpcommercial', 'Bimp_Facture', $filtres, $joins)));
         $facts = BimpObject::getBimpObjectObjects('bimpcommercial', 'Bimp_Facture', $filtres, $joins);
         $nb = 0;
-        foreach($facts as $fact){
+        foreach ($facts as $fact) {
             $nb++;
             $sends[$fact->getCommercialId()][] = $fact->getLink();
         }
-        
+
         $nbMail = 0;
-        foreach($sends as $idUser => $data){
+        foreach ($sends as $idUser => $data) {
             $msg = 'Bonjour, voici les factures qui n\'ont pas d\'expertise Bimp<br/>';
-            foreach($data as $link){
-                $msg .= '<br/>'.$link;
+            foreach ($data as $link) {
+                $msg .= '<br/>' . $link;
             }
-            $user = BimpCache::getBimpObjectInstance('bimpcore', 'Bimp_User', $idUser); 
+            $user = BimpCache::getBimpObjectInstance('bimpcore', 'Bimp_User', $idUser);
 //            echo '<pre>';
 //            print_r(array('Factures sans expertise BIMP', $user->getData('email'), null, $msg));
 //            echo '<br/><br/>';
             mailSyn2('Factures sans expertise BIMP', $user->getData('email'), null, $msg);
             $nbMail++;
         }
-        $this->output = $nb. ' factures '.$nbMail.' mail';
+        $this->output = $nb . ' factures ' . $nbMail . ' mail';
+
+        return 0;
     }
-    
-    
-    
-    public function sendRappelMargeNegative(){
+
+    public function sendRappelMargeNegative()
+    {
         global $db;
         $facts = array();
         $html = '';
-        
+
         $sql = $db->query("SELECT (a.total_ht / a.qty) - (buy_price_ht * a.qty / ABS(a.qty)) as marge, a.* FROM llx_facturedet a LEFT JOIN llx_facture f ON a.fk_facture = f.rowid LEFT JOIN llx_product_extrafields p ON p.fk_object = a.fk_product WHERE ((a.total_ht / a.qty)+0.01) < (buy_price_ht * a.qty / ABS(a.qty)) AND f.datef > '2022-04-01' AND p.type_compta NOT IN (3);");
-        while($ln = $db->fetch_object($sql)){
-            $factLine = BimpCache::findBimpObjectInstance('bimpcommercial', 'Bimp_FactureLine', array('id_line'=>$ln->rowid));
+        while ($ln = $db->fetch_object($sql)) {
+            $factLine = BimpCache::findBimpObjectInstance('bimpcommercial', 'Bimp_FactureLine', array('id_line' => $ln->rowid));
             $margeF = $factLine->getTotalMarge();
-            if($margeF < 0){
+            if ($margeF < 0) {
 //                $this->output .= $ln->fk_facture . ' MARGE I : '.$factLine->getMargin() .' MARGE F : '.$factLine->getTotalMarge(). '<br/>';
                 $facts[$ln->fk_facture][$ln->rowid] = $factLine;
             }
         }
-        
-        foreach($facts as $idFact => $lines){
+
+        foreach ($facts as $idFact => $lines) {
             $fact = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_Facture', $idFact);
-            $html .= '<h2>'.$fact->getLink().'</h2>';
-            foreach($lines as $line){
-                $html .= $line->getLink().'<br/>';
+            $html .= '<h2>' . $fact->getLink() . '</h2>';
+            foreach ($lines as $line) {
+                $html .= $line->getLink() . '<br/>';
             }
             $html .= '<br/>';
         }
-        
+
         mailSyn2('Ligne de facture marge négative', 'f.pineri@bimp.fr, tommy@bimp.fr, a.alimi@bimp.fr', null, $html);
-        
+
         $this->output = $html;
         return 0;
     }

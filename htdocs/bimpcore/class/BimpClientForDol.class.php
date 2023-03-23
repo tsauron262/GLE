534483f3f1dbd2f75b2a7987a304bc482cc5f1ba
flo <flo@192.168.52.71>
2023-03-23 23:50:02 +0100

    Maj cron
    Former-commit-id: a8b421fa7ac578cff5aa7a2a14536e1d1a57bed7

--- htdocs/bimpcore/class/BimpClientForDol.class.php
+++ htdocs/bimpcore/class/BimpClientForDol.class.php
@@ -93,8 +93,9 @@ class BimpClientForDol extends Bimp_Client
             BimpObject::loadClass('bimpcore', 'BimpNote');
 
             foreach ($clients as $c) {
-                $where = 'obj_module = \'bimpcore\' AND obj_name = \'Bimp_Client\' AND id_obj ' . $c->id;
-                $where .= ' AND content LIKE \'%L\'encours ICBA pour ce client n\'est valable que jusqu\'au%\'';
+                $where = 'obj_module = \'bimpcore\' AND obj_name = \'Bimp_Client\' AND id_obj = ' . $c->id;
+                $where .= ' AND content LIKE \'%' . $bdb->db->escape('L\'encours ICBA pour ce client n\'est valable que jusqu\'au') . '%\'';
+                $bdb->delete('bimpcore_note', $where);
 
                 $date_validite = new DateTime($c->getData('date_depot_icba'));
                 $date_validite->add(new DateInterval('P1Y'));
@@ -169,11 +170,13 @@ class BimpClientForDol extends Bimp_Client
         $nb_rappels = 0;
 
         if (!empty($clients)) {
-
-            if (!BimpObject::loadClass('bimpcore', 'BimpNote'))
-                $this->addError("Erreur lors du chargement de la classe BimpNote");
+            $bdb = BimpCache::getBdb();
+            BimpObject::loadClass('bimpcore', 'BimpNote');
 
             foreach ($clients as $c) {
+                $where = 'obj_module = \'bimpcore\' AND obj_name = \'Bimp_Client\' AND id_obj = ' . $c->id;
+                $where .= ' AND content LIKE \'%' . $bdb->db->escape('La limite de crédit autorisée par Atradius arrive à échéance le') . '%\'';
+                $bdb->delete('bimpcore_note', $where);
 
                 $commercial = $c->getCommercial();
 
--- htdocs/bimpcore/test.php
+++ htdocs/bimpcore/test.php
@@ -26,20 +26,6 @@ if (!$user->admin) {
     exit;
 }
 
-$id_client = 23;
-$where = 'obj_module = \'bimpcore\' AND obj_name = \'Bimp_Client\' AND id_obj = ' . $id_client;
-$where .= ' AND content LIKE \'%' . $bdb->db->escape('L\'encours ICBA pour ce client n\'est valable que jusqu\'au') . '%\'';
-
-$where .= ' AND date_create < \'2023-02-21 00:00:00\'';
-
-echo 'WHERE : ' . $where . '<br/><br/>';
-
-if ($bdb->delete('bimpcore_note', $where) <= 0) {
-    echo 'FAIL - ' . $bdb->err();
-} else {
-    echo 'ok';
-}
-
 echo '<br/>FIN';
 echo '</body></html>';
 

0befbcbc42d24e6db5c828271708631a553c5c61
flo <flo@192.168.52.71>
2023-03-24 17:10:55 +0100

    Debug dates
    Former-commit-id: c63c77d62992764797cd354b248c2530d0c99a4d

--- htdocs/bimpcore/classes/BimpTools.php
+++ htdocs/bimpcore/classes/BimpTools.php
@@ -461,11 +461,12 @@ class BimpTools
             if (preg_match('/^[0-9]+$/', $date_tms)) {
                 $date_tms = (int) $date_tms;
             } elseif (is_string($date_tms)) {
-                $date_tms = strtotime($date_tms);
+                $new_date_tms = strtotime($date_tms);
                 BimpCore::addlog('BimpTools::getDateFromTimestamp() - tms au format string', 4, 'bimpcore', null, array(
                     'tms initial'  => (string) '"' . $date_tms . '"',
-                    'tms converti' => $date_tms
+                    'tms converti' => $new_date_tms
                 ));
+                $date_tms = $new_date_tms;
             } else {
                 BimpCore::addlog('Erreur BimpTools::getDateFromTimestamp() - tms invalide', 4, 'bimpcore', null, array(
                     'tms' => (string) $date_tms

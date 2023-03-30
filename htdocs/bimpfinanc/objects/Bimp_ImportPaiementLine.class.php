6bd6d8850d2a88985a2f726e539ab564043069bc
tommy <tommy@localhost>
2023-03-30 09:56:16 +0200

    agenda
    Former-commit-id: ce772f929b8ec83f7a0a7811acc1ea56669518fe

--- htdocs/bimpfinanc/objects/Bimp_ImportPaiementLine.class.php
+++ htdocs/bimpfinanc/objects/Bimp_ImportPaiementLine.class.php
@@ -36,7 +36,7 @@ class Bimp_ImportPaiementLine extends BimpObject
 
         $type = '';
         
-        $codes = array(array('0517806000000669EUR2E0416135704405'), array('0417806000000669EUR2E04161357044C2', array('price' => 'methode2'))/*virement trés peut d'info (surement virement instentannée)*/);
+        $codes = array(array('0517806000000669EUR2E0416135704405'), array('0417806000000669EUR2E04161357044C2', array('price' => 'methode2')), array('0417806001600669EUR2E041613570444', array('price' => 'methode2'))/*virement trés peut d'info (surement virement instentannée)*/);
         
         foreach ($codes as $data){
             $code = $data[0];
@@ -54,6 +54,14 @@ class Bimp_ImportPaiementLine extends BimpObject
                         }
                         elseif(preg_match('/0417806000000669EUR2E04161357044C2[0-9 .A-Z()\.-]*(00000)([0-9A-Z\.,}{]+)/', $this->getData('data'), $matches)){
 //                            print_r($matches);
+                            $price = $matches[2];
+                            $lettre = substr($price, -1,1);
+                            $price = intval(str_replace($lettre, $this->lettreToChiffre(str_replace('{', '}', $lettre)), $price)) / 100;
+//                            die('deux');
+                        }
+                        elseif(preg_match('/0417806001600669EUR2E041613570444[0-9 .A-Z()\.-]*(00000)([0-9A-Z\.,}{]+)/', $this->getData('data'), $matches)){
+//                            print_r($matches);
+//                            die('rrr');
                             $price = $matches[2];
                             $lettre = substr($price, -1,1);
                             $price = intval(str_replace($lettre, $this->lettreToChiffre(str_replace('{', '}', $lettre)), $price)) / 100;

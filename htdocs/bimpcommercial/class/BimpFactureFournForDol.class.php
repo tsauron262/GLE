a5a6ffdbe329ee6955ca4be613c2c4822cf7bdcd
tommy <tommy@localhost>
2023-03-31 08:43:32 +0200

    cron
    Former-commit-id: a1678383904e6aca8a1495ad274b6270b1d77608

--- htdocs/bimpcommercial/class/BimpFactureFournForDol.class.php
+++ htdocs/bimpcommercial/class/BimpFactureFournForDol.class.php
@@ -15,9 +15,6 @@ class BimpFactureFournForDol extends Bimp_FactureFourn
 
     public function sendRelanceExpertiseBimp()
     {
-    ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
         $sends = array();
         $filtres = array('expertise' => 0, 'datef' => array('operator' => '>=', 'value' => '2022-04-01'), 'a__ef.type' => array('operator' => '!=', 'value' => 'S'), 'exported' => 1);

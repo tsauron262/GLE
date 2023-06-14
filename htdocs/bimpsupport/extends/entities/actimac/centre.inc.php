5984d24d5d28efac4762e87f2ffea6846a35180b
flo <flo@localhost>
2023-06-14 21:16:26 +0200

    Maj Actimac
    Former-commit-id: e1e80f6c00caa4f5726d756e71399a7a170577f7

--- htdocs/bimpsupport/extends/entities/actimac/centre.inc.php
+++ htdocs/bimpsupport/extends/entities/actimac/centre.inc.php
@@ -1,4 +1,6 @@
 <?php
 
 global $tabCentre;
-$tabCentre = array();
+$tabCentre = array(
+    "T"     => array("01 02 03 04 05", "test1@test.fr", "Centre test 1", 1, "1234567", "69000", "TEST 1", "adresse test 1", 1, 1),
+);
--- htdocs/bimpsupport/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/objects/BS_SAV.class.php
@@ -164,8 +164,6 @@ class BS_SAV extends BimpObject
         parent::__construct("bimpsupport", get_class($this));
 
         $this->useCaisseForPayments = (int) BimpCore::getConf('use_caisse_for_payments');
-
-        BimpMail::$defaultType = 'ldlc';
     }
 
     // Gestion des droits et autorisations: 

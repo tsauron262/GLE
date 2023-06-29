804b966e30a309470866ffd7c86a3cbac195e7b6
flo <flo@localhost>
2023-06-29 15:32:37 +0200

    Maj impors actimac
    Former-commit-id: da8456168ee242c2d6d638b9114661549ea50885

--- htdocs/bimpcore/clean_actimac.sql
+++ htdocs/bimpcore/clean_actimac.sql
@@ -1,3 +1,95 @@
+-- Avant
+TRUNCATE TABLE `llx_actioncomm_resources`;
+TRUNCATE TABLE `llx_actioncomm_extrafields`;
+TRUNCATE TABLE `llx_actioncomm`;
+
+TRUNCATE TABLE `llx_br_reservation`;
+
+-- Après
+TRUNCATE TABLE `llx_bimp_apple_internal_stock`;
+TRUNCATE TABLE `llx_bimp_apple_part_stock_mvt`;
+TRUNCATE TABLE `llx_be_equipment`;
+TRUNCATE TABLE `llx_be_equipment_place`;
+TRUNCATE TABLE `llx_bimp_commande_fourn_line`;
+TRUNCATE TABLE `llx_bimp_commande_line`;
+TRUNCATE TABLE `llx_bimp_facture_line`;
+TRUNCATE TABLE `llx_bimp_product_cur_pa`;
+TRUNCATE TABLE `llx_bimp_propal_line`;
+TRUNCATE TABLE `llx_bimp_remise_globale`;
+
+TRUNCATE TABLE `llx_element_element`;
+TRUNCATE TABLE `llx_element_contact`;
+
+TRUNCATE TABLE `llx_object_line_equipment`;
+TRUNCATE TABLE `llx_object_line_remise_arriere`;
+TRUNCATE TABLE `llx_object_line_remise`;
+
+TRUNCATE TABLE `llx_commandedet_extrafields`;
+TRUNCATE TABLE `llx_commandedet`;
+TRUNCATE TABLE `llx_commande_extrafields`;
+TRUNCATE TABLE `llx_commande`;
+
+TRUNCATE TABLE `llx_commande_fournisseurdet_extrafields`;
+TRUNCATE TABLE `llx_commande_fournisseurdet`;
+TRUNCATE TABLE `llx_commande_fournisseur_extrafields`;
+TRUNCATE TABLE `llx_commande_fournisseur`;
+
+TRUNCATE TABLE `llx_facturedet_extrafields`;
+TRUNCATE TABLE `llx_facturedet`;
+TRUNCATE TABLE `llx_facture_extrafields`;
+TRUNCATE TABLE `llx_facture`;
+
+TRUNCATE TABLE `llx_bs_sav`;
+TRUNCATE TABLE `llx_bs_sav_propal_line`;
+
+TRUNCATE TABLE `llx_facture_fourn_det_extrafields`;
+TRUNCATE TABLE `llx_facture_fourn_det`;
+TRUNCATE TABLE `llx_facture_fourn_extrafields`;
+TRUNCATE TABLE `llx_facture_fourn`;
+
+
+
+
+
+
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+TRUNCATE TABLE ``;
+
+
+
+
 TRUNCATE TABLE `llx_bank`;
 TRUNCATE TABLE `llx_bank_url`;
 
@@ -8,6 +100,9 @@ TRUNCATE TABLE `llx_bc_paiement`;
 TRUNCATE TABLE `llx_be_package_product`;
 TRUNCATE TABLE `llx_bimp_gsx_repair`;
 
+TRUNCATE TABLE `llx_bimpcore_log`;
+TRUNCATE TABLE `llx_bimpcore_file`;
+TRUNCATE TABLE `llx_bimpcore_hashtag`;
 TRUNCATE TABLE `llx_bimpcore_history`;
 TRUNCATE TABLE `llx_bimpcore_link`;
 TRUNCATE TABLE `llx_bimpcore_note`;
@@ -20,7 +115,7 @@ TRUNCATE TABLE `llx_bimpmail`;
 TRUNCATE TABLE `llx_bl_commande_fourn_reception`;
 TRUNCATE TABLE `llx_bl_commande_shipment`;
 
-TRUNCATE TABLE `llx_br_reservation`;
+
 
 TRUNCATE TABLE `llx_bs_apple_part`;
 TRUNCATE TABLE `llx_bs_sav_issue`;
@@ -28,10 +123,9 @@ TRUNCATE TABLE `llx_bs_sav_pret`;
 TRUNCATE TABLE `llx_bs_sav_product`;
 TRUNCATE TABLE `llx_bs_sav_propal_line`;
 
-TRUNCATE TABLE `llx_element_contact`;
 
-TRUNCATE TABLE `llx_object_line_remise_arriere`;
-TRUNCATE TABLE `llx_object_line_remise`;
+
+
 
 TRUNCATE TABLE `llx_paiement`;
 TRUNCATE TABLE `llx_paiement_facture`;

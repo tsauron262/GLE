42ee61ee3d31127b0dd2bd925606aac91fae563f
tommy <tommy@localhost>
2023-07-10 14:47:14 +0200

    ecologic api
    Former-commit-id: 0ce8d6808991933115e6348dd90077a55c3bb2a3

--- htdocs/bimpsupport/extends/entities/bimp/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/extends/entities/bimp/objects/BS_SAV.class.php
@@ -10,15 +10,20 @@ class BS_SAV_ExtEntity extends BS_SAV{
         99          => array('label' => 'Déclarré', 'icon' => 'arrow-right', 'classes' => array('danger'))
     );
     
-    public function actionToRestitute($data, &$success){
+    public function actionClose($data, &$success){
         $return = parent::actionToRestitute($data, $success);
         if(!count($return['errors'])){
-            $this->verifCologic();
+            if($this->asProdEcologic()){
+                $this->updateField('status_ecologic',1);
+            }
+            else{
+                $this->updateField('status_ecologic',-1);
+            }
         }
         return $return;
     }
     
-    public function verifCologic(){
+    public function asProdEcologic(){
         $asProd = false;
         $tabIdProd = json_decode(BimpCore::getConf('prod_ecologic', '', 'bimpsupport'));
         if(is_array($tabIdProd)){
@@ -28,11 +33,44 @@ class BS_SAV_ExtEntity extends BS_SAV{
                         $asProd = true;
             }
         }
-        if($asProd){
-            $this->updateField('status_ecologic',1);
+        return $asProd;
+    }
+    
+    public function getIRISSymtoms($type = null){
+        require_once DOL_DOCUMENT_ROOT . '/bimpapi/BimpApi_Lib.php';
+
+        $api = BimpAPI::getApiInstance('ecologic');
+        
+        $result = $api->executereqWithCache('printproducttypewithlabellist');
+        
+        $resultList = array();
+        foreach($result['ResponseData'] as $typeMat){
+            if(is_null($type) || $typeMat['ProductId'] == $type){
+                foreach($typeMat['RepairCodes'] as $val)
+                    $resultList[$val['Code']] = $val['Label'];
+            }
         }
-        else{
-            $this->updateField('status_ecologic',-1);
-        }
+        return $resultList;
+    }
+    
+    public function getEcologicProductId(){
+        $label = $this->getEquipmentData('product_label');
+        if(stripos($label, 'mac') !== false)
+            return 'EEE.M2.044';
+        if(stripos($label, 'ipad') !== false)
+            return 'EEE.M2.057';
+        if(stripos($label, 'iphone') !== false)
+            return 'EEE.M6.060';
+        
+        
+        return '';
+    }
+    public function actionToRestitute($data, &$success) {
+        $datas = $this->getData('ecologic_data');
+        $datas['IRISSymtoms'] = $data['IRISSymtoms'];
+        $this->updateField('ecologic_data', $datas);
+        
+        
+        return parent::actionToRestitute($data, $success);
     }
 }

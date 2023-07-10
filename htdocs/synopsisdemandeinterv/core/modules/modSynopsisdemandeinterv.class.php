8831d4636b5bccc95e2ced841694f831494be597
tommy <tommy@localhost>
2023-07-10 12:46:55 +0200

    module constructeur
    Former-commit-id: 9fb073ff1b2ba88242f888b4cbd23b631d20367a

--- htdocs/synopsisdemandeinterv/core/modules/modSynopsisdemandeinterv.class.php
+++ htdocs/synopsisdemandeinterv/core/modules/modSynopsisdemandeinterv.class.php
@@ -60,7 +60,7 @@ class modsynopsisdemandeinterv  extends DolibarrModules
     *   \brief      Constructeur. Definit les noms, constantes et boites
     *   \param      DB      handler d'acces base
     */
-  function modsynopsisdemandeinterv($DB)
+  function __construct($DB)
   {
     $this->db = $DB ;
     $this->numero = 22229 ;

f3d5dbdf113731c29f8941d2883adc5ced02bb66
flo <flo@localhost>
2023-07-11 16:47:13 +0200

    PDF GSX
    Former-commit-id: 6c6d938f6f62e4d411cf1783ca70d284465aae41

--- htdocs/bimpapple/controllers/gsxController.php
+++ htdocs/bimpapple/controllers/gsxController.php
@@ -2835,7 +2835,7 @@ class gsxController extends BimpController
                             'foldable' => true
                 ));
 
-                if (BimpCore::isEntity('actimac')) {
+                if (BimpCore::isEntity('bimp')) {
                     $sav_dir = $sav->getFilesDir();
                     if (!file_exists($sav_dir . 'infos_materiel.pdf')) {
                         require_once DOL_DOCUMENT_ROOT . '/bimpsupport/pdf/InfosMateriel.php';

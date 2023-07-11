2f27dc78225a1a6d1f9ba262215bedc9f62c410e
tommy <tommy@localhost>
2023-07-11 11:56:17 +0200

    pdf
    Former-commit-id: 0e7f6e8de01ab1f6229ba0d88aae5a9b9d329970

--- htdocs/core/db/mysqli.class.php
+++ htdocs/core/db/mysqli.class.php
@@ -1285,7 +1285,7 @@ class DoliDBMysqli extends DoliDB
         }
         if($deadLock){
             $this->transaction_opened = 0;
-            static::stopAll ();
+            static::stopAll ('catch');
         }
     }
     

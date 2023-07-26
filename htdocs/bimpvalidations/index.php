b240c7209045477e579cf2273a552b37a94f0133
flo <flo@localhost>
2023-07-26 13:03:11 +0200

    Maj bimpvalidation
    Former-commit-id: fefd0e98ebb8a0f4f7f5e55919f98efe6e16bc32

--- htdocs/bimpvalidations/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once '../bimpcore/main.php';
-
-require_once DOL_DOCUMENT_ROOT . '/bimpcore/Bimp_Lib.php';
-
-$controller = BimpController::getInstance('bimpvalidations');
-$controller->display();

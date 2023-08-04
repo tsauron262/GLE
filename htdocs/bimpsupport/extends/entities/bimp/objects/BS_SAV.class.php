7bcf76d9cf49a765d31068f5b5fc8ceeaab997f8
tommy <tommy@localhost>
2023-08-04 13:13:55 +0200

    sav sms
    Former-commit-id: bbd10b04f2791b7f0a58a8e732a6febd83dd19aa

--- htdocs/bimpsupport/extends/entities/bimp/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/extends/entities/bimp/objects/BS_SAV.class.php
@@ -128,7 +128,7 @@ class BS_SAV_ExtEntity extends BS_SAV{
     
     public function traiteVilleNameEcologic($name){
         $tabName = explode('15', $name);
-        $anme = trim($tabName[0]);
+        $name = trim($tabName[0]);
         $name = str_replace('-', ' ', $name);
         $name = str_replace('ç', 'c', $name);
         $name = str_replace('é', 'e', $name);

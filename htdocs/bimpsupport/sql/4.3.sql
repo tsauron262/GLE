2735bcdc4a687481034ab299f9da3d3212191816
tommy <tommy@localhost>
2023-08-24 14:41:12 +0200

    entity
    Former-commit-id: 5bf8ffa4991948a039c67478b36082692610dd86

---
+++ htdocs/bimpsupport/sql/4.3.sql
@@ -0,0 +1,8 @@
+ALTER TABLE llx_bs_sav ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_bs_pret ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_bs_ticket ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_bimp_gsx_shipment ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_bs_sac ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_bimp_apple_consigned_stock ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_bimp_apple_internal_stock ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_bimp_apple_part_stock_mvt ADD entity int(11) NOT NULL DEFAULT '1';

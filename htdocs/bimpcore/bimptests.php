87a13dba8886d4ebf61132f6a2069051e68d885a
flo <flo@192.168.79.71>
2023-09-05 13:25:37 +0200

    MAJ BimpValidation
    Former-commit-id: 781413f89762986f63318db150acad5dcc9b8a65

---
+++ htdocs/bimpcore/bimptests.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once("../main.inc.php");
+
+ini_set('display_errors', 1);
+require_once __DIR__ . '/Bimp_Lib.php';
+set_time_limit(0);
+
+ignore_user_abort(0);
+
+top_htmlhead('', 'TESTS', 0, 0, array(), array());
+
+echo '<body>';
+
+BimpCore::displayHeaderFiles();
+
+global $db, $user;
+
+if (!BimpObject::objectLoaded($user)) {
+    echo BimpRender::renderAlerts('Aucun utilisateur connecté');
+    exit;
+}
+
+if (!$user->admin) {
+    echo BimpRender::renderAlerts('Seuls les admin peuvent exécuter ce script');
+    exit;
+}
+
+$type_test = BimpTools::getValue('test_type', '');
+if (!$type_test) {
+    echo BimpRender::renderAlerts('Type de test absent');
+    exit;
+}
+
+switch ($type_test) {
+    case 'sms':
+        $num = BimpTools::getValue('num', '');
+        if (!$num) {
+            echo BimpRender::renderAlerts('Numéro de tel. absent (param url "num"');
+        } else {
+            $smsfile = new CSMSFile($num, 'TEST', 'Test');
+            if (!$smsfile->sendfile()) {
+                echo BimpRender::renderAlerts('ECHEC');
+            } else {
+                echo BimpRender::renderAlerts('Envoi OK', 'success');
+            }
+        }
+        break;
+}
+
+echo '<br/>FIN';
+echo '</body></html>';
+
+//llxFooter();

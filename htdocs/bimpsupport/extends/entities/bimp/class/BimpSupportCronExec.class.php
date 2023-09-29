eb7e4e9bbaa871782a97a36745e18fbfe125fa7a
tommy <tommy@localhost>
2023-09-29 14:31:28 +0200

    cgv boutique
    Former-commit-id: f00ebfe45d39cdd0657b8d03a9bf3132b281d3b4

--- htdocs/bimpsupport/extends/entities/bimp/class/BimpSupportCronExec.class.php
+++ htdocs/bimpsupport/extends/entities/bimp/class/BimpSupportCronExec.class.php
@@ -9,17 +9,27 @@ class BimpSupportCronExec extends BimpCron
         BimpObject::loadClass('bimpsupport', 'BS_SAV');
         
         $savs = BimpCache::getBimpObjectObjects('bimpsupport', 'BS_SAV', array('status_ecologic' => 1));
+        $mail = '';
         foreach($savs as $sav){
             $suc = '';
             $result = $sav->actionSendDemandeEcologic(array(), $suc);
             if (1) {
-                $this->output .= ($this->output ? '<br/><br/>' : '');
-                $this->output .= $sav->getLink();
-                if(count($result['errors']))
-                    $this->output .= implode('<br/>', $result['errors']);
-                if(count($result['warnings']))
-                    $this->output .= implode('<br/>', $result['warnings']);
+                $msg = '';
+                $msg .= $sav->getLink();
+                if(count($result['errors'])){
+                    $msg .= '<br/>'.implode('<br/>', $result['errors']);
+                }
+                if(count($result['warnings'])){
+                    $msg .= '<br/>'.implode('<br/>', $result['warnings']);
+                }
+                $this->output .= ($this->output ? '<br/><br/>' : '').$msg;
+                if(count($result['errors']) || count($result['warnings'])){
+                    $mail = ($mail != '' ? '<br/><br/>' : '').$msg;
+                }
             }
+        }
+        if($mail != ''){
+            mailSyn2('Probléme envoie ecologic', 'tommy@bimp.fr', null, $mail);
         }
         return 0;
     }

cc9fbb05a7c80e66b67157031201327bb852120d
tommy <tommy@localhost>
2023-10-06 11:23:36 +0200

    multisoc
    Former-commit-id: bd6adec964008aca2234c0734bdaaba4f17b1871

--- htdocs/bookmarks/bookmarks.lib.php
+++ htdocs/bookmarks/bookmarks.lib.php
@@ -137,7 +137,7 @@ function printDropdownBookmarksList()
 			}
 			$i = 0;
 			while ((empty($conf->global->BOOKMARKS_SHOW_IN_MENU) || $i < $conf->global->BOOKMARKS_SHOW_IN_MENU) && $obj = $db->fetch_object($resql)) {
-				$searchForm .= '<option name="bookmark'.$obj->rowid.'" value="'.$obj->rowid.'" '.($obj->target == 1 ? ' target="_blank" rel="noopener noreferrer"' : '').' rel="'.dol_escape_htmltag($obj->url).'" >';
+				$searchForm .= '<option name="bookmark'.$obj->rowid.'" value="'.$obj->rowid.'" '.($obj->target == 1 ? ' target="_blank" rel="noopener noreferrer"' : '').' rel="'.dol_escape_htmltag($obj->url).'" href="'.dol_escape_htmltag($obj->url).'">';
 				$searchForm .= dol_escape_htmltag($obj->title);
 				$searchForm .= '</option>';
 				$i++;

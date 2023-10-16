3bf0d93a4fabce56a78650b284e7cff70e3d8954
tommy <tommy@localhost>
2023-10-16 18:13:10 +0200

    multicompany
    Former-commit-id: ea06ac11fbc59e8fa97877f5b74949de6109c497

--- htdocs/bimptheme/switch.php
+++ htdocs/bimptheme/switch.php
@@ -4,14 +4,14 @@
 require_once("../main.inc.php");
 
 // $conf->global->MAIN_THEME n'est pas mis à jour lors du switch
-$sql = $db->query("SELECT * FROM `llx_user_param` WHERE `fk_user` = ".$user->id." AND `param` LIKE 'MAIN_THEME' ORDER BY `fk_user` ASC");
+$sql = $db->query("SELECT * FROM `llx_user_param` WHERE `fk_user` = ".$user->id." AND `param` LIKE 'MAIN_THEME' AND entity = '".getEntity("")."' ORDER BY `fk_user` ASC");
 
 if($db->num_rows($sql) > 0){
     while ($ln = $db->fetch_object($sql)){
         if($ln->value == "BimpTheme")
-            $db->query("UPDATE `llx_user_param` SET `value`= 'eldy' WHERE `fk_user` = ".$user->id." AND `param` LIKE 'MAIN_THEME'");
+            $db->query("UPDATE `llx_user_param` SET `value`= 'eldy' WHERE `fk_user` = ".$user->id." AND `param` LIKE 'MAIN_THEME' AND entity = ".$ln->entity);
         else
-            $db->query("UPDATE `llx_user_param` SET `value`= 'BimpTheme' WHERE `fk_user` = ".$user->id." AND `param` LIKE 'MAIN_THEME'");
+            $db->query("UPDATE `llx_user_param` SET `value`= 'BimpTheme' WHERE `fk_user` = ".$user->id." AND `param` LIKE 'MAIN_THEME' AND entity = ".$ln->entity);
     }
 }
 else{

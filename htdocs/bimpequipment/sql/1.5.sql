88b926662500a2a5d27783a7b7ee85946b13caf6
tommy <tommy@localhost>
2023-10-17 13:36:29 +0200

    multicompany
    Former-commit-id: 3fdf5f77f51acccdf7f18089f58771cf5075c084

---
+++ htdocs/bimpequipment/sql/1.5.sql
@@ -0,0 +1,3 @@
+ALTER TABLE llx_be_package ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_be_package_product ADD entity int(11) NOT NULL DEFAULT '1';
+ALTER TABLE llx_be_equipment ADD entity int(11) NOT NULL DEFAULT '1';

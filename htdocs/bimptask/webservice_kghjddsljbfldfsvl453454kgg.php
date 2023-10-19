43f5596c737ae194d7f85ce6a4826a474cd779ea
tommy <tommy@localhost>
2023-10-19 21:37:14 +0200

    mail retour sav
    Former-commit-id: 35ffe3ba226a0df844e7ac7839cd033cd84db07f

--- htdocs/bimptask/webservice_kghjddsljbfldfsvl453454kgg.php
+++ htdocs/bimptask/webservice_kghjddsljbfldfsvl453454kgg.php
@@ -10,9 +10,6 @@
 }*/
 define("NOLOGIN", 1); 
 $errors = array();
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 require_once '../bimpcore/main.php';
 

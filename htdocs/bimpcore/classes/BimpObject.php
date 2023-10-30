57f9d921f8a22810b80cb8d643bbb660e50bfb03
tommy <tommy@localhost>
2023-10-30 14:21:44 +0100

    bundle
    Former-commit-id: 0935fac8e91225a0bcc98d9d46672cebffa336af

--- htdocs/bimpcore/classes/BimpObject.php
+++ htdocs/bimpcore/classes/BimpObject.php
@@ -6428,7 +6428,7 @@ Nouvelle : ' . $this->displayData($champAddNote, 'default', false, true));
     public function can($right)
     {
 //        echo '<pre>'. get_class($this);
-        if(isset($this->dol_object)){//peut être un peut lourd, mais plus safe...
+        if(!BimpCore::isContextPublic() && isset($this->dol_object)){//peut être un peut lourd, mais plus safe...
             $return = BimpCache::dol_can($this->dol_object);
             if(!$return)
                 return 0;

310ad655a662095bab1540ccf68a7f5714b604b1
tommy <tommy@localhost>
2023-11-13 13:08:03 +0100

    blyyd
    Former-commit-id: 2b84dd794214460e7c6dbf171e5dd09b8a921dd0

--- htdocs/bimpcore/extends/entities/blyyd/objects/Bimp_Product.class.php
+++ htdocs/bimpcore/extends/entities/blyyd/objects/Bimp_Product.class.php
@@ -7,6 +7,9 @@ require_once DOL_DOCUMENT_ROOT . '/bimpcore/objects/Bimp_Product.class.php';
 class Bimp_Product_ExtEntity extends Bimp_Product
 {
     public static $sousTypes = array(
+        -1 => array(
+            0 => ''
+        ),
         0 => array(
             1 => 'De série',
             2 => 'En option'

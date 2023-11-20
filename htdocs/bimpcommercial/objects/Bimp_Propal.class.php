cb9cabc9595bec7e30f94ee8b147a961d76b5009
flo <flo@mbpmartzflorian>
2023-11-20 17:18:19 +0100

    Correctif erreur SQL
    Former-commit-id: d21bfcc11e8f1c04378419a7a075d1d39d63ee26

--- htdocs/bimpcommercial/objects/Bimp_Propal.class.php
+++ htdocs/bimpcommercial/objects/Bimp_Propal.class.php
@@ -316,16 +316,14 @@ class Bimp_Propal extends Bimp_PropalTemp
                 }
 
                 $items = BimpTools::getDolObjectLinkedObjectsList($this->dol_object, $this->db, array('bimp_contrat'));
-//                print_r($items);
-                foreach ($items as $id) {
-                    $obj = BimpCache::getBimpObjectInstance('bimpcontrat', 'BCT_Contrat', $id['id_object']);
-                    if ($obj->isLoaded()) {
-                        $errors[] = 'Contrat d\'abonnement déjà créé';
-                        return 0;
-                    }
-                }
                 if (!empty($items)) {
-                    
+                    foreach ($items as $id) {
+                        $obj = BimpCache::getBimpObjectInstance('bimpcontrat', 'BCT_Contrat', $id['id_object']);
+                        if ($obj->isLoaded()) {
+                            $errors[] = 'Contrat d\'abonnement déjà créé';
+                            return 0;
+                        }
+                    }
                 }
                 return 1;
 

2bec761540b78f9dd6456dc7ac47718f59666f23
tommy <tommy@localhost>
2023-11-23 10:18:06 +0100

    debug js
    Former-commit-id: d2ff7a1207aa6180672baaeee4ddf64ed9e472e6

--- htdocs/bimpcore/updates/florian/23.2.sql
+++ htdocs/bimpcore/updates/florian/23.2.sql
@@ -1,1 +1,7 @@
-ALTER TABLE `llx_bimp_c_secteur` ADD `email_from` varchar(30) NOT NULL DEFAULT '';
+CREATE TABLE `llx_bimp_c_secteur` (
+  `clef` varchar(30) NOT NULL,
+  `valeur` varchar(200) NOT NULL,
+  `email_from` varchar(30) NOT NULL DEFAULT '',
+  `public_entity` varchar(10) NOT NULL DEFAULT '',
+  PRIMARY KEY (`clef`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

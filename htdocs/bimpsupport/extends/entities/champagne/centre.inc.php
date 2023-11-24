4d7bf0288e8caaf0b5b5e08093be1f12103dfb8c
tommy <tommy@localhost>
2023-11-24 12:58:11 +0100

    debug js
    Former-commit-id: 8812bb0c7d937b59389a5817f5f3a632dc993957

--- htdocs/bimpsupport/extends/entities/champagne/centre.inc.php
+++ htdocs/bimpsupport/extends/entities/champagne/centre.inc.php
@@ -2,5 +2,5 @@
 
 global $tabCentre;
 $tabCentre = array(
-    "CH" => array("04 27 46 60 09", "sav_ch@ldlc.com", "Champagne-au-Mont-d'Or", 0, "1442050", "69410", "Champagne-au-Mont-d'Or", "Adresse", 40, 1),
+    "CH" => array("04 27 46 60 09", "sav_ch@ldlc.com", "Champagne-au-Mont-d'Or", 0, "1442050", "69410", "Champagne-au-Mont-d'Or", "2 avenue Charles de Gaulle", 40, 1),
 );

425d1d39c13f45da6483ffaa5b1de19a3a0b2bce
flo <flo@mbpmartzflorian>
2023-12-07 11:51:46 +0100

    Maj BimpContrat
    Former-commit-id: 632cbf73108d3b053fe5d6d391c7bc94d97e30e5

--- htdocs/bimpcontrat/class/BimpContratCronProcess.php
+++ htdocs/bimpcontrat/class/BimpContratCronProcess.php
@@ -8,8 +8,24 @@ class BimpContratCronProcess extends BimpCron
     public function dailyChecks()
     {
         $this->current_cron_name = 'Vérifs quotidiennes contrats v2';
-        
+
         // Vérif abonnemens fermés: 
-        $rows = $this->db->getRows();
+        $lines = BimpCache::getBimpObjectObjects('bimpcontrat', 'BCT_ContratLine', array(
+                    'statut'            => 4,
+                    'date_fin_validite' => array(
+                        'operator' => '<',
+                        'value'    => date('Y-m-d') . ' 00:00:00'
+                    )
+        ));
+
+        if (!empty($lines)) {
+            foreach ($lines as $line) {
+                $line->checkStatus();
+            }
+
+            $this->output .= 'Statut vérifié pour ' . count($lines) . ' ligne(s)';
+        }
+
+        return 0;
     }
 }

898720bb060b4f8e085e7c7eedc71f638ed86efe
tommy <tommy@localhost>
2024-01-04 16:35:51 +0100

    apple parts
    Former-commit-id: b3379c97d622711125d4b6671ecc171a7182b8da

--- htdocs/bimpsupport/extends/entities/bimp/objects/BS_SAV.class.php
+++ htdocs/bimpsupport/extends/entities/bimp/objects/BS_SAV.class.php
@@ -259,7 +259,7 @@ class BS_SAV_ExtEntity extends BS_SAV
             "ProductId"          => $this->getEcologicProductId(),
             "BrandId"            => "243",
             "CommercialRef"      => $prod->ref,
-            "SerialNumber"       => $this->getSerial(true),
+            "SerialNumber"       => $this->getSerial(false, true),
             "PurchaseDate"       => "",
             "IRISCondition"      => "",
             "IRISConditionEX"    => "",

46c028bea5ad9ccde175b84dc2ffdcb50979e87a
tommy <tommy@localhost>
2024-01-15 15:04:29 +0100

    apple parts
    Former-commit-id: e0a5bc47c655a75e16ead2862efb6a4014d291ce

--- htdocs/bimpapi/classes/apis/EcologicAPI.php
+++ htdocs/bimpapi/classes/apis/EcologicAPI.php
@@ -200,7 +200,7 @@ class EcologicAPI extends BimpAPI
                         $paramsFile = array();
                         $paramsFile['fields']['FileContent'] = base64_encode(file_get_contents($fileT[0] . $fileT[1].'.'.$fileT[2]));
                         $paramsFile['url_params'] = array('ClaimId' => $ecologicData['ClaimId'], 'FileName' => $fileT[1], 'FileExtension' => $fileT[2], 'DocumentType' => $fileT[3]);
-                        $return = $this->execCurl('AttachFile', $paramsFile, $errors);
+                        $return = $this->execCurl('AttachFile', $paramsFile, $warnings);
                         if(stripos($return, 'Code 200') !== false){
 //                        if(isset($return['ResponseData']) && $return['ResponseData']['IsValid']){
                             $ecologicData['files'][] = $fileT[1];

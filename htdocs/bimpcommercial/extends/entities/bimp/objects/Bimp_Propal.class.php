8b59eb1d483c08ec071a7103a1e7228466411731
flo <flo@mbpflormartinez>
2024-02-26 18:02:44 +0100

    Maj propales
    Former-commit-id: cb4a001251976cd2ab47407fbf08865f1c1038b8

--- htdocs/bimpcommercial/extends/entities/bimp/objects/Bimp_Propal.class.php
+++ htdocs/bimpcommercial/extends/entities/bimp/objects/Bimp_Propal.class.php
@@ -117,15 +117,15 @@ class Bimp_Propal_ExtEntity extends Bimp_Propal
         return parent::isActionAllowed($action, $errors);
     }
 
-    public function isFieldContratEditable()
-    {
-        if (BimpTools::getPostFieldValue('field_name') == 'duree_mois') {
-            $fields = BimpTools::getPostFieldValue('fields');
-            if ($fields['objet_contrat'] == 'ASMX')
-                return 1;
-            return 0;
-        }
-    }
+//    public function isFieldContratEditable()
+//    { // A checker... 
+//        if (BimpTools::getPostFieldValue('field_name') == 'duree_mois') {
+//            $fields = BimpTools::getPostFieldValue('fields');
+//            if ($fields['objet_contrat'] == 'ASMX')
+//                return 1;
+//            return 0;
+//        }
+//    }
 
     public function isDemandeFinAllowed(&$errors = array())
     {

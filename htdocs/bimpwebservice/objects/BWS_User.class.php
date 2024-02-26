cb5db17ec483a15f4fe7f45be85a158c23a9da4a
flo <flo@mbpflormartinez>
2024-02-26 16:08:39 +0100

    Debug webservice
    Former-commit-id: e5d219bb755ecd31e6a38c49fbbe0ad94058ebb4

--- htdocs/bimpwebservice/objects/BWS_User.class.php
+++ htdocs/bimpwebservice/objects/BWS_User.class.php
@@ -534,7 +534,7 @@ class BWS_User extends BimpObject
                     $error = 'Token invalide';
                     return false;
                 }
-                $error = 'Token expiré';
+                $error = 'Token expiré (' . $expire . ') - Veuillez reconnecter votre compte';
                 return false;
             }
             $error = 'Token absent';

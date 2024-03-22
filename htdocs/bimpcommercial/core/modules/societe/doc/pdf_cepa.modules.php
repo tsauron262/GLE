9a93629338cc4a0419dbc6ff9ab3681adaacb1f7
tommy <tommy@localhost>
2024-03-22 15:22:03 +0100

    separation rib
    Former-commit-id: d66894b0a685721590361c1e783ac36b4310fa9b

--- htdocs/bimpcommercial/core/modules/societe/doc/pdf_cepa.modules.php
+++ htdocs/bimpcommercial/core/modules/societe/doc/pdf_cepa.modules.php
@@ -1,6 +1,6 @@
 <?php
 
-if (defined('BIMP_LIB') && in_array(BimpCore::getExtendsEntity(), array('bimp'))) {
+//if (defined('BIMP_LIB') && in_array(BimpCore::getExtendsEntity(), array('bimp'))) {
     require_once DOL_DOCUMENT_ROOT . '/bimpcore/pdf/classes/CepaPDF.php';
 
     class pdf_cepa extends CepaPDF
@@ -12,17 +12,17 @@ if (defined('BIMP_LIB') && in_array(BimpCore::getExtendsEntity(), array('bimp'))
         }
     }
 
-} else {
-    require_once DOL_DOCUMENT_ROOT . '/bimpcore/pdf/classes/CepaPDF_old.php';
-
-    class pdf_cepa extends CepaPDF_Old
-    {
-
-        public function initData()
-        {
-            parent::initData();
-        }
-    }
-}
+//} else {
+//    require_once DOL_DOCUMENT_ROOT . '/bimpcore/pdf/classes/CepaPDF_old.php';
+//
+//    class pdf_cepa extends CepaPDF_Old
+//    {
+//
+//        public function initData()
+//        {
+//            parent::initData();
+//        }
+//    }
+//}
 
 

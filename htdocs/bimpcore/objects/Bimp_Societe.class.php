99f8a563436810ecac41ec1039e5de6ba03db708
flo <flo@mbpflormartinez>
2024-03-26 15:39:31 +0100

    Maj bimpContrat
    Former-commit-id: 68a26166cfe9e63a3bf9e6bb415b5e32aa1bb6fa

--- htdocs/bimpcore/objects/Bimp_Societe.class.php
+++ htdocs/bimpcore/objects/Bimp_Societe.class.php
@@ -500,7 +500,7 @@ class Bimp_Societe extends BimpDolObject
 
     public function useAtradius()
     {
-        1;//return ($this->useEncours() && BimpCore::getConf('useAtradius'));
+        1; //return ($this->useEncours() && BimpCore::getConf('useAtradius'));
     }
 
     // Getters params: 
@@ -1389,7 +1389,7 @@ class Bimp_Societe extends BimpDolObject
                         'operator' => '!=',
                         'value'    => 2
                     ),
-                    'c.entity' => explode(',', getEntity('commande'))
+                    'c.entity'         => explode(',', getEntity('commande'))
         ));
 
         $rows = $this->db->executeS($sql, 'array');
@@ -2205,14 +2205,16 @@ class Bimp_Societe extends BimpDolObject
                 $s = (count($contrats) > 1) ? 's' : '';
                 $html .= 'Contrat' . $s . ' d\'abonnement : ';
 
-                $fl = true;
+//                $fl = true;
                 foreach ($contrats as $contrat) {
-                    if (!$fl) {
-                        $html .= ', ';
-                    } else {
-                        $fl = false;
-                    }
-                    $html .= $contrat->getLink(array('object_icons' => 0));
+//                    if (!$fl) {
+//                        $html .= ', ';
+//                    } else {
+//                        $fl = false;
+//                    }
+                    $html .= '<div>';
+                    $html .= $contrat->getLink(array('object_icons' => 0, 'syntaxe' => '<ref> - <name>'));
+                    $html .= '</div>';
                 }
                 $html .= '</div>';
             }
@@ -3040,7 +3042,7 @@ class Bimp_Societe extends BimpDolObject
                 mailSyn2($subject, $emails, '', $msg);
 
             if (strlen($this->getData('siren')) == 9) {
-                $this->db->db->query("UPDATE " . MAIN_DB_PREFIX . "societe_atradius SET outstanding_limit = '" . $this->getData('outstanding_limit') . "' WHERE entity = ".$conf->entity." AND id_soc IN (SELECT rowid FROM ".MAIN_DB_PREFIX."societe WHERE siren = '" . $this->getData('siren') . "')");
+                $this->db->db->query("UPDATE " . MAIN_DB_PREFIX . "societe_atradius SET outstanding_limit = '" . $this->getData('outstanding_limit') . "' WHERE entity = " . $conf->entity . " AND id_soc IN (SELECT rowid FROM " . MAIN_DB_PREFIX . "societe WHERE siren = '" . $this->getData('siren') . "')");
             }
         }
     }

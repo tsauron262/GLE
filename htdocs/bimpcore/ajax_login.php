ecfc1ff977458dd5ba611aa5f0f6103b7346b81c
tommy <tommy@localhost>
2024-03-29 09:53:37 +0100

    token with relog
    Former-commit-id: 87070dea2c0ed902ea4f76ea755c88bd0b6a697e

--- htdocs/bimpcore/ajax_login.php
+++ htdocs/bimpcore/ajax_login.php
@@ -5,4 +5,12 @@ require_once '../main.inc.php';
 <html>
    <head></head>
    <body><span id="login_ok"></span></body>
+   <script>
+       <?php
+        if(class_exists('Session')){
+            echo "localStorage.setItem('bimp_hash', '".Session::getHash()."');";
+        }
+       ?>
+   
+   </script>
 </html>

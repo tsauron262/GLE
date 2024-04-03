c2e795e1641d06ae59a2042804c83cf04bd21c93
flo <flo@mbpflormartinez>
2024-04-03 18:22:04 +0200

    Envois mails relances en masse
    Former-commit-id: 9b7e37a6c0a36395f08b5473ec193c7e6baa548a

--- htdocs/bimpcommercial/objects/BimpRelanceClients.class.php
+++ htdocs/bimpcommercial/objects/BimpRelanceClients.class.php
@@ -337,7 +337,7 @@ class BimpRelanceClients extends BimpObject
         $html .= '<span class="badge badge-' . ($nb_att_mails > 0 ? 'warning' : 'success') . '">' . $nb_att_mails . '</span>';
         $html .= '</td>';
         $html .= '<td>';
-        if ($this->canSetAction('sendAllEmails')) {
+        if ($nb_att_mails > 0 && $this->canSetAction('sendAllEmails')) {
             $onclick = $this->getJsActionOnclick('sendAllEmails', array(
                 'all_relances' => 1
                     ), array(
@@ -504,8 +504,8 @@ class BimpRelanceClients extends BimpObject
             $lines = BimpCache::getBimpObjectObjects('bimpcore', 'BimpRelanceClientsLine', array(
                         'status' => BimpRelanceClientsLine::RELANCE_ATTENTE_MAIL
             ));
-
-            if (!empty($lines)) {
+//
+//            if (!empty($lines)) {
                 $this->db->db->commitAll();
                 $nOk = 0;
 
@@ -527,12 +527,12 @@ class BimpRelanceClients extends BimpObject
                     break;
                 }
 
-                if ($nOk) {
+//                if ($nOk) {
                     $success .= $nOk . ' relance(s) envoyée(s) par e-mail avec succès';
-                }
-            } else {
-                $errors[] = 'Il n\'y a aucune relance en attente d\'envoi par e-mail';
-            }
+//                }
+//            } else {
+//                $errors[] = 'Il n\'y a aucune relance en attente d\'envoi par e-mail';
+//            }
         } elseif ($this->isLoaded($errors)) {
             $errors = $this->sendEmails(false, $warnings);
 

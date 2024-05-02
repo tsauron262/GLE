ee61e6fa4de9971d09faad55c308914cb58bc0af
tommy <tommy@localhost>
2024-05-02 11:23:07 +0200

    secu
    Former-commit-id: a2c3a7f64b68833801e5ce0a5ae39205c17d5cb8

--- htdocs/bimpcore/classes/BimpObject.php
+++ htdocs/bimpcore/classes/BimpObject.php
@@ -2930,7 +2930,7 @@ class BimpObject extends BimpCache
                 }
             }
 
-            $allowed_tags = array('a', '@');
+            $allowed_tags = array('a');
             // Traitement des cas particuliers des listes de valeurs: 
             if ($type === 'items_list') {
                 if (is_string($value)) {
@@ -2966,6 +2966,7 @@ class BimpObject extends BimpCache
 
                     if ($auto_strip_tags && is_string($value) && !in_array($item_type, array('html', 'json', 'object_filters'))) {
                         $value = BimpTools::replaceBr($value);
+                        $value = BimpTools::replaceEmailTags($value);
                         $new_val = strip_tags($value, $allowed_tags);
 
                         if ($new_val !== $value) {
@@ -2983,6 +2984,7 @@ class BimpObject extends BimpCache
             } else {
                 if ($auto_strip_tags && is_string($value) && !in_array($type, array('html', 'json', 'object_filters'))) {
                     $value = BimpTools::replaceBr($value);
+                    $value = BimpTools::replaceEmailTags($value);
                     $new_val = strip_tags($value, $allowed_tags);
 
                     if ($new_val !== $value) {

611d80aeb0df80e2fe1dfc3c3bb9a2f7bccf9ab9
tommy <tommy@localhost>
2024-05-28 12:14:44 +0200

    admin affichage fonctions
    Former-commit-id: edf3ad7dd6c0811af00c0ec1327c8b5a98789970

--- htdocs/bimpinterfaceclient/controllers/savFormController.php
+++ htdocs/bimpinterfaceclient/controllers/savFormController.php
@@ -267,6 +267,13 @@ class savFormController extends BimpPublicController
 
                 $html .= '</h4>';
                 $html .= '</div>';
+                
+                /* pour vérif des tel */
+                if ((int) $userClient->getData('id_contact')) {
+                    $contact = $userClient->getChildObject('contact');
+                }
+                if($contact->getData('phone') != '' || $contact->getData('phone_perso') != '' || $contact->getData('phone_mobile') != '')
+                    $html .= '<input type="hidden" name="phone_ok" value="1"/>';
                 return $html;
             } else {
                 // Recherche d'un contact client:                 

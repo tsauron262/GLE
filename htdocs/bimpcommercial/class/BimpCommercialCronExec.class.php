d213d787779d5b9b51ec1109fd043b641f04dc8e
flo <flo@mbpflormartinez>
2024-05-29 13:05:22 +0200

    Maj BDS Check marges
    Former-commit-id: d9780f682550407accd8457891c4699f8067a745

--- htdocs/bimpcommercial/class/BimpCommercialCronExec.class.php
+++ htdocs/bimpcommercial/class/BimpCommercialCronExec.class.php
@@ -31,15 +31,15 @@ class BimpCommercialCronExec extends BimpCron
     {
         $this->current_cron_name = 'Vérifs toutes les heures des pièces commerciales';
         
-        $this->output .= '***** Vérifs Marges + reval OK des dernières factures mises à jour *****<br/>';
-        $facture_class = '';
-        BimpObject::loadClass('bimpcommercial', 'Bimp_Facture', $facture_class);
-        $this->output .= $facture_class::checkMargesRevalAll();
+//        $this->output .= '***** Vérifs Marges + reval OK des dernières factures mises à jour *****<br/>';
+//        $facture_class = '';
+//        BimpObject::loadClass('bimpcommercial', 'Bimp_Facture', $facture_class);
+//        $this->output .= $facture_class::checkMargesRevalAll();
         
-//        $this->output .= '***** Vérifs Marges commandes mises à jour *****<br/>';
-//        $commande_class = '';
-//        BimpObject::loadClass('bimpcommercial', 'Bimp_Commande', $commande_class);
-//        $this->output .= $commande_class::checkMargesAll();
+        $this->output .= '***** Vérifs Marges commandes mises à jour *****<br/>';
+        $commande_class = '';
+        BimpObject::loadClass('bimpcommercial', 'Bimp_Commande', $commande_class);
+        $this->output .= $commande_class::checkMargesAll();
         
         return 0;
     }

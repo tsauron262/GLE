4ccaaf906deeeffe8cadb77216552481b5765c82
flo <flo@mbpflormartinez>
2024-05-31 15:12:17 +0200

    Debug PDF
    Former-commit-id: 498a8613ba72bb11096f26f9f79d55917f73b6c3

--- htdocs/bimpcore/test.php
+++ htdocs/bimpcore/test.php
@@ -27,20 +27,6 @@ if (!$user->admin) {
     exit;
 }
 
-echo 'TEST OK';
-//$sql = 'SELECT s.code_client FROM llx_societe s WHERE s.client != 0 AND (s.outstanding_limit_credit_check > 0 OR s.outstanding_limit_atradius > 0) 
-//AND (SELECT COUNT(DISTINCT c.rowid) FROM llx_commande c WHERE c.fk_soc = s.rowid AND c.date_creation >= \'2024-04-01 00:00:00\') = 0
-//AND (SELECT COUNT(DISTINCT p.rowid) FROM llx_propal p WHERE p.fk_soc = s.rowid AND p.datec >= \'2024-04-01 00:00:00\') = 0;';
-//
-//$rows = $bdb->executeS($sql, 'array');
-//
-//echo count($rows) . ' clients.<br/><br/>';
-//
-//foreach ($rows as $r) {
-//    echo $r['code_client'] . '<br/>';
-//}
-
-
 echo '<br/>FIN';
 echo '</body></html>';
 

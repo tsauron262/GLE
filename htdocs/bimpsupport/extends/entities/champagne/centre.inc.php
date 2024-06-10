c04f304a757d058ebe8bf8cb066d085d67e82b61
tommy <tommy@localhost>
2024-06-10 15:05:51 +0200

    ajout type dans suivie recouvrement
    Former-commit-id: 6cb7ff4bd8b4d9a86b643e2959a3924a757f0e61

--- htdocs/bimpsupport/extends/entities/champagne/centre.inc.php
+++ htdocs/bimpsupport/extends/entities/champagne/centre.inc.php
@@ -2,5 +2,6 @@
 
 global $tabCentre;
 $tabCentre = array(
-    "CH" => array("04 27 46 60 09", "sav_ch@ldlc.com", "Champagne-au-Mont-d'Or", 0, "1442050", "69410", "Champagne-au-Mont-d'Or", "2 avenue Charles de Gaulle", 40, 1, null, '310065b3-815e-4043-a221-8ae34d463dc7'),
+    "CH" => array("04 27 46 60 09", "sav_ch@ldlc.com", "Champagne-au-Mont-d'Or", 0, "1745442", "69410", "Champagne-au-Mont-d'Or", "2 avenue Charles de Gaulle", 40, 1, null, '310065b3-815e-4043-a221-8ae34d463dc7'),
+    "LI" => array("04 27 46 60 09", "sav_ch@ldlc.com", "Limonest", 0, "1442050", "69760", "Limonest", "2 Rue des Érables", 40, 1, null, '310065b3-815e-4043-a221-8ae34d463dc7'),
 );

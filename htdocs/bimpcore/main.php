00c1bace29f3080f614eda2e002be9c5b48bc6aa
tommy <tommy@localhost>
2024-06-18 16:19:56 +0200

    cyber - cookie
    Former-commit-id: d7243ddeac9e61636f1d96a7875a0cfdcb5306b9

--- htdocs/bimpcore/main.php
+++ htdocs/bimpcore/main.php
@@ -11,7 +11,7 @@ if ($context == 'public') {
     define('XFRAMEOPTIONS_ALLOWALL', true);
     
     $sessionname = 'publicerp';
-        session_set_cookie_params(array('SameSite' => 'None', 'Secure' => true, 'path' => '/'));
+        session_set_cookie_params(array('SameSite' => 'Lax', 'Secure' => true, 'path' => '/', 'httponly' => true));
 	session_name($sessionname);
         
         $test = session_id();

742322feb86244b7b48d51da0ace8467085dcf8e
flo <flo@localhost>
2024-07-02 14:36:13 +0200

    Maj bds receptions lines
    Former-commit-id: 94b3ca84c28abe8166a0d68657845953c53bdf3d

--- htdocs/bimpdatasync/classes/process_overrides/BDS_ConvertProcess.php
+++ htdocs/bimpdatasync/classes/process_overrides/BDS_ConvertProcess.php
@@ -458,7 +458,7 @@ class BDS_ConvertProcess extends BDSProcess
                                 'dest_object_type'   => 'bimp_object'
                             );
 
-                            $this->db->delete('bimpcore_objects_associations', 'association = \'equipments\' AND src_object_name = \'BL_ShipmentLine\' AND src_id_object = ' . $id_shipment_line . ' AND dest_object_name = \'Equipment\'');
+//                            $this->db->delete('bimpcore_objects_associations', 'association = \'equipments\' AND src_object_name = \'BL_ShipmentLine\' AND src_id_object = ' . $id_shipment_line . ' AND dest_object_name = \'Equipment\'');
 
                             foreach ($shipment_data['equipments'] as $id_equipment) {
                                 if (!(int) $id_equipment) {

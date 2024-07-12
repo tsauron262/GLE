d1b7e3a7b3c23d317f70fecc4f864dede6be3f5d
flo <flo@mbpflormartinez>
2024-07-12 15:03:45 +0200

    Statut fermée CF
    Former-commit-id: abe972b570607fda2bb77f95d65e89f2a468604e

--- htdocs/bimpcommercial/sql/7.3.sql
+++ htdocs/bimpcommercial/sql/7.3.sql
@@ -1,3 +1,6 @@
 ALTER TABLE llx_commande_fournisseur 
     ADD `closed` tinyint NOT NULL DEFAULT 0,
     ADD `date_closed` DATETIME DEFAULT NULL;
+
+UPDATE llx_commande_fournisseur SET closed = 1, date_closed = tms WHERE fk_statut IN (6,7,9);
+UPDATE llx_commande_fournisseur SET closed = 1, date_closed = tms WHERE fk_statut = 5 AND invoice_status = 2;

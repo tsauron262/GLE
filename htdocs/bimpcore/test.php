1e6415fdb3c450c0e71245e06ad4e65e3b75c2e1
flo <flo@mbpflormartinez>
2024-07-18 14:13:28 +0200

    Debug rapport Apple
    Former-commit-id: 528da17ba543306fed39fe4077b6b04a95cb770f

--- htdocs/bimpcore/test.php
+++ htdocs/bimpcore/test.php
@@ -27,16 +27,51 @@ if (!$user->admin) {
     exit;
 }
 
-$dir = DOL_DATA_ROOT . '/bimpcore/apple_csv/2024/';
+//$dir = DOL_DATA_ROOT . '/bimpcore/apple_csv/2024/';
+//
+//foreach (scandir($dir) as $f) {
+//    if (in_array($f, array('.', '..'))) {
+//        continue;
+//    }
+//
+//    echo '<br/>';
+//    echo '<a href="' . DOL_URL_ROOT . '/document.php?modulepart=bimpcore&file=' . urlencode('apple_csv/2024/' . $f) . '" target="_blank">' . $f . '</a>';
+//}
 
-foreach (scandir($dir) as $f) {
-    if (in_array($f, array('.', '..'))) {
-        continue;
+$host = BimpCore::getConf('exports_ldlc_ftp_serv');
+$port = 21;
+$login = BimpCore::getConf('exports_ldlc_ftp_user');
+$pword = BimpCore::getConf('exports_ldlc_ftp_mdp');
+
+$ftp = ftp_connect($host, $port);
+
+if ($ftp === false) {
+    $errors[] = 'Echec de la connexion FTP avec le serveur "' . $host . '"';
+} else {
+    if (!ftp_login($ftp, $login, $pword)) {
+        $errors[] = 'Echec de la connexion FTP - Identifiant ou mot de passe incorrect';
+    } else {
+        if (defined('FTP_SORTANT_MODE_PASSIF')) {
+            ftp_pasv($ftp, true);
+        } else {
+            ftp_pasv($ftp, false);
+        }
+
+        $ftp_dir = '/' . BimpCore::getConf('exports_ldlc_ftp_dir') . '/statsapple/' . date('Y') . '/';
+        $files = ftp_nlist($ftp, $ftp_dir);
+
+        echo 'FICHIERS du dossier "' . $ftp_dir . '"';
+        echo '<pre>';
+        print_r($files);
+        echo '</pre>';
     }
 
-    echo '<br/>';
-    echo '<a href="' . DOL_URL_ROOT . '/document.php?modulepart=bimpcore&file=' . urlencode('apple_csv/2024/' . $f) . '" target="_blank">' . $f . '</a>';
+    ftp_close($ftp);
 }
+
+echo 'Erreurs : <pre>';
+print_r($errors);
+echo '</pre>';
 
 echo '<br/>FIN';
 echo '</body></html>';

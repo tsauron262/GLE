37b235623506dabca7139c71539f429d57040fe9
flo <flo@mbpflormartinez>
2024-07-18 10:32:12 +0200

    Debug rapport Apple
    Former-commit-id: 7599b433573a9c9d2c01de9c7fde42c78afcf768

--- htdocs/bimpcore/test.php
+++ htdocs/bimpcore/test.php
@@ -27,47 +27,11 @@ if (!$user->admin) {
     exit;
 }
 
-//$sql = 'SELECT fdet.fk_product as id_product, fdet.qty, fl.id as id_line, fl.linked_object_name, fl.linked_id_object, fl.id_obj as id_facture, av.rowid as id_avoir FROM llx_bimp_facture_line fl
-//LEFT JOIN llx_facture f ON f.rowid = fl.id_obj
-//LEFT JOIN llx_facturedet fdet ON fdet.rowid = fl.id_line
-//LEFT JOIN llx_facture av ON av.fk_facture_source = f.rowid
-//WHERE f.type = 0 
-//AND fdet.fk_product > 0
-//AND av.rowid > 0
-//AND fl.linked_object_name = \'contrat_line\' AND fl.linked_id_object > 0
-//AND (SELECT COUNT(avl.id) FROM llx_bimp_facture_line avl WHERE avl.linked_id_object = fl.linked_id_object AND avl.id_obj = av.rowid) = 0
-//AND fl.id NOT IN (5331348);';
-//
-//$rows = $bdb->executeS($sql, 'array');
-//
-//foreach ($rows as $r) {
-//    $av_lines = $bdb->getRows('facturedet', 'fk_facture = ' . $r['id_avoir'] . ' AND fk_product = ' . $r['id_product'] . ' AND qty = ' . $r['qty'], null, 'array', array('rowid'));
-//
-//    if (is_array($av_lines)) {
-//        echo $r['id_line'] . ' - ';
-//
-//        if (count($av_lines) == 1) {
-//            if ($bdb->update('bimp_facture_line', array(
-//                        'linked_object_name' => $r['linked_object_name'],
-//                        'linked_id_object'   => $r['linked_id_object']
-//                            ), 'id_line = ' . $av_lines[0]['rowid']) <= 0) {
-//                echo 'FAIL : ' . $bdb->err();
-//            } else {
-//                echo 'OK';
-//            }
-//        } elseif (count($av_lines) > 1) {
-//            echo 'PLUSIEURS AV LINES';
-//        } else {
-//            echo 'NO AV LINES';
-//        }
-//        echo '<br/>';
-//    } else {
-//        echo 'FAIL - ' . $bdb->err();
-//    }
-//
-//
-//    $av_line = BimpCache::getBimpObjectInstance('bimpcommercial', 'Bimp_FactureLine', $id_object);
-//}
+$dir = DOL_DATA_ROOT . '/bimpcore/apple_csv/2024/';
+
+echo '<pre>';
+print_r(scandir($dir));
+echo '</pre>';
 
 echo '<br/>FIN';
 echo '</body></html>';

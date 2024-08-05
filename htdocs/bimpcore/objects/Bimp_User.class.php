dcc35a57bb79a8f6dbad70670a63d06c676f7728
tommy <tommy@localhost>
2024-08-05 15:12:52 +0200

    Apple premium
    Former-commit-id: c96986bc5b050d4d0e0cea75419eb9b4b996a82e

--- htdocs/bimpcore/objects/Bimp_User.class.php
+++ htdocs/bimpcore/objects/Bimp_User.class.php
@@ -2777,7 +2777,7 @@ class Bimp_User extends BimpObject
         $boxObj->boxlabel .= ' sur ' . $nbJ . ' jours';
 
         $sql = "SELECT u.lastname, u.firstname, SUM(a.total_ht) as total, COUNT(DISTINCT a.rowid) as nbTot, SUM(IF(fk_product_type=1, a.total_ht, 0)) as totalServ, SUM(IF(fk_product_type=1, 1, 0)) as nbServ, SUM(a.qty) as qtyTot, SUM(IF(fk_product_type=1, a.qty, 0)) as qtyServ
-    FROM llx_facturedet a
+    FROM ".MAIN_DB_PREFIX."facturedet a
     LEFT JOIN llx_facture f ON f.rowid = a.fk_facture
     LEFT JOIN llx_element_contact elemcont ON elemcont.element_id = a.fk_facture
     LEFT JOIN llx_c_type_contact typecont ON elemcont.fk_c_type_contact = typecont.rowid

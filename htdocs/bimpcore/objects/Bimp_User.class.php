fa2f34a54bd30ff7e2aeb83e7b420ccee6d6fa62
tommy <tommy@localhost>
2024-08-06 11:47:12 +0200

    add onglet ldap dans user
    Former-commit-id: f2fbd0314bdb4c9ff731bdaa4cd0b3ed6d7bff26

--- htdocs/bimpcore/objects/Bimp_User.class.php
+++ htdocs/bimpcore/objects/Bimp_User.class.php
@@ -940,6 +940,14 @@ class Bimp_User extends BimpObject
             'content' => $this->renderMaterielView()
         );
 
+        $tabs[] = array(
+            'id'      => 'ldap',
+            'title'   => BimpRender::renderIcon('link', 'iconLeft') . 'LDAP',
+//            'content' => $this->renderLdapView()
+            'ajax'          => 1,
+            'ajax_callback' => $this->getJsLoadCustomContent('renderLdapView', '$(\'#ldap .nav_tab_ajax_result\')', array('ldap'), array('button' => ''))
+        );
+
         return BimpRender::renderNavTabs($tabs);
     }
 
@@ -979,6 +987,126 @@ class Bimp_User extends BimpObject
         );
 
         return BimpRender::renderNavTabs($tabs, 'tasks');
+    }
+    
+    public function renderLdapView(){
+        ob_start();
+        
+        
+        global $langs, $conf;
+        $langs->loadLangs(array('users', 'admin', 'companies', 'ldap'));
+        
+        
+        require_once DOL_DOCUMENT_ROOT.'/core/class/ldap.class.php';
+        require_once DOL_DOCUMENT_ROOT.'/core/lib/ldap.lib.php';
+
+        $object = $this->dol_object;
+
+                print '<div class="fichecenter">';
+        print '<div class="underbanner clearboth"></div>';
+
+        print '<table class="border centpercent tableforfield">';
+
+        // Login
+        print '<tr><td class="titlefield">'.$langs->trans("Login").'</td>';
+        //if ($object->ldap_sid) {
+        //	print '<td class="warning">'.$langs->trans("LoginAccountDisableInDolibarr").'</td>';
+        //} else {
+                print '<td>'.$object->login.'</td>';
+        //}
+        print '</tr>';
+
+        if ($conf->global->LDAP_SERVER_TYPE == "activedirectory") {
+                $ldap = new Ldap();
+                $result = $ldap->connect_bind();
+                if ($result > 0) {
+                        $userSID = $ldap->getObjectSid($object->login);
+                }
+                print '<tr><td class="valigntop">'.$langs->trans("SID").'</td>';
+                print '<td>'.$userSID.'</td>';
+                print "</tr>\n";
+        }
+
+        // LDAP DN
+        print '<tr><td>LDAP '.$langs->trans("LDAPUserDn").'</td><td class="valeur">'.getDolGlobalString('LDAP_USER_DN')."</td></tr>\n";
+
+        // LDAP Cle
+        print '<tr><td>LDAP '.$langs->trans("LDAPNamingAttribute").'</td><td class="valeur">'.getDolGlobalString('LDAP_KEY_USERS')."</td></tr>\n";
+
+        // LDAP Server
+        print '<tr><td>LDAP '.$langs->trans("Type").'</td><td class="valeur">'.getDolGlobalString('LDAP_SERVER_TYPE')."</td></tr>\n";
+        print '<tr><td>LDAP '.$langs->trans("Version").'</td><td class="valeur">'.getDolGlobalString('LDAP_SERVER_PROTOCOLVERSION')."</td></tr>\n";
+        print '<tr><td>LDAP '.$langs->trans("LDAPPrimaryServer").'</td><td class="valeur">'.getDolGlobalString('LDAP_SERVER_HOST')."</td></tr>\n";
+        print '<tr><td>LDAP '.$langs->trans("LDAPSecondaryServer").'</td><td class="valeur">'.getDolGlobalString('LDAP_SERVER_HOST_SLAVE')."</td></tr>\n";
+        print '<tr><td>LDAP '.$langs->trans("LDAPServerPort").'</td><td class="valeur">'.getDolGlobalString('LDAP_SERVER_PORT')."</td></tr>\n";
+
+        print '</table>';
+
+        print '</div>';
+
+        print dol_get_fiche_end();
+
+        /*
+         * Action bar
+         */
+        print '<div class="tabsAction">';
+
+        if (getDolGlobalInt('LDAP_SYNCHRO_ACTIVE') === Ldap::SYNCHRO_DOLIBARR_TO_LDAP) {
+                print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&amp;action=dolibarr2ldap">'.$langs->trans("ForceSynchronize").'</a>';
+        }
+
+        print "</div>\n";
+
+        if (getDolGlobalInt('LDAP_SYNCHRO_ACTIVE') === Ldap::SYNCHRO_DOLIBARR_TO_LDAP) {
+                print "<br>\n";
+        }
+
+
+
+        // Affichage attributs LDAP
+        print load_fiche_titre($langs->trans("LDAPInformationsForThisUser"));
+
+        print '<table class="noborder centpercent">';
+
+        print '<tr class="liste_titre">';
+        print '<td>'.$langs->trans("LDAPAttributes").'</td>';
+        print '<td>'.$langs->trans("Value").'</td>';
+        print '</tr>';
+
+        // Lecture LDAP
+        $ldap = new Ldap();
+        $result = $ldap->connect_bind();
+        if ($result > 0) {
+                $info = $object->_load_ldap_info();
+                $dn = $object->_load_ldap_dn($info, 1);
+                $search = "(".$object->_load_ldap_dn($info, 2).")";
+
+                $records = $ldap->getAttribute($dn, $search);
+
+                //print_r($records);
+
+                // Affichage arbre
+                if (((!is_numeric($records)) || $records != 0) && (!isset($records['count']) || $records['count'] > 0)) {
+                        if (!is_array($records)) {
+                                print '<tr class="oddeven"><td colspan="2"><span class="error">'.$langs->trans("ErrorFailedToReadLDAP").'</span></td></tr>';
+                        } else {
+                                $result = show_ldap_content($records, 0, $records['count'], true);
+                        }
+                } else {
+                        print '<tr class="oddeven"><td colspan="2">'.$langs->trans("LDAPRecordNotFound").' (dn='.dol_escape_htmltag($dn).' - search='.dol_escape_htmltag($search).')</td></tr>';
+                }
+
+                $ldap->unbind();
+        } else {
+                setEventMessages($ldap->error, $ldap->errors, 'errors');
+        }
+
+        print '</table>';
+        
+        
+        $html = ob_get_clean();
+        
+        return $html;
     }
 
     public function renderMaterielView()

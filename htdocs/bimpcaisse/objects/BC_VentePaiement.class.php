537a0b84932a7063d84260d25553cff88d794136
tommy <tommy@localhost>
2024-08-06 13:09:55 +0200

    refact cegid export
    Former-commit-id: f8742fbe6bfef060ee5b568fca4e7bc19db0faba

--- htdocs/bimpcaisse/objects/BC_VentePaiement.class.php
+++ htdocs/bimpcaisse/objects/BC_VentePaiement.class.php
@@ -6,7 +6,7 @@ class BC_VentePaiement extends BimpObject
     public static $codes = array(
         'LIQ' => array('label' => 'Paiement Liquide', 'icon' => 'money'),
         'CB'  => array('label' => 'Paiement CB', 'icon' => 'credit-card'),
-        'CHQ' => array('label' => 'Paiement Chèque', 'icon' => 'pencil'),
+        'CHQ' => array('label' => 'Paiement Chèque', 'icon' => 'fas_pencil-alt'),
         'AE'  => array('label' => 'Paiement American Express', 'icon' => 'cc-amex'),
         'CG'  => array('label' => 'Chèque galerie', 'icon' => 'fas_money-check'),
         'FIN'  => array('label' => 'Financement', 'icon' => 'fas_hand-holding-usd'),

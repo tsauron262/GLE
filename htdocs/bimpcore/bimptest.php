f09cc66324fc14a400dee4a36d054eca3b1573d4
flo <flo@mbpflormartinez>
2024-09-12 17:48:09 +0200

    Aalerte factures d'abonnement impayées
    Former-commit-id: 125d6864adc905f43d9668dd01c96ed95e7e7ff2

--- htdocs/bimpcore/bimptest.php
+++ htdocs/bimpcore/bimptest.php
@@ -27,57 +27,8 @@ if (!$user->admin) {
     exit;
 }
 
-
-$sql = BimpTools::getSqlFullSelectQuery('object_line_equipment', array('DISTINCT a.id_equipment', 'e.serial', 'p.ref as ref_prod', 'p.label', 'f.datef', 'f.ref as ref_fac'), array(
-            'a.object_type' => 'facture',
-            'or_soc'        => array(
-                'or' => array(
-                    'f.id_client_final' => 142,
-                    'f.fk_soc'          => 142
-                )
-            ),
-            'f.datef'       => array(
-                'min' => '2021-09-01'
-            ),
-            'or_prod'       => array(
-                'or' => array(
-                    'p.label'         => array(
-                        'part_type' => 'middle',
-                        'part'      => 'mac'
-                    ),
-                    'e.product_label' => array(
-                        'part_type' => 'middle',
-                        'part'      => 'mac'
-                    )
-                )
-            )
-                ), array(
-            'e'  => array(
-                'table' => 'be_equipment',
-                'on'    => 'a.id_equipment = e.id'
-            ),
-            'fl' => array(
-                'table' => 'bimp_facture_line',
-                'on'    => 'fl.id = a.id_object_line'
-            ),
-            'f'  => array(
-                'table' => 'facture',
-                'on'    => 'f.rowid = fl.id_obj'
-            ),
-            'p'  => array(
-                'table' => 'product',
-                'on'    => 'p.rowid = e.id_product'
-            )
-        ));
-
-$rows = $bdb->executeS($sql, 'array');
-
-$str = '"N°de série";"Réf. produit";"Libellé produit";"Réf. facture";"Date facture"';
-foreach ($rows as $r) {
-    $str .= "<br/>" . '"' . $r['serial'] . '";"' . $r['ref_prod'] . '";"' . $r['label'] . '";"' . $r['ref_fac'] . '";"' . date('d / m / Y', strtotime($r['datef'])) . '"';
-}
-
-echo $str;
+BimpObject::loadClass('bimpcontrat', 'BCT_Contrat');
+BCT_Contrat::sendAlertUnpaidFacsAbo();
 
 echo '<br/>FIN';
 echo '</body></html>';

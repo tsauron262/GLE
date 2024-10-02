68b3234a91fceca41953ab24ea6d51342d422dfb
flo <flo@mbpflormartinez>
2024-10-02 15:28:55 +0200

    Maj combinaisons
    Former-commit-id: f2d46c3219a88f6737d67b4a9f3e5c7a794fd2be

--- htdocs/bimpcore/objects/Bimp_ProductAttributeValue.class.php
+++ htdocs/bimpcore/objects/Bimp_ProductAttributeValue.class.php
@@ -1,9 +1,30 @@
 <?php
 
-class Bimp_ProductattributeValue extends BimpObject
+class Bimp_ProductAttributeValue extends BimpObject
 {
 
     public $no_dol_right_check = true;
+
+    // Getters array : 
+
+    public static function getAttributeValuesArray($id_attribute, $include_empty = true)
+    {
+        $cache_key = 'product_attribute_' . $id_attribute . '_values_array';
+
+        if (!isset(self::$cache[$cache_key])) {
+            self::$cache[$cache_key] = array();
+
+            $rows = self::getBdb()->getRows('product_attribute_value', 'fk_product_attribute = ' . $id_attribute, null, 'array', array('rowid', 'ref', 'value'), 'position', 'asc');
+
+            if (is_array($rows)) {
+                foreach ($rows as $r) {
+                    self::$cache[$cache_key][(int) $r['rowid']] = $r['ref'] . ' : ' . $r['value'];
+                }
+            }
+        }
+
+        return self::getCacheArray($cache_key, $include_empty);
+    }
 
     // Surcharges : 
 
@@ -11,7 +32,9 @@ class Bimp_ProductattributeValue extends BimpObject
     {
         $result = parent::hydrateFromDolObject($bimpObjectFields);
 
-        $bimpObjectFields[] = 'position';
+        if (!in_array('position', $bimpObjectFields)) {
+            $bimpObjectFields[] = 'position';
+        }
 
         return $result;
     }
--- htdocs/bimpcore/objects/Bimp_ProductCombination.class.php
+++ htdocs/bimpcore/objects/Bimp_ProductCombination.class.php
@@ -7,6 +7,32 @@ class Bimp_ProductCombination extends BimpObject
 
     public $no_dol_right_check = true;
 
+    // Droits Users
+
+    public function canView()
+    {
+        global $user;
+        return !empty($user->rights->variants->read);
+    }
+
+    public function canCreate()
+    {
+        global $user;
+        return !empty($user->rights->variants->write);
+    }
+
+    public function canEdit()
+    {
+        global $user;
+        return !empty($user->rights->variants->write);
+    }
+
+    public function canDelete()
+    {
+        global $user;
+        return !empty($user->rights->variants->delete);
+    }
+
     // Getters params: 
 
     public function getProductListHeaderButtons()
@@ -16,28 +42,25 @@ class Bimp_ProductCombination extends BimpObject
         $id_prod = (int) $this->getData('fk_product_parent');
 
         if ($id_prod) {
-            $url = DOL_URL_ROOT . '/variants/combinations.php?id=' . $id_prod;
-            $buttons[] = array(
-                'label'   => 'Gérer les déclinaisons' . BimpRender::renderIcon('fas_external-link-alt', 'iconRight'),
-                'icon'    => 'fas_cog',
-                'onclick' => 'window.open(\'' . $url . '\')'
-            );
+//            $url = DOL_URL_ROOT . '/variants/combinations.php?id=' . $id_prod;
+//            $buttons[] = array(
+//                'label'   => 'Gérer les déclinaisons' . BimpRender::renderIcon('fas_external-link-alt', 'iconRight'),
+//                'icon'    => 'fas_cog',
+//                'onclick' => 'window.open(\'' . $url . '\')'
+//            );
 
-//            if ($this->can('create')) {
-//                $prod = BimpCache::getBimpObjectInstance('bimpcore', 'Bimp_Product', $id_prod);
-//
-//                if (BimpObject::objectLoaded($prod)) {
-//                    if ($this->isActionAllowed('createCombination') && $this->canSetAction('createCombination')) {
-//                        $buttons[] = array(
-//                            'label'   => 'Ajouter une combinaison',
-//                            'icon'    => 'fas_plus-circle',
-//                            'onclick' => $prod->getJsActionOnclick('createCombination', array(), array(
-//                                'form_name' => 'combination'
-//                            ))
-//                        );
-//                    }
-//                }
-//            }
+            $prod = BimpCache::getBimpObjectInstance('bimpcore', 'Bimp_Product', $id_prod);
+            if (BimpObject::objectLoaded($prod)) {
+                if ($prod->isActionAllowed('createCombination') && $prod->canSetAction('createCombination')) {
+                    $buttons[] = array(
+                        'label'   => 'Ajouter une combinaison',
+                        'icon'    => 'fas_plus-circle',
+                        'onclick' => $prod->getJsActionOnclick('createCombination', array(), array(
+                            'form_name' => 'combination'
+                        ))
+                    );
+                }
+            }
         }
 
         return $buttons;
@@ -47,7 +70,7 @@ class Bimp_ProductCombination extends BimpObject
     public function getCombinations()
     {
         $comb2val = new ProductCombination2ValuePair($this->db->db);
-        return  $comb2val->fetchByFkCombination($this->id);
+        return $comb2val->fetchByFkCombination($this->id);
     }
 
     // Affichages : 

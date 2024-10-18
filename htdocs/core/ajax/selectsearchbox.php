d4ede890e2e46dfa53321cb6a2d8759054c58b20
flo <flo@mbpflormartinez>
2024-10-18 12:29:35 +0200

    debug searchbox
    Former-commit-id: 1abffd5329adfda9670df99d022cc5ea42c8f4d4

--- htdocs/core/ajax/selectsearchbox.php
+++ htdocs/core/ajax/selectsearchbox.php
@@ -203,11 +203,11 @@ $arrayresult = dol_sort_array($arrayresult, 'position' );
 
 
 /*moddrsi pour le trie (20.2) */
-if ($user->login == 'f.martinez') {
-    echo '<pre>';
-    print_r($arrayresult);
-    echo '</pre>';
-}
+//if ($user->login == 'f.martinez') {
+//    echo '<pre>';
+//    print_r($arrayresult);
+//    echo '</pre>';
+//}
 
 if(strlen($search_boxvalue) > 10 && strlen($search_boxvalue) < 16 && stripos($search_boxvalue, " ") === false && isset($arrayresult["searchintosn"]))
 	$arrayresult=array_merge(array($arrayresult["searchintosn"]), $arrayresult);

c93bc6d76d5d228b57f3a0ae20cd67d24ca35b6a
flo <flo@mbpflormartinez>
2024-10-22 11:54:47 +0200

    Debug URL BimpMail
    Former-commit-id: 245314776c2e7cc4de993e7575c5c93bd7a2aea4

--- htdocs/bimpcore/classes/BimpMailCore.php
+++ htdocs/bimpcore/classes/BimpMailCore.php
@@ -61,19 +61,20 @@ class BimpMailCore
 
         $subject = str_replace(array($dolibarr_main_url_root, $_SERVER['SERVER_NAME'] . DOL_URL_ROOT), DOL_URL_ROOT, $subject);
         $subject = str_replace(DOL_URL_ROOT, $dolibarr_main_url_root, $subject);
-        
+
         if ($user->login == 'f.martinez') {
             echo 'MSG Before : ' . $msg . '<br/><br/>';
         }
-        
+
         $msg = str_replace(array($dolibarr_main_url_root, $_SERVER['SERVER_NAME'] . DOL_URL_ROOT), DOL_URL_ROOT, $msg);
-        
+
         if ($user->login == 'f.martinez') {
             echo 'MSG after 1 : ' . $msg . '<br/><br/>';
+            echo 'main url root : ' . $dolibarr_main_url_root . '<br/><br/>';
         }
-        
+
         $msg = str_replace(DOL_URL_ROOT, $dolibarr_main_url_root, $msg);
-        
+
         if ($user->login == 'f.martinez') {
             echo 'MSG after 2 : ' . $msg . '<br/><br/>';
             die('');

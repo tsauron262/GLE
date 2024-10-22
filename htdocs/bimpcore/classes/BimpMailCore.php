aa635b476e6b3e93b15ecf3c2ecad2b4a96ccfa8
flo <flo@mbpflormartinez>
2024-10-22 12:15:39 +0200

    Debug URL BimpMail
    Former-commit-id: edacdc7c4f1d526582cefd4fac2e3bc598407dd6

--- htdocs/bimpcore/classes/BimpMailCore.php
+++ htdocs/bimpcore/classes/BimpMailCore.php
@@ -76,20 +76,20 @@ class BimpMailCore
 
         if ($user->login == 'f.martinez') {
             echo 'MSG after 2 : ' . preg_replace('/^(' . preg_quote(DOL_URL_ROOT) . ')(.*)$/', $dolibarr_main_url_root . '$2', $msg) . '<br/><br/>';
-            
+
             echo 'Pattern : /^(' . preg_quote(DOL_URL_ROOT) . ')(.*)$/<br/><br/>';
-            
-            if (preg_match('/^(' . preg_quote(DOL_URL_ROOT) . ')(.*)$/', $msg, $matches)) {
+
+            if (preg_match('/^(' . preg_quote(DOL_URL_ROOT, '/') . ')(.*)$/', $msg, $matches)) {
                 echo 'MATCHES<pre>';
                 print_r($matches);
                 echo '</pre>';
             } else {
                 echo 'NO MATCHES';
             }
-            
+
             die('');
-        } 
-        
+        }
+
         $msg = str_replace(DOL_URL_ROOT, $dolibarr_main_url_root, $msg);
 
         if (BimpTools::cleanEmailsStr($to) == '') {

0414ffe02380578cfa60ddcbfa6ec682dd1761fa
flo <flo@mbpflormartinez>
2024-11-15 14:32:47 +0100

    debug agenda
    Former-commit-id: f52834e2a9af52f7a9e888bda9a0f2b8c0aea506

--- htdocs/core/lib/functions.lib.php
+++ htdocs/core/lib/functions.lib.php
@@ -3251,6 +3251,10 @@ function dol_strftime($fmt, $ts = false, $is_gmt = false)
  */
 function dol_print_date($time, $format = '', $tzoutput = 'auto', $outputlangs = null, $encodetooutput = false)
 {
+    global $user; 
+    if ($user->login === 'f.martine') {
+        $_SESSION['dol_tz_string'] = 'Europe/Paris';
+    }
 	global $conf, $langs;
 
 	// If date undefined or "", we return ""

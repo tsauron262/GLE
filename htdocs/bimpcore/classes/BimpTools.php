9e0217e615bf464d15cf1c271a065d5397f544db
tommy <tommy@localhost>
2024-11-19 17:47:15 +0100

    security entry
    Former-commit-id: 896567dbafc75c031452842e827a9fdd1e8dd52c

--- htdocs/bimpcore/classes/BimpTools.php
+++ htdocs/bimpcore/classes/BimpTools.php
@@ -100,8 +100,10 @@ class BimpTools
                 $val_temp = self::sanitizeVal($val_temp, $check, $filter, $options, $value);
                 $val_temp = str_replace('ù£ù', '"', $val_temp);
                 
-                $val_temp = str_replace(' <br', '<br', $val_temp);
-                $value = str_replace(' <br', '<br', $value);
+                if(is_string($value)){
+                    $val_temp = str_replace(' <br', '<br', $val_temp);
+                    $value = str_replace(' <br', '<br', $value);
+                }
 
                 if (!in_array($key, array(// temporaire : pour éviter logs inutiles
                             'diagnostic', 'notecreditsafe', 'accessoires', 'search_value', 'ref_client'

b78920870afce951cb5642f66a240cc9b7760bd8
tommy <tommy@localhost>
2024-12-03 15:48:49 +0100

    bimpressources
    Former-commit-id: df9a77634cd36822e15eb5f426a3dbbce10ecab9

--- htdocs/bimpcore/public/test_server.php
+++ htdocs/bimpcore/public/test_server.php
@@ -26,6 +26,9 @@ try{
         require_once("../../main.inc.php");
         $cacheServeur = (isset($_REQUEST['cache_serveur'])? $_REQUEST['cache_serveur'] : 0);
         $result = BimpCache::getCommercialBimpComm('propal', $cacheServeur);
+        $result = BimpCache::getCommercialBimpComm('propal', $cacheServeur);
+        $result = BimpCache::getCommercialBimpComm('propal', $cacheServeur);
+        $result = BimpCache::getCommercialBimpComm('propal', $cacheServeur);
         echo 'ok '.count($result).'<br/>';
         echo 'cache_serveur = '.$cacheServeur.'<br/>';
     }

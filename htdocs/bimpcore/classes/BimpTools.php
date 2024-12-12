2c82cac49c15cee8ed5984f89af48868f32657fd
tommy <tommy@localhost>
2024-12-12 15:45:33 +0100

    Correct pa facture SAV
    Former-commit-id: 253e276d77b758c38e788220b1ef97896d8348d7

--- htdocs/bimpcore/classes/BimpTools.php
+++ htdocs/bimpcore/classes/BimpTools.php
@@ -120,7 +120,8 @@ class BimpTools
                                 'Valeur initiale' => (is_array($valueInitForTest) ? 'ARRAY' : '"' . (string) htmlentities($valueInitForTest) . '" (' . gettype($valueInitForTest) . ')'),
                             ));
 
-                            BimpController::addAjaxWarnings('Attention, donnée invalide (' . $key . ')');
+                            if ((int) !BimpCore::getConf('post_data_check_log_only'))
+                                BimpController::addAjaxWarnings('Attention, donnée invalide (' . $key . ')');
                         } else {
                             BimpCore::addlog('Donnée modifiée (' . $key . ')', 2, 'secu', null, array(
                                 'check'                 => $check,
@@ -133,7 +134,8 @@ class BimpTools
                                 'Valeur modifiée ascii' => '"' . BimpTools::toAscii($val_temp) . '"'
                             ));
 
-                            BimpController::addAjaxWarnings('Attention, donnée modifiée (' . $key . '). Nouvelle valeur : '.((strlen($val_temp) > 40)? substr($val_temp,40).'...' : $val_temp));
+                            if ((int) !BimpCore::getConf('post_data_check_log_only'))
+                                BimpController::addAjaxWarnings('Attention, donnée modifiée (' . $key . '). Nouvelle valeur : '.((strlen($val_temp) > 40)? substr($val_temp,40).'...' : $val_temp));
                         }
                         if ((int) !BimpCore::getConf('post_data_check_log_only') || $key == 'fc') {
                             $value = $val_temp;

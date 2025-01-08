cb1c2f2f5cf057fe77e5dac190f1a894ff8e42d9
flo <flo@mbpflormartinez>
2025-01-08 21:14:44 +0100

    Aj paiement chèque cadeau
    Former-commit-id: 4dab238953502c9e09d54dbfd517b072ec2df4fc

---
+++ htdocs/bimpcaisse/extends/entities/abs/objects/BC_VentePaiement.class.php
@@ -0,0 +1,10 @@
+<?php
+
+// Entité : abs
+
+BC_VentePaiement::$codes['CHC'] = array('icon' => 'fas_gift', 'label' => 'Chèque cadeau');
+
+class BC_VentePaiement_ExtEntity extends BC_VentePaiement
+{
+    
+}
--- htdocs/bimpcaisse/objects/BC_VentePaiement.class.php
+++ htdocs/bimpcaisse/objects/BC_VentePaiement.class.php
@@ -4,10 +4,10 @@ class BC_VentePaiement extends BimpObject
 {
 
     public static $codes = array(
-        'LIQ' => array('label' => 'Paiement Liquide', 'icon' => 'money'),
-        'CB'  => array('label' => 'Paiement CB', 'icon' => 'credit-card'),
+        'LIQ' => array('label' => 'Paiement Liquide', 'icon' => 'fas_money-bill-wave'),
+        'CB'  => array('label' => 'Paiement CB', 'icon' => 'fas_credit-card'),
         'CHQ' => array('label' => 'Paiement Chèque', 'icon' => 'fas_pencil-alt'),
-        'AE'  => array('label' => 'Paiement American Express', 'icon' => 'cc-amex'),
+        'AE'  => array('label' => 'Paiement American Express', 'icon' => 'fab_cc-amex'),
         'CG'  => array('label' => 'Chèque galerie', 'icon' => 'fas_money-check'),
         'FIN'  => array('label' => 'Financement', 'icon' => 'fas_hand-holding-usd'),
         'FIN_YC'  => array('label' => 'Financement Younited', 'icon' => 'fas_hand-holding-usd')

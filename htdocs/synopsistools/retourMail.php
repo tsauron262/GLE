be984c79fb54e2df87c15978f3aa9170a44afd45
tommy <tommy@localhost>
2025-01-13 15:52:02 +0100

    debug retour mail
    Former-commit-id: 557cbfaebc8b4bf3a05def82b9ad75a448b2c64a

--- htdocs/synopsistools/retourMail.php
+++ htdocs/synopsistools/retourMail.php
@@ -2,13 +2,14 @@
 
 
 $json = json_decode(file_get_contents('php://input'), true);
-
+//$json = array('message-id' => 'hhhh');
 define("NOLOGIN", 1);
 require("../main.inc.php");
 require_once DOL_DOCUMENT_ROOT . '/bimpcore/Bimp_Lib.php';
 
 
 
+BimpCore::addlog('Weeb hook appel ');
 BimpCore::addlog('Weeb hook mail '.'ici '.print_r($json,1));
 
 if($json['email'] == 'tommy@bimp.fr')
@@ -35,4 +36,4 @@ if($msgId != 0 && $msgId != ''){
 }
 
 
-
+echo 'ok';

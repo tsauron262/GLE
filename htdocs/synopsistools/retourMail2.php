093a06be702752bfcbf2d44513a9431c7d46c11f
tommy <tommy@localhost>
2025-01-13 15:43:50 +0100

    debug retour mail
    Former-commit-id: 9ea9362f81ef084f542430587f926cf749455493

---
+++ htdocs/synopsistools/retourMail2.php
@@ -0,0 +1,38 @@
+<?php
+
+
+//$json = json_decode(file_get_contents('php://input'), true);
+//$json = array('message-id' => 'hhhh');
+define("NOLOGIN", 1);
+require("../main.inc.php");
+require_once DOL_DOCUMENT_ROOT . '/bimpcore/Bimp_Lib.php';
+
+
+
+BimpCore::addlog('Weeb hook mail '.'ici '.print_r($json,1));
+
+if($json['email'] == 'tommy@bimp.fr')
+    mailSyn2('test', 't.sauron@bimp.fr', null, 'ici '.print_r($json,1));
+
+
+$msgId = str_replace(array('<', '>'), '', $json['message-id']);
+
+if($msgId != 0 && $msgId != ''){
+    $result = BimpCache::findBimpObjectInstance('bimpcore', 'BimpMailLog', array('msg_id' => $msgId), true);
+    if(!$result){
+        $result = BimpCache::findBimpObjectInstance('bimpcore', 'BimpMailLog', array('mail_to' => array(
+                            'operator' => 'like',
+                            'value'    => '%'.$json['email'].'%'
+                        ), 'mail_subject' => addslashes($json['subject']), 
+                           'msg_id' => ''
+            ), true);
+    }
+    if($result && is_object($result)){
+        if($result->getData('msg_id') == '')
+            $result->updateField('msg_id', $msgId);
+        $result->changeStatut($json['event'], $json['email'], $json['date']);
+    }
+}
+
+
+echo 'ok';

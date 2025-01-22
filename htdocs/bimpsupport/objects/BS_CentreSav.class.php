2ae4f6ab909962907e2d014b851d43066da1038e
f.lauby <f.lauby@ldlc.com>
2025-01-22 09:36:29 +0100

    Ajout centre SAV
    Former-commit-id: 863a546ba4c10c6cc9a5a2735e79ce1b8ce0e8f3

--- htdocs/bimpsupport/objects/BS_CentreSav.class.php
+++ htdocs/bimpsupport/objects/BS_CentreSav.class.php
@@ -1,5 +1,14 @@
 <?php
 
 class BS_CentreSav extends BimpObject	{
-
+	public function canCreate()
+	{
+		global $user;
+		if ($user->admin) return 1;
+		else return 0;
+	}
+	public function canDelete()
+	{
+		return $this->canCreate();
+	}
 }

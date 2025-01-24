e615f3b52367a61965df2af2ffffbe4d294bd8d8
f.lauby <f.lauby@ldlc.com>
2025-01-24 14:54:05 +0100

    Centre SAV : Ajout de l'onglet SAV du centre dans la fiche d'un centre
    Former-commit-id: 6b5b9a5746018c5294addc8387715205666e814c

--- htdocs/bimpsupport/objects/BS_CentreSav.class.php
+++ htdocs/bimpsupport/objects/BS_CentreSav.class.php
@@ -26,4 +26,35 @@ class BS_CentreSav extends BimpObject	{
 	public static function getCentreSav($code)	{
 		return BimpCache::findBimpObjectInstance('bimpsupport', 'BS_CentreSav', array('code' => $code));
 	}
+
+	public function renderLinkedListes($list_type='sav_centre')	{
+		global $db, $conf, $langs;
+
+		$html = '';
+
+		$errors = array();
+		if (!$this->isLoaded($errors)) {
+			return BimpRender::renderAlerts($errors);
+		}
+
+		$list = null;
+
+		switch ($list_type) {
+			case 'sav_centre':
+				$list = new BC_ListTable(BimpObject::getInstance('bimpsupport', 'BS_SAV'));
+				$list->addJoin('bs_centre_sav', 'a.code_centre = cs.code', 'cs');
+				$list->addFieldFilterValue('cs.id', BimpTools::getValue('id'));
+				break;
+		}
+
+		if (is_a($list, 'BC_ListTable')) {
+			$html .= $list->renderHtml();
+		} elseif ($list_type) {
+			$html .= BimpRender::renderAlerts('La liste de type "' . $list_type . '" n\'existe pas');
+		} else {
+			$html .= BimpRender::renderAlerts('Type de liste non spécifié');
+		}
+
+        return $html;
+	}
 }

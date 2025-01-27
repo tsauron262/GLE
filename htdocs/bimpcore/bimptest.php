bc769b3b32fc7e19f13fa5f06f75330448be6013
flo <f.martinez@bimp.fr>
2025-01-27 10:46:34 +0100

    Maj Envoi SMS SAV
    Former-commit-id: 38f85f830b454bc904364dbf8cc08fbb3c2a0afa

--- htdocs/bimpcore/bimptest.php
+++ htdocs/bimpcore/bimptest.php
@@ -18,16 +18,32 @@ global $db, $user;
 $bdb = new BimpDb($db);
 
 if (!BimpObject::objectLoaded($user)) {
-    echo BimpRender::renderAlerts('Aucun utilisateur connecté');
-    exit;
+	echo BimpRender::renderAlerts('Aucun utilisateur connecté');
+	exit;
 }
 
 if (!$user->admin) {
-    echo BimpRender::renderAlerts('Seuls les admin peuvent exécuter ce script');
-    exit;
+	echo BimpRender::renderAlerts('Seuls les admin peuvent exécuter ce script');
+	exit;
 }
 
-echo '';
+$client = BimpCache::getBimpObjectInstance('bimpcore', 'Bimp_Client', 142370);
+foreach (array(
+	'test-n0sf2zo7p@srv1.mail-tester.com',
+	'grunchy99@gmail.com'
+		 ) as $to) {
+	echo '<br/>TEST ' . $to . ' : ';
+	$mail = new BimpMail($client, 'TEST', '', '', 'TEST');
+
+	$errors = array();
+	$mail->send($errors);
+
+	if (count($errors)) {
+		echo '<pre>' . print_r($errors) . '<pre>';
+	} else {
+		echo 'OK';
+	}
+}
 
 echo '<br/>FIN';
 echo '</body></html>';

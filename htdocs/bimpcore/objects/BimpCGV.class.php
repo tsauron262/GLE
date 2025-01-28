a93d016a984883387d012cc5af0d60c82187ba0f
f.lauby <f.lauby@ldlc.com>
2025-01-28 17:20:28 +0100

    formulaire d'upload d'un fichier CGV
    Former-commit-id: 66bb32468c034d47046b74d61bbbd8fa6276a260

--- htdocs/bimpcore/objects/BimpCGV.class.php
+++ htdocs/bimpcore/objects/BimpCGV.class.php
@@ -2,6 +2,8 @@
 
 class BimpCGV extends BimpObject
 {
+	// Droits users:
+
 	public function canCreate()
 	{
 		global $user;
@@ -12,6 +14,44 @@ class BimpCGV extends BimpObject
 	public function canDelete()
 	{
 		return $this->canCreate();
+	}
+
+	public function canSetAction($action)
+	{
+		global $user;
+		switch ($action) {
+			case 'modifyPdfForm':
+				return $this->canCreate();
+		}
+
+		return parent::canSetAction($action);
+	}
+
+	// Getters booléen:
+	public function isActionAllowed($action, &$errors = [])
+	{
+		switch ($action) {
+			case 'modifyPdfForm':
+				return 1;
+		}
+		return parent::isActionAllowed($action, $errors);
+	}
+
+	// Getters params:
+	public function getListExtraButtons()
+	{
+		$buttons = array();
+		if ($this->isActionAllowed('modifyPdfForm') && $this->canSetAction('modifyPdfForm')) {
+			$buttons[] = array(
+				'label'   => 'Modifier le PDF',
+				'icon'    => 'far_file-pdf',
+				'onclick' => $this->getJsActionOnclick('modifyPdfForm', array(), array(
+					'form_name' => 'modifyPdfForm',
+				))
+			);
+		}
+
+		return $buttons;
 	}
 
 	static function getCGV($type, $secteur, $date_piece, $id_centre)
@@ -61,5 +101,31 @@ class BimpCGV extends BimpObject
 			}
 		}
 		return $html;
+	}
+
+	// Actions:
+	public function actionModifyPdfForm($data, &$success)
+	{
+		$errors = array();
+		$warnings = array();
+		$success = '';
+
+		$file = BimpTools::getPostFieldValue('file', array(), 'array');
+
+		if (!empty($file)) {
+			$files_dir = $this->getFilesDir();
+			if(file_exists($files_dir . 'CGV_file.pdf'))	{
+				unlink($files_dir . 'CGV_file.pdf');
+			}
+			BimpTools::moveTmpFiles($warnings, $file, $files_dir, 'CGV_file.pdf');
+			$success = 'Fichier PDF modifié';
+		} else {
+			$errors[] = 'Aucun fichier PDF reçu';
+		}
+
+		return array(
+			'errors'   => $errors,
+			'warnings' => $warnings
+		);
 	}
 }

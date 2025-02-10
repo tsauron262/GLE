0497caaac997a86852879272e18e6d5e4a968b46
flo <f.martinez@bimp.fr>
2025-02-10 13:20:53 +0100

    tests rate limiting
    Former-commit-id: d4ebae9a3749fb3b6c9b0a1f7f0fb48bcb96e3c1

--- htdocs/bimpcore/classes/BimpCore.php
+++ htdocs/bimpcore/classes/BimpCore.php
@@ -1632,6 +1632,10 @@ class BimpCore
 			return 1;
 		}
 
+		if ($user->login == 'f.martinez') {
+			$limit = 3;
+		}
+
 		$period = (int) BimpCore::getConf('rate_limiting_' . $type . '_period', 60);
 		$reset_delay = (int) BimpCore::getConf('rate_limiting_' . $type . '_reset_delay', 30);
 

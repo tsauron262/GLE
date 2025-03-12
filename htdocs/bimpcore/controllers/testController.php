511124415dcb92db919b47fb2c7e03849f3f3a7e
flo <f.martinez@bimp.fr>
2025-03-12 16:50:26 +0100

    Test notifs actioncomm
    Former-commit-id: a930912f2964f81b982d78fa3221b6a29d90fe6a

--- htdocs/bimpcore/controllers/testController.php
+++ htdocs/bimpcore/controllers/testController.php
@@ -3,20 +3,20 @@
 class testController extends BimpController
 {
 
-    public function renderHtml()
-    {
-        if (!BimpCore::isUserDev()) {
-            return BimpRender::renderAlerts('Page réservée aux développeurs');
-        }
+	public function renderHtml()
+	{
+		if (!BimpCore::isUserDev()) {
+			return BimpRender::renderAlerts('Page réservée aux développeurs');
+		}
 
-		$id_user = 270;
+		$id_user = BimpTools::getValue('id_user', 270);
 
 		$html = '';
 		BimpObject::loadClass('bimpcore', 'Bimp_ActionComm');
 		$data = Bimp_ActionComm::getActionCommEventsForUser($id_user, '');
 
-		$html .= '<pre>' . print_r($data, 1) . '</pre>';
+		$html .= 'User ' . $id_user . '<pre>' . print_r($data, 1) . '</pre>';
 
 		return $html;
-    }
+	}
 }

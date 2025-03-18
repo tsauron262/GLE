2a0d9b13f05fd8324c801f1c4aba0cb7cf866f98
f.lauby <f.lauby@ldlc.com>
2025-03-18 12:30:38 +0100

    modification de la mise a jour de la date_der_contact selon c_actioncomm
    Former-commit-id: 0a737d4dcc51e08ecb9cb7b28e650cd0024b2450

--- htdocs/bimpcore/extends/entities/rdc/objects/Bimp_ActionComm.class.php
+++ htdocs/bimpcore/extends/entities/rdc/objects/Bimp_ActionComm.class.php
@@ -4,18 +4,16 @@ class Bimp_ActionComm_ExtEntity extends Bimp_ActionComm {
 	public function onSave(&$errors = [], &$warnings = [])
 	{
 		$id = BimpTools::getPostFieldValue('id');
-		if ($id && $this->isLoaded()) {
-			$actions_date_der_contact = explode(',', BimpCore::getConf('actions_date_der_contact'));
-			$fk_action = BimpTools::getPostFieldValue('fk_action');
-			if (in_array($fk_action, $actions_date_der_contact)) {
-				$now = new DateTime();
-				$dateActionComm = new DateTime(BimpTools::getPostFieldValue('datep'));
-				$difference = $now->diff($dateActionComm);
+		$fk_action = $this->getData('fk_action');
+		$maj = $this->db->getValue('c_actioncomm', 'maj_dercontact_rdc', 'id = ' . $fk_action);
+		if ($id && $maj && $this->isLoaded()) {
+			$now = new DateTime();
+			$dateActionComm = new DateTime(BimpTools::getPostFieldValue('datep'));
+			$difference = $now->diff($dateActionComm);
 
-				if ($difference->invert == 1) { // if the date is in the past => update date_der_contact
-					$sql = "UPDATE " . MAIN_DB_PREFIX . "societe_rdc SET date_der_contact = '" . $dateActionComm->format('Y-m-d') . "' WHERE fk_soc = " . $id;
-					$this->db->execute($sql);
-				}
+			if ($difference->invert == 1) { // if the date is in the past => update date_der_contact
+				$sql = "UPDATE " . MAIN_DB_PREFIX . "societe_rdc SET date_der_contact = '" . $dateActionComm->format('Y-m-d') . "' WHERE fk_soc = " . $id;
+				$this->db->execute($sql);
 			}
 		}
 

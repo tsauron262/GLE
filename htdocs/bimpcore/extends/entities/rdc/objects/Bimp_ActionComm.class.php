8268151d0335d7dfa24fab3e237666f06dd40f77
f.lauby <f.lauby@ldlc.com>
2025-03-19 16:00:07 +0100

    suppression table llx_societe_rdc + bouton action ouvrant formulaire
    Former-commit-id: f58863bdacb6f4e8b05835e18d9323fa9b1becb0

--- htdocs/bimpcore/extends/entities/rdc/objects/Bimp_ActionComm.class.php
+++ htdocs/bimpcore/extends/entities/rdc/objects/Bimp_ActionComm.class.php
@@ -12,7 +12,7 @@ class Bimp_ActionComm_ExtEntity extends Bimp_ActionComm {
 			$difference = $now->diff($dateActionComm);
 
 			if ($difference->invert == 1) { // if the date is in the past => update date_der_contact
-				$sql = "UPDATE " . MAIN_DB_PREFIX . "societe_rdc SET date_der_contact = '" . $dateActionComm->format('Y-m-d') . "' WHERE fk_soc = " . $id;
+				$sql = "UPDATE " . MAIN_DB_PREFIX . "societe SET date_der_contact = '" . $dateActionComm->format('Y-m-d') . "' WHERE rowid = " . $id;
 				$this->db->execute($sql);
 			}
 		}

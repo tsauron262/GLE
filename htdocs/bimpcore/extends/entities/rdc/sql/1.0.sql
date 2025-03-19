487af5df98c8c95321ab25243e61179b275c7765
f.lauby <f.lauby@ldlc.com>
2025-03-19 16:00:07 +0100

    suppression table llx_societe_rdc + bouton action ouvrant formulaire
    Former-commit-id: ba4f72b7cfcd25701c89feaefbeeef82cd45aff0

--- htdocs/bimpcore/extends/entities/rdc/sql/1.0.sql
+++ htdocs/bimpcore/extends/entities/rdc/sql/1.0.sql
@@ -1,25 +1,4 @@
 CREATE TABLE IF NOT EXISTS
-llx_societe_rdc (
-	id int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	fk_soc int(11) NOT NULL,
-	date_der_contact date NULL,
-	date_debut_prospect date NULL,
-	date_ouverture date NULL,
-	fk_priorite int(11) NOT NULL DEFAULT '0',
-	fk_statut_rdc int(11) NOT NULL DEFAULT '0',
-	date_changement_statut_rdc date NULL,
-	delai_ouv int(11) NOT NULL DEFAULT '0',
-	commentaire text NULL,
-	fk_source int(11) NOT NULL DEFAULT '0',
-	fk_user_attr int(11) NOT NULL DEFAULT '0', -- Attribution
-	fk_group int(11) NOT NULL DEFAULT '0', -- Team RDC
-	shopid int(11) NOT NULL DEFAULT '0',
-	fk_master_cat int(11) NOT NULL DEFAULT '0', -- catégorie maitre
-	contrefacon tinyint(1) NOT NULL DEFAULT '0'
-);
-
-
-CREATE TABLE IF NOT EXISTS
 llx_c_societe_rdc_priorite (
 	rowid int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	libelle varchar(255) NOT NULL,

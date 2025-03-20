8e9648b3045196e82e2ad312e9c37fe7175ca4be
f.lauby <f.lauby@ldlc.com>
2025-03-20 11:59:38 +0100

    Prepa concurence et CA
    Former-commit-id: e161285db0b06b759ea16c8ec65ab20453adfe4d

--- htdocs/bimpcore/extends/entities/rdc/sql/1.3.sql
+++ htdocs/bimpcore/extends/entities/rdc/sql/1.3.sql
@@ -3,27 +3,22 @@ CREATE TABLE IF NOT EXISTS `llx_concurrence_rdc` (
   `fk_soc` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `site` varchar(100) NOT NULL DEFAULT '',
-  `user_update` int(11) NOT NULL,
-  `date_update` datetime NOT NULL ,
-  `user_create` int(11) NOT NULL,
-  `date_create` datetime NOT NULL
+  `user_update` int(11) NOT NULL DEFAULT '0',
+  `date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_create` int(11) NOT NULL DEFAULT '0',
+  `date_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `llx_ca_rdc` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `fk_soc` int(11) NOT NULL,
-  `fk_category` int(11) NOT NULL,
-  `ca_ytd` decimal(24,8) NOT NULL,
-  `ca_ytd_n1` decimal(24,8) NOT NULL,
-  `evol_ytd_n1` decimal(24,8) NOT NULL,
-  `ca_n1` decimal(24,8) NOT NULL,
-  `ca_s1` decimal(24,8) NOT NULL,
-  `ca_m1` decimal(24,8) NOT NULL,
-  `ca_m2` decimal(24,8) NOT NULL,
-  `evol_m1_m2` decimal(24,8) NOT NULL,
-  `ca_total` decimal(24,8) NOT NULL,
-  `user_update` int(11) NOT NULL,
-  `date_update` datetime NOT NULL ,
-  `user_create` int(11) NOT NULL,
-  `date_create` datetime NOT NULL
+  `id_obj` int(11) NOT NULL DEFAULT 0,
+  `type_obj` int(11) NOT NULL DEFAULT 0,
+  `ca` decimal(24,8) NOT NULL DEFAULT 0,
+  `fk_category` int(11) NOT NULL DEFAULT 0,
+  `fk_period` int(11) NOT NULL DEFAULT 0,
+  `debut_period` date DEFAULT NULL,
+  `user_update` int(11) NOT NULL DEFAULT '0',
+  `date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_create` int(11) NOT NULL DEFAULT '0',
+  `date_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );

8ad7da4daa43ff9f6f183b6304a898a3ceecfa1a
flo <f.martinez@bimp.fr>
2025-03-24 12:12:10 +0100

    Merge Branche Franck
    Former-commit-id: fac74f0c1eb37141e7e4fb3eda53fad64ef46c79

--- htdocs/bimpsupport/objects/BS_CentreSav.class.php
+++ htdocs/bimpsupport/objects/BS_CentreSav.class.php
@@ -1,33 +1,47 @@
 <?php
 
-class BS_CentreSav extends BimpObject	{
+class BS_CentreSav extends BimpObject
+{
 	public function canCreate()
 	{
 		global $user;
-		if ($user->admin) return 1;
-		else return 0;
+		if ($user->admin) {
+			return 1;
+		} else {
+			return 0;
+		}
 	}
+
+	public function canEdit()
+	{
+		return $this->canCreate();
+	}
+
 	public function canDelete()
 	{
 		return $this->canCreate();
 	}
+
 	public function validate()
 	{
 		$errors = parent::validate();
 		$code = $this->getData('code');
-		if($code)	{
-			$id_centre = $this->db->getValue($this->getTable(), "id", 'code = "'.$code.'"'. ($this->isLoaded() ? ' AND id != ' . $this->id : ''));
-			if($id_centre)	{
-				$errors[] = "Centre '".$code."' existe déjà.";
+		if ($code) {
+			$id_centre = $this->db->getValue($this->getTable(), "id", 'code = "' . $code . '"' . ($this->isLoaded() ? ' AND id != ' . $this->id : ''));
+			if ($id_centre) {
+				$errors[] = "Centre '" . $code . "' existe déjà.";
 			}
 		}
 		return $errors;
 	}
-	public static function getCentreSav($code)	{
+
+	public static function getCentreSav($code)
+	{
 		return BimpCache::findBimpObjectInstance('bimpsupport', 'BS_CentreSav', array('code' => $code));
 	}
 
-	public function renderLinkedListes($list_type='sav_centre')	{
+	public function renderLinkedListes($list_type = 'sav_centre')
+	{
 		global $db, $conf, $langs;
 
 		$html = '';
@@ -57,6 +71,6 @@ class BS_CentreSav extends BimpObject	{
 			$html .= BimpRender::renderAlerts('Type de liste non spécifié');
 		}
 
-        return $html;
+		return $html;
 	}
 }

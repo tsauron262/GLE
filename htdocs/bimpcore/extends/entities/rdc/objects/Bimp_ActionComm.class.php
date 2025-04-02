ccf41fd85b21228150dd0a85376f4ba5fbdb37e5
f.lauby <f.lauby@ldlc.com>
2025-04-02 17:08:42 +0200

    Modif actioncomm pour la notion de CR echange RDC
    Former-commit-id: 04574e2185d043f460179625e14f1b243632cc82

--- htdocs/bimpcore/extends/entities/rdc/objects/Bimp_ActionComm.class.php
+++ htdocs/bimpcore/extends/entities/rdc/objects/Bimp_ActionComm.class.php
@@ -1,6 +1,45 @@
 <?php
 
 class Bimp_ActionComm_ExtEntity extends Bimp_ActionComm {
+	public static $motifEchange = array(
+		'0' => array('label' => 'N/A', 'classes' => array('danger')),
+		'1' => array('label' => 'Résolution de problème'),
+		'2' => array('label' => 'Offre commerciale'),
+		'3' => array('label' => 'Suivi'),
+		'4' => array('label' => 'Qualité'),
+		'5' => array('label' => 'Contrefaçon'),
+		'6' => array('label' => 'Réclamation')
+	);
+
+	public static $c_actioncomm_AfficheEchange = array('1', '5'); // 'AC_EMAIL'
+
+	public function getIdsAfficheEchange()
+	{
+		return self::$c_actioncomm_AfficheEchange;
+	}
+
+	public function getAfficheEchange()
+	{
+		return (int)in_array($this->getData('fk_action'), self::$c_actioncomm_AfficheEchange);
+	}
+
+	public function getTypesArray($include_empty = false)
+	{
+		$cache_key = 'action_comm_types_values_array';
+
+		if (!isset(self::$cache[$cache_key])) {
+			$rows = $this->db->getRows('c_actioncomm', 'active = 1', null, 'array', array('id', 'icon', 'libelle'), 'position', 'asc');
+
+			if (is_array($rows)) {
+				foreach ($rows as $r) {
+					self::$cache[$cache_key][(int) $r['id']] = array('label' => $r['libelle'], 'icon' => $r['icon']);
+				}
+			}
+		}
+
+		return self::getCacheArray($cache_key, $include_empty);
+	}
+
 	public function onSave(&$errors = [], &$warnings = [])
 	{
 		$id = BimpTools::getPostFieldValue('id');

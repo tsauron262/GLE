b2ea12495237a39992b084397db67f3069a02ed8
tommy <tommy@localhost>
2025-04-17 16:37:45 +0200

    collect de mail
    Former-commit-id: a85de9d455c1461557e78428eec3dc24a822ad12

--- htdocs/bimpticket/class/actions_bimpticket.class.php
+++ htdocs/bimpticket/class/actions_bimpticket.class.php
@@ -78,6 +78,10 @@ class ActionsBimpticket {
 	{
 		global $db;
 
+		$msg = $parameters['imapemail']->bodies['html'];
+		if($msg == '')
+			$msg = $parameters['imapemail']->bodies['text'];
+
 		$traite = 0;
 
 		$matches = array();
@@ -90,7 +94,6 @@ class ActionsBimpticket {
 				/*
 				 * on purifie le message
 				 */
-				$msg = $parameters['imapemail']->bodies['html'];
 				$tabT = explode('lineBreakAtBeginningOfMessage', $msg);
 				if (isset($tabT[1])) {
 					$msg = $tabT[0] . '">';
@@ -124,6 +127,9 @@ class ActionsBimpticket {
 			if (!$traite && isset($parameters['objectemail']) && is_a($parameters['objectemail'], 'ticket')) {
 				$ticket = $parameters['objectemail'];
 				$Bimp_Ticket = BimpCache::getBimpObjectInstance('bimpticket', 'Bimp_Ticket', $ticket->id);
+
+				$Bimp_Ticket->addObjectLog($Bimp_Ticket->getData('message'));
+				$Bimp_Ticket->updateField("message", $msg);
 
 				$contact_static = new Contact($db);
 				$contact_static->fetch(0, null, '', $Bimp_Ticket->getData('origin_email'));

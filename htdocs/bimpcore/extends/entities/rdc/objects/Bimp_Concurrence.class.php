7536c0610606d87f471d252069c64ca827047bb6
tommy <tommy@localhost>
2025-05-06 11:15:55 +0200

    presentation concurence ca
    Former-commit-id: cf384944e870703bf629f4c92b3e448764ca52fa

--- htdocs/bimpcore/extends/entities/rdc/objects/Bimp_Concurrence.class.php
+++ htdocs/bimpcore/extends/entities/rdc/objects/Bimp_Concurrence.class.php
@@ -9,13 +9,19 @@ class Bimp_Concurrence_ExtEntity extends Bimp_Concurrence	{
 	{
 		$buttons = array();
 
-		$ca = BimpObject::getBimpObjectInstance($this->module, 'Bimp_ChiffreAffaire');
+//		$ca = BimpObject::getBimpObjectInstance($this->module, 'Bimp_ChiffreAffaire');
 		if ($this->isActionAllowed('setProcessed') && $this->canSetAction('processed')) {
 			$buttons[] = array(
 				'label'   => 'Liste des Ca',
 				'icon'    => 'fas_euro-sign',
 				'onclick' => $this->getJsLoadModalView('ca')
-//				'onclick' => $ca->getJsLoadModalList('default', array('extra_filters'=>array('type_obj'=>1, 'id_obj'=>$this->id), 'title'=>'Chiffre d\\\'affaire de la concurrence "'.$this->getData('nom').'"')),
+	//				'onclick' => $ca->getJsLoadModalList('default', array('extra_filters'=>array('type_obj'=>1, 'id_obj'=>$this->id), 'title'=>'Chiffre d\\\'affaire de la concurrence "'.$this->getData('nom').'"')),
+			);
+			$buttons[] = array(
+				'label'   => 'Graph des Ca',
+				'icon'    => 'fa_signal',
+				'onclick' => $this->getJsLoadModalView('ca_graph')
+	//				'onclick' => $ca->getJsLoadModalList('default', array('extra_filters'=>array('type_obj'=>1, 'id_obj'=>$this->id), 'title'=>'Chiffre d\\\'affaire de la concurrence "'.$this->getData('nom').'"')),
 			);
 		}
 		return $buttons;

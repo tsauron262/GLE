c00336343e3d80b43e135d38a788125b79ce6785
f.lauby <f.lauby@ldlc.com>
2025-05-14 13:14:19 +0200

    import et affichage site marchand multiple
    Former-commit-id: 9e2ec1636c6b4f467b00acc11ff9c735c34923a0

--- htdocs/bimpcore/extends/entities/rdc/objects/Bimp_Client.class.php
+++ htdocs/bimpcore/extends/entities/rdc/objects/Bimp_Client.class.php
@@ -207,10 +207,13 @@ class Bimp_Client_ExtEntity extends Bimp_Client
 	public function getUrlMarchand()	{
 		$html = ' ';
 		if ($this->getData('url')) {
-			$sites = explode('<br>', $this->getData('url'));
-			foreach ($sites as $site) {
-				if($html != '') $html .= '<br>';
-				$html .= $this->getHref($site);
+			$urls = explode("%0D%0A", urlencode($this->getData('url')));
+			foreach ($urls as $url) {
+				$lien = $this->getHref(urldecode($url));
+				if ($html != ' ') {
+					$html .= '<br>';
+				}
+				$html .= $lien;
 			}
 		}
 		return $html;
@@ -522,7 +525,11 @@ class Bimp_Client_ExtEntity extends Bimp_Client
 		$this->checkPassageLive();
 		$this->AlerteQualite();
 		$this->alertePassage_XX($this->getData('fk_statut_rdc'));
-
+//		if ($this->getData('url') && $this->getData('url') != $this->getInitData('url')) {
+//			$url = nl2br($this->getData('url'));
+//			echo '<pre>';
+//			exit(var_dump(substr($url, 70, 10),strpos($url, html_entity_decode('&para;')), html_entity_decode('&para;')));
+//		}
 		return parent::update($warnings, $force_update);
 	}
 

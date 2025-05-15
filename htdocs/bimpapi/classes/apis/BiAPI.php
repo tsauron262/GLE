9a894ff12e3c9ac0b72aa60a68e5a5937b36b928
tommy <tommy@localhost>
2025-05-15 17:46:03 +0200

    import rdc
    Former-commit-id: a35ed3c6530303ed8a58421a3a3f322a3321a00b

--- htdocs/bimpapi/classes/apis/BiAPI.php
+++ htdocs/bimpapi/classes/apis/BiAPI.php
@@ -142,7 +142,7 @@ class BiAPI extends BimpAPI
 		$mois = array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
 		$annee = array("2020", "2021", "2022", "2023");
 		$annee = array("2024","2025");
-		$categorie = 0;
+		$categorie = 1;
 		$this->majCa($annee, $mois, $categorie, $warnings, $errors);
 	}
 

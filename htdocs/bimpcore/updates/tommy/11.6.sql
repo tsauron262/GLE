ee76a198872d9b873cc951de805907ba1f34862d
tommy <tommy@localhost>
2025-05-23 11:02:08 +0200

    maj tarifs apple
    Former-commit-id: 82a2379b47345e9c95d64bd184bb8b60be45c5d4

---
+++ htdocs/bimpcore/updates/tommy/11.6.sql
@@ -0,0 +1,11 @@
+CREATE TABLE llx_usergroup_revue (
+	  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	  `id_group` int(10) UNSIGNED NOT NULL DEFAULT '0',
+	  `fk_user` INT NOT NULL DEFAULT '0',
+	  `data_revue` TEXT NULL DEFAULT NULL,
+	  entity INT default 1
+) ENGINE=InnoDB;
+
+
+
+INSERT into llx_usergroup_revue (SELECT null, u.rowid,fk_user, data_revue,1 FROM llx_usergroup u, llx_usergroup_extrafields ue WHERE ue.fk_object = u.rowid);

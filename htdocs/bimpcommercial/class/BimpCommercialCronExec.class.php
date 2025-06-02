5ecebdc8564add4c66070ea4727137f024c59e5b
tommy <tommy@localhost>
2025-06-02 11:35:27 +0200

    tva monaco
    Former-commit-id: 2dd49695b4093df6c37bf1f8233eeef87aa9f8ce

--- htdocs/bimpcommercial/class/BimpCommercialCronExec.class.php
+++ htdocs/bimpcommercial/class/BimpCommercialCronExec.class.php
@@ -8,39 +8,44 @@ class BimpCommercialCronExec extends BimpCron
     public function sendRappelsCommandes()
     {
         $this->current_cron_name = 'Envoi des rappels pour les commandes';
-        
+
         $commande_class = '';
         BimpObject::loadClass('bimpcommercial', 'Bimp_Commande', $commande_class);
         $this->output = $commande_class::sendRappels();
-        
+
         return 0;
     }
 
     public function sendRappelsFactures()
     {
         $this->current_cron_name = 'Envoi des rappels pour les factures';
-        
+
         $facture_class = '';
         BimpObject::loadClass('bimpcommercial', 'Bimp_Facture', $facture_class);
         $this->output = $facture_class::sendRappels();
-        
+
         return 0;
     }
-    
+
     public function hourlyChecks()
     {
         $this->current_cron_name = 'Vérifs toutes les heures des pièces commerciales';
-        
+
         $this->output .= '***** Vérifs Marges + reval OK des dernières factures mises à jour *****<br/>';
         $facture_class = '';
         BimpObject::loadClass('bimpcommercial', 'Bimp_Facture', $facture_class);
         $this->output .= $facture_class::checkMargesRevalAll();
-        
-        $this->output .= '<br/><br/>***** Vérifs Marges commandes mises à jour *****<br/>';
-        $commande_class = '';
-        BimpObject::loadClass('bimpcommercial', 'Bimp_Commande', $commande_class);
-        $this->output .= $commande_class::checkMargesAll();
-        
+
+		$this->output .= '<br/><br/>***** Vérifs Marges commandes mises à jour *****<br/>';
+		$commande_class = '';
+		BimpObject::loadClass('bimpcommercial', 'Bimp_Commande', $commande_class);
+		$this->output .= $commande_class::checkMargesAll();
+
+		$this->output .= '<br/><br/>***** Vérifs Marges propal mises à jour *****<br/>';
+		$propal_class = '';
+		BimpObject::loadClass('bimpcommercial', 'Bimp_Propal', $propal_class);
+		$this->output .= $propal_class::checkMargesAll();
+
         return 0;
     }
 }

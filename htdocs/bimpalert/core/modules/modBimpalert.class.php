6675b16bc54bee04c289d0ddca8480ad7a239e86
f.lauby <f.lauby@ldlc.com>
2025-06-10 15:29:18 +0200

    modif bimpalert tache bimp #106221
    Former-commit-id: 1250a0f87da48e2999eb8a8459344cba1992ef79

---
+++ htdocs/bimpalert/core/modules/modBimpalert.class.php
@@ -0,0 +1,13 @@
+<?php
+include_once(DOL_DOCUMENT_ROOT ."/bimpcore/core/BimpModules.class.php");
+
+/**     \class      modAlert
+ *  \brief      Module pour inclure Bimp Alerte dans Dolibarr
+ */
+
+class modBimpalert extends BimpModules {
+	var $numero = 8598;
+	var $description = "Bimp Alerte";
+	var $picto = 'bell';
+}
+?>

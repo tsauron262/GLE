ea543d62462b08f1311f8dc4d6cf5239cdda5ab2
flo <f.martinez@bimp.fr>
2025-06-26 18:35:46 +0200

    forçage git pull post process
    Former-commit-id: 629a709b23cca1e14727f044f7637284a739641d

--- htdocs/bimpcore/controllers/testController.php
+++ htdocs/bimpcore/controllers/testController.php
@@ -8,36 +8,14 @@ class testController extends BimpController
 		if (!BimpCore::isUserDev()) {
 			return BimpRender::renderAlerts('Page réservée aux développeurs');
 		}
-
 		$html = '';
 
-		$ticket = BimpCache::getBimpObjectInstance('bimpticket', 'Bimp_Ticket', 8428);
-		if (BimpObject::objectLoaded($ticket)) {
-			$html .= '<h1>TESTS</h1>';
-
-			$txt = $ticket->getData('message');
-
-			$html .= htmlentities($txt);
-			$html .= '<br/><br/>';
-			$html .= $txt;
-
-			$txt = BimpTools::replaceBr($txt, '[[BR]]');
-			foreach (array('div', 'p') as $tag) {
-				$txt = str_replace('</' . $tag . '>', '</' . $tag . '>[[BR]]', $txt);
-			}
-
-			$html .= '<br/>------------------<br/>';
-			$html .= htmlentities($txt);
-			$html .= '<br/><br/>';
-			$html .= $txt;
-
-			$html .= '<br/>------------------<br/>';
-			$txt = strip_tags($txt);
-			$txt = str_replace('[[BR]]', "\n", $txt);
-			$txt = preg_replace("(\n+)", '<br/>', $txt);
-			$html .= $txt;
-
+		if ((int) BimpTools::getValue('err_fat', 0, 'int')) {
+			dfkmjghdlkjghfd();
+		} else {
+			$html .= 'Aj err_fat=1 dans l\'url pour déclencher une err fatale';
 		}
+
 		return $html;
 	}
 }

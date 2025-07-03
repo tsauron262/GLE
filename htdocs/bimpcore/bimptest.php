e2edd28be0c56fd16761986cd329671f91f8f8fa
flo <f.martinez@bimp.fr>
2025-07-03 17:53:00 +0200

    Debug logs
    Former-commit-id: 4cce7096e5ee68ccb9b16c53fe6a3d0287a8c8fd

--- htdocs/bimpcore/bimptest.php
+++ htdocs/bimpcore/bimptest.php
@@ -31,14 +31,14 @@ if (!$user->admin) {
 }
 
 // Test log urgent :
-echo 'LOG URGENT : ';
-$err = BimpCore::addlog('TEST', 4);
-
-if (count($err)) {
-	echo 'Err<pre>' . print_r($err, 1) . '</pre>';
-} else {
-	echo 'ok';
-}
+//echo 'LOG URGENT : ';
+//$err = BimpCore::addlog('TEST', 4);
+//
+//if (count($err)) {
+//	echo 'Err<pre>' . print_r($err, 1) . '</pre>';
+//} else {
+//	echo 'ok';
+//}
 
 echo '<br/>FIN';
 echo '</body></html>';

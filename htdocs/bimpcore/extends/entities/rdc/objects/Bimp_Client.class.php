038598781ef2a8b2de77dd9311afb8bc6d99e990
f.lauby <f.lauby@ldlc.com>
2025-07-17 09:26:19 +0200

    debug contact multiple
    Former-commit-id: 60b0e09fe30ffdfb19ae59bc85de613b23f7a9f6

--- htdocs/bimpcore/extends/entities/rdc/objects/Bimp_Client.class.php
+++ htdocs/bimpcore/extends/entities/rdc/objects/Bimp_Client.class.php
@@ -806,6 +806,7 @@ class Bimp_Client_ExtEntity extends Bimp_Client
 
 	public function appelMiraklS20(&$warnings = array())
 	{
+global $user;
 		$shopid = $this->getData('shopid');
 		if (BimpTools::isModuleDoliActif('bimpapi')) {
 			require_once DOL_DOCUMENT_ROOT . '/bimpapi/BimpApi_Lib.php';
@@ -819,8 +820,10 @@ class Bimp_Client_ExtEntity extends Bimp_Client
 				$warnings[] = 'Erreur lors de la récupération des données Mirakl';
 				return;
 			}
-echo '<pre>' . print_r($this, true) . '</pre>';
-  exit();
+			if($user->id == 4) {
+				echo '<pre>' . print_r($this, true) . '</pre>';
+				exit();
+			}
 			$errors = array();
 			if ($data['total_count'] == 0) {
 				$warnings[] = 'ShopId ' . $shopid . ' non trouvé sur mirakl';

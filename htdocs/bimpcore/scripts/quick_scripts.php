629796fcf29c4743caece5e63df4e526fb204c02
f.lauby <f.lauby@ldlc.com>
2025-07-21 16:51:20 +0200

    debug purge contact rdc
    Former-commit-id: 4f1de6f8cd5cd7303a70f5eff92cc2b5f398eec8

--- htdocs/bimpcore/scripts/quick_scripts.php
+++ htdocs/bimpcore/scripts/quick_scripts.php
@@ -865,6 +865,11 @@ AND ROUND(pl.remise, 4) != ROUND(pdet.`remise_percent`, 4);";
 		var_dump($query);
 		while ($log = $db->fetch_object($query)) {
 			$bt = json_decode($log->backtrace);
+			$erreur = $bt[0]['lines'][0];
+			$pos = strpos($erreur, 'UPDATE');
+			$sql = substr($erreur, $pos, -1);
+			echo '<pre>' . print_r($bt, true) . '</pre>';
+			  exit($sql);
 		}
 		// en extraire la bonne requette
 

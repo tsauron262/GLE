6deaf758db754129e7b00c6902de2e917c81ba13
tommy <tommy@localhost>
2025-07-30 11:00:05 +0200

    debug apple
    Former-commit-id: 867a9b239ec7684851ada6b122ec86cc332821ac

--- htdocs/bimpapple/extends/entities/actimac/classes/GSX_Const_entity.php
+++ htdocs/bimpapple/extends/entities/actimac/classes/GSX_Const_entity.php
@@ -2,7 +2,8 @@
 
 class GSX_Const_entity{
     public static $default_ids = array(
-        'apple_id'    => 't.sauron@ldlc.com',
+//		'apple_id'    => 't.sauron@ldlc.com',
+		'apple_id'    => 'admin.gle@bimp.fr',
         'apple_pword' => '@LdLc.com#69760',
         'sold_to'     => '608111',
         'ship_to'     => '608111'

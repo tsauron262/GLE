baf76bba195dfa8457678acf6cf9e6954c9dca86
f.lauby <f.lauby@ldlc.com>
2025-07-30 12:23:24 +0200

    createRenouvTasks au comm_client si com_contrat inactif
    Former-commit-id: 2d734a6e495df423fb59143c9145ddc53005c64c

--- htdocs/bimpcontrat/objects/BCT_Contrat.class.php
+++ htdocs/bimpcontrat/objects/BCT_Contrat.class.php
@@ -2390,10 +2390,18 @@ class BCT_Contrat extends BimpDolObject
 
 				$infos .= '<br/><br/>Contrat ' . $contrat->getLink() . ' : <br/>';
 
-				if (!$id_commercial) {
-					BimpCore::addlog('Aucun commercial pour renouvellements manuels', Bimp_Log::BIMP_LOG_URGENT, 'contrat', $contrat);
-					$infos .= '<span class="danger">Aucun commercial</span>';
-					continue;
+				$suppl_desc = '';
+				if (!$id_commercial || true) {
+					if ($contrat->getData('fk_soc'))	{
+						$comm = $client->getCommercial();
+						$id_commercial = $comm->id;
+						$suppl_desc = 'Vous recevez cette tache car il n\'a pas de commercial actif sur ce contrat<br />';
+					}
+					if (!$id_commercial) {
+						BimpCore::addlog('Aucun commercial pour renouvellements manuels', Bimp_Log::BIMP_LOG_URGENT, 'contrat', $contrat);
+						$infos .= '<span class="danger">Aucun commercial</span>';
+						continue;
+					}
 				}
 
 				foreach ($lines as $id_line) {
@@ -2414,7 +2422,7 @@ class BCT_Contrat extends BimpDolObject
 
 						BimpObject::createBimpObject('bimptask', 'BIMP_Task', array(
 							'subj'          => 'Contrat d\'abonnement ' . $contrat->getRef() . ' - Renouvellement abonnement à effectuer',
-							'txt'           => $desc,
+							'txt'           => $suppl_desc . $desc,
 							'comment'       => 'Cette tâche sera automatiquement fermée lors du renouvellement',
 							'id_user_owner' => $id_commercial,
 							'test_ferme'    => 'contratdet:rowid = ' . $line->id . ' AND id_line_renouv > 0'

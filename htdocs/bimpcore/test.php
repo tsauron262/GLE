5f9a23865cac15f8cf777e1ee15e37700961b1db
f.lauby <f.lauby@ldlc.com>
2025-08-05 14:52:30 +0200

    test
    Former-commit-id: 87c6d37c213be36828ebad7751d47e6fc4ef89c6

--- htdocs/bimpcore/test.php
+++ htdocs/bimpcore/test.php
@@ -1,8 +1,11 @@
 <?php
-
+//        $pulls = file_get_contents(PATH_TMP.'/git_logs_commit/logs.logs');
 require_once("../main.inc.php");
-$sql = "update llx_user SET  admin = 1 where login = 'f.lauby'";
+	$sql = "DELETE FROM llx_bimpcore_file
+       				WHERE parent_module = 'bimptask'
+       				  AND parent_object_name = 'BIMP_Task'
+       				  AND id_parent  = 98033
+       				  AND file_name LIKE '%_deleted'";
 $db->query($sql);
 
-//        $pulls = file_get_contents(PATH_TMP.'/git_logs_commit/logs.logs');
-        die('llllicicicicicicic');
+die('llllicicicicicicic');

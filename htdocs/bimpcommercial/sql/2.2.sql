091bbec33a70326c48a7ad77d7595c9cb3217b9d
tommy <tommy@localhost>
2025-08-06 13:56:55 +0200

    REFACTORE SQL
    Former-commit-id: 7350e261d836b9bf2cdc7c364f93e9d8e427ab3e

--- htdocs/bimpcommercial/sql/2.2.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `llx_product_fournisseur_price` ADD `stockFourn` INT NOT NULL DEFAULT '0';
--- htdocs/bimpcore/updates/florian/7.2.sql
+++ htdocs/bimpcore/updates/florian/7.2.sql
@@ -1,16 +1,4 @@
 
 
-ALTER TABLE `llx_bimp_propal_line` CHANGE `remise` `remise` FLOAT(24,8) NOT NULL DEFAULT '0';
-
-ALTER TABLE `llx_bimp_propal_line` ADD `remisable` BOOLEAN NOT NULL DEFAULT TRUE;
-ALTER TABLE `llx_bimp_facture_line` ADD `remisable` BOOLEAN NOT NULL DEFAULT TRUE;
-ALTER TABLE `llx_bimp_commande_line` ADD `remisable` BOOLEAN NOT NULL DEFAULT TRUE;
 
 ALTER TABLE `llx_bc_vente_remise` CHANGE `percent` `percent` FLOAT(24,6) NOT NULL DEFAULT '0', CHANGE `montant` `montant` FLOAT(24,2) NOT NULL DEFAULT '0';
-
-ALTER TABLE `llx_propal` ADD `remise_globale` DOUBLE(24,8) NOT NULL DEFAULT '0';
-ALTER TABLE `llx_facture` ADD `remise_globale` DOUBLE(24,8) NOT NULL DEFAULT '0';
-ALTER TABLE `llx_commande` ADD `remise_globale` DOUBLE(24,8) NOT NULL DEFAULT '0';
-
-ALTER TABLE `llx_object_line_remise` CHANGE `percent` `percent` FLOAT(24,8) NOT NULL DEFAULT '0';
-ALTER TABLE `llx_bimp_propal_line` ADD UNIQUE( `id_line`);

656cf0a938cbb722b92162fc8bedc32cd7dbf9e0
tommy <tommy@localhost>
2025-08-06 15:46:43 +0200

    Refact sql
    Former-commit-id: 88b2d750af65dfc7361758b32e47ced7c04a6f08

--- htdocs/bimpcore/updates/florian/15.2.sql
+++
@@ -1,4 +0,0 @@
-
-ALTER TABLE `llx_br_commande_shipment` ADD `id_avoir` INT NOT NULL DEFAULT '0' AFTER `id_facture`; 
-
-ALTER TABLE `llx_br_commande_shipment` RENAME TO llx_bl_commande_shipment;
--- htdocs/bimpcore/updates/florian/4.1.sql
+++ htdocs/bimpcore/updates/florian/4.1.sql
@@ -1,14 +1,4 @@
 
-ALTER TABLE `llx_br_commande_shipment` DROP `qty`;
-ALTER TABLE `llx_br_commande_shipment` ADD `id_contact` INT UNSIGNED NOT NULL DEFAULT '0' AFTER `num_livraison`;
-ALTER TABLE `llx_br_commande_shipment` ADD `status` INT UNSIGNED NOT NULL DEFAULT '1' AFTER `num_livraison`;
-ALTER TABLE `llx_br_commande_shipment` CHANGE `date` `date_shipped` DATETIME NULL DEFAULT NULL;
-
-ALTER TABLE `llx_br_commande_shipment` ADD `date_update` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `date_shipped`;
-ALTER TABLE `llx_br_commande_shipment` ADD `user_update` INT UNSIGNED NOT NULL DEFAULT '0' AFTER `date_shipped`;
-ALTER TABLE `llx_br_commande_shipment` ADD `date_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `date_shipped`; 
-ALTER TABLE `llx_br_commande_shipment` ADD `user_create` INT UNSIGNED NOT NULL DEFAULT '0' AFTER `date_shipped`;
-
 ALTER TABLE `llx_br_reservation_shipment` DROP `date`;
 ALTER TABLE `llx_br_reservation_shipment` ADD `id_commande_client_line` INT UNSIGNED NOT NULL DEFAULT '0' AFTER `id_commande_client`;
 ALTER TABLE `llx_br_reservation_shipment` ADD `group_articles` BOOLEAN NOT NULL DEFAULT FALSE AFTER `id_user`;

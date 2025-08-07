abdb98cd1f616f704dc87fbc79ba9b2ad69c72eb
tommy <tommy@localhost>
2025-08-07 10:56:15 +0200

    Refact sql
    Former-commit-id: 3bb80c2835e4b3152140a75426bd0db481161cd9

--- htdocs/bimpcore/updates/florian/21.8.sql
+++ htdocs/bimpcore/updates/florian/21.8.sql
@@ -1,21 +1,5 @@
 ALTER TABLE `llx_bimpcore_log` ADD INDEX `obj_data` (`obj_module`, `obj_name`, `id_object`);
 
 
-ALTER TABLE `llx_bds_process_match` ADD INDEX `process` (`id_process`);
-ALTER TABLE `llx_bds_process_match_custom_values` ADD INDEX `match` (`id_match`);
-
-ALTER TABLE `llx_bds_process_operation` ADD INDEX `process` (`id_process`);
-ALTER TABLE `llx_bds_process_param` ADD INDEX `process` (`id_process`);
-ALTER TABLE `llx_bds_process_trigger` ADD INDEX `process` (`id_process`);
-
-ALTER TABLE `llx_bds_report` ADD INDEX `process` (`id_process`);
-ALTER TABLE `llx_bds_report` ADD INDEX `operation` (`id_operation`);
-
-ALTER TABLE `llx_bds_report_line` ADD INDEX `report` (`id_report`);
-ALTER TABLE `llx_bds_report_line` ADD INDEX `obj_data` (`obj_module`, `obj_name`, `id_obj`);
-
-ALTER TABLE `llx_bds_report_object_data` ADD INDEX `report` (`id_report`);
-ALTER TABLE `llx_bds_report_object_data` ADD INDEX `obj_data` (`obj_module`, `obj_name`);
-
 ALTER TABLE `llx_bimp_relance_clients_line` ADD INDEX `relance` (`id_relance`);
 ALTER TABLE `llx_bimp_relance_clients_line` ADD INDEX `client` (`id_client`);
